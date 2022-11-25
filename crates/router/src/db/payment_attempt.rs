10db53d832b544955d340c280f4e46dc3ba23960
dracarys18 <karthikey.hegde@juspay.in>
2022-11-25 20:40:02 +0530

    payment_attempt: Add browser info column into payment attempt table

--- crates/router/src/db/payment_attempt.rs
+++ crates/router/src/db/payment_attempt.rs
@@ -199,6 +199,7 @@ mod storage {
                 amount_to_capture: payment_attempt.amount_to_capture,
                 cancellation_reason: payment_attempt.cancellation_reason.clone(),
                 mandate_id: payment_attempt.mandate_id.clone(),
+                browser_info: payment_attempt.browser_info.clone(),
             };
             // TODO: Add a proper error for serialization failure
             let redis_value = serde_json::to_string(&created_attempt)
