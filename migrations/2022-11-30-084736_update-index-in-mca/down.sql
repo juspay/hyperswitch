eb29f963eeee1049a1bd3cba94176d4956d35910
Abhishek Marrivagu <abhishek.marrivagu@juspay.in>
2022-12-01 15:04:43 +0530

    fix(migrations): readded merchant_connector_account_connector_type_index and merchant_connector_account_merchant_id_index

--- migrations/2022-11-30-084736_update-index-in-mca/down.sql
+++ migrations/2022-11-30-084736_update-index-in-mca/down.sql
@@ -1,4 +1,1 @@
--- This file should undo anything in `up.sql`
 DROP INDEX merchant_connector_account_merchant_id_connector_name_index;
-CREATE INDEX merchant_connector_account_connector_type_index ON merchant_connector_account (connector_type);
-CREATE INDEX merchant_connector_account_merchant_id_index ON merchant_connector_account (merchant_id);
--- migrations/2022-11-30-084736_update-index-in-mca/up.sql
+++ migrations/2022-11-30-084736_update-index-in-mca/up.sql
@@ -1,3 +1,1 @@
-DROP INDEX merchant_connector_account_connector_type_index;
-DROP INDEX merchant_connector_account_merchant_id_index;
 CREATE UNIQUE INDEX merchant_connector_account_merchant_id_connector_name_index ON merchant_connector_account (merchant_id, connector_name);
