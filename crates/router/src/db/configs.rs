e4ea7d87ab3a23c1b486569e4d50cd1720549b20
Kostiantyn Mysnyk <kostiantyn.m.ext@juspay.in>
2022-12-05 10:47:05 +0000

    refactor(router): remove `SqlDb`

--- crates/router/src/db/configs.rs
+++ crates/router/src/db/configs.rs
@@ -19,12 +19,12 @@ pub trait ConfigInterface {
 #[async_trait::async_trait]
 impl ConfigInterface for super::Store {
     async fn insert_config(&self, config: ConfigNew) -> CustomResult<Config, errors::StorageError> {
-        let conn = pg_connection(&self.master_pool.conn).await;
+        let conn = pg_connection(&self.master_pool).await;
         config.insert(&conn).await
     }
 
     async fn find_config_by_key(&self, key: &str) -> CustomResult<Config, errors::StorageError> {
-        let conn = pg_connection(&self.master_pool.conn).await;
+        let conn = pg_connection(&self.master_pool).await;
         Config::find_by_key(&conn, key).await
     }
 
@@ -33,7 +33,7 @@ impl ConfigInterface for super::Store {
         key: &str,
         config_update: ConfigUpdate,
     ) -> CustomResult<Config, errors::StorageError> {
-        let conn = pg_connection(&self.master_pool.conn).await;
+        let conn = pg_connection(&self.master_pool).await;
         Config::update_by_key(&conn, key, config_update).await
     }
 }
