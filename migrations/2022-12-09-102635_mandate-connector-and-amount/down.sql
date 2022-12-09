6ad76e80324106c18e61acd6f1ec1c928dd61aec
Nishant Joshi <nishant.joshi@juspay.in>
2022-12-09 18:13:05 +0530

    feat(mandate): add amount based mandate information

---
+++ migrations/2022-12-09-102635_mandate-connector-and-amount/down.sql
@@ -0,0 +1,8 @@
+-- This file should undo anything in `up.sql`
+ALTER TABLE mandate
+RENAME COLUMN mandate_amount TO single_use_amount;
+ALTER TABLE mandate
+RENAME COLUMN mandate_currency TO single_use_currency;
+ALTER TABLE mandate
+DROP COLUMN IF EXISTS amount_captured
+DROP COLUMEN IF EXISTS connector;
