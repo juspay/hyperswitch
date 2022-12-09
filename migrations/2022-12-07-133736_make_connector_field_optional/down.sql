87294427e0ad420e320ef12f326197a98e8b0423
Narayan Bhat <narayan.bhat@juspay.in>
2022-12-09 13:32:54 +0530

    fix(migration): minor fix in make connector name optional migration

--- migrations/2022-12-07-133736_make_connector_field_optional/down.sql
+++ migrations/2022-12-07-133736_make_connector_field_optional/down.sql
@@ -1,2 +1,2 @@
 ALTER TABLE payment_attempt ALTER COLUMN connector SET NOT NULL;
-ALTER TABLE connector_response ALTER COLUMN connector_name DROP NOT NULL;
+ALTER TABLE connector_response ALTER COLUMN connector_name SET NOT NULL;
