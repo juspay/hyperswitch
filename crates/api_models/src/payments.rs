d7c0dcaeed65660c964df9a7ecf74f08df78322c
Narayan Bhat <narayan.bhat@juspay.in>
2022-12-12 16:54:10 +0530

    fix(core): change payment id to string

--- crates/api_models/src/payments.rs
+++ crates/api_models/src/payments.rs
@@ -532,13 +532,8 @@ impl From<PaymentsStartRequest> for PaymentsResponse {
 
 impl From<PaymentsSessionRequest> for PaymentsResponse {
     fn from(item: PaymentsSessionRequest) -> Self {
-        let payment_id = match item.payment_id {
-            PaymentIdType::PaymentIntentId(id) => Some(id),
-            _ => None,
-        };
-
         Self {
-            payment_id,
+            payment_id: Some(item.payment_id),
             ..Default::default()
         }
     }
@@ -668,7 +663,7 @@ pub struct ConnectorSessionToken {
 
 #[derive(Default, Debug, serde::Deserialize, Clone)]
 pub struct PaymentsSessionRequest {
-    pub payment_id: PaymentIdType,
+    pub payment_id: String,
     pub client_secret: String,
 }
 
