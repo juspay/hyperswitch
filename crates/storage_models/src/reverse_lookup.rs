f0c54459c1683f21b4fb058189b9bc9f90e0431b
dracarys18 <karthikey.hegde@juspay.in>
2022-12-14 20:06:58 +0530

    fix: Make lookup id as primary key

--- crates/storage_models/src/reverse_lookup.rs
+++ crates/storage_models/src/reverse_lookup.rs
@@ -6,11 +6,11 @@ use crate::schema::reverse_lookup;
     Clone, Debug, serde::Serialize, serde::Deserialize, Identifiable, Queryable, Eq, PartialEq,
 )]
 #[diesel(table_name = reverse_lookup)]
-#[diesel(primary_key(sk_id))]
+#[diesel(primary_key(lookup_id))]
 pub struct ReverseLookup {
-    pub sk_id: i32,
-    pub pk_id: String,
     pub lookup_id: String,
+    pub sk_id: String,
+    pub pk_id: String,
     pub result_id: String,
     pub source: String,
 }
@@ -24,15 +24,5 @@ pub struct ReverseLookupNew {
     pub lookup_id: String,
     pub result_id: String,
     pub source: String,
-}
-
-impl ReverseLookupNew {
-    pub fn new(pk_id: String, lookup_id: String, result_id: String, source: String) -> Self {
-        Self {
-            pk_id,
-            lookup_id,
-            result_id,
-            source,
-        }
-    }
+    pub sk_id: String,
 }
