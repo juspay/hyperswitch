d8701be35d6fb69210a41ec9c10a04e142f05538
Abhishek Marrivagu <abhishek.marrivagu@juspay.in>
2022-12-15 12:54:54 +0530

    refactor(drainer): formatted

--- crates/drainer/src/utils.rs
+++ crates/drainer/src/utils.rs
@@ -114,10 +114,7 @@ pub fn increment_stream_index(index: u8, total_streams: u8) -> u8 {
 }
 
 pub(crate) fn get_steam_key_flag(store: Arc<router::services::Store>, stream_index: u8) -> String {
-    format!(
-        "{}_in_use",
-        get_drainer_stream(store, stream_index)
-    )
+    format!("{}_in_use", get_drainer_stream(store, stream_index))
 }
 
 pub(crate) fn get_drainer_stream(store: Arc<Store>, stream_index: u8) -> String {
