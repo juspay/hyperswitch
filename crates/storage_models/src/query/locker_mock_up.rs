8d6d41f2ebc5aae0eb3466b05c6535a0afd32dfc
Abhishek <abhishek.marrivagu@juspay.in>
2022-12-19 13:41:56 +0530

    refactor(storage_models): changed CustomResult in storage_models to StorageResult (#158)

--- crates/storage_models/src/query/locker_mock_up.rs
+++ crates/storage_models/src/query/locker_mock_up.rs
@@ -3,28 +3,21 @@ use router_env::{tracing, tracing::instrument};
 
 use super::generics;
 use crate::{
-    errors,
     locker_mock_up::{LockerMockUp, LockerMockUpNew},
     schema::locker_mock_up::dsl,
-    CustomResult, PgPooledConn,
+    PgPooledConn, StorageResult,
 };
 
 impl LockerMockUpNew {
     #[instrument(skip(conn))]
-    pub async fn insert(
-        self,
-        conn: &PgPooledConn,
-    ) -> CustomResult<LockerMockUp, errors::DatabaseError> {
+    pub async fn insert(self, conn: &PgPooledConn) -> StorageResult<LockerMockUp> {
         generics::generic_insert(conn, self).await
     }
 }
 
 impl LockerMockUp {
     #[instrument(skip(conn))]
-    pub async fn find_by_card_id(
-        conn: &PgPooledConn,
-        card_id: &str,
-    ) -> CustomResult<Self, errors::DatabaseError> {
+    pub async fn find_by_card_id(conn: &PgPooledConn, card_id: &str) -> StorageResult<Self> {
         generics::generic_find_one::<<Self as HasTable>::Table, _, _>(
             conn,
             dsl::card_id.eq(card_id.to_owned()),
@@ -33,10 +26,7 @@ impl LockerMockUp {
     }
 
     #[instrument(skip(conn))]
-    pub async fn delete_by_card_id(
-        conn: &PgPooledConn,
-        card_id: &str,
-    ) -> CustomResult<Self, errors::DatabaseError> {
+    pub async fn delete_by_card_id(conn: &PgPooledConn, card_id: &str) -> StorageResult<Self> {
         generics::generic_delete_one_with_result::<<Self as HasTable>::Table, _, _>(
             conn,
             dsl::card_id.eq(card_id.to_owned()),
