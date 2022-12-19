4ebab9d119c9a96e031e417c09f730ff776c50fc
dracarys18 <karthikey.hegde@juspay.in>
2022-12-19 12:29:59 +0530

    chore: Remove unused import

--- crates/router/src/types/storage/refund.rs
+++ crates/router/src/types/storage/refund.rs
@@ -2,7 +2,5 @@ pub use storage_models::refund::{
     Refund, RefundCoreWorkflow, RefundNew, RefundUpdate, RefundUpdateInternal,
 };
 
-use crate::utils::storage_partitioning::KvStorePartition;
-
 #[cfg(feature = "kv_store")]
-impl KvStorePartition for Refund {}
+impl crate::utils::storage_partitioning::KvStorePartition for Refund {}
