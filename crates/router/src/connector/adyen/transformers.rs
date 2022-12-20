f3d3abf06030ecd318eaa263a0ddde3c98a95f60
Sangamesh Kulkarni <59434228+Sangamesh26@users.noreply.github.com>
2022-12-20 13:36:59 +0530

    refactor(connectors): remove specific imports from connectors (#161)

--- crates/router/src/connector/adyen/transformers.rs
+++ crates/router/src/connector/adyen/transformers.rs
@@ -7,7 +7,7 @@ use serde::{Deserialize, Serialize};
 use crate::{
     consts,
     core::errors,
-    pii::{PeekInterface, Secret},
+    pii::{self, PeekInterface},
     services,
     types::{
         self,
@@ -153,9 +153,9 @@ pub enum AdyenPaymentMethod {
 pub struct AdyenCard {
     #[serde(rename = "type")]
     payment_type: String,
-    number: Option<Secret<String>>,
-    expiry_month: Option<Secret<String>>,
-    expiry_year: Option<Secret<String>>,
+    number: Option<pii::Secret<String>>,
+    expiry_month: Option<pii::Secret<String>>,
+    expiry_year: Option<pii::Secret<String>>,
     cvc: Option<String>,
 }
 
