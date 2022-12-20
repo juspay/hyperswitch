50fce5502ec300cb1bdb5c6b7ecacc6c3d40c604
Kostiantyn Mysnyk <kostiantyn.m.ext@juspay.in>
2022-12-20 15:06:10 +0000

    testing

--- hexagonal_architecture/crates/app/tests/hello.rs
+++ hexagonal_architecture/crates/app/tests/hello.rs
@@ -1,9 +1,17 @@
 use actix_web::test::{call_and_read_body, TestRequest};
 
 #[actix_web::test]
-async fn test() {
+async fn test_hello() {
     let service = hexagonal_architecture::mk_service().await;
     let request = TestRequest::get().uri("/").to_request();
 
     assert_eq!(&call_and_read_body(&service, request).await[..], b"hello :)");
+}
+
+#[actix_web::test]
+async fn test_paymentlist() {
+    let service = hexagonal_architecture::mk_service().await;
+    let request = TestRequest::get().uri("/payments/list").to_request();
+
+    assert_eq!(&call_and_read_body(&service, request).await[..], b"[]");
 }
