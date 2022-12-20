16d0b961596e26efe17ced1ee1c5d303c250ec46
Sanchith Hegde <sanchith.hegde@juspay.in>
2022-12-20 19:24:40 +0530

    refactor: rename some columns
    The updated columns are as follows:
    
    `connector_response` table:
      - `txn_id` -> `attempt_id`
    
    `mandate` table:
      - `previous_transaction_id` -> `previous_attempt_id`
    
    `payment_methods` table:
      - `network_transaction_id` removed
    
    `refund` table:
      - `transaction_id` -> `connector_transaction_id`
      - `pg_refund_id` -> `connector_refund_id`

--- crates/router/src/core/payments/operations/payment_capture.rs
+++ crates/router/src/core/payments/operations/payment_capture.rs
@@ -86,7 +86,7 @@ impl<F: Send + Clone> GetTracker<F, payments::PaymentData<F>, api::PaymentsCaptu
         amount = payment_attempt.amount.into();
 
         let connector_response = db
-            .find_connector_response_by_payment_id_merchant_id_txn_id(
+            .find_connector_response_by_payment_id_merchant_id_attempt_id(
                 &payment_attempt.payment_id,
                 &payment_attempt.merchant_id,
                 &payment_attempt.attempt_id,
