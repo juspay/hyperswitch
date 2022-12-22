d79c3527352de2105ad8b85b371c58c4c38f19cd
Sanchith Hegde <sanchith.hegde@juspay.in>
2022-12-23 00:27:31 +0530

    chore: remove crate-specific lints

--- crates/router_env/src/lib.rs
+++ crates/router_env/src/lib.rs
@@ -1,17 +1,5 @@
 #![forbid(unsafe_code)]
-#![warn(
-    missing_docs,
-    rust_2018_idioms,
-    missing_debug_implementations,
-    clippy::expect_used,
-    clippy::missing_panics_doc,
-    clippy::panic,
-    clippy::panic_in_result_fn,
-    clippy::panicking_unwrap,
-    clippy::unreachable,
-    clippy::unwrap_in_result,
-    clippy::unwrap_used
-)]
+#![warn(missing_docs, missing_debug_implementations)]
 
 //!
 //! Environment of payment router: logger, basic config, its environment awareness.
