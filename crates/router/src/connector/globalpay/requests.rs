86c8a58699fa39e3c00b1fea5de23901327def29
Jagan Elavarasan <jaganelavarasan@gmail.com>
2023-01-10 15:22:02 +0530

    address review comments

--- crates/router/src/connector/globalpay/requests.rs
+++ crates/router/src/connector/globalpay/requests.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Default, Serialize, Deserialize)]
+#[derive(Debug, Default, Serialize)]
 pub struct GlobalpayPaymentsRequest {
     /// A meaningful label for the merchant account set by Global Payments.
     pub account_name: String,
