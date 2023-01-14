6d0462c55856a8260994bd7f877393fead44feda
Sanchith Hegde <22217505+SanchithHegde@users.noreply.github.com>
2023-01-14 22:54:22 +0530

    chore: fix formatting

--- crates/router/src/connector.rs
+++ crates/router/src/connector.rs
@@ -17,6 +17,6 @@ pub mod worldpay;
 pub use self::{
     aci::Aci, adyen::Adyen, applepay::Applepay, authorizedotnet::Authorizedotnet,
     braintree::Braintree, checkout::Checkout, cybersource::Cybersource, fiserv::Fiserv,
-    globalpay::Globalpay, klarna::Klarna, payu::Payu, shift4::Shift4, stripe::Stripe, 
+    globalpay::Globalpay, klarna::Klarna, payu::Payu, shift4::Shift4, stripe::Stripe,
     worldpay::Worldpay,
 };
