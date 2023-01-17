00400e07c94c1152de212340070624151fa4bf44
Arun Raj M <jarnura47@gmail.com>
2023-01-17 12:44:07 +0530

    chore: add docs for Applicative trait

--- crates/common_utils/src/fp_utils.rs
+++ crates/common_utils/src/fp_utils.rs
@@ -1,8 +1,12 @@
 //! Functional programming utilities
 
+/// The Applicative trait provides a pure behaviour,
+/// which can be used to create values of type f a from values of type a.
 pub trait Applicative<R> {
+    /// The Associative type acts as a (f a) wrapper for Self.
     type WrappedSelf<T>;
 
+    /// Applicative::pure(_) is abstraction with lifts any arbitrary type to underlying higher order type
     fn pure(v: R) -> Self::WrappedSelf<R>;
 }
 
@@ -20,7 +24,7 @@ impl<R, E> Applicative<R> for Result<R, E> {
     }
 }
 
-// This function allows lazy evaluation of the `f` argument
+/// This function allows lazy evaluation of the `f` argument
 pub fn when<W: Applicative<(), WrappedSelf<()> = W>, F>(predicate: bool, f: F) -> W
 where
     F: FnOnce() -> W,
