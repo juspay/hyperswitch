4552b1605efa535762df76d76bdcceb055b0d6b0
Sanchith Hegde <sanchith.hegde@juspay.in>
2023-01-17 12:51:09 +0530

    refactor(fp_utils): minor documentation fixes

--- crates/common_utils/src/fp_utils.rs
+++ crates/common_utils/src/fp_utils.rs
@@ -1,12 +1,13 @@
 //! Functional programming utilities
 
-/// The Applicative trait provides a pure behaviour,
+/// The Applicative trait provides a pure behavior,
 /// which can be used to create values of type f a from values of type a.
 pub trait Applicative<R> {
     /// The Associative type acts as a (f a) wrapper for Self.
     type WrappedSelf<T>;
 
-    /// Applicative::pure(_) is abstraction with lifts any arbitrary type to underlying higher order type
+    /// Applicative::pure(_) is abstraction with lifts any arbitrary type to underlying higher
+    /// order type
     fn pure(v: R) -> Self::WrappedSelf<R>;
 }
 
