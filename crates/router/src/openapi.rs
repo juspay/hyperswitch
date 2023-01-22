2e1b3c40f210cd04ef4f487a26250e2f6e1bc7f2
dracarys18 <karthikey.hegde@juspay.in>
2023-01-22 15:53:23 +0530

    chore: fmt

--- crates/router/src/openapi.rs
+++ crates/router/src/openapi.rs
@@ -54,7 +54,6 @@ Never share your secret api keys. Keep them guarded and secure.
         (name = "Mandates", description = "Manage mandates"),
         (name = "Customers", description = "Create and manage customers"),
         (name = "Payment Methods", description = "Create and manage payment methods of customers")
-        
     ),
     paths(
         crate::routes::refunds::refunds_create,
