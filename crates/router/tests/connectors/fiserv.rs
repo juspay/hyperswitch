e913430d9cbe1e00749e567b5aca37d369f2129f
bernard eugine <bernard.eugine@bernard.eugine-MacBookPro>
2023-01-23 11:36:47 +0530

    rename CCard to card in tests

--- crates/router/tests/connectors/fiserv.rs
+++ crates/router/tests/connectors/fiserv.rs
@@ -86,7 +86,7 @@ async fn should_capture_already_authorized_payment() {
     let authorize_response = connector
         .authorize_payment(
             Some(types::PaymentsAuthorizeData {
-                payment_method_data: types::api::PaymentMethod::Card(api::CCard {
+                payment_method_data: types::api::PaymentMethod::Card(api::Card {
                     card_number: Secret::new("4005550000000019".to_string()),
                     card_exp_month: Secret::new("02".to_string()),
                     card_exp_year: Secret::new("2035".to_string()),
@@ -119,7 +119,7 @@ async fn should_fail_payment_for_missing_cvc() {
     let response = Fiserv {}
         .make_payment(
             Some(types::PaymentsAuthorizeData {
-                payment_method_data: types::api::PaymentMethod::Card(api::CCard {
+                payment_method_data: types::api::PaymentMethod::Card(api::Card {
                     card_number: Secret::new("4005550000000019".to_string()),
                     card_exp_month: Secret::new("02".to_string()),
                     card_exp_year: Secret::new("2035".to_string()),
