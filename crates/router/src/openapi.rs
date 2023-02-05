d91bd187f055187cd0bcc7e9a022e4b29e8aa60b
Sanchith Hegde <sanchith.hegde@juspay.in>
2023-02-05 19:14:32 +0530

    chore(openapi): format

--- crates/router/src/openapi.rs
+++ crates/router/src/openapi.rs
@@ -17,7 +17,10 @@ Our APIs accept and return JSON in the HTTP body, and return standard HTTP respo
 You can consume the APIs directly using your favorite HTTP/REST library.
 
 We have a testing environment referred to "sandbox", which you can setup to test API calls without
-affecting production data. Currently, our sandbox environment is live while our production environment is under development and will be available soon. You can sign up on our Dashboard to get API keys to access Hyperswitch API.
+affecting production data.
+Currently, our sandbox environment is live while our production environment is under development
+and will be available soon.
+You can sign up on our Dashboard to get API keys to access Hyperswitch API.
 
 ### Environment
 
@@ -32,13 +35,13 @@ Use the following base URLs when making requests to the APIs:
 
 When you sign up on our [dashboard](https://app.hyperswitch.io) and create a merchant
 account, you are given a secret key (also referred as api-key) and a publishable key.
-You may authenticate all API requests with Hyperswitch server by providing the appropriate key in the
-request Authorization header.
+You may authenticate all API requests with Hyperswitch server by providing the appropriate key in
+the request Authorization header.
 
 | Key           |  Description                                                                                  |
 |---------------|-----------------------------------------------------------------------------------------------|
 | Sandbox       | Private key. Used to authenticate all API requests from your merchant server                  |
-| Production    | Unique identifier for your account. Used to authenticate API requests from your app’s client  |
+| Production    | Unique identifier for your account. Used to authenticate API requests from your app's client  |
 
 Never share your secret api keys. Keep them guarded and secure.
 "#,
