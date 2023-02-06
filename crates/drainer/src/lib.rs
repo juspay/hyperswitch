2d800369d4f57e8e1203a08f6e29614669fda2cf
Nishant Joshi <nishant.joshi@juspay.in>
2023-02-06 14:38:55 +0530

    chore: make sx -> tx for channels in drainer

--- crates/drainer/src/lib.rs
+++ crates/drainer/src/lib.rs
@@ -32,9 +32,9 @@ pub async fn start_drainer(
             .change_context(errors::DrainerError::SignalError(
                 "Failed while getting allowed signals".to_string(),
             ))?;
-    let (sx, mut rx) = oneshot::channel();
+    let (tx, mut rx) = oneshot::channel();
     let handle = signal.handle();
-    let task_handle = tokio::spawn(common_utils::signals::signal_handler(signal, sx));
+    let task_handle = tokio::spawn(common_utils::signals::signal_handler(signal, tx));
 
     let active_tasks = Arc::new(atomic::AtomicU64::new(0));
     'event: loop {
