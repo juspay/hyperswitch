1b6394fdcb3dc3870322843e20bde325dff11c4a
Narayan Bhat <narayan.bhat@juspay.in>
2023-02-07 14:44:06 +0530

    refactor: remove enum type in database

--- migrations/2023-02-02-055700_add_payment_issuer_and_experience_in_payment_attempt/down.sql
+++ migrations/2023-02-02-055700_add_payment_issuer_and_experience_in_payment_attempt/down.sql
@@ -1,7 +1,3 @@
 ALTER TABLE payment_attempt DROP COLUMN IF EXISTS payment_issuer;
 
 ALTER TABLE payment_attempt DROP COLUMN IF EXISTS payment_experience;
-
-DROP TYPE IF EXISTS "PaymentIssuer";
-
-DROP TYPE IF EXISTS "PaymentExperience";
