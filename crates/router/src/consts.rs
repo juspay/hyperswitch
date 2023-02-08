2ab955b1b13a3c1c10510b86014f85fdc1ffc4c3
Sanchith Hegde <sanchith.hegde@juspay.in>
2023-02-08 13:15:17 +0530

    refactor(consts): reduce plaintext API key length from 128 to 64

--- crates/router/src/consts.rs
+++ crates/router/src/consts.rs
@@ -22,4 +22,4 @@ pub(crate) const BASE64_ENGINE: base64::engine::GeneralPurpose =
 pub(crate) const BASE64_ENGINE_URL_SAFE: base64::engine::GeneralPurpose =
     base64::engine::general_purpose::URL_SAFE;
 
-pub(crate) const API_KEY_LENGTH: usize = 128;
+pub(crate) const API_KEY_LENGTH: usize = 64;
