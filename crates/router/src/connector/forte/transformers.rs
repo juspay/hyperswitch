e7fd682f2edfa9afd75cc4a862a8e5de5a39c86f
Prakhar Prakash <prakhar.prakash@juspay.in>
2023-02-11 22:26:16 +0530

    types, transforms for FortePaymentRequest

--- crates/router/src/connector/forte/transformers.rs
+++ crates/router/src/connector/forte/transformers.rs
@@ -1,15 +1,113 @@
 use serde::{Deserialize, Serialize};
-use crate::{core::errors,types::{self,api, storage::enums}};
+use crate::{
+    core::errors,
+    pii::PeekInterface,
+    types::{self, api, storage::enums},
+};
+//Types Start
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct FortePaymentsRequest {
+    #[serde(rename = "authorization_amount")]
+    pub authorization_amount: f64,
+    #[serde(rename = "subtotal_amount")]
+    pub subtotal_amount: f64,
+    #[serde(rename = "billing_address")]
+    pub billing_address: BillingAddress,
+    pub card: Card,
+}
 
-//TODO: Fill the struct with respective fields
-#[derive(Default, Debug, Serialize, Eq, PartialEq)]
-pub struct FortePaymentsRequest {}
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct BillingAddress {
+    #[serde(rename = "first_name")]
+    pub first_name: String,
+    #[serde(rename = "last_name")]
+    pub last_name: String,
+}
 
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Card {
+    #[serde(rename = "card_type")]
+    pub card_type: String,
+    #[serde(rename = "name_on_card")]
+    pub name_on_card: String,
+    #[serde(rename = "account_number")]
+    pub account_number: String,
+    #[serde(rename = "expire_month")]
+    pub expire_month: String,
+    #[serde(rename = "expire_year")]
+    pub expire_year: String,
+    #[serde(rename = "card_verification_value")]
+    pub card_verification_value: String,
+}
+
+//Res Types Start
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct FortePaymentsResponse {
+    #[serde(rename = "transaction_id")]
+    pub transaction_id: String,
+    #[serde(rename = "location_id")]
+    pub location_id: String,
+    pub action: String,
+    #[serde(rename = "authorization_amount")]
+    pub authorization_amount: f64,
+    #[serde(rename = "entered_by")]
+    pub entered_by: String,
+    #[serde(rename = "billing_address")]
+    pub billing_address: BillingAddress,
+    pub card: Card,
+    pub response: Response,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Response {
+    pub environment: String,
+    #[serde(rename = "response_type")]
+    pub response_type: String,
+    #[serde(rename = "response_code")]
+    pub response_code: String,
+    #[serde(rename = "response_desc")]
+    pub response_desc: String,
+    #[serde(rename = "authorization_code")]
+    pub authorization_code: String,
+    #[serde(rename = "avs_result")]
+    pub avs_result: String,
+    #[serde(rename = "cvv_result")]
+    pub cvv_result: String,
+}
+
+//Res Types end
+//Types End
 impl TryFrom<&types::PaymentsAuthorizeRouterData> for FortePaymentsRequest  {
     type Error = error_stack::Report<errors::ConnectorError>;
-    fn try_from(_item: &types::PaymentsAuthorizeRouterData) -> Result<Self,Self::Error> {
-        todo!()
+    fn try_from(item: &types::PaymentsAuthorizeRouterData) -> Result<Self,Self::Error> {
+
+        match item.request.payment_method_data {
+            api::PaymentMethod::Card(ref ccard) => {
+                let request  =  FortePaymentsRequest{
+                    billing_address : BillingAddress { first_name: ccard.card_holder_name.peek().clone(), last_name: ccard.card_holder_name.peek().clone() },
+                    card: Card {
+                        card_type               : String::from("visa"),
+                        name_on_card            : ccard.card_holder_name.peek().clone(),
+                        account_number          : ccard.card_number.peek().clone(),
+                        expire_month            : ccard.card_exp_month.peek().clone(),
+                        expire_year             : ccard.card_exp_year.peek().clone(),
+                        card_verification_value : ccard.card_cvc.peek().clone(),
+                    },
+                    authorization_amount: item.request.amount as f64,
+                    subtotal_amount: item.request.amount as f64,
+                };
+                Ok(request)
+            }
+            _ => Err(
+                errors::ConnectorError::NotImplemented("Current Payment Method".to_string()).into(),
+            ),
     }
+}
 }
 
 //TODO: Fill the struct with respective fields
@@ -35,30 +133,13 @@ pub enum FortePaymentStatus {
     Processing,
 }
 
-impl From<FortePaymentStatus> for enums::AttemptStatus {
-    fn from(item: FortePaymentStatus) -> Self {
-        match item {
-            FortePaymentStatus::Succeeded => Self::Charged,
-            FortePaymentStatus::Failed => Self::Failure,
-            FortePaymentStatus::Processing => Self::Authorizing,
-        }
-    }
-}
-
-//TODO: Fill the struct with respective fields
-#[derive(Default, Debug, Clone, Serialize, Deserialize, PartialEq)]
-pub struct FortePaymentsResponse {
-    status: FortePaymentStatus,
-    id: String,
-}
-
 impl<F,T> TryFrom<types::ResponseRouterData<F, FortePaymentsResponse, T, types::PaymentsResponseData>> for types::RouterData<F, T, types::PaymentsResponseData> {
     type Error = error_stack::Report<errors::ParsingError>;
     fn try_from(item: types::ResponseRouterData<F, FortePaymentsResponse, T, types::PaymentsResponseData>) -> Result<Self,Self::Error> {
         Ok(Self {
-            status: enums::AttemptStatus::from(item.response.status),
+            status: enums::AttemptStatus::Authorized,
             response: Ok(types::PaymentsResponseData::TransactionResponse {
-                resource_id: types::ResponseId::ConnectorTransactionId(item.response.id),
+                resource_id: types::ResponseId::ConnectorTransactionId(item.response.transaction_id),
                 redirection_data: None,
                 redirect: false,
                 mandate_reference: None,
