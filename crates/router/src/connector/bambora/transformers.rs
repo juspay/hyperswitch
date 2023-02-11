0b4cca0a91eb3cf43cbe4468a12509479b985f76
Shobhit <shobhit.maheswari@juspay.in>
2023-02-11 17:15:35 +0530

    fixed some compile errors-4

--- crates/router/src/connector/bambora/transformers.rs
+++ crates/router/src/connector/bambora/transformers.rs
@@ -91,63 +91,63 @@ impl<F,T> TryFrom<types::ResponseRouterData<F, BamboraPaymentsResponse, T, types
 
 // //TODO: Fill the struct with respective fields
 // // REFUND :
-// // Type definition for RefundRequest
-// #[derive(Default, Debug, Serialize)]
-// pub struct BamboraRefundRequest {}
+// Type definition for RefundRequest
+#[derive(Default, Debug, Serialize)]
+pub struct BamboraRefundRequest {}
 
-// impl<F> TryFrom<&types::RefundsRouterData<F>> for BamboraRefundRequest {
-//     type Error = error_stack::Report<errors::ParsingError>;
-//     fn try_from(_item: &types::RefundsRouterData<F>) -> Result<Self,Self::Error> {
-//        todo!()
-//     }
-// }
+impl<F> TryFrom<&types::RefundsRouterData<F>> for BamboraRefundRequest {
+    type Error = error_stack::Report<errors::ParsingError>;
+    fn try_from(_item: &types::RefundsRouterData<F>) -> Result<Self,Self::Error> {
+       todo!()
+    }
+}
 
-// // Type definition for Refund Response
+// Type definition for Refund Response
 
-// #[allow(dead_code)]
-// #[derive(Debug, Serialize, Default, Deserialize, Clone)]
-// pub enum RefundStatus {
-//     Succeeded,
-//     Failed,
-//     #[default]
-//     Processing,
-// }
+#[allow(dead_code)]
+#[derive(Debug, Serialize, Default, Deserialize, Clone)]
+pub enum RefundStatus {
+    Succeeded,
+    Failed,
+    #[default]
+    Processing,
+}
 
-// impl From<RefundStatus> for enums::RefundStatus {
-//     fn from(item: RefundStatus) -> Self {
-//         match item {
-//             RefundStatus::Succeeded => Self::Success,
-//             RefundStatus::Failed => Self::Failure,
-//             RefundStatus::Processing => Self::Pending,
-//             //TODO: Review mapping
-//         }
-//     }
-// }
+impl From<RefundStatus> for enums::RefundStatus {
+    fn from(item: RefundStatus) -> Self {
+        match item {
+            RefundStatus::Succeeded => Self::Success,
+            RefundStatus::Failed => Self::Failure,
+            RefundStatus::Processing => Self::Pending,
+            //TODO: Review mapping
+        }
+    }
+}
 
-// //TODO: Fill the struct with respective fields
-// #[derive(Default, Debug, Clone, Serialize, Deserialize)]
-// pub struct RefundResponse {
-// }
+//TODO: Fill the struct with respective fields
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+pub struct RefundResponse {
+}
 
-// impl TryFrom<types::RefundsResponseRouterData<api::Execute, RefundResponse>>
-//     for types::RefundsRouterData<api::Execute>
-// {
-//     type Error = error_stack::Report<errors::ParsingError>;
-//     fn try_from(
-//         _item: types::RefundsResponseRouterData<api::Execute, RefundResponse>,
-//     ) -> Result<Self, Self::Error> {
-//         todo!()
-//     }
-// }
+impl TryFrom<types::RefundsResponseRouterData<api::Execute, RefundResponse>>
+    for types::RefundsRouterData<api::Execute>
+{
+    type Error = error_stack::Report<errors::ParsingError>;
+    fn try_from(
+        _item: types::RefundsResponseRouterData<api::Execute, RefundResponse>,
+    ) -> Result<Self, Self::Error> {
+        todo!()
+    }
+}
 
-// impl TryFrom<types::RefundsResponseRouterData<api::RSync, RefundResponse>> for types::RefundsRouterData<api::RSync>
-// {
-//      type Error = error_stack::Report<errors::ParsingError>;
-//     fn try_from(_item: types::RefundsResponseRouterData<api::RSync, RefundResponse>) -> Result<Self,Self::Error> {
-//          todo!()
-//      }
-//  }
+impl TryFrom<types::RefundsResponseRouterData<api::RSync, RefundResponse>> for types::RefundsRouterData<api::RSync>
+{
+     type Error = error_stack::Report<errors::ParsingError>;
+    fn try_from(_item: types::RefundsResponseRouterData<api::RSync, RefundResponse>) -> Result<Self,Self::Error> {
+         todo!()
+     }
+ }
 
-// //TODO: Fill the struct with respective fields
-// #[derive(Default, Debug, Serialize, Deserialize, PartialEq)]
-// pub struct BamboraErrorResponse {}
+//TODO: Fill the struct with respective fields
+#[derive(Default, Debug, Serialize, Deserialize, PartialEq)]
+pub struct BamboraErrorResponse {}
