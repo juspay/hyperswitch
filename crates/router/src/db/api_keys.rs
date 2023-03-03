a2913fbe1c247ff98dd2eebd25f18c4501723a6c
Sanchith Hegde <sanchith.hegde@juspay.in>
2023-03-03 18:06:23 +0530

    feat(api_keys): implement query to fetch API key by hashed API key

--- crates/router/src/db/api_keys.rs
+++ crates/router/src/db/api_keys.rs
@@ -27,6 +27,11 @@ pub trait ApiKeyInterface {
         key_id: &str,
     ) -> CustomResult<Option<storage::ApiKey>, errors::StorageError>;
 
+    async fn find_api_key_by_hash_optional(
+        &self,
+        hashed_api_key: storage::HashedApiKey,
+    ) -> CustomResult<Option<storage::ApiKey>, errors::StorageError>;
+
     async fn list_api_keys_by_merchant_id(
         &self,
         merchant_id: &str,
@@ -80,6 +85,17 @@ impl ApiKeyInterface for Store {
             .into_report()
     }
 
+    async fn find_api_key_by_hash_optional(
+        &self,
+        hashed_api_key: storage::HashedApiKey,
+    ) -> CustomResult<Option<storage::ApiKey>, errors::StorageError> {
+        let conn = pg_connection(&self.master_pool).await?;
+        storage::ApiKey::find_optional_by_hashed_api_key(&conn, hashed_api_key)
+            .await
+            .map_err(Into::into)
+            .into_report()
+    }
+
     async fn list_api_keys_by_merchant_id(
         &self,
         merchant_id: &str,
@@ -121,6 +137,14 @@ impl ApiKeyInterface for MockDb {
     async fn find_api_key_by_key_id_optional(
         &self,
         _key_id: &str,
+    ) -> CustomResult<Option<storage::ApiKey>, errors::StorageError> {
+        // [#172]: Implement function for `MockDb`
+        Err(errors::StorageError::MockDbError)?
+    }
+
+    async fn find_api_key_by_hash_optional(
+        &self,
+        _hashed_api_key: storage::HashedApiKey,
     ) -> CustomResult<Option<storage::ApiKey>, errors::StorageError> {
         // [#172]: Implement function for `MockDb`
         Err(errors::StorageError::MockDbError)?
