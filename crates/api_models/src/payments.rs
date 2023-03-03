bf6f64e2a4bac97526d109275ce08d1de4c839a5
Narayan Bhat <narayan.bhat@juspay.in>
2023-03-03 22:10:51 +0530

    refactor: rename statement descriptor to statement descriptor name

--- crates/api_models/src/payments.rs
+++ crates/api_models/src/payments.rs
@@ -155,7 +155,7 @@ pub struct PaymentsRequest {
 
     /// For non-card charges, you can use this value as the complete description that appears on your customers’ statements. Must contain at least one letter, maximum 22 characters.
     #[schema(max_length = 255, example = "Hyperswitch Router")]
-    pub statement_descriptor: Option<String>,
+    pub statement_descriptor_name: Option<String>,
 
     /// Provides information about a card payment that customers see on their statements. Concatenated with the prefix (shortened descriptor) or statement descriptor that’s set on the account to form the complete statement descriptor. Maximum 22 characters for the concatenated descriptor.
     #[schema(max_length = 255, example = "Payment for shoes purchase")]
@@ -862,7 +862,7 @@ pub struct PaymentsResponse {
     pub authentication_type: Option<api_enums::AuthenticationType>,
     /// For non-card charges, you can use this value as the complete description that appears on your customers’ statements. Must contain at least one letter, maximum 22 characters.
     #[schema(max_length = 255, example = "Hyperswitch Router")]
-    pub statement_descriptor: Option<String>,
+    pub statement_descriptor_name: Option<String>,
     /// Provides information about a card payment that customers see on their statements. Concatenated with the prefix (shortened descriptor) or statement descriptor that’s set on the account to form the complete statement descriptor. Maximum 255 characters for the concatenated descriptor.
     #[schema(max_length = 255, example = "Payment for shoes purchase")]
     pub statement_descriptor_suffix: Option<String>,
