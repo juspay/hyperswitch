ccb1639f0d2b816f5ac45a338a861e1691f13cfe
sai harsha <sai.harsha@sai.harsha-MacBookPro>
2023-03-06 21:31:23 +0530

    reverted unnecessary changes

--- crates/router/src/services/api.rs
+++ crates/router/src/services/api.rs
@@ -188,7 +188,6 @@ where
                 Some(request) => {
                     logger::debug!(connector_request=?request);
                     let response = call_connector_api(state, request).await;
-                    print!(">>>response{:?}", response);
                     logger::debug!(connector_response=?response);
                     match response {
                         Ok(body) => {
