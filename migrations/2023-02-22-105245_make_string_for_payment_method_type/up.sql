16833ba479866619fd9f755e65ac335e4c9e8540
sai harsha <sai.harsha@sai.harsha-MacBookPro>
2023-03-06 21:26:51 +0530

    revered unnecessary changes

--- migrations/2023-02-22-105245_make_string_for_payment_method_type/up.sql
+++
@@ -1,8 +0,0 @@
--- Your SQL goes here
-ALTER TABLE payment_attempt
-ALTER COLUMN payment_method TYPE VARCHAR(50);
-
-ALTER TABLE payment_methods
-ALTER COLUMN payment_method TYPE VARCHAR(50);
-
-DROP TYPE "PaymentMethodType";
