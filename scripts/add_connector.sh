c0f1771fc67e741a8f0f07fd91cefd1cbf831de9
Sanchith Hegde <sanchith.hegde@juspay.in>
2023-03-07 14:28:59 +0530

    chore(add_connector): add newlines

--- scripts/add_connector.sh
+++ scripts/add_connector.sh
@@ -9,9 +9,11 @@ if [[ -z "$pg" ]]; then
     exit
 fi
 cd $SCRIPT/..
+
 # remove template files if already created for this connector
 rm -rf $conn/$pg $conn/$pg.rs
 git checkout $conn.rs $src/types/api.rs $src/configs/settings.rs config/Development.toml config/docker_compose.toml config/config.example.toml loadtest/config/Development.toml crates/api_models/src/enums.rs
+
 # add enum for this connector in required places
 sed -i'' -e "s/pub use self::{/pub mod ${pg};\n\npub use self::{/" $conn.rs
 sed -i'' -e "s/};/${pg}::${pgc},\n};/" $conn.rs 
@@ -27,23 +29,30 @@ sed -i'' -e "s/\[connectors.supported\]/${pg}.base_url = ""\n\n[connectors.suppo
 sed  -r -i'' -e "s/cards = \[/cards = [\n\t\"${pg}\",/" loadtest/config/Development.toml
 sed -i'' -e "s/Dummy,/Dummy,\n\t${pgc},/" crates/api_models/src/enums.rs
 sed -i'' -e "s/pub enum RoutableConnectors {/pub enum RoutableConnectors {\n\t${pgc},/" crates/api_models/src/enums.rs
+
 # remove temporary files created in above step
 rm $conn.rs-e $src/types/api.rs-e $src/configs/settings.rs-e config/Development.toml-e config/docker_compose.toml-e config/config.example.toml-e loadtest/config/Development.toml-e crates/api_models/src/enums.rs-e
 cd $conn/ 
+
 # generate template files for the connector
 cargo install cargo-generate
 cargo gen-pg $pg
+
 # move sub files and test files to appropriate folder
 mv $pg/mod.rs $pg.rs
 mv $pg/test.rs ${tests}/$pg.rs
+
 # remove changes from tests if already done for this connector
 git checkout ${tests}/main.rs ${tests}/connector_auth.rs 
+
 # add enum for this connector in test folder
 sed -i'' -e "s/mod utils;/mod ${pg};\nmod utils;/" ${tests}/main.rs
 sed -i'' -e "s/struct ConnectorAuthentication {/struct ConnectorAuthentication {\n\tpub ${pg}: Option<HeaderKey>,/" ${tests}/connector_auth.rs 
+
 # remove temporary files created in above step
 rm ${tests}/main.rs-e ${tests}/connector_auth.rs-e 
 cargo build
 echo "Successfully created connector. Running the tests of "$pg.rs
+
 # runs tests for the new connector
 cargo test --package router --test connectors -- $pg
