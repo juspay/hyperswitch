42d3f81513f154cb9cf32646c77b6cfe6ee74fa6
Abhishek Marrivagu <abhi.codes10@gmail.com>
2023-03-14 20:17:48 +0530

    enhance(migrations): add query to populate attempt_id column in payment_intent table

--- migrations/2023-01-20-113235_add_attempt_id_to_payment_intent/up.sql
+++ migrations/2023-01-20-113235_add_attempt_id_to_payment_intent/up.sql
@@ -1,5 +1,11 @@
 -- Your SQL goes here
-ALTER TABLE payment_intent ADD COLUMN attempt_id VARCHAR(255) NOT NULL DEFAULT 'deprecated_payment_intent';
-ALTER TABLE payment_intent ALTER COLUMN attempt_id DROP DEFAULT;
-CREATE UNIQUE INDEX payment_attempt_attempt_id_index ON payment_attempt (attempt_id);
+ALTER TABLE payment_intent ADD COLUMN attempt_id VARCHAR(64);
+
+UPDATE payment_intent SET attempt_id = payment_attempt.attempt_id from payment_attempt where payment_intent.payment_id = payment_attempt.payment_id;
+
+ALTER TABLE payment_intent ALTER COLUMN attempt_id NOT NULL;
+
+CREATE UNIQUE INDEX payment_attempt_attempt_id_merchant_id_index ON payment_attempt (attempt_id, merchant_id);
+
+-- Because payment_attempt table can have rows with same payment_id and merchant_id, this index is dropped.
 DROP index payment_attempt_payment_id_merchant_id_index;
