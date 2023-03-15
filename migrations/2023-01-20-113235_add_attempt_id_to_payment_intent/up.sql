b8e1eb4d799dc50c1b9cd19f90cf994cb0dde0c5
Abhishek Marrivagu <abhi.codes10@gmail.com>
2023-03-15 11:34:24 +0530

    fix(migrations): correct index name in down sql and add default in up sql

--- migrations/2023-01-20-113235_add_attempt_id_to_payment_intent/up.sql
+++ migrations/2023-01-20-113235_add_attempt_id_to_payment_intent/up.sql
@@ -1,9 +1,7 @@
 -- Your SQL goes here
-ALTER TABLE payment_intent ADD COLUMN attempt_id VARCHAR(64);
+ALTER TABLE payment_intent ADD COLUMN attempt_id VARCHAR(64) NOT NULL DEFAULT 'xxx';
 
 UPDATE payment_intent SET attempt_id = payment_attempt.attempt_id from payment_attempt where payment_intent.payment_id = payment_attempt.payment_id;
-
-ALTER TABLE payment_intent ALTER COLUMN attempt_id NOT NULL;
 
 CREATE UNIQUE INDEX payment_attempt_attempt_id_merchant_id_index ON payment_attempt (attempt_id, merchant_id);
 
