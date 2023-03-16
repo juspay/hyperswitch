5a769e62d189372f76a12c7dfaa523d47bfb4f00
SamraatBansal <samraatbansal7@gmail.com>
2023-03-16 16:19:55 +0530

    fix bugs

--- crates/router/src/core/payments/flows.rs
+++ crates/router/src/core/payments/flows.rs
@@ -115,6 +115,10 @@ macro_rules! default_imp_for_connector_redirect_response{
 default_imp_for_connector_redirect_response!(
     connector::Aci,
     connector::Adyen,
+    connector::Applepay,
+    connector::Authorizedotnet,
+    connector::Bambora,
+    connector::Bluesnap,
     connector::Braintree,
     connector::Cybersource,
     connector::Dlocal,
