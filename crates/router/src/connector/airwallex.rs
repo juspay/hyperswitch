de2cc3e181919a3dc6b152b9d2cc17d9de81bbb4
SamraatBansal <samraatbansal7@gmail.com>
2023-03-16 17:15:54 +0530

    redact redundant code

--- crates/router/src/connector/airwallex.rs
+++ crates/router/src/connector/airwallex.rs
@@ -511,7 +511,6 @@ impl
             &req_obj,
         )
         .change_context(errors::ConnectorError::RequestEncodingFailed)?;
-        println!("reqqqqqq {:?}", req);
         Ok(Some(req))
     }
     fn build_request(
