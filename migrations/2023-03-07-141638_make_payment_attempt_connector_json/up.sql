d8a21aabb85c00c4fb8d9373c7622bc6bc016146
shashank_attarde <shashank.attarde@juspay.in>
2023-03-16 14:11:48 +0530

    refactor(migrations): no need to fill in algorithm in payment attempt connector

--- migrations/2023-03-07-141638_make_payment_attempt_connector_json/up.sql
+++ migrations/2023-03-07-141638_make_payment_attempt_connector_json/up.sql
@@ -4,12 +4,5 @@ ALTER TABLE payment_attempt
 ALTER COLUMN connector TYPE JSONB
 USING jsonb_build_object(
     'routed_through', connector,
-    'algorithm',      CASE WHEN connector IS NULL THEN
-        NULL
-    ELSE
-        jsonb_build_object(
-            'type', 'single',
-            'data', connector
-        )
-    END
+    'algorithm',      NULL
 );
