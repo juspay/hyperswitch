949c8fca50844fe9181512af4ce88ffc936106a2
sai harsha <sai.harsha@sai.harsha-MacBookPro>
2023-03-17 15:42:46 +0530

    latest main pull

--- crates/router/src/connector/mollie.rs
+++ crates/router/src/connector/mollie.rs
@@ -477,7 +477,7 @@ impl api::IncomingWebhook for Mollie {
     fn get_webhook_object_reference_id(
         &self,
         _request: &api::IncomingWebhookRequestDetails<'_>,
-    ) -> CustomResult<String, errors::ConnectorError> {
+    ) -> CustomResult<api_models::webhooks::ObjectReferenceId, errors::ConnectorError> {
         Err(errors::ConnectorError::WebhooksNotImplemented).into_report()
     }
 
