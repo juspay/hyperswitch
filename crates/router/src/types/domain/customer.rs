0b1a54bff57d74ceb773c876459552191b22b8f4
dracarys18 <karthikey.hegde@juspay.in>
2023-03-31 16:22:41 +0530

    feat: implement domain type conversion for customer and merchant account

--- crates/router/src/types/domain/customer.rs
+++ crates/router/src/types/domain/customer.rs
@@ -2,7 +2,7 @@ use common_utils::pii;
 use masking::Secret;
 use time::PrimitiveDateTime;
 
-use super::behaviour;
+use crate::errors::{CustomResult, ValidationError};
 
 #[derive(Clone, Debug)]
 pub struct Customer {
@@ -16,4 +16,57 @@ pub struct Customer {
     pub description: Option<String>,
     pub created_at: PrimitiveDateTime,
     pub metadata: Option<pii::SecretSerdeValue>,
+}
+
+#[async_trait::async_trait]
+impl super::behaviour::Conversion for Customer {
+    type DstType = storage_models::customers::Customer;
+    type NewDstType = storage_models::customers::CustomerNew;
+    async fn convert(self) -> CustomResult<Self::DstType, ValidationError> {
+        Ok(storage_models::customers::Customer {
+            id: self.id.ok_or(ValidationError::MissingRequiredField {
+                field_name: "id".to_string(),
+            })?,
+            customer_id: self.customer_id,
+            merchant_id: self.merchant_id,
+            name: self.name,
+            email: self.email,
+            phone: self.phone,
+            phone_country_code: self.phone_country_code,
+            description: self.description,
+            created_at: self.created_at,
+            metadata: self.metadata,
+        })
+    }
+
+    async fn convert_back(item: Self::DstType) -> CustomResult<Self, ValidationError>
+    where
+        Self: Sized,
+    {
+        Ok(Self {
+            id: Some(item.id),
+            customer_id: item.customer_id,
+            merchant_id: item.merchant_id,
+            name: item.name,
+            email: item.email,
+            phone: item.phone,
+            phone_country_code: item.phone_country_code,
+            description: item.description,
+            created_at: item.created_at,
+            metadata: item.metadata,
+        })
+    }
+
+    async fn construct_new(self) -> CustomResult<Self::NewDstType, ValidationError> {
+        Ok(storage_models::customers::CustomerNew {
+            customer_id: self.customer_id,
+            merchant_id: self.merchant_id,
+            name: self.name,
+            email: self.email,
+            phone: self.phone,
+            description: self.description,
+            phone_country_code: self.phone_country_code,
+            metadata: self.metadata,
+        })
+    }
 }
--- crates/router/src/types/domain/merchant_account.rs
+++ crates/router/src/types/domain/merchant_account.rs
@@ -1,9 +1,8 @@
 use common_utils::pii;
-use masking::{Secret, StrongSecret};
+use masking::StrongSecret;
 use storage_models::enums;
-use time::PrimitiveDateTime;
 
-use super::behaviour;
+use crate::errors::{CustomResult, ValidationError};
 
 #[derive(Clone, Debug)]
 pub struct MerchantAccount {
@@ -24,4 +23,78 @@ pub struct MerchantAccount {
     pub metadata: Option<pii::SecretSerdeValue>,
     pub routing_algorithm: Option<serde_json::Value>,
     pub api_key: Option<StrongSecret<String>>,
+}
+
+#[async_trait::async_trait]
+impl super::behaviour::Conversion for MerchantAccount {
+    type DstType = storage_models::merchant_account::MerchantAccount;
+    type NewDstType = storage_models::merchant_account::MerchantAccountNew;
+    async fn convert(self) -> CustomResult<Self::DstType, ValidationError> {
+        Ok(storage_models::merchant_account::MerchantAccount {
+            id: self.id.ok_or(ValidationError::MissingRequiredField {
+                field_name: "id".to_string(),
+            })?,
+            merchant_id: self.merchant_id,
+            return_url: self.return_url,
+            enable_payment_response_hash: self.enable_payment_response_hash,
+            payment_response_hash_key: self.payment_response_hash_key,
+            redirect_to_merchant_with_http_post: self.redirect_to_merchant_with_http_post,
+            merchant_name: self.merchant_name,
+            merchant_details: self.merchant_details,
+            webhook_details: self.webhook_details,
+            sub_merchants_enabled: self.sub_merchants_enabled,
+            parent_merchant_id: self.parent_merchant_id,
+            publishable_key: self.publishable_key,
+            storage_scheme: self.storage_scheme,
+            locker_id: self.locker_id,
+            metadata: self.metadata,
+            routing_algorithm: self.routing_algorithm,
+            api_key: self.api_key,
+        })
+    }
+
+    async fn convert_back(item: Self::DstType) -> CustomResult<Self, ValidationError>
+    where
+        Self: Sized,
+    {
+        Ok(Self {
+            id: Some(item.id),
+            merchant_id: item.merchant_id,
+            return_url: item.return_url,
+            enable_payment_response_hash: item.enable_payment_response_hash,
+            payment_response_hash_key: item.payment_response_hash_key,
+            redirect_to_merchant_with_http_post: item.redirect_to_merchant_with_http_post,
+            merchant_name: item.merchant_name,
+            merchant_details: item.merchant_details,
+            webhook_details: item.webhook_details,
+            sub_merchants_enabled: item.sub_merchants_enabled,
+            parent_merchant_id: item.parent_merchant_id,
+            publishable_key: item.publishable_key,
+            storage_scheme: item.storage_scheme,
+            locker_id: item.locker_id,
+            metadata: item.metadata,
+            routing_algorithm: item.routing_algorithm,
+            api_key: item.api_key,
+        })
+    }
+
+    async fn construct_new(self) -> CustomResult<Self::NewDstType, ValidationError> {
+        Ok(storage_models::merchant_account::MerchantAccountNew {
+            merchant_id: self.merchant_id,
+            merchant_name: self.merchant_name,
+            merchant_details: self.merchant_details,
+            return_url: self.return_url,
+            webhook_details: self.webhook_details,
+            sub_merchants_enabled: self.sub_merchants_enabled,
+            parent_merchant_id: self.parent_merchant_id,
+            enable_payment_response_hash: Some(self.enable_payment_response_hash),
+            payment_response_hash_key: self.payment_response_hash_key,
+            redirect_to_merchant_with_http_post: Some(self.redirect_to_merchant_with_http_post),
+            publishable_key: self.publishable_key,
+            locker_id: self.locker_id,
+            metadata: self.metadata,
+            routing_algorithm: self.routing_algorithm,
+            api_key: self.api_key,
+        })
+    }
 }
