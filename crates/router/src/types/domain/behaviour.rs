1fc0d6e6e3799ca82c7183dc79b1d16818d7ed06
Nishant Joshi <nishant.joshi@juspay.in>
2023-03-31 15:50:22 +0530

    feat(domain): add function for creating new storage type

--- crates/router/src/types/domain/behaviour.rs
+++ crates/router/src/types/domain/behaviour.rs
@@ -4,11 +4,14 @@ use common_utils::errors::{CustomResult, ValidationError};
 #[async_trait::async_trait]
 pub trait Conversion {
     type DstType;
+    type NewDstType;
     async fn convert(self) -> CustomResult<Self::DstType, ValidationError>;
 
     async fn convert_back(item: Self::DstType) -> CustomResult<Self, ValidationError>
     where
         Self: Sized;
+
+    async fn construct_new(self) -> CustomResult<Self::NewDstType, ValidationError>;
 }
 
 #[async_trait::async_trait]
