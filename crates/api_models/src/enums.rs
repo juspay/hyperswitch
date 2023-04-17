bc38bc47d808932160d373916eeacf775a321f50
rishavkar <73836104+rishavkar@users.noreply.github.com>
2023-04-17 23:58:31 +0530

    Frm integration with hyperswitch (#857)
    Co-authored-by: Arun Raj M <jarnura47@gmail.com>
    Co-authored-by: SamraatBansal <55536657+SamraatBansal@users.noreply.github.com>

--- crates/api_models/src/enums.rs
+++ crates/api_models/src/enums.rs
@@ -842,6 +842,40 @@ pub enum DisputeStatus {
     DisputeLost,
 }
 
+#[derive(
+    Clone,
+    Debug,
+    serde::Deserialize,
+    serde::Serialize,
+    strum::Display,
+    strum::EnumString,
+    frunk::LabelledGeneric,
+    ToSchema,
+)]
+#[strum(serialize_all = "snake_case")]
+#[serde(rename_all = "snake_case")]
+pub enum FrmAction {
+    CancelTxn,
+    AutoRefund,
+    ManualReview,
+}
+
+#[derive(
+    Clone,
+    Debug,
+    serde::Deserialize,
+    serde::Serialize,
+    strum::Display,
+    strum::EnumString,
+    frunk::LabelledGeneric,
+    ToSchema,
+)]
+#[strum(serialize_all = "snake_case")]
+#[serde(rename_all = "snake_case")]
+pub enum FrmPreferredFlowTypes {
+    Pre,
+    Post,
+}
 #[derive(Debug, Eq, PartialEq, Clone, serde::Serialize, serde::Deserialize)]
 pub struct UnresolvedResponseReason {
     pub code: String,
