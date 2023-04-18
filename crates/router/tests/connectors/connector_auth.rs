dee5f6156a4861acfa78dee3bc68054c478bb2a6
chikke srujan <121822803+srujanchikke@users.noreply.github.com>
2023-04-18 13:15:17 +0530

    feat(connector) : add template code for connector nexinets (#852)
    Signed-off-by: chikke srujan <121822803+srujanchikke@users.noreply.github.com>

--- crates/router/tests/connectors/connector_auth.rs
+++ crates/router/tests/connectors/connector_auth.rs
@@ -20,6 +20,7 @@ pub(crate) struct ConnectorAuthentication {
     pub globalpay: Option<HeaderKey>,
     pub mollie: Option<HeaderKey>,
     pub multisafepay: Option<HeaderKey>,
+    pub nexinets: Option<HeaderKey>,
     pub nuvei: Option<SignatureKey>,
     pub opennode: Option<HeaderKey>,
     pub payeezy: Option<SignatureKey>,
