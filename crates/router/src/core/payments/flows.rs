9aabf8323241a4e5bcace4f7805f74f34fecfe31
SamraatBansal <samraatbansal7@gmail.com>
2023-04-25 17:41:41 +0530

    add zen in dispute macro

--- crates/router/src/core/payments/flows.rs
+++ crates/router/src/core/payments/flows.rs
@@ -235,7 +235,8 @@ default_imp_for_accept_dispute!(
     connector::Trustpay,
     connector::Opennode,
     connector::Worldline,
-    connector::Worldpay
+    connector::Worldpay,
+    connector::Zen
 );
 
 macro_rules! default_imp_for_file_upload{
@@ -281,7 +282,8 @@ default_imp_for_file_upload!(
     connector::Trustpay,
     connector::Opennode,
     connector::Worldline,
-    connector::Worldpay
+    connector::Worldpay,
+    connector::Zen
 );
 
 macro_rules! default_imp_for_submit_evidence{
@@ -327,5 +329,6 @@ default_imp_for_submit_evidence!(
     connector::Trustpay,
     connector::Opennode,
     connector::Worldline,
-    connector::Worldpay
+    connector::Worldpay,
+    connector::Zen
 );
