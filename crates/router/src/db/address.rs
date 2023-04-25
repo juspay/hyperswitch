62b6c7db71506da61fce31e6692e8739ff0667c9
jeeva <jeevaprakashdr@hotmail.com>
2023-04-25 11:20:47 +0100

    return result types from address interface method implementations

--- crates/router/src/db/address.rs
+++ crates/router/src/db/address.rs
@@ -95,37 +95,56 @@ impl AddressInterface for MockDb {
     #[allow(clippy::unwrap_used)]
     async fn find_address(
         &self,
-        _address_id: &str,
+        address_id: &str,
     ) -> CustomResult<storage::Address, errors::StorageError> {
-        let addresses = self.addresses.lock().await;
-        Ok(addresses
+        match self
+            .addresses
+            .lock()
+            .await
             .iter()
-            .find(|address| address.address_id == _address_id)
-            .unwrap()
-            .clone())
+            .find(|address| address.address_id == address_id)
+        {
+            Some(address) => return Ok(address.clone()),
+            None => {
+                return Err(
+                    errors::StorageError::ValueNotFound("address not found".to_string()).into(),
+                )
+            }
+        }
     }
 
     #[allow(clippy::unwrap_used)]
     async fn update_address(
         &self,
-        _address_id: String,
-        _address: storage::AddressUpdate,
+        address_id: String,
+        address_update: storage::AddressUpdate,
     ) -> CustomResult<storage::Address, errors::StorageError> {
-        let mut addresses = self.addresses.lock().await;
-        let address = addresses
+        match self
+            .addresses
+            .lock()
+            .await
             .iter_mut()
-            .find(|address| address.address_id == _address_id)
-            .unwrap();
-
-        let address_updated = AddressUpdateInternal::from(_address).create_address(address.clone());
-        *address = address_updated.clone();
-        Ok(address_updated)
+            .find(|address| address.address_id == address_id)
+            .map(|a| {
+                let address_updated =
+                    AddressUpdateInternal::from(address_update).create_address(a.clone());
+                *a = address_updated.clone();
+                address_updated
+            }) {
+            Some(address_updated) => Ok(address_updated),
+            None => {
+                return Err(errors::StorageError::ValueNotFound(
+                    "cannot find address to update".to_string(),
+                )
+                .into())
+            }
+        }
     }
 
     #[allow(clippy::unwrap_used)]
     async fn insert_address(
         &self,
-        _address: storage::AddressNew,
+        address_new: storage::AddressNew,
     ) -> CustomResult<storage::Address, errors::StorageError> {
         let mut addresses = self.addresses.lock().await;
         let now = common_utils::date_time::now();
@@ -133,22 +152,22 @@ impl AddressInterface for MockDb {
         let address = storage::Address {
             #[allow(clippy::as_conversions)]
             id: addresses.len() as i32,
-            address_id: _address.address_id,
-            city: _address.city,
-            country: _address.country,
-            line1: _address.line1,
-            line2: _address.line2,
-            line3: _address.line3,
-            state: _address.state,
-            zip: _address.zip,
-            first_name: _address.first_name,
-            last_name: _address.last_name,
-            phone_number: _address.phone_number,
-            country_code: _address.country_code,
+            address_id: address_new.address_id,
+            city: address_new.city,
+            country: address_new.country,
+            line1: address_new.line1,
+            line2: address_new.line2,
+            line3: address_new.line3,
+            state: address_new.state,
+            zip: address_new.zip,
+            first_name: address_new.first_name,
+            last_name: address_new.last_name,
+            phone_number: address_new.phone_number,
+            country_code: address_new.country_code,
             created_at: now,
             modified_at: now,
-            customer_id: _address.customer_id,
-            merchant_id: _address.merchant_id,
+            customer_id: address_new.customer_id,
+            merchant_id: address_new.merchant_id,
         };
 
         addresses.push(address.clone());
@@ -159,21 +178,30 @@ impl AddressInterface for MockDb {
     #[allow(clippy::unwrap_used)]
     async fn update_address_by_merchant_id_customer_id(
         &self,
-        _customer_id: &str,
-        _merchant_id: &str,
-        _address: storage::AddressUpdate,
+        customer_id: &str,
+        merchant_id: &str,
+        address_update: storage::AddressUpdate,
     ) -> CustomResult<Vec<storage::Address>, errors::StorageError> {
-        let addresses = self.addresses.lock().await;
-
-        let address = addresses
-            .iter()
+        match self
+            .addresses
+            .lock()
+            .await
+            .iter_mut()
             .find(|address| {
-                address.customer_id == _customer_id && address.merchant_id == _merchant_id
+                address.customer_id == customer_id && address.merchant_id == merchant_id
             })
-            .cloned()
-            .map(|address| vec![address])
-            .unwrap();
-
-        Ok(address)
+            .map(|a| {
+                let address_updated =
+                    AddressUpdateInternal::from(address_update).create_address(a.clone());
+                *a = address_updated.clone();
+                address_updated
+            }) {
+            Some(address) => Ok(vec![address.clone()]),
+            None => {
+                return Err(
+                    errors::StorageError::ValueNotFound("address not found".to_string()).into(),
+                )
+            }
+        }
     }
 }
