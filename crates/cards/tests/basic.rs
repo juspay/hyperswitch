7f75e79b95536df933809915a730386382e7a6d3
Naman Agarwal <namanddev7@gmail.com>
2023-04-26 00:10:01 +0530

    fix(cards): tests allow expect

--- crates/cards/tests/basic.rs
+++ crates/cards/tests/basic.rs
@@ -1,4 +1,4 @@
-#![allow(clippy::unwrap_used)]
+#![allow(clippy::unwrap_used, clippy::expect_used)]
 
 use cards::{CardExpiration, CardExpirationMonth, CardExpirationYear, CardSecurityCode};
 use common_utils::date_time;
