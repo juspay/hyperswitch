d0334b5c02102d07ae9da294bf4235edf69a6b39
Nachiket Kanore <nachiket.kanore@gmail.com>
2023-04-28 16:38:10 +0530

    add suggested changes on PR

--- crates/router/src/compatibility/stripe/app.rs
+++ crates/router/src/compatibility/stripe/app.rs
@@ -13,9 +13,12 @@ impl PaymentIntents {
                 web::resource("/sync")
                     .route(web::post().to(payment_intents_retrieve_with_gateway_creds)),
             )
-            .service(web::resource("/").route(web::post().to(payment_intents_create)))
-            .service(web::resource("/{payment_id}").route(web::get().to(payment_intents_retrieve)))
-            .service(web::resource("/{payment_id}").route(web::post().to(payment_intents_update)))
+            .service(web::resource("").route(web::post().to(payment_intents_create)))
+            .service(
+                web::resource("/{payment_id}")
+                    .route(web::get().to(payment_intents_retrieve))
+                    .route(web::post().to(payment_intents_update)),
+            )
             .service(
                 web::resource("/{payment_id}/confirm")
                     .route(web::post().to(payment_intents_confirm)),
@@ -33,9 +36,12 @@ impl SetupIntents {
     pub fn server(state: routes::AppState) -> Scope {
         web::scope("/setup_intents")
             .app_data(web::Data::new(state))
-            .service(web::resource("/").route(web::post().to(setup_intents_create)))
-            .service(web::resource("/{setup_id}").route(web::get().to(setup_intents_retrieve)))
-            .service(web::resource("/{setup_id}").route(web::post().to(setup_intents_update)))
+            .service(web::resource("").route(web::post().to(setup_intents_create)))
+            .service(
+                web::resource("/{setup_id}")
+                    .route(web::get().to(setup_intents_retrieve))
+                    .route(web::post().to(setup_intents_update)),
+            )
             .service(
                 web::resource("/{setup_id}/confirm").route(web::post().to(setup_intents_confirm)),
             )
@@ -48,11 +54,14 @@ impl Refunds {
     pub fn server(config: routes::AppState) -> Scope {
         web::scope("/refunds")
             .app_data(web::Data::new(config))
-            .service(web::resource("/").route(web::post().to(refund_create)))
-            .service(web::resource("/{refund_id}").route(web::get().to(refund_retrieve)))
-            .service(web::resource("/{refund_id}").route(web::post().to(refund_update)))
+            .service(web::resource("").route(web::post().to(refund_create)))
             .service(
                 web::resource("/sync").route(web::post().to(refund_retrieve_with_gateway_creds)),
+            )
+            .service(
+                web::resource("/{refund_id}")
+                    .route(web::get().to(refund_retrieve))
+                    .route(web::post().to(refund_update)),
             )
     }
 }
@@ -63,10 +72,13 @@ impl Customers {
     pub fn server(config: routes::AppState) -> Scope {
         web::scope("/customers")
             .app_data(web::Data::new(config))
-            .service(web::resource("/").route(web::post().to(customer_create)))
-            .service(web::resource("/{customer_id}").route(web::get().to(customer_retrieve)))
-            .service(web::resource("/{customer_id}").route(web::post().to(customer_update)))
-            .service(web::resource("/{customer_id}").route(web::delete().to(customer_delete)))
+            .service(web::resource("").route(web::post().to(customer_create)))
+            .service(
+                web::resource("/{customer_id}")
+                    .route(web::get().to(customer_retrieve))
+                    .route(web::post().to(customer_update))
+                    .route(web::delete().to(customer_delete)),
+            )
             .service(
                 web::resource("/{customer_id}/payment_methods")
                     .route(web::get().to(list_customer_payment_method_api)),
