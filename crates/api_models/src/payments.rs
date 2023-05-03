da818899387094e2c282820153c791a2f5c38466
Sangamesh <sangamesh.kulkarni@juspay.in>
2023-05-03 22:04:48 +0530

    chore: remove camelcase for applepay response

--- crates/api_models/src/payments.rs
+++ crates/api_models/src/payments.rs
@@ -1574,7 +1574,6 @@ pub struct ApplepaySessionTokenResponse {
 }
 
 #[derive(Debug, Clone, serde::Serialize, ToSchema, serde::Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct ApplePaySessionResponse {
     /// Timestamp at which session is requested
     pub epoch_timestamp: u64,
