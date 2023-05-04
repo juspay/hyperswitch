5fbbf23e9da55965f3fe5e6d8c8e58bf7fc7ca49
AkshayaFoiger <akshaya.shankar@juspay.in>
2023-05-04 14:46:21 +0530

    Formatted

--- crates/router/src/connector/stripe.rs
+++ crates/router/src/connector/stripe.rs
@@ -1305,8 +1305,9 @@ impl api::IncomingWebhook for Stripe {
             .change_context(errors::ConnectorError::WebhookEventTypeNotFound)?;
 
         Ok(match details.event_type.as_str() {
-            "payment_intent.payment_failed" | 
-            "payment_intent.failed" => api::IncomingWebhookEvent::PaymentIntentFailure,
+            "payment_intent.payment_failed" | "payment_intent.failed" => {
+                api::IncomingWebhookEvent::PaymentIntentFailure
+            }
             "payment_intent.succeeded" => api::IncomingWebhookEvent::PaymentIntentSuccess,
             _ => Err(errors::ConnectorError::WebhookEventTypeNotFound).into_report()?,
         })
