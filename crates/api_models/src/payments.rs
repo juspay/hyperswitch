5d5cb88b0fe426782ea53abb1c37a93b033c4204
Sk Sakil Mostak <skmahim71@gmail.com>
2023-05-08 15:48:47 +0530

    remove option from email in przelewy24

--- crates/api_models/src/payments.rs
+++ crates/api_models/src/payments.rs
@@ -629,7 +629,7 @@ pub enum BankRedirectData {
     },
     Przelewy24 {
         // Shopper Email
-        email: Option<Email>,
+        email: Email,
     },
     Sofort {
         /// The billing details for bank redirection
--- crates/router/src/connector/aci/transformers.rs
+++ crates/router/src/connector/aci/transformers.rs
@@ -196,7 +196,7 @@ impl TryFrom<&types::PaymentsAuthorizeRouterData> for AciPaymentsRequest {
                             bank_account_bank_name: None,
                             bank_account_bic: None,
                             bank_account_iban: None,
-                            customer_email: email.clone(),
+                            customer_email: Some(email.to_owned()),
                             shopper_result_url: item.request.router_return_url.clone(),
                         }))
                     }
