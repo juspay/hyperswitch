46b40ecce540b61eced7156555c0fcdcec170405
Sakil Mostak <73734619+Sakilmostak@users.noreply.github.com>
2023-05-13 14:58:41 +0530

    feat(connector): [ACI] Implement Trustly Bank Redirect (#1130)

--- crates/router/src/connector/adyen/transformers.rs
+++ crates/router/src/connector/adyen/transformers.rs
@@ -1165,11 +1165,11 @@ impl<'a> TryFrom<&api_models::payments::BankRedirectData> for AdyenPaymentMethod
                     payment_type: PaymentType::Sofort,
                 })),
             ),
-            api_models::payments::BankRedirectData::Trustly {} => Ok(AdyenPaymentMethod::Trustly(
-                Box::new(BankRedirectionPMData {
+            api_models::payments::BankRedirectData::Trustly { .. } => Ok(
+                AdyenPaymentMethod::Trustly(Box::new(BankRedirectionPMData {
                     payment_type: PaymentType::Trustly,
-                }),
-            )),
+                })),
+            ),
             _ => Err(errors::ConnectorError::NotImplemented("Payment method".to_string()).into()),
         }
     }
