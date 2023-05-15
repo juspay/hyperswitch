a6d8bf9b945c9a0c991e1343847deea107bdd54a
SamraatBansal <samraatbansal7@gmail.com>
2023-05-15 17:38:13 +0530

    feat: generate base script code for noon

--- crates/api_models/src/enums.rs
+++ crates/api_models/src/enums.rs
@@ -618,6 +618,7 @@ pub enum Connector {
     Multisafepay,
     Nexinets,
     Nmi,
+    Noon,
     Nuvei,
     // Payeezy, As psync and rsync are not supported by this connector, it is added as template code for future usage
     Paypal,
@@ -699,6 +700,7 @@ pub enum RoutableConnectors {
     Multisafepay,
     Nexinets,
     Nmi,
+    Noon,
     Nuvei,
     Opennode,
     // Payeezy, As psync and rsync are not supported by this connector, it is added as template code for future usage
