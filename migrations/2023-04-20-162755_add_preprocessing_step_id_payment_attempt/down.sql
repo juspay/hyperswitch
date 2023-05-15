37341de454df57654b59af17e1904a543ffd559e
Sangamesh <sangamesh.kulkarni@juspay.in>
2023-05-15 11:05:43 +0530

    fix: indexing error for preprocessing-step_id column

--- migrations/2023-04-20-162755_add_preprocessing_step_id_payment_attempt/down.sql
+++ migrations/2023-04-20-162755_add_preprocessing_step_id_payment_attempt/down.sql
@@ -1,3 +1,3 @@
 -- This file should undo anything in `up.sql`
-ALTER TABLE payment_attempt DROP COLUMN preprocessing_step_id;
 DROP INDEX preprocessing_step_id_index;
+ALTER TABLE payment_attempt DROP COLUMN preprocessing_step_id;
