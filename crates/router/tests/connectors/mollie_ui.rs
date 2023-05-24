3b9166e6dd1a44bb86374c07733c7d2b5a423fc7
swangi-kumari <swangi.12015941@lpu.in>
2023-05-24 11:43:52 +0530

    feat(Test): [Mollie] add test for giropay and eps

--- crates/router/tests/connectors/mollie_ui.rs
+++ crates/router/tests/connectors/mollie_ui.rs
@@ -84,6 +84,52 @@ async fn should_make_mollie_ideal_payment(c: WebDriver) -> Result<(), WebDriverE
     Ok(())
 }
 
+async fn should_make_mollie_eps_payment(c: WebDriver) -> Result<(), WebDriverError> {
+    let conn = MollieSeleniumTest {};
+    conn.make_redirection_payment(
+        c,
+        vec![
+            Event::Trigger(Trigger::Goto(&format!("{CHEKOUT_BASE_URL}/saved/38"))),
+            Event::Trigger(Trigger::Click(By::Id("card-submit-btn"))),
+            Event::Assert(Assert::IsPresent("Test profile")),
+            Event::Trigger(Trigger::Click(By::Css("input[value='paid']"))),
+            Event::Trigger(Trigger::Click(By::Css(
+                "button[class='button form__button']",
+            ))),
+            Event::Assert(Assert::IsPresent("Google")),
+            Event::Assert(Assert::Contains(
+                Selector::QueryParamStr,
+                "status=succeeded",
+            )),
+        ],
+    )
+    .await?;
+    Ok(())
+}
+
+async fn should_make_mollie_giropay_payment(c: WebDriver) -> Result<(), WebDriverError> {
+    let conn = MollieSeleniumTest {};
+    conn.make_redirection_payment(
+        c,
+        vec![
+            Event::Trigger(Trigger::Goto(&format!("{CHEKOUT_BASE_URL}/saved/41"))),
+            Event::Trigger(Trigger::Click(By::Id("card-submit-btn"))),
+            Event::Assert(Assert::IsPresent("Test profile")),
+            Event::Trigger(Trigger::Click(By::Css("input[value='paid']"))),
+            Event::Trigger(Trigger::Click(By::Css(
+                "button[class='button form__button']",
+            ))),
+            Event::Assert(Assert::IsPresent("Google")),
+            Event::Assert(Assert::Contains(
+                Selector::QueryParamStr,
+                "status=succeeded",
+            )),
+        ],
+    )
+    .await?;
+    Ok(())
+}
+
 #[test]
 #[serial]
 fn should_make_mollie_paypal_payment_test() {
@@ -100,4 +146,16 @@ fn should_make_mollie_sofort_payment_test() {
 #[serial]
 fn should_make_mollie_ideal_payment_test() {
     tester!(should_make_mollie_ideal_payment);
+}
+
+#[test]
+#[serial]
+fn should_make_mollie_eps_payment_test() {
+    tester!(should_make_mollie_eps_payment);
+}
+
+#[test]
+#[serial]
+fn should_make_mollie_giropay_payment_test() {
+    tester!(should_make_mollie_giropay_payment);
 }
