28fac33f48c161ec2ddefe46a541a6a6a35717f7
Sk Sakil Mostak <skmahim71@gmail.com>
2023-05-24 12:29:39 +0530

    refactor: format resolve

--- crates/router/tests/connectors/shift4_ui.rs
+++ crates/router/tests/connectors/shift4_ui.rs
@@ -13,69 +13,98 @@ impl SeleniumTest for Shift4SeleniumTest {
 
 async fn should_make_3ds_payment(c: WebDriver) -> Result<(), WebDriverError> {
     let conn = Shift4SeleniumTest {};
-    conn.make_redirection_payment(c, vec![
+    conn.make_redirection_payment(
+        c,
+        vec![
             Event::Trigger(Trigger::Goto(&format!("{CHEKOUT_BASE_URL}/saved/37"))),
             Event::Trigger(Trigger::Click(By::Id("card-submit-btn"))),
             Event::Trigger(Trigger::Click(By::ClassName("btn-success"))),
             Event::Assert(Assert::IsPresent("Google")),
-            Event::Assert(Assert::Contains(Selector::QueryParamStr, "status=succeeded")),
-
-    ]).await?;
+            Event::Assert(Assert::Contains(
+                Selector::QueryParamStr,
+                "status=succeeded",
+            )),
+        ],
+    )
+    .await?;
     Ok(())
 }
 
 async fn should_make_giropay_payment(c: WebDriver) -> Result<(), WebDriverError> {
     let conn = Shift4SeleniumTest {};
-    conn.make_redirection_payment(c, vec![
+    conn.make_redirection_payment(
+        c,
+        vec![
             Event::Trigger(Trigger::Goto(&format!("{CHEKOUT_BASE_URL}/saved/39"))),
             Event::Trigger(Trigger::Click(By::Id("card-submit-btn"))),
             Event::Trigger(Trigger::Click(By::ClassName("btn-success"))),
             Event::Assert(Assert::IsPresent("Google")),
-            Event::Assert(Assert::Contains(Selector::QueryParamStr, "status=succeeded")),
-
-    ]).await?;
+            Event::Assert(Assert::Contains(
+                Selector::QueryParamStr,
+                "status=succeeded",
+            )),
+        ],
+    )
+    .await?;
     Ok(())
 }
 
 async fn should_make_ideal_payment(c: WebDriver) -> Result<(), WebDriverError> {
     let conn = Shift4SeleniumTest {};
-    conn.make_redirection_payment(c, vec![
+    conn.make_redirection_payment(
+        c,
+        vec![
             Event::Trigger(Trigger::Goto(&format!("{CHEKOUT_BASE_URL}/saved/42"))),
             Event::Trigger(Trigger::Click(By::Id("card-submit-btn"))),
             Event::Trigger(Trigger::Click(By::ClassName("btn-success"))),
             Event::Assert(Assert::IsPresent("Google")),
-            Event::Assert(Assert::Contains(Selector::QueryParamStr, "status=succeeded")),
-
-    ]).await?;
+            Event::Assert(Assert::Contains(
+                Selector::QueryParamStr,
+                "status=succeeded",
+            )),
+        ],
+    )
+    .await?;
     Ok(())
 }
 
 async fn should_make_sofort_payment(c: WebDriver) -> Result<(), WebDriverError> {
     let conn = Shift4SeleniumTest {};
-    conn.make_redirection_payment(c, vec![
+    conn.make_redirection_payment(
+        c,
+        vec![
             Event::Trigger(Trigger::Goto(&format!("{CHEKOUT_BASE_URL}/saved/43"))),
             Event::Trigger(Trigger::Click(By::Id("card-submit-btn"))),
             Event::Trigger(Trigger::Click(By::ClassName("btn-success"))),
             Event::Assert(Assert::IsPresent("Google")),
-            Event::Assert(Assert::Contains(Selector::QueryParamStr, "status=succeeded")),
-
-    ]).await?;
+            Event::Assert(Assert::Contains(
+                Selector::QueryParamStr,
+                "status=succeeded",
+            )),
+        ],
+    )
+    .await?;
     Ok(())
 }
 
 async fn should_make_eps_payment(c: WebDriver) -> Result<(), WebDriverError> {
     let conn = Shift4SeleniumTest {};
-    conn.make_redirection_payment(c, vec![
+    conn.make_redirection_payment(
+        c,
+        vec![
             Event::Trigger(Trigger::Goto(&format!("{CHEKOUT_BASE_URL}/saved/44"))),
             Event::Trigger(Trigger::Click(By::Id("card-submit-btn"))),
             Event::Trigger(Trigger::Click(By::ClassName("btn-success"))),
             Event::Assert(Assert::IsPresent("Google")),
-            Event::Assert(Assert::Contains(Selector::QueryParamStr, "status=succeeded")),
-
-    ]).await?;
+            Event::Assert(Assert::Contains(
+                Selector::QueryParamStr,
+                "status=succeeded",
+            )),
+        ],
+    )
+    .await?;
     Ok(())
 }
-
 
 #[test]
 #[serial]
