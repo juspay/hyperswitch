68084c967cf0d829baa8dbb79493c8e832602e8a
Sangamesh <sangamesh.kulkarni@juspay.in>
2023-05-24 20:39:48 +0530

    feat: flow specific metrics added

--- crates/router/src/core/payments/flows/authorize_flow.rs
+++ crates/router/src/core/payments/flows/authorize_flow.rs
@@ -143,6 +143,16 @@ impl types::PaymentsAuthorizeRouterData {
                     .execute_pretasks(self, state)
                     .await
                     .map_err(|error| error.to_payment_failed_response())?;
+
+                metrics::EXECUTE_PRETASK_COUNT.add(
+                    &metrics::CONTEXT,
+                    1,
+                    &[metrics::request::add_attributes(
+                        "connector",
+                        connector.connector_name.to_string(),
+                    )],
+                );
+
                 logger::debug!(completed_pre_tasks=?true);
                 if self.should_proceed_with_authorize() {
                     self.decide_authentication_type();
@@ -256,6 +266,15 @@ pub async fn authorize_preprocessing_steps<F: Clone>(
         )
         .await
         .map_err(|error| error.to_payment_failed_response())?;
+
+        metrics::PREPROCESSING_STEPS_COUNT.add(
+            &metrics::CONTEXT,
+            1,
+            &[metrics::request::add_attributes(
+                "connector",
+                connector.connector_name.to_string(),
+            )],
+        );
 
         let authorize_router_data =
             payments::helpers::router_data_type_conversion::<_, F, _, _, _, _>(
