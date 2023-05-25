b63e8b1b292010a85044eedd9242989785fc47a7
Aprabhat19 <amishaprabhat@gmail.com>
2023-05-25 12:28:15 +0530

    refactor(enums): minor changes

--- crates/router/src/core/admin.rs
+++ crates/router/src/core/admin.rs
@@ -16,7 +16,7 @@ use crate::{
     types::{
         self, api,
         storage::{self, MerchantAccount},
-        transformers::{ ForeignTryFrom, ForeignTryInto},
+        transformers::{ForeignTryFrom, ForeignTryInto},
     },
     utils::{self, OptionExt},
 };
