597ec16907a83ce228228b8c00e329495ade117b
rishavkar <73836104+rishavkar@users.noreply.github.com>
2023-05-26 13:19:33 +0530

    revert(order_details): revert back `order_details` to be an object, and removing `meta_data` from `PaymentIntent` (#1279)

--- crates/router/src/core/payments/helpers.rs
+++ crates/router/src/core/payments/helpers.rs
@@ -1431,7 +1431,6 @@ mod tests {
             active_attempt_id: "nopes".to_string(),
             business_country: storage_enums::CountryAlpha2::AG,
             business_label: "no".to_string(),
-            meta_data: None,
         };
         let req_cs = Some("1".to_string());
         let merchant_fulfillment_time = Some(900);
@@ -1471,7 +1470,6 @@ mod tests {
             active_attempt_id: "nopes".to_string(),
             business_country: storage_enums::CountryAlpha2::AG,
             business_label: "no".to_string(),
-            meta_data: None,
         };
         let req_cs = Some("1".to_string());
         let merchant_fulfillment_time = Some(10);
@@ -1511,7 +1509,6 @@ mod tests {
             active_attempt_id: "nopes".to_string(),
             business_country: storage_enums::CountryAlpha2::AG,
             business_label: "no".to_string(),
-            meta_data: None,
         };
         let req_cs = Some("1".to_string());
         let merchant_fulfillment_time = Some(10);
