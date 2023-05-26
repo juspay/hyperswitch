763b1b81dd5a6eea62a52b3c5e454f85b8963c46
chikke srujan <121822803+srujanchikke@users.noreply.github.com>
2023-05-26 13:22:18 +0530

    clippy fixes
    Signed-off-by: chikke srujan <121822803+srujanchikke@users.noreply.github.com>

--- crates/router/tests/connectors/globalpay_ui.rs
+++ crates/router/tests/connectors/globalpay_ui.rs
@@ -13,7 +13,12 @@ impl SeleniumTest for GlobalpaySeleniumTest {
 
 async fn should_make_gpay_payment(c: WebDriver) -> Result<(), WebDriverError> {
     let conn = GlobalpaySeleniumTest {};
-    let pub_key = conn.get_configs().globalpay_gateway_merchant_id.unwrap();
+    let pub_key = conn
+        .get_configs()
+        .automation_configs
+        .unwrap()
+        .globalpay_gateway_merchant_id
+        .unwrap();
     conn.make_gpay_payment(c,
         &format!("{CHEKOUT_BASE_URL}/gpay?amount=10.00&country=US&currency=USD&gatewayname=globalpayments&gatewaymerchantid={pub_key}"),
         vec![
@@ -129,7 +134,7 @@ async fn should_make_globalpay_sofort_payment(c: WebDriver) -> Result<(), WebDri
             Event::Assert(Assert::IsPresent("Home")),
             Event::Trigger(Trigger::Click(By::Id("card-submit-btn"))),
             Event::RunIf(
-                Assert::IsPresent("Wählen Sie Ihre Bank"),
+                Assert::IsPresent("Wählen"),
                 vec![Event::Trigger(Trigger::Click(By::Css("p.description")))],
             ),
             Event::Assert(Assert::IsPresent("Demo Bank")),
