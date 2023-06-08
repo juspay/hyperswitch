426f5e3785f50aaa44456b4361c7820b4aa9f9f3
Nachiket Kanore <nachiket.kanore@gmail.com>
2023-06-08 17:47:05 +0530

    implement MandateInterface for MockDB - fix clippy warnings from CI checks

--- crates/router/src/db/mandate.rs
+++ crates/router/src/db/mandate.rs
@@ -1,11 +1,10 @@
 use error_stack::IntoReport;
 use storage_models::mandate::{Mandate, MandateUpdate};
-use time::PrimitiveDateTime;
 
 use super::{MockDb, Store};
 use crate::{
     connection,
-    core::errors::{self, CustomResult, StorageError},
+    core::errors::{CustomResult, StorageError},
     types::{
         storage::{self, MandateDbExt},
         transformers::ForeignInto,
@@ -18,31 +17,31 @@ pub trait MandateInterface {
         &self,
         merchant_id: &str,
         mandate_id: &str,
-    ) -> CustomResult<storage::Mandate, errors::StorageError>;
+    ) -> CustomResult<Mandate, StorageError>;
 
     async fn find_mandate_by_merchant_id_customer_id(
         &self,
         merchant_id: &str,
         customer_id: &str,
-    ) -> CustomResult<Vec<storage::Mandate>, errors::StorageError>;
+    ) -> CustomResult<Vec<Mandate>, StorageError>;
 
     async fn update_mandate_by_merchant_id_mandate_id(
         &self,
         merchant_id: &str,
         mandate_id: &str,
-        mandate: storage::MandateUpdate,
-    ) -> CustomResult<storage::Mandate, errors::StorageError>;
+        mandate: MandateUpdate,
+    ) -> CustomResult<Mandate, StorageError>;
 
     async fn find_mandates_by_merchant_id(
         &self,
         merchant_id: &str,
         mandate_constraints: api_models::mandates::MandateListConstraints,
-    ) -> CustomResult<Vec<storage::Mandate>, errors::StorageError>;
+    ) -> CustomResult<Vec<Mandate>, StorageError>;
 
     async fn insert_mandate(
         &self,
         mandate: storage::MandateNew,
-    ) -> CustomResult<storage::Mandate, errors::StorageError>;
+    ) -> CustomResult<Mandate, StorageError>;
 }
 
 #[async_trait::async_trait]
@@ -51,9 +50,9 @@ impl MandateInterface for Store {
         &self,
         merchant_id: &str,
         mandate_id: &str,
-    ) -> CustomResult<storage::Mandate, errors::StorageError> {
+    ) -> CustomResult<Mandate, StorageError> {
         let conn = connection::pg_connection_read(self).await?;
-        storage::Mandate::find_by_merchant_id_mandate_id(&conn, merchant_id, mandate_id)
+        Mandate::find_by_merchant_id_mandate_id(&conn, merchant_id, mandate_id)
             .await
             .map_err(Into::into)
             .into_report()
@@ -63,9 +62,9 @@ impl MandateInterface for Store {
         &self,
         merchant_id: &str,
         customer_id: &str,
-    ) -> CustomResult<Vec<storage::Mandate>, errors::StorageError> {
+    ) -> CustomResult<Vec<Mandate>, StorageError> {
         let conn = connection::pg_connection_read(self).await?;
-        storage::Mandate::find_by_merchant_id_customer_id(&conn, merchant_id, customer_id)
+        Mandate::find_by_merchant_id_customer_id(&conn, merchant_id, customer_id)
             .await
             .map_err(Into::into)
             .into_report()
@@ -75,10 +74,10 @@ impl MandateInterface for Store {
         &self,
         merchant_id: &str,
         mandate_id: &str,
-        mandate: storage::MandateUpdate,
-    ) -> CustomResult<storage::Mandate, errors::StorageError> {
+        mandate: MandateUpdate,
+    ) -> CustomResult<Mandate, StorageError> {
         let conn = connection::pg_connection_write(self).await?;
-        storage::Mandate::update_by_merchant_id_mandate_id(&conn, merchant_id, mandate_id, mandate)
+        Mandate::update_by_merchant_id_mandate_id(&conn, merchant_id, mandate_id, mandate)
             .await
             .map_err(Into::into)
             .into_report()
@@ -88,9 +87,9 @@ impl MandateInterface for Store {
         &self,
         merchant_id: &str,
         mandate_constraints: api_models::mandates::MandateListConstraints,
-    ) -> CustomResult<Vec<storage::Mandate>, errors::StorageError> {
+    ) -> CustomResult<Vec<Mandate>, StorageError> {
         let conn = connection::pg_connection_read(self).await?;
-        storage::Mandate::filter_by_constraints(&conn, merchant_id, mandate_constraints)
+        Mandate::filter_by_constraints(&conn, merchant_id, mandate_constraints)
             .await
             .map_err(Into::into)
             .into_report()
@@ -99,7 +98,7 @@ impl MandateInterface for Store {
     async fn insert_mandate(
         &self,
         mandate: storage::MandateNew,
-    ) -> CustomResult<storage::Mandate, errors::StorageError> {
+    ) -> CustomResult<Mandate, StorageError> {
         let conn = connection::pg_connection_write(self).await?;
         mandate
             .insert(&conn)
@@ -115,7 +114,7 @@ impl MandateInterface for MockDb {
         &self,
         merchant_id: &str,
         mandate_id: &str,
-    ) -> CustomResult<storage::Mandate, errors::StorageError> {
+    ) -> CustomResult<Mandate, StorageError> {
         match self
             .mandate_info
             .lock()
@@ -132,7 +131,7 @@ impl MandateInterface for MockDb {
         &self,
         merchant_id: &str,
         customer_id: &str,
-    ) -> CustomResult<Vec<storage::Mandate>, errors::StorageError> {
+    ) -> CustomResult<Vec<Mandate>, StorageError> {
         return Ok(self
             .mandate_info
             .lock()
@@ -149,8 +148,8 @@ impl MandateInterface for MockDb {
         &self,
         merchant_id: &str,
         mandate_id: &str,
-        mandate_update: storage::MandateUpdate,
-    ) -> CustomResult<storage::Mandate, errors::StorageError> {
+        mandate_update: MandateUpdate,
+    ) -> CustomResult<Mandate, StorageError> {
         let mut mandate_info = self.mandate_info.lock().await;
         match mandate_info
             .iter_mut()
@@ -176,11 +175,12 @@ impl MandateInterface for MockDb {
         }
     }
 
+    #[allow(clippy::as_conversions)]
     async fn find_mandates_by_merchant_id(
         &self,
         merchant_id: &str,
         mandate_constraints: api_models::mandates::MandateListConstraints,
-    ) -> CustomResult<Vec<storage::Mandate>, errors::StorageError> {
+    ) -> CustomResult<Vec<Mandate>, StorageError> {
         let mandate_info_iter = self.mandate_info.lock().await;
         let mandate_info_iter = mandate_info_iter.iter().filter(|mandate| {
             let mut checker = mandate.merchant_id == merchant_id;
@@ -210,44 +210,51 @@ impl MandateInterface for MockDb {
             checker
         });
 
-        let mandates: Vec<storage::Mandate> = if let Some(limit) = mandate_constraints.limit {
-            mandate_info_iter.take(limit).cloned().collect()
+        let mandates: Vec<Mandate> = if let Some(limit) = mandate_constraints.limit {
+            mandate_info_iter.take(limit as usize).cloned().collect()
         } else {
             mandate_info_iter.cloned().collect()
         };
         Ok(mandates)
     }
 
+    #[allow(clippy::as_conversions)]
     async fn insert_mandate(
         &self,
         mandate_new: storage::MandateNew,
-    ) -> CustomResult<storage::Mandate, errors::StorageError> {
+    ) -> CustomResult<Mandate, StorageError> {
         let mut mandate_info = self.mandate_info.lock().await;
-        let mandate = Mandate {
-            id: mandate_info.len() as i32,
-            mandate_id: mandate_new.mandate_id,
-            customer_id: mandate_new.customer_id,
-            merchant_id: mandate_new.merchant_id,
-            payment_method_id: mandate_new.payment_method_id,
-            mandate_status: mandate_new.mandate_status,
-            mandate_type: mandate_new.mandate_type,
-            customer_accepted_at: mandate_new.customer_accepted_at,
-            customer_ip_address: mandate_new.customer_ip_address,
-            customer_user_agent: mandate_new.customer_user_agent,
-            network_transaction_id: mandate_new.network_transaction_id,
-            previous_attempt_id: mandate_new.previous_attempt_id,
-            created_at: mandate_new.created_at.unwrap(),
-            mandate_amount: mandate_new.mandate_amount,
-            mandate_currency: mandate_new.mandate_currency,
-            amount_captured: mandate_new.amount_captured,
-            connector: mandate_new.connector,
-            connector_mandate_id: mandate_new.connector_mandate_id,
-            start_date: mandate_new.start_date,
-            end_date: mandate_new.end_date,
-            metadata: mandate_new.metadata,
-            connector_mandate_ids: mandate_new.connector_mandate_ids,
-        };
-        mandate_info.push(mandate.clone());
-        Ok(mandate)
+        match mandate_new.created_at {
+            Some(created_at) => {
+                let mandate = Mandate {
+                    id: mandate_info.len() as i32,
+                    mandate_id: mandate_new.mandate_id.clone(),
+                    customer_id: mandate_new.customer_id,
+                    merchant_id: mandate_new.merchant_id,
+                    payment_method_id: mandate_new.payment_method_id,
+                    mandate_status: mandate_new.mandate_status,
+                    mandate_type: mandate_new.mandate_type,
+                    customer_accepted_at: mandate_new.customer_accepted_at,
+                    customer_ip_address: mandate_new.customer_ip_address,
+                    customer_user_agent: mandate_new.customer_user_agent,
+                    network_transaction_id: mandate_new.network_transaction_id,
+                    previous_attempt_id: mandate_new.previous_attempt_id,
+                    created_at,
+                    mandate_amount: mandate_new.mandate_amount,
+                    mandate_currency: mandate_new.mandate_currency,
+                    amount_captured: mandate_new.amount_captured,
+                    connector: mandate_new.connector,
+                    connector_mandate_id: mandate_new.connector_mandate_id,
+                    start_date: mandate_new.start_date,
+                    end_date: mandate_new.end_date,
+                    metadata: mandate_new.metadata,
+                    connector_mandate_ids: mandate_new.connector_mandate_ids,
+                };
+                mandate_info.push(mandate.clone());
+                Ok(mandate)
+
+            }
+            None => Err(StorageError::ValueNotFound("created_date not provided".to_string()).into()),
+        }
     }
 }
