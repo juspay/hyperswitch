f486c527d36325edd3e84220b1d3cb1b88a3d9b0
Nachiket Kanore <nachiket.kanore@gmail.com>
2023-06-08 15:06:32 +0530

    implement MandateInterface for MockDB - remove TODO comments

--- crates/router/src/db/mandate.rs
+++ crates/router/src/db/mandate.rs
@@ -174,8 +174,6 @@ impl MandateInterface for MockDb {
             }
             None => Err(StorageError::ValueNotFound("mandate not found".to_string()).into()),
         }
-        // [#172]: Implement function for `MockDb`
-        // Err(errors::StorageError::MockDbError)?
     }
 
     async fn find_mandates_by_merchant_id(
@@ -183,38 +181,30 @@ impl MandateInterface for MockDb {
         merchant_id: &str,
         mandate_constraints: api_models::mandates::MandateListConstraints,
     ) -> CustomResult<Vec<storage::Mandate>, errors::StorageError> {
-        // [#172]: Implement function for `MockDb`
         let mandate_info_iter = self.mandate_info.lock().await;
         let mandate_info_iter = mandate_info_iter.iter().filter(|mandate| {
             let mut checker = mandate.merchant_id == merchant_id;
             if let Some(created_time) = mandate_constraints.created_time {
-                // filter = filter.filter(dsl::created_at.eq(created_time));
                 checker &= mandate.created_at == created_time;
             }
             if let Some(created_time_lt) = mandate_constraints.created_time_lt {
-                // filter = filter.filter(dsl::created_at.lt(created_time_lt));
                 checker &= mandate.created_at < created_time_lt;
             }
             if let Some(created_time_gt) = mandate_constraints.created_time_gt {
-                // filter = filter.filter(dsl::created_at.gt(created_time_gt));
                 checker &= mandate.created_at > created_time_gt;
             }
             if let Some(created_time_lte) = mandate_constraints.created_time_lte {
-                // filter = filter.filter(dsl::created_at.le(created_time_lte));
                 checker &= mandate.created_at <= created_time_lte;
             }
             if let Some(created_time_gte) = mandate_constraints.created_time_gte {
-                // filter = filter.filter(dsl::created_at.ge(created_time_gte));
                 checker &= mandate.created_at >= created_time_gte;
             }
             if let Some(connector) = &mandate_constraints.connector {
                 checker &= mandate.connector == *connector;
-                // filter = filter.filter(dsl::connector.eq(connector));
             }
             if let Some(mandate_status) = mandate_constraints.mandate_status {
                 let storage_mandate_status: storage_models::enums::MandateStatus =
                     mandate_status.foreign_into();
-                // filter = filter.filter(dsl::mandate_status.eq(storage_mandate_status));
                 checker &= mandate.mandate_status == storage_mandate_status;
             }
             checker
@@ -226,18 +216,6 @@ impl MandateInterface for MockDb {
             mandate_info_iter.cloned().collect()
         };
         Ok(mandates)
-
-        // Err(errors::StorageError::MockDbError)?
-        // return Ok(self
-        //     .mandate_info
-        //     .lock()
-        //     .await
-        //     .iter()
-        //     .filter(|mandate| {
-        //         mandate.merchant_id == merchant_id
-        //     })
-        //     .cloned()
-        //     .collect());
     }
 
     async fn insert_mandate(
@@ -271,7 +249,5 @@ impl MandateInterface for MockDb {
         };
         mandate_info.push(mandate.clone());
         Ok(mandate)
-        // [#172]: Implement function for `MockDb`
-        // Err(errors::StorageError::MockDbError)?
     }
 }
