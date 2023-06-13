14168fa50fa11ae64feebe7c1ee3cc568aac4a03
Aprabhat19 <amishaprabhat@gmail.com>
2023-06-13 15:21:37 +0530

    changed auth type and also changed the default flow to non-3ds

--- crates/api_models/src/enums.rs
+++ crates/api_models/src/enums.rs
@@ -61,9 +61,9 @@ pub enum AttemptStatus {
 #[strum(serialize_all = "snake_case")]
 pub enum AuthenticationType {
     /// If the card is enrolled for 3DS authentication, the 3DS based authentication will be activated. The liability of chargeback shift to the issuer
-    #[default]
     ThreeDs,
     /// 3DS based authentication will not be activated. The liability of chargeback stays with the merchant.
+    #[default]
     NoThreeDs,
 }
 
