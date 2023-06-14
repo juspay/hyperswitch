21a10aa1174f8ac54bbe36f1040acad38ff59b93
sai-harsha-vardhan <harsha111hero@gmail.com>
2023-06-14 20:11:46 +0530

    resolve comments

--- crates/router/src/services/api.rs
+++ crates/router/src/services/api.rs
@@ -145,27 +145,27 @@ pub trait ConnectorIntegration<T, Req, Resp>: ConnectorIntegrationAny<T, Req, Re
         Ok(ErrorResponse::get_not_implemented())
     }
 
-    fn get_server_error_response(
+    fn get_5xx_error_response(
         &self,
         res: types::Response,
     ) -> CustomResult<ErrorResponse, errors::ConnectorError> {
-        let error_mssg = match res.status_code {
-            500 => "InternalServerError",
-            501 => "NotImplemented",
-            502 => "BadGateway",
-            503 => "ServiceUnavailable",
-            504 => "GatewayTimeout",
-            505 => "HttpVersionNotSupported",
-            506 => "VariantAlsoNegotiates",
-            507 => "InsufficientStorage",
-            508 => "LoopDetected",
-            510 => "NotExtended",
-            511 => "NetworkAuthenticationRequired",
-            _ => "UnknownError",
+        let error_message = match res.status_code {
+            500 => "internal_server_error",
+            501 => "not_implemented",
+            502 => "bad_gateway",
+            503 => "service_unavailable",
+            504 => "gateway_timeout",
+            505 => "http_version_not_supported",
+            506 => "variant_also_negotiates",
+            507 => "insufficient_storage",
+            508 => "loop_detected",
+            510 => "not_extended",
+            511 => "network_authentication_required",
+            _ => "unknown_error",
         };
         Ok(ErrorResponse {
             code: res.status_code.to_string(),
-            message: error_mssg.to_string(),
+            message: error_message.to_string(),
             reason: String::from_utf8(res.response.to_vec()).ok(),
             status_code: res.status_code,
         })
@@ -306,7 +306,7 @@ where
                                     );
                                     let error = match body.status_code {
                                         500..=511 => {
-                                            connector_integration.get_server_error_response(body)?
+                                            connector_integration.get_5xx_error_response(body)?
                                         }
                                         _ => connector_integration.get_error_response(body)?,
                                     };
