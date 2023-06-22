9189e894bbd1a59ec9f5f26fddb0cddcce7a1050
Chethan <chethan.rao@juspay.in>
2023-06-22 20:01:35 +0530

    refactor: add serde rename_all for refund enums

--- crates/storage_models/src/enums.rs
+++ crates/storage_models/src/enums.rs
@@ -528,6 +528,7 @@ pub enum ProcessTrackerStatus {
 )]
 #[router_derive::diesel_enum(storage_type = "pg_enum")]
 #[strum(serialize_all = "snake_case")]
+#[serde(rename_all = "snake_case")]
 pub enum RefundStatus {
     Failure,
     ManualReview,
@@ -551,6 +552,7 @@ pub enum RefundStatus {
 )]
 #[router_derive::diesel_enum(storage_type = "pg_enum")]
 #[strum(serialize_all = "snake_case")]
+#[serde(rename_all = "snake_case")]
 pub enum RefundType {
     InstantRefund,
     #[default]
