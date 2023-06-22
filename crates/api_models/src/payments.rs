2aa94745cc886afb964e443f1b2161201ba028f2
Nitesh Balla <nitesh.balla@juspay.in>
2023-06-22 16:14:42 +0530

    refacor: resolved PR comments

--- crates/api_models/src/payments.rs
+++ crates/api_models/src/payments.rs
@@ -913,7 +913,7 @@ pub struct CardResponse {
 #[derive(Debug, Clone, Eq, PartialEq, serde::Serialize, serde::Deserialize, ToSchema)]
 #[serde(rename_all = "snake_case")]
 pub struct RewardData {
-    // The merchant id with which we have to call the connector.
+    /// The merchant id with which we have to call the connector.
     #[schema(value_type = String, example = "")]
     pub mid: String,
 }
