784847b08ca00ee5b77abf6faaeb9673b57adec3
BallaNitesh <126162378+BallaNitesh@users.noreply.github.com>
2023-06-27 15:05:23 +0530

    feat(connector): Add connector cashtocode (#1429)

--- crates/api_models/src/payments.rs
+++ crates/api_models/src/payments.rs
@@ -589,6 +589,7 @@ pub enum PaymentMethodData {
     BankTransfer(Box<BankTransferData>),
     Crypto(CryptoData),
     MandatePayment,
+    Reward(RewardData),
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
@@ -607,6 +608,7 @@ pub enum AdditionalPaymentData {
     Crypto {},
     BankDebit {},
     MandatePayment {},
+    Reward {},
 }
 
 impl From<&PaymentMethodData> for AdditionalPaymentData {
@@ -634,6 +636,7 @@ impl From<&PaymentMethodData> for AdditionalPaymentData {
             PaymentMethodData::Crypto(_) => Self::Crypto {},
             PaymentMethodData::BankDebit(_) => Self::BankDebit {},
             PaymentMethodData::MandatePayment => Self::MandatePayment {},
+            PaymentMethodData::Reward(_) => Self::Reward {},
         }
     }
 }
@@ -940,6 +943,13 @@ pub struct CardResponse {
     exp_year: String,
 }
 
+#[derive(Debug, Clone, Eq, PartialEq, serde::Serialize, serde::Deserialize, ToSchema)]
+#[serde(rename_all = "snake_case")]
+pub struct RewardData {
+    /// The merchant ID with which we have to call the connector
+    pub merchant_id: String,
+}
+
 #[derive(Debug, Clone, Eq, PartialEq, serde::Serialize)]
 #[serde(rename_all = "snake_case")]
 pub enum PaymentMethodDataResponse {
@@ -953,6 +963,7 @@ pub enum PaymentMethodDataResponse {
     Crypto(CryptoData),
     BankDebit(BankDebitData),
     MandatePayment,
+    Reward(RewardData),
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize, ToSchema)]
@@ -1582,6 +1593,7 @@ impl From<PaymentMethodData> for PaymentMethodDataResponse {
             PaymentMethodData::Crypto(crpto_data) => Self::Crypto(crpto_data),
             PaymentMethodData::BankDebit(bank_debit_data) => Self::BankDebit(bank_debit_data),
             PaymentMethodData::MandatePayment => Self::MandatePayment,
+            PaymentMethodData::Reward(reward_data) => Self::Reward(reward_data),
         }
     }
 }
