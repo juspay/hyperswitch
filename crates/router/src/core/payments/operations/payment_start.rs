26213ec2d09422a3f6d1842420462116cd6cde1c
shreyashjiodndian2 <Shreyash.pandey.katni@gmail.com>
2023-06-30 18:16:49 +0000

    CI issues resolved

--- crates/router/src/core/payments/operations/payment_start.rs
+++ crates/router/src/core/payments/operations/payment_start.rs
@@ -241,15 +241,14 @@ where
     #[instrument(skip_all)]
     async fn make_pm_data<'a>(
         &'a self,
-        state: &'a AppState,
-        payment_data: &mut PaymentData<F>,
+        _state: &'a AppState,
+        _payment_data: &mut PaymentData<F>,
         _storage_scheme: storage_enums::MerchantStorageScheme,
     ) -> RouterResult<(
         BoxedOperation<'a, F, api::PaymentsStartRequest>,
         Option<api::PaymentMethodData>,
     )> {
         Ok((Box::new(self), None))
-        
     }
 
     async fn get_connector<'a>(
