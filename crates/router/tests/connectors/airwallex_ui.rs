7489c870d9d85f169fb7fca469778fad5b2cc37a
Sakil Mostak <73734619+Sakilmostak@users.noreply.github.com>
2023-07-03 11:30:33 +0530

    feat(connector): [Airwallex] Implement Google Pay in Wallets (#1316)
    Co-authored-by: AkshayaFoiger <akshaya.shankar@juspay.in>
    Co-authored-by: Jagan Elavarasan <jaganelavarasan@gmail.com>
    Co-authored-by: chikke srujan <121822803+srujanchikke@users.noreply.github.com>

--- crates/router/tests/connectors/airwallex_ui.rs
+++ crates/router/tests/connectors/airwallex_ui.rs
@@ -37,8 +37,34 @@ async fn should_make_airwallex_3ds_payment(web_driver: WebDriver) -> Result<(), 
     Ok(())
 }
 
+async fn should_make_airwallex_gpay_payment(web_driver: WebDriver) -> Result<(), WebDriverError> {
+    let conn = AirwallexSeleniumTest {};
+    let merchant_name = conn
+        .get_configs()
+        .automation_configs
+        .unwrap()
+        .airwallex_merchant_name
+        .unwrap();
+    conn.make_gpay_payment(web_driver,
+        &format!("{CHEKOUT_BASE_URL}/gpay?gatewayname=airwallex&gatewaymerchantid={merchant_name}&amount=70.00&country=US&currency=USD"),
+        vec![
+            Event::Trigger(Trigger::Query(By::ClassName("title"))),
+            Event::Assert(Assert::Eq(Selector::Title, "Airwallex - Create 3D Secure Payment")),
+            Event::Trigger(Trigger::SendKeys(By::Id("challengeDataEntry"), "1234")),
+            Event::Trigger(Trigger::Click(By::Id("submit"))),
+            Event::Assert(Assert::IsPresent("succeeded")),
+    ]).await?;
+    Ok(())
+}
+
 #[test]
 #[serial]
 fn should_make_airwallex_3ds_payment_test() {
     tester!(should_make_airwallex_3ds_payment);
+}
+
+#[test]
+#[serial]
+fn should_make_airwallex_gpay_payment_test() {
+    tester!(should_make_airwallex_gpay_payment);
 }
