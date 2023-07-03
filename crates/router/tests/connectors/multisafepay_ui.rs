fc77ade1657adead2e3e75666e63e9fc73949103
AkshayaFoiger <akshaya.shankar@juspay.in>
2023-07-03 12:32:02 +0530

    updated

--- crates/router/tests/connectors/multisafepay_ui.rs
+++ crates/router/tests/connectors/multisafepay_ui.rs
@@ -11,10 +11,10 @@ impl SeleniumTest for MultisafepaySeleniumTest {
     }
 }
 
-async fn should_make_gpay_payment(c: WebDriver) -> Result<(), WebDriverError> {
+async fn should_make_gpay_payment(web_driver: WebDriver) -> Result<(), WebDriverError> {
     let conn = MultisafepaySeleniumTest {};
     conn.make_redirection_payment(
-        c,
+        web_driver,
         vec![
             Event::Trigger(Trigger::Goto(&format!("{CHEKOUT_BASE_URL}/saved/153"))),
             Event::Trigger(Trigger::Click(By::Id("card-submit-btn"))),
@@ -26,10 +26,10 @@ async fn should_make_gpay_payment(c: WebDriver) -> Result<(), WebDriverError> {
     Ok(())
 }
 
-async fn should_make_paypal_payment(c: WebDriver) -> Result<(), WebDriverError> {
+async fn should_make_paypal_payment(web_driver: WebDriver) -> Result<(), WebDriverError> {
     let conn = MultisafepaySeleniumTest {};
     conn.make_redirection_payment(
-        c,
+        web_driver,
         vec![
             Event::Trigger(Trigger::Goto(&format!("{CHEKOUT_BASE_URL}/saved/154"))),
             Event::Trigger(Trigger::Click(By::Id("card-submit-btn"))),
