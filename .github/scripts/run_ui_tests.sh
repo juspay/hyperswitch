ab36edec78908cf9ba14a8a134c655a11a22efbb
Jagan Elavarasan <jaganelavarasan@gmail.com>
2023-07-04 19:37:03 +0530

    Fix auto kill script exuecution

--- .github/scripts/run_ui_tests.sh
+++ .github/scripts/run_ui_tests.sh
@@ -5,8 +5,22 @@ mkdir tests
 #download connector ui tests
 mv .github/testcases/ui_tests.json $HOME/target/test/connector_tests.json
 
-firefox --version
-$GECKOWEBDRIVER/geckodriver > tests/geckodriver.log 2>&1 &
+#install geckodriver
+wget -c https://github.com/mozilla/geckodriver/releases/download/v0.33.0/geckodriver-v0.33.0-linux-aarch64.tar.gz && tar -xvzf geckodriver*
+chmod +x geckodriver
+mv geckodriver /usr/local/bin/
+geckodriver > tests/geckodriver.log 2>&1 &
+
+#install and run firefox
+sudo add-apt-repository -y ppa:mozillateam/ppa
+echo ' 
+Package: * 
+Pin: release o=LP-PPA-mozillateam 
+Pin-Priority: 1001 
+' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
+sudo apt install -y firefox
+firefox
 
 #start server and run ui tests
 cargo build
