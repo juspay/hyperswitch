58e5d4f46257a036774769e82127d9c957232331
Jagan Elavarasan <jaganelavarasan@gmail.com>
2023-07-04 15:15:14 +0530

    Check geckodriver

--- .github/scripts/run_ui_tests.sh
+++ .github/scripts/run_ui_tests.sh
@@ -7,12 +7,18 @@ mv .github/testcases/ui_tests.json $HOME/target/test/connector_tests.json
 
 echo $GECKOWEBDRIVER
 
-sh $GECKOWEBDRIVER > tests/geckodriver.log 2>&1 &
 firefox --version
+sh $GECKOWEBDRIVER
 
 #start server and run ui tests
 cargo build
 target/debug/router &
 
 #Wait for the server to start in port 8080
-while netstat -lnt | awk '$4 ~ /:8080$/ {exit 1}'; do sleep 10; done
+while netstat -lnt | awk '$4 ~ /:8080$/ {exit 1}'; do 
+    if $SECONDS -gt 900 then
+        exit 1
+    else 
+        sleep 10; 
+    fi
+done
