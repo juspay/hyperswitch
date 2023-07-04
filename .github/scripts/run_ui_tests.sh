3d365b020bf1ebeffd7e70235ec366dd01ee1e7e
Jagan Elavarasan <jaganelavarasan@gmail.com>
2023-07-04 18:04:54 +0530

    Fix auto kill script exuecution

--- .github/scripts/run_ui_tests.sh
+++ .github/scripts/run_ui_tests.sh
@@ -14,9 +14,10 @@ target/debug/router &
 
 #Wait for the server to start in port 8080
 while netstat -lnt | awk '$4 ~ /:8080$/ {exit 1}'; do 
-    if $SECONDS -gt 900 then
+    if [ $SECONDS > 900 ]
+    then
         exit 1
     else 
-        sleep 10; 
+        sleep 10
     fi
 done
