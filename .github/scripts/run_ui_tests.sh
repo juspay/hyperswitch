6588575263f989210fab1e12a4a628c9dc9ccc7b
Jagan Elavarasan <jaganelavarasan@gmail.com>
2023-07-04 14:17:01 +0530

    Use default firefox and geckdriver

--- .github/scripts/run_ui_tests.sh
+++ .github/scripts/run_ui_tests.sh
@@ -6,25 +6,12 @@ mkdir tests
 #download connector ui tests
 mv .github/testcases/ui_tests.json $HOME/target/test/connector_tests.json
 
-#install geckodriver
-wget -c https://github.com/mozilla/geckodriver/releases/download/v0.33.0/geckodriver-v0.33.0-linux-aarch64.tar.gz && tar -xvzf geckodriver*
-chmod +x geckodriver
-mv geckodriver /usr/local/bin/
-geckodriver > tests/geckodriver.log 2>&1 &
-
-#install and run firefox
-sudo add-apt-repository -y ppa:mozillateam/ppa
-echo ' 
-Package: * 
-Pin: release o=LP-PPA-mozillateam 
-Pin-Priority: 1001 
-' | sudo tee /etc/apt/preferences.d/mozilla-firefox
-echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
-sudo apt install -y firefox
-firefox
+$GECKOWEBDRIVER > tests/geckodriver.log 2>&1 &
+firefox --version
 
 #start server and run ui tests
-cargo run &
+cargo build
+target/debug/router &
 
 #Wait for the server to start in port 8080
 while netstat -lnt | awk '$4 ~ /:8080$/ {exit 1}'; do sleep 10; done
