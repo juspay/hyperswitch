a5d3976b81561afb4129a2d5f258f7647843aaa4
Jagan Elavarasan <jaganelavarasan@gmail.com>
2023-07-05 12:49:33 +0530

    Fix test failures

--- .github/scripts/run_ui_tests.sh
+++ .github/scripts/run_ui_tests.sh
@@ -9,8 +9,7 @@ firefox --version
 $GECKOWEBDRIVER/geckodriver > tests/geckodriver.log 2>&1 &
 
 #start server and run ui tests
-cargo build
-target/debug/router &
+cargo run &
 
 #Wait for the server to start in port 8080
 while netstat -lnt | awk '$4 ~ /:8080$/ {exit 1}'; do 
