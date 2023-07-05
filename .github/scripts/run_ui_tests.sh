5f520a782b04f6e3c6a37281027a7b196cb977d3
Jagan Elavarasan <jaganelavarasan@gmail.com>
2023-07-05 11:34:47 +0530

    Fix test failures

--- .github/scripts/run_ui_tests.sh
+++ .github/scripts/run_ui_tests.sh
@@ -5,26 +5,11 @@ mkdir tests
 #download connector ui tests
 mv .github/testcases/ui_tests.json $HOME/target/test/connector_tests.json
 
-#install geckodriver
-wget -c https://github.com/mozilla/geckodriver/releases/download/v0.33.0/geckodriver-v0.33.0-linux-aarch64.tar.gz && tar -xvzf geckodriver*
-chmod +x geckodriver
-mv geckodriver /usr/local/bin/
-geckodriver > tests/geckodriver.log 2>&1 &
-
-#install and run firefox
-sudo add-apt-repository -y ppa:mozillateam/ppa
-echo ' 
-Package: * 
-Pin: release o=LP-PPA-mozillateam 
-Pin-Priority: 1001 
-' | sudo tee /etc/apt/preferences.d/mozilla-firefox
-echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
-sudo apt install -y firefox
-firefox
+firefox --version
+$GECKOWEBDRIVER/geckodriver > tests/geckodriver.log 2>&1 &
 
 #start server and run ui tests
-cargo build
-target/debug/router &
+cargo run &
 
 #Wait for the server to start in port 8080
 while netstat -lnt | awk '$4 ~ /:8080$/ {exit 1}'; do 
