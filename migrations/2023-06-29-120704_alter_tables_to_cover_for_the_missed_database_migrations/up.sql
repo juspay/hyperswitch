f780a7ee1ad96e564b970c354b502df703dafbf4
Kritik Modi <kritik.modi@juspay.in>
2023-07-05 16:59:23 +0530

    chore: test commit

--- migrations/2023-06-29-120704_alter_tables_to_cover_for_the_missed_database_migrations/up.sql
+++ migrations/2023-06-29-120704_alter_tables_to_cover_for_the_missed_database_migrations/up.sql
@@ -1,9 +1,8 @@
-ALTER TABLE file_metadata
-    DROP COLUMN connector_label;
+ALTER TABLE file_metadata DROP COLUMN connector_label;
 
-ALTER TABLE merchant_account
-    ALTER COLUMN merchant_name SET NOT NULL,
-    ALTER COLUMN primary_business_details DROP DEFAULT;
+ALTER TABLE merchant_account ALTER COLUMN merchant_name SET NOT NULL;
+
+ALTER TABLE merchant_account ALTER COLUMN primary_business_details DROP DEFAULT;
 
 ALTER TABLE merchant_key_store
     ALTER COLUMN merchant_id TYPE VARCHAR(255);
