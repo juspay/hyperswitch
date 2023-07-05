b80b64d11ca204aa74920be87b7f9052df143dd1
Kritik Modi <kritik.modi@juspay.in>
2023-07-05 17:09:04 +0530

    chore: test commit

--- migrations/2023-06-29-120704_alter_tables_to_cover_for_the_missed_database_migrations/up.sql
+++ migrations/2023-06-29-120704_alter_tables_to_cover_for_the_missed_database_migrations/up.sql
@@ -6,14 +6,14 @@ ALTER TABLE merchant_accoun
     ALTER COLUMN primary_business_details DROP DEFAULT;
 
 ALTER TABLE merchant_key_store
-    ALTER COLUMN merchant_id SET TYPE VARCHAR(255);
+    ALTER COLUMN merchant_id TYPE VARCHAR(255);
 
 ALTER TABLE payment_intent
     ALTER COLUMN metadata SET DEFAULT '{}'::JSONB,
     ADD COLUMN meta_data JSONB;
 
 ALTER TABLE payment_methods
-    ALTER COLUMN payment_method_type SET TYPE VARCHAR(64);
+    ALTER COLUMN payment_method_type TYPE VARCHAR(64);
 
 ALTER TABLE prod_intent
     ALTER COLUMN is_completed NULL;
