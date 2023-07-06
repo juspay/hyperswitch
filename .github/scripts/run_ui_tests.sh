4aad85a78ea8d77614977942c3ca063f9a9cb485
Jagan Elavarasan <jaganelavarasan@gmail.com>
2023-07-06 22:45:55 +0530

    Fix ci test log

--- .github/scripts/run_ui_tests.sh
+++ .github/scripts/run_ui_tests.sh
@@ -30,8 +30,8 @@ while netstat -lnt | awk '$4 ~ /:8080$/ {exit 1}'; do
     fi
 done
 
-connectors=(${INPUT//|/ })
-for i in "${connectors[@]}"; do
+IN="$INPUT"
+for i in $(echo $IN | tr ";" "\n"); do
     cargo test --package router --test connectors -- "${i}_ui::" --test-threads=1 >> tests/test_results.log 2>&1
 done
 cat tests/test_results.log
