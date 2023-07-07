093cc6a71cb3060c06bc4e6238af8896b36308db
Arjun Karthik <m.arjunkarthik@gmail.com>
2023-07-07 12:40:02 +0530

    feat(connector): [Payme] add Authorize, Sync, Capture, Refund, Refund Sync, Mandate & web hooks support for cards (#1594)

--- crates/api_models/src/enums.rs
+++ crates/api_models/src/enums.rs
@@ -653,7 +653,7 @@ pub enum Connector {
     // Opayo, added as template code for future usage
     Opennode,
     // Payeezy, As psync and rsync are not supported by this connector, it is added as template code for future usage
-    // Payme,
+    Payme,
     Paypal,
     Payu,
     Rapyd,
@@ -758,7 +758,7 @@ pub enum RoutableConnectors {
     // Opayo, added as template code for future usage
     Opennode,
     // Payeezy, As psync and rsync are not supported by this connector, it is added as template code for future usage
-    // Payme,
+    Payme,
     Paypal,
     Payu,
     Rapyd,
