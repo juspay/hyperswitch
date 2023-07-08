f91a9921afb8e59b015822ef99d0b4efeb7b004a
PanGan21 <ganelis.pan@gmail.com>
2023-07-08 20:46:19 +0300

    remove webhook api error switch and implement error mapping with error switch

--- crates/router/src/core/webhooks.rs
+++ crates/router/src/core/webhooks.rs
@@ -1,11 +1,10 @@
 pub mod transformers;
 pub mod utils;
 
-use common_utils::{crypto::SignMessage, ext_traits};
+use common_utils::{crypto::SignMessage, errors::ErrorSwitch, ext_traits};
 use error_stack::{report, IntoReport, ResultExt};
 use masking::ExposeInterface;
 use router_env::{instrument, tracing};
-use utils::WebhookApiErrorSwitch;
 
 use super::{errors::StorageErrorExt, metrics};
 use crate::{
