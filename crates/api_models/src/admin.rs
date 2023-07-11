5c6ef431e6c7e974d8234a48159d4d9efda84310
Narayan Bhat <narayan.bhat@juspay.in>
2023-07-11 14:56:48 +0530

    feat: refactor multiple mca feature

--- crates/api_models/src/admin.rs
+++ crates/api_models/src/admin.rs
@@ -67,12 +67,7 @@ pub struct MerchantAccountCreate {
     pub locker_id: Option<String>,
 
     ///Default business details for connector routing
-    #[cfg(feature = "multiple_mca")]
     #[schema(value_type = PrimaryBusinessDetails)]
-    pub primary_business_details: Vec<PrimaryBusinessDetails>,
-
-    #[cfg(not(feature = "multiple_mca"))]
-    #[schema(value_type = Option<PrimaryBusinessDetails>)]
     pub primary_business_details: Option<Vec<PrimaryBusinessDetails>>,
 
     /// The frm routing algorithm to be used for routing payments to desired FRM's
@@ -456,21 +451,10 @@ pub struct MerchantConnectorCreate {
     ]))]
     pub frm_configs: Option<FrmConfigs>,
 
-    /// Business Country of the connector
-    #[cfg(feature = "multiple_mca")]
     #[schema(value_type = CountryAlpha2, example = "US")]
     pub business_country: api_enums::CountryAlpha2,
 
-    #[cfg(not(feature = "multiple_mca"))]
-    #[schema(value_type = Option<CountryAlpha2>, example = "US")]
-    pub business_country: Option<api_enums::CountryAlpha2>,
-
-    ///Business Type of the merchant
-    #[schema(example = "travel")]
-    #[cfg(feature = "multiple_mca")]
     pub business_label: String,
-    #[cfg(not(feature = "multiple_mca"))]
-    pub business_label: Option<String>,
 
     /// Business Sub label of the merchant
     #[schema(example = "chase")]
