673580c6022104a6fd50078aa51889a342068a55
Kritik Modi <kritik.modi@juspay.in>
2023-07-13 14:48:24 +0530

    chore: update down.sql

--- migrations/2023-07-13-090126_update-tables-to-cover-for-missed-database-migrations/down.sql
+++ migrations/2023-07-13-090126_update-tables-to-cover-for-missed-database-migrations/down.sql
@@ -1,6 +1,6 @@
 ALTER TABLE merchant_account
      ALTER COLUMN merchant_name DROP NOT NULL,
-     ALTER COLUMN primary_business_details SET DEFAULT '[{""country"": ""US"", ""business"": ""default""}]';
+     ALTER COLUMN primary_business_details SET DEFAULT '[{"country": "US", "business": "default"}]';
 
  ALTER TABLE merchant_key_store
      ALTER COLUMN merchant_id TYPE VARCHAR(64);
