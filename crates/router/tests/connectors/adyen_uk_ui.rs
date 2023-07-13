0e773d3d78271eed65ae4055943d143143cb2dda
AkshayaFoiger <akshaya.shankar@juspay.in>
2023-07-13 16:07:07 +0530

    Added Test Case

--- crates/router/tests/connectors/adyen_uk_ui.rs
+++ crates/router/tests/connectors/adyen_uk_ui.rs
@@ -171,6 +171,23 @@ async fn should_make_adyen_alipay_hk_payment(c: WebDriver) -> Result<(), WebDriv
     Ok(())
 }
 
+async fn should_make_adyen_swish_payment(c: WebDriver) -> Result<(), WebDriverError> {
+    let conn = AdyenSeleniumTest {};
+    conn.make_redirection_payment(
+        c,
+        vec![
+            Event::Trigger(Trigger::Goto(&format!("{CHEKOUT_BASE_URL}/saved/210"))),
+            Event::Trigger(Trigger::Click(By::Id("card-submit-btn"))),
+            Event::Assert(Assert::IsPresent("status")),
+            Event::Assert(Assert::IsPresent("processing")),
+            Event::Assert(Assert::IsPresent("Next Action Type")),
+            Event::Assert(Assert::IsPresent("qr_code_information"))
+        ],
+    )
+    .await?;
+    Ok(())
+}
+
 #[test]
 #[serial]
 #[ignore]
@@ -214,6 +231,12 @@ fn should_make_adyen_3ds_payment_success_test() {
 #[serial]
 fn should_make_adyen_alipay_hk_payment_test() {
     tester!(should_make_adyen_alipay_hk_payment);
+}
+
+#[test]
+#[serial]
+fn should_make_adyen_swish_payment_test() {
+    tester!(should_make_adyen_swish_payment);
 }
 
 // https://hs-payments-test.netlify.app/paypal-redirect?amount=70.00&country=US&currency=USD&mandate_data[customer_acceptance][acceptance_type]=offline&mandate_data[customer_acceptance][accepted_at]=1963-05-03T04:07:52.723Z&mandate_data[customer_acceptance][online][ip_address]=127.0.0.1&mandate_data[customer_acceptance][online][user_agent]=amet%20irure%20esse&mandate_data[mandate_type][multi_use][amount]=700&mandate_data[mandate_type][multi_use][currency]=USD&apikey=dev_uFpxA0r6jjbVaxHSY3X0BZLL3erDUzvg3i51abwB1Bknu3fdiPxw475DQgnByn1z
