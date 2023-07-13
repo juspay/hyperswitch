2deed890855201729758efa90c0a5082292dafa1
Kritik Modi <kritik.modi@juspay.in>
2023-07-13 14:34:19 +0530

    chore: update migrations

---
+++ migrations/2023-07-13-090126_update-tables-to-cover-for-missed-database-migrations/down.sql
@@ -0,0 +1,12 @@
+ALTER TABLE merchant_account
+     ALTER COLUMN merchant_name DROP NOT NULL,
+     ALTER COLUMN primary_business_details SET DEFAULT '[{""country"": ""US"", ""business"": ""default""}]';
+
+ ALTER TABLE merchant_key_store
+     ALTER COLUMN merchant_id TYPE VARCHAR(64);
+
+ ALTER TABLE payment_intent
+     ALTER COLUMN metadata DROP DEFAULT;
+
+ ALTER TABLE payment_methods
+     ALTER COLUMN payment_method_type TYPE VARCHAR;
---
+++ migrations/2023-07-13-090126_update-tables-to-cover-for-missed-database-migrations/up.sql
@@ -0,0 +1,12 @@
+ALTER TABLE merchant_account
+     ALTER COLUMN merchant_name SET NOT NULL,
+     ALTER COLUMN primary_business_details DROP DEFAULT;
+
+ ALTER TABLE merchant_key_store
+     ALTER COLUMN merchant_id TYPE VARCHAR(255);
+
+ ALTER TABLE payment_intent
+     ALTER COLUMN metadata SET DEFAULT '{}'::JSONB;
+
+ ALTER TABLE payment_methods
+     ALTER COLUMN payment_method_type TYPE VARCHAR(64); 
