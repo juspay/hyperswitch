660dc39b5a17fc8ea4bb898dce74441ff7953b04
swangi-kumari <swangi.12015941@lpu.in>
2023-07-14 16:02:54 +0530

    add test for 3ds cards mandates

--- crates/router/tests/connectors/noon_ui.rs
+++ crates/router/tests/connectors/noon_ui.rs
@@ -13,26 +13,65 @@ impl SeleniumTest for NoonSeleniumTest {
 
 async fn should_make_noon_3ds_payment(web_driver: WebDriver) -> Result<(), WebDriverError> {
     let conn = NoonSeleniumTest {};
-    conn.make_redirection_payment(web_driver, vec![
+    conn.make_redirection_payment(
+        web_driver,
+        vec![
             Event::Trigger(Trigger::Goto(&format!("{CHEKOUT_BASE_URL}/saved/176"))),
             Event::Trigger(Trigger::Click(By::Id("card-submit-btn"))),
+            Event::Trigger(Trigger::SwitchFrame(By::Id("redirectTo3ds1Frame"))),
+            Event::Trigger(Trigger::SwitchFrame(By::Css("iframe[frameborder='0']"))),
+            Event::Trigger(Trigger::SendKeys(By::Css("input.input-field"), "1234")),
+            Event::Trigger(Trigger::Click(By::Css("input.button.primary"))),
             Event::Trigger(Trigger::Sleep(5)),
-            // Event::Trigger(Trigger::SendKeys(
-            //     By::Css("input.input-field"),
-            //     "1234",
-            // )),
-            Event::Trigger(Trigger::SendKeys(
-                By::Css("input[name='challengeDataEntry']"),
-                "1234",
-            )),
-            Event::Trigger(Trigger::Click(
-                By::Css("input.button.primary")
-            )),
-            // Event::Assert(Assert::IsPresent("Google")),
-            // Event::Assert(Assert::Contains(Selector::QueryParamStr, "status=succeeded")),
             Event::Assert(Assert::IsPresent("succeeded")),
+        ],
+    )
+    .await?;
+    Ok(())
+}
 
-    ]).await?;
+async fn should_make_noon_3ds_mandate_payment(web_driver: WebDriver) -> Result<(), WebDriverError> {
+    let conn = NoonSeleniumTest {};
+    conn.make_redirection_payment(
+        web_driver,
+        vec![
+            Event::Trigger(Trigger::Goto(&format!("{CHEKOUT_BASE_URL}/saved/214"))),
+            Event::Trigger(Trigger::Click(By::Id("card-submit-btn"))),
+            Event::Trigger(Trigger::SwitchFrame(By::Id("redirectTo3ds1Frame"))),
+            Event::Trigger(Trigger::SwitchFrame(By::Css("iframe[frameborder='0']"))),
+            Event::Trigger(Trigger::SendKeys(By::Css("input.input-field"), "1234")),
+            Event::Trigger(Trigger::Click(By::Css("input.button.primary"))),
+            Event::Trigger(Trigger::Sleep(5)),
+            Event::Assert(Assert::IsPresent("succeeded")),
+            Event::Assert(Assert::IsPresent("Mandate ID")),
+            Event::Assert(Assert::IsPresent("man_")), // mandate id starting with man_
+            Event::Trigger(Trigger::Click(By::Css("a.btn"))),
+            Event::Trigger(Trigger::Click(By::Id("card-submit-btn"))),
+            Event::Assert(Assert::IsPresent("succeeded")),
+        ],
+    )
+    .await?;
+    Ok(())
+}
+
+async fn should_make_noon_non_3ds_mandate_payment(
+    web_driver: WebDriver,
+) -> Result<(), WebDriverError> {
+    let conn = NoonSeleniumTest {};
+    conn.make_redirection_payment(
+        web_driver,
+        vec![
+            Event::Trigger(Trigger::Goto(&format!("{CHEKOUT_BASE_URL}/saved/215"))),
+            Event::Trigger(Trigger::Click(By::Id("card-submit-btn"))),
+            Event::Assert(Assert::IsPresent("succeeded")),
+            Event::Assert(Assert::IsPresent("Mandate ID")),
+            Event::Assert(Assert::IsPresent("man_")), // mandate id starting with man_
+            Event::Trigger(Trigger::Click(By::Css("a.btn"))),
+            Event::Trigger(Trigger::Click(By::Id("card-submit-btn"))),
+            Event::Assert(Assert::IsPresent("succeeded")),
+        ],
+    )
+    .await?;
     Ok(())
 }
 
@@ -40,4 +79,16 @@ async fn should_make_noon_3ds_payment(web_driver: WebDriver) -> Result<(), WebDr
 #[serial]
 fn should_make_noon_3ds_payment_test() {
     tester!(should_make_noon_3ds_payment);
+}
+
+#[test]
+#[serial]
+fn should_make_noon_3ds_mandate_payment_test() {
+    tester!(should_make_noon_3ds_mandate_payment);
+}
+
+#[test]
+#[serial]
+fn should_make_noon_non_3ds_mandate_payment_test() {
+    tester!(should_make_noon_non_3ds_mandate_payment);
 }
