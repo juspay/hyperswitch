fa29cd90142828ddf64340b4f611e6e477c57cff
Arjun Karthik <m.arjunkarthik@gmail.com>
2023-07-18 19:09:24 +0530

    fix(connector): [PowerTranz] fix psync not implemented error

--- crates/router/src/connector/powertranz.rs
+++ crates/router/src/connector/powertranz.rs
@@ -332,16 +332,7 @@ impl
 impl ConnectorIntegration<api::PSync, types::PaymentsSyncData, types::PaymentsResponseData>
     for Powertranz
 {
-    fn build_request(
-        &self,
-        _req: &types::PaymentsSyncRouterData,
-        _connectors: &settings::Connectors,
-    ) -> CustomResult<Option<services::Request>, errors::ConnectorError> {
-        Err(errors::ConnectorError::FlowNotSupported {
-            flow: "Payment Sync".to_string(),
-            connector: "PowerTranz".to_string(),
-        })?
-    }
+    // default implementation of build_request method will be executed
 }
 
 impl ConnectorIntegration<api::Capture, types::PaymentsCaptureData, types::PaymentsResponseData>
@@ -574,6 +565,7 @@ impl ConnectorIntegration<api::Execute, types::RefundsData, types::RefundsRespon
 impl ConnectorIntegration<api::RSync, types::RefundsData, types::RefundsResponseData>
     for Powertranz
 {
+    // default implementation of build_request method will be executed
 }
 
 #[async_trait::async_trait]
