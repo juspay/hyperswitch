2d2ce32643e7264b81de22bb0f7d431bc086a008
hrithikesh026 <hrithikeshmylatty@gmail.com>
2023-07-20 13:44:29 +0530

    fix(Connector): trigger connector for succeeded redirect response to update payment_method_id in mandate table if needed(bank redirect)

--- crates/router/src/connector/stripe.rs
+++ crates/router/src/connector/stripe.rs
@@ -1885,7 +1885,8 @@ impl services::ConnectorRedirectResponse for Stripe {
                 payments::CallConnectorAction::Trigger,
                 |status| match status {
                     transformers::StripePaymentStatus::Failed
-                    | transformers::StripePaymentStatus::Pending => {
+                    | transformers::StripePaymentStatus::Pending 
+                    | transformers::StripePaymentStatus::Succeeded => {
                         payments::CallConnectorAction::Trigger
                     }
                     _ => payments::CallConnectorAction::StatusUpdate {
