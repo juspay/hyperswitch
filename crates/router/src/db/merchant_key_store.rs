42a30d8674988f650c28fef655b6e772df5fa352
Sanchith Hegde <sanchith.hegde@juspay.in>
2023-07-23 20:01:11 +0530

    feat(db): implement `MerchantKeyStoreInterface` for `MockDb`

--- crates/router/src/db/merchant_key_store.rs
+++ crates/router/src/db/merchant_key_store.rs
@@ -8,9 +8,12 @@ use crate::{
     core::errors::{self, CustomResult},
     db::MockDb,
     services::Store,
-    types::domain::{
-        self,
-        behaviour::{Conversion, ReverseConversion},
+    types::{
+        domain::{
+            self,
+            behaviour::{Conversion, ReverseConversion},
+        },
+        storage,
     },
 };
 
@@ -49,6 +52,7 @@ impl MerchantKeyStoreInterface for Store {
             .await
             .change_context(errors::StorageError::DecryptionError)
     }
+
     async fn get_merchant_key_store_by_merchant_id(
         &self,
         merchant_id: &str,
@@ -95,18 +99,116 @@ impl MerchantKeyStoreInterface for Store {
 impl MerchantKeyStoreInterface for MockDb {
     async fn insert_merchant_key_store(
         &self,
-        _merchant_key_store: domain::MerchantKeyStore,
-        _key: &Secret<Vec<u8>>,
+        merchant_key_store: domain::MerchantKeyStore,
+        key: &Secret<Vec<u8>>,
     ) -> CustomResult<domain::MerchantKeyStore, errors::StorageError> {
-        // [#172]: Implement function for `MockDb`
-        Err(errors::StorageError::MockDbError.into())
+        let mut locked_merchant_key_store = self.merchant_key_store.lock().await;
+
+        if locked_merchant_key_store
+            .iter()
+            .any(|merchant_key| merchant_key.merchant_id == merchant_key_store.merchant_id)
+        {
+            Err(errors::StorageError::MockDbError)?;
+        }
+
+        let merchant_key = storage::MerchantKeyStore {
+            merchant_id: merchant_key_store.merchant_id,
+            key: merchant_key_store.key.into(),
+            created_at: merchant_key_store.created_at,
+        };
+        locked_merchant_key_store.push(merchant_key.clone());
+
+        merchant_key
+            .convert(key)
+            .await
+            .change_context(errors::StorageError::DecryptionError)
     }
+
     async fn get_merchant_key_store_by_merchant_id(
         &self,
-        _merchant_id: &str,
-        _key: &Secret<Vec<u8>>,
+        merchant_id: &str,
+        key: &Secret<Vec<u8>>,
     ) -> CustomResult<domain::MerchantKeyStore, errors::StorageError> {
-        // [#172]: Implement function for `MockDb`
-        Err(errors::StorageError::MockDbError.into())
+        self.merchant_key_store
+            .lock()
+            .await
+            .iter()
+            .find(|merchant_key| merchant_key.merchant_id == merchant_id)
+            .cloned()
+            .ok_or(errors::StorageError::MockDbError)?
+            .convert(key)
+            .await
+            .change_context(errors::StorageError::DecryptionError)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use time::macros::datetime;
+
+    use crate::{
+        db::{merchant_key_store::MerchantKeyStoreInterface, MasterKeyInterface, MockDb},
+        services,
+        types::domain::{self, types as domain_types},
+    };
+
+    #[allow(clippy::unwrap_used)]
+    #[tokio::test]
+    async fn test_mock_db_merchant_key_store_interface() {
+        let mock_db = MockDb::new(&Default::default()).await;
+        let master_key = mock_db.get_master_key();
+        let merchant_id = "merchant1";
+
+        let merchant_key1 = mock_db
+            .insert_merchant_key_store(
+                domain::MerchantKeyStore {
+                    merchant_id: merchant_id.into(),
+                    key: domain_types::encrypt(
+                        services::generate_aes256_key().unwrap().to_vec().into(),
+                        master_key,
+                    )
+                    .await
+                    .unwrap(),
+                    created_at: datetime!(2023-02-01 0:00),
+                },
+                &master_key.to_vec().into(),
+            )
+            .await
+            .unwrap();
+
+        let found_merchant_key1 = mock_db
+            .get_merchant_key_store_by_merchant_id(merchant_id, &master_key.to_vec().into())
+            .await
+            .unwrap();
+
+        assert_eq!(found_merchant_key1.merchant_id, merchant_key1.merchant_id);
+        assert_eq!(found_merchant_key1.key, merchant_key1.key);
+
+        let insert_duplicate_merchant_key1_result = mock_db
+            .insert_merchant_key_store(
+                domain::MerchantKeyStore {
+                    merchant_id: merchant_id.into(),
+                    key: domain_types::encrypt(
+                        services::generate_aes256_key().unwrap().to_vec().into(),
+                        master_key,
+                    )
+                    .await
+                    .unwrap(),
+                    created_at: datetime!(2023-02-01 0:00),
+                },
+                &master_key.to_vec().into(),
+            )
+            .await;
+        assert!(insert_duplicate_merchant_key1_result.is_err());
+
+        let find_non_existent_merchant_key_result = mock_db
+            .get_merchant_key_store_by_merchant_id("non_existent", &master_key.to_vec().into())
+            .await;
+        assert!(find_non_existent_merchant_key_result.is_err());
+
+        let find_merchant_key_with_incorrect_master_key_result = mock_db
+            .get_merchant_key_store_by_merchant_id(merchant_id, &vec![0; 32].into())
+            .await;
+        assert!(find_merchant_key_with_incorrect_master_key_result.is_err());
     }
 }
