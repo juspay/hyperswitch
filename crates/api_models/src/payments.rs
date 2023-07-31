7405103668b53f731d883c2a685d4878f3c920e5
AkshayaFoiger <akshaya.shankar@juspay.in>
2023-07-31 17:27:34 +0530

    generate openapi_spec

--- crates/api_models/src/payments.rs
+++ crates/api_models/src/payments.rs
@@ -1541,6 +1541,7 @@ pub struct DokuBankTransferInstructions {
     pub expires_at: Option<String>,
     #[schema(value_type = String, example = "122385736258")]
     pub reference: Secret<String>,
+    #[schema(value_type = String)]
     pub instructions_url: Option<Url>,
 }
 
