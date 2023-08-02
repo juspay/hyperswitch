7d09f39ea81222f0886e1c2af617cbe985287254
Narayan Bhat <narayan.bhat@juspay.in>
2023-08-02 19:09:37 +0530

    chore: cargo clippy

--- crates/router/src/core/webhooks.rs
+++ crates/router/src/core/webhooks.rs
@@ -474,7 +474,7 @@ pub async fn create_event_and_trigger_outgoing_webhook<W: types::OutgoingWebhook
     primary_object_type: enums::EventObjectType,
     content: api::OutgoingWebhookContent,
 ) -> CustomResult<(), errors::ApiErrorResponse> {
-    let event_id = format!("{primary_object_id}_{}", event_type.to_string());
+    let event_id = format!("{primary_object_id}_{}", event_type);
     let new_event = storage::EventNew {
         event_id: event_id.clone(),
         event_type,
