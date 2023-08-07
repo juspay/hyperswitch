b98a14b3e4b13a58c9075c2387280cd9a5ab6436
pixincreate@work <69745008+pixincreate@users.noreply.github.com>
2023-08-07 16:31:25 +0530

    Complete connector validation

--- .github/scripts/connector_validate.sh
+++ .github/scripts/connector_validate.sh
@@ -7,6 +7,9 @@ wget $CONNECTORS_PATH && mv Connectors Connectors.json
 # Read the JSON file content into a variable
 JSON_DATA=$(cat Connectors.json)
 
+RED='\033[0;31m'
+RESET='\033[0m'
+
 # Extract the connectors and ignore list values using jq
 CONNECTORS=$(echo "$JSON_DATA" | jq -r '.CONNECTORS')
 IGNORE_LIST=$(echo "$JSON_DATA" | jq -r '.IGNORE_LIST')
@@ -38,19 +41,38 @@ function exclude_folder() {
     
     for folder in $filter; do
         if [ "$folder" != "Happy Cases" ] && [ "$folder" != "Variation Cases" ] && [ "$folder" != "Flow Testcases" ]; then
-        echo "$connector_name:$folder"
+        if [ -n "$filtered_folders" ]; then
+                filtered_folders+=",$connector_name:$folder"
+            else
+                filtered_folders="$connector_name:$folder"
+            fi
         fi
     done
+
+    echo "$filtered_folders"
 }
 
 # Loop through the connectors' array and add elements that are not in the IGNORE_LIST_ARRAY
 FILTERED_CONNECTORS=()
 
-for CONNECTOR in "${CONNECTORS_ARRAY[@]}"; do
-    if [[ ! " ${IGNORE_LIST_ARRAY[*]} " =~ " $CONNECTOR " ]]; then
-        FILTERED_CONNECTORS+=("$CONNECTOR")
+for CONNECTOR in "${connectors_array[@]}"; do
+    ignore=false
+    for IGNORED_CONNECTOR in "${IGNORE_LIST_ARRAY[@]}"; do
+        if [[ "$CONNECTOR" == "$IGNORED_CONNECTOR" ]]; then
+            ignore=true
+            break
+        fi
+    done
+
+    if [[ "$ignore" == "false" ]]; then
+        if [[ -n "$filtered_connectors" ]]; then
+            FILTERED_CONNECTORS+=",$CONNECTOR"
+        else
+            FILTERED_CONNECTORS="$CONNECTOR"
+        fi
     fi
 done
+
 INCLUDED_FOLDERS=()
 for CONNECTOR in "${IGNORE_LIST_ARRAY[@]}"; do
     if [[ "$CONNECTOR" == *:* ]]; then
@@ -61,5 +83,33 @@ for CONNECTOR in "${IGNORE_LIST_ARRAY[@]}"; do
     INCLUDED_FOLDERS+="$RESULT"
 done
 
-export "${INCLUDED_FOLDERS[*]}"
-export CONNECTORS="${FILTERED_CONNECTORS[@]/$NAME}"
+export FOLDERS="${INCLUDED_FOLDERS[*]}"
+export CONNECTORS="${FILTERED_CONNECTORS[@]}"
+
+if [ -n "$FOLDERS" ]; then
+    echo "$FOLDERS"
+fi
+
+failed_connectors=()
+validated_connectors=()
+
+for i in $(echo "$CONNECTORS" | tr "," "\n"); do
+    for j in "$FOLDERS"; do
+        connector_folder="$j%%:*"
+        if [[ " ${validated_connectors[*]} " =~ " $connector_folder " ]]; then
+            continue
+        else
+            validated_connectors+=("$connector_folder")
+            if ! cargo run --bin test_utils -- --connector_name="$i" --base_url="$BASE_URL" --admin_api_key="$ADMIN_API_KEY" --folder_name="$j"; then
+                failed_connectors+=("$i")
+            fi
+        fi
+    done
+done
+
+if [ ${#failed_connectors[@]} -gt 0 ]; then
+    echo -e "${RED}One or more connectors failed to run:${RESET}"
+    printf '%s\n' "${failed_connectors[@]}"
+    exit 1
+fi
+    
