7d363bfd453bcb6fe6b841bf0a0c9709320bf448
sai-harsha-vardhan <harsha111hero@gmail.com>
2023-08-08 14:03:03 +0530

    skip payment_intent fetch for single business details merchants

--- crates/router/src/utils.rs
+++ crates/router/src/utils.rs
@@ -283,22 +283,51 @@ pub async fn get_connector_label_using_object_reference_id(
     merchant_account: &domain::MerchantAccount,
     connector_name: &str,
 ) -> CustomResult<String, errors::ApiErrorResponse> {
-    let payment_intent = match object_reference_id {
-        webhooks::ObjectReferenceId::PaymentId(payment_id_type) => {
-            find_payment_intent_from_payment_id_type(db, payment_id_type, merchant_account).await?
+    let mut primary_business_details = merchant_account
+        .primary_business_details
+        .clone()
+        .parse_value::<Vec<api_models::admin::PrimaryBusinessDetails>>("PrimaryBusinessDetails")
+        .change_context(errors::ApiErrorResponse::InternalServerError)
+        .attach_printable("failed to parse primary business details")?;
+    //check if there is only one primary business details and get those details
+    let (option_business_country, option_business_label) = match primary_business_details.pop() {
+        Some(business_details) => {
+            if primary_business_details.is_empty() {
+                (
+                    Some(business_details.country),
+                    Some(business_details.business),
+                )
+            } else {
+                (None, None)
+            }
         }
-        webhooks::ObjectReferenceId::RefundId(refund_id_type) => {
-            find_payment_intent_from_refund_id_type(
-                db,
-                refund_id_type,
-                merchant_account,
-                connector_name,
-            )
-            .await?
-        }
+        None => (None, None),
     };
-    Ok(format!(
-        "{connector_name}_{}_{}",
-        payment_intent.business_country, payment_intent.business_label
-    ))
+    match (option_business_country, option_business_label) {
+        (Some(business_country), Some(business_label)) => Ok(format!(
+            "{connector_name}_{}_{}",
+            business_country, business_label
+        )),
+        _ => {
+            let payment_intent = match object_reference_id {
+                webhooks::ObjectReferenceId::PaymentId(payment_id_type) => {
+                    find_payment_intent_from_payment_id_type(db, payment_id_type, merchant_account)
+                        .await?
+                }
+                webhooks::ObjectReferenceId::RefundId(refund_id_type) => {
+                    find_payment_intent_from_refund_id_type(
+                        db,
+                        refund_id_type,
+                        merchant_account,
+                        connector_name,
+                    )
+                    .await?
+                }
+            };
+            Ok(format!(
+                "{connector_name}_{}_{}",
+                payment_intent.business_country, payment_intent.business_label
+            ))
+        }
+    }
 }
