2f539d765e8726836a66b45558fcb1b7b0d9883c
Kritik Modi <kritik.modi@juspay.in>
2023-08-08 16:08:27 +0530

    chore: remove some queries

--- migrations/2023-07-28-111829_update_columns_to_fix_db_diff/up.sql
+++ migrations/2023-07-28-111829_update_columns_to_fix_db_diff/up.sql
@@ -3,18 +3,3 @@ ALTER COLUMN payment_id TYPE VARCHAR(64);
 
 ALTER TABLE payment_methods
 ALTER COLUMN payment_method_type TYPE VARCHAR(255);
-
-ALTER TABLE merchant_account
-ALTER COLUMN primary_business_details DROP NOT NULL;
-
-ALTER TABLE merchant_connector_account
-ALTER COLUMN business_country DROP NOT NULL;
-
-ALTER TABLE merchant_connector_account
-ALTER COLUMN business_label DROP NOT NULL;
-
-ALTER TABLE payment_intent
-ALTER COLUMN business_country DROP NOT NULL;
-
-ALTER TABLE payment_intent
-ALTER COLUMN business_label DROP NOT NULL;
