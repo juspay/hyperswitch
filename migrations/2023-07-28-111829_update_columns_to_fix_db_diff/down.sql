5f10b38c6f62dfc0c3b7516bc4b7c7ebd54b0d48
Kritik Modi <kritik.modi@juspay.in>
2023-08-08 15:02:02 +0530

    chore: update migrations

--- migrations/2023-07-28-111829_update_columns_to_fix_db_diff/down.sql
+++ migrations/2023-07-28-111829_update_columns_to_fix_db_diff/down.sql
@@ -1,8 +1,5 @@
 ALTER TABLE dispute
 ALTER COLUMN payment_id TYPE VARCHAR(255);
 
-ALTER TABLE merchant_key_store
-ALTER COLUMN created_at DROP DEFAULT;
-
 ALTER TABLE payment_methods
 ALTER COLUMN payment_method_type TYPE VARCHAR;
