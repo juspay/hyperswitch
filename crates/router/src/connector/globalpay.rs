0367e3acadc58b1b12d2ac04250b21d998589087
hrithikeshvm <hrithikeshmylatty@gmail.com>
2023-08-09 15:33:09 +0530

    feat: enable multiple partial capture for global pay

--- crates/router/src/connector/globalpay.rs
+++ crates/router/src/connector/globalpay.rs
@@ -5,7 +5,6 @@ pub mod transformers;
 use std::fmt::Debug;
 
 use ::common_utils::{errors::ReportSwitchExt, ext_traits::ByteSliceExt};
-use diesel_models::enums;
 use error_stack::{IntoReport, ResultExt};
 use masking::PeekInterface;
 use serde_json::Value;
@@ -21,7 +20,6 @@ use super::utils::RefundsRequestData;
 use crate::{
     configs::settings,
     connector::utils as conn_utils,
-    consts,
     core::{
         errors::{self, CustomResult},
         payments,
@@ -587,17 +585,9 @@ impl ConnectorIntegration<api::Authorize, types::PaymentsAuthorizeData, types::P
 
     fn get_url(
         &self,
-        req: &types::PaymentsAuthorizeRouterData,
+        _req: &types::PaymentsAuthorizeRouterData,
         connectors: &settings::Connectors,
     ) -> CustomResult<String, errors::ConnectorError> {
-        if req.request.capture_method == Some(enums::CaptureMethod::ManualMultiple) {
-            return Err(errors::ConnectorError::NotImplemented(format!(
-                "{}{}",
-                consts::MANUAL_MULTIPLE_NOT_IMPLEMENTED_ERROR_MESSAGE,
-                self.id()
-            ))
-            .into());
-        }
         Ok(format!("{}transactions", self.base_url(connectors)))
     }
 
