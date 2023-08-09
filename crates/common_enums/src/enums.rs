f8ef52c645d353aac438d6af5b00d9097332fdcb
Narayan Bhat <48803246+Narayanbhat166@users.noreply.github.com>
2023-08-09 11:25:56 +0530

    feat(docs): add multiple examples support and webhook schema (#1864)

--- crates/common_enums/src/enums.rs
+++ crates/common_enums/src/enums.rs
@@ -736,6 +736,7 @@ impl Currency {
     serde::Serialize,
     strum::Display,
     strum::EnumString,
+    ToSchema,
 )]
 #[router_derive::diesel_enum(storage_type = "pg_enum")]
 #[serde(rename_all = "snake_case")]
