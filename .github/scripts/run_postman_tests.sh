860e9212611dec1044233dda0f33a6b8e33956a9
pixincreate@work <69745008+pixincreate@users.noreply.github.com>
2023-08-11 17:05:46 +0530

    Fix runner not running

--- .github/scripts/run_postman_tests.sh
+++ .github/scripts/run_postman_tests.sh
@@ -1,21 +1,21 @@
 #! /usr/bin/env bash
-
 mkdir -p $HOME/target/test
 
 JSON_FILE_PATH=$HOME/target/test/Connectors.json
 
 # Fetches the Connector's list and stores it in Connectors.json file
-wget -q --output-document "$JSON_FILE_PATH" "$POSTMAN_CONNECTOR_PATHS"
+wget --output-document "$JSON_FILE_PATH" "$POSTMAN_CONNECTOR_PATHS"
 
 # Read the JSON file content into a variable
 JSON_DATA=$(cat "$JSON_FILE_PATH")
+
 # Extract the connectors and ignore_list values using jq
-connectors=$(echo "$JSON_DATA" | jq -r '.connectors | @tsv')
-ignore_list=$(echo "$JSON_DATA" | jq -r '.ignore_list | @tsv')
+CONNECTORS=$(echo "$JSON_DATA" | jq -r '.connectors | @tsv')
+IGNORE_LIST=$(echo "$JSON_DATA" | jq -r '.ignore_list | @tsv')
 
 # Convert the comma-separated strings into arrays
-IFS=$'\t' read -ra CONNECTORS_ARRAY <<< "$connectors"
-IFS=$'\t' read -ra IGNORE_LIST_ARRAY <<< "$ignore_list"
+IFS=$'\t' read -ra CONNECTORS_ARRAY <<< "$CONNECTORS"
+IFS=$'\t' read -ra IGNORE_LIST_ARRAY <<< "$IGNORE_LIST"
 IFS=$'\n'
 
 # Colors for the output
@@ -30,7 +30,7 @@ function exclude_folder() {
     local collection_file="postman/$connector_name.postman_collection.json"
 
     # Fetches the folder names and excludes the folder that is passed as an argument
-    local filter=$(jq -r --arg exclude "$exclude_folder" '
+    filter=$(jq -r --arg exclude "$exclude_folder" '
         def print_folder_names($items; $indent):
         $items[] | select(has("item")) as $folder |
         if ($exclude | split(",") | index($folder.name) == null) then
@@ -53,67 +53,71 @@ function exclude_folder() {
             fi
         fi
     done
-
     # Returns the folders that contains tests
     echo "$filtered_folders"
 }
 
+# Loop through the connectors_array and add elements that are not in the ignore_list_array
 FILTERED_CONNECTORS=()
 
 # Loop through the connectors' array and add elements that are not in the IGNORE_LIST_ARRAY
-for CONNECTOR in "${CONNECTORS_ARRAY[@]}"; do
+for connector in "${CONNECTORS_ARRAY[@]}"; do
     ignore=false
-    for IGNORED_CONNECTOR in "${IGNORE_LIST_ARRAY[@]}"; do
-        if [[ "$CONNECTOR" == "$IGNORED_CONNECTOR" ]]; then
+    for ignored_connector in "${IGNORE_LIST_ARRAY[@]}"; do
+        if [[ "$connector" == "$ignored_connector" ]]; then
             ignore=true
             break
         fi
     done
-
+    
     if [[ "$ignore" == "false" ]]; then
-        if [[ -n "$filtered_connectors" ]]; then
-            FILTERED_CONNECTORS+=",$CONNECTOR"
+        if [[ -n "$FILTERED_CONNECTORS" ]]; then
+            FILTERED_CONNECTORS+=",$connector"
         else
-            FILTERED_CONNECTORS="$CONNECTOR"
+            FILTERED_CONNECTORS="$connector"
         fi
     fi
 done
 
-export CONNECTORS="${FILTERED_CONNECTORS[@]}"
+export FILTERED_CONNECTORS="${FILTERED_CONNECTORS[@]}"
 
 FAILED_CONNECTORS=()
-validated_connectors=()
+VALIDATED_CONNECTORS=()
 
 # Loop through the CONNECTORS_ARRAY and run the tests
 for i in $(echo "$FILTERED_CONNECTORS" | tr "," "\n"); do
     run_connector=false
 
     # Loop through the IGNORE_LIST_ARRAY and exclude the folders that are not needed, run the test for the rest of the folders
-    for CONNECTOR in "${IGNORE_LIST_ARRAY[@]}"; do
-        if [[ "$CONNECTOR" == *:* ]]; then
-            IFS=':' read -r name folder_to_ignore <<< "$CONNECTOR"
+    for connector in "${IGNORE_LIST_ARRAY[@]}"; do
+        if [[ "$connector" == *:* ]]; then
+            IFS=':' read -r name folder_to_ignore <<< "$connector"
             included_folders="$(exclude_folder "$name" "$folder_to_ignore")"
-
             for j in "$included_folders"; do
                 IFS=':' read -r connector connector_folder <<< "$j"
                 only_folders="${j//$connector:/}"
-                if [[ "$CONNECTOR" == "$i" && ! " ${validated_connectors[*]} " =~ " $CONNECTOR " ]]; then
-                    echo "cargo run --bin test_utils -- --connector_name=$i --base_url=$BASE_URL --admin_api_key=$ADMIN_API_KEY --folder_name=$only_folders"
+                if [[ "$connector" == "$i" && ! " ${VALIDATED_CONNECTORS[*]} " =~ " $connector " ]]; then
+                    echo "cargo run --bin test_utils -- --connector_name=$connector --base_url=$BASE_URL --admin_api_key=$ADMIN_API_KEY --folder_name=$only_folders"
+                    FAILED_CONNECTORS+=("$i")
                     run_connector=true
+
                     # if ! cargo run --bin test_utils -- --connector_name="$i" --base_url="$BASE_URL" --admin_api_key="$ADMIN_API_KEY" --folder_name="$only_folders"; then
                     #     FAILED_CONNECTORS+=("$i")
                     #     run_connector=true
                     # fi
                 fi
-                validated_connectors+=("$CONNECTOR")
+                VALIDATED_CONNECTORS+=("$connector")
             done
         fi
     done
     
     # Run the tests for the rest
     if [[ "$run_connector" == "false" ]]; then
-        if [[ ! " ${validated_connectors[*]} " =~ " $i " ]]; then
+        if [[ ! " ${VALIDATED_CONNECTORS[*]} " =~ " $i " ]]; then
             echo "cargo run --bin test_utils -- --connector_name=$i --base_url=$BASE_URL --admin_api_key=$ADMIN_API_KEY"
+            FAILED_CONNECTORS+=("$i")
+            VALIDATED_CONNECTORS+=("$i")
+
             # if ! cargo run --bin test_utils -- --connector_name="$i" --base_url="$BASE_URL" --admin_api_key="$ADMIN_API_KEY"; then
             #   FAILED_CONNECTORS+=("$i")
             # fi
