dd57c2b0cf24d2f4fe2e6fedc757e9f81d7e072d
Sampras Lopes <lsampras@pm.me>
2023-08-17 13:19:22 +0530

    chore(payment): remove commented module

--- crates/router/src/types/storage.rs
+++ crates/router/src/types/storage.rs
@@ -18,7 +18,6 @@ pub mod merchant_account;
 pub mod merchant_connector_account;
 pub mod merchant_key_store;
 pub mod payment_attempt;
-// pub mod payment_intent;
 pub mod payment_method;
 pub mod payout_attempt;
 pub mod payouts;
