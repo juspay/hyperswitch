53956d6f8379f90e4070b49bd2322950aa11a7f2
Narayan Bhat <48803246+Narayanbhat166@users.noreply.github.com>
2023-08-18 14:53:29 +0530

    feat(business_profile): add business profile table and CRUD endpoints (#1928)

--- crates/router/src/types/api/admin.rs
+++ crates/router/src/types/api/admin.rs
@@ -1,5 +1,6 @@
 pub use api_models::admin::{
-    payout_routing_algorithm, MerchantAccountCreate, MerchantAccountDeleteResponse,
+    payout_routing_algorithm, BusinessProfileCreate, BusinessProfileResponse,
+    BusinessProfileUpdate, MerchantAccountCreate, MerchantAccountDeleteResponse,
     MerchantAccountResponse, MerchantAccountUpdate, MerchantConnectorCreate,
     MerchantConnectorDeleteResponse, MerchantConnectorDetails, MerchantConnectorDetailsWrap,
     MerchantConnectorId, MerchantConnectorResponse, MerchantDetails, MerchantId,
@@ -7,8 +8,12 @@ pub use api_models::admin::{
     RoutingAlgorithm, StraightThroughAlgorithm, ToggleKVRequest, ToggleKVResponse, WebhookDetails,
 };
 use common_utils::ext_traits::ValueExt;
+use masking::Secret;
 
-use crate::{core::errors, types::domain};
+use crate::{
+    core::errors,
+    types::{domain, storage, transformers::ForeignTryFrom},
+};
 
 impl TryFrom<domain::MerchantAccount> for MerchantAccountResponse {
     type Error = error_stack::Report<errors::ParsingError>;
@@ -38,6 +43,30 @@ impl TryFrom<domain::MerchantAccount> for MerchantAccountResponse {
             payout_routing_algorithm: item.payout_routing_algorithm,
             organization_id: item.organization_id,
             is_recon_enabled: item.is_recon_enabled,
+        })
+    }
+}
+
+impl ForeignTryFrom<storage::business_profile::BusinessProfile> for BusinessProfileResponse {
+    type Error = error_stack::Report<errors::ParsingError>;
+
+    fn foreign_try_from(
+        item: storage::business_profile::BusinessProfile,
+    ) -> Result<Self, Self::Error> {
+        Ok(Self {
+            merchant_id: item.merchant_id,
+            profile_id: item.profile_id,
+            profile_name: item.profile_name,
+            return_url: item.return_url,
+            enable_payment_response_hash: item.enable_payment_response_hash,
+            payment_response_hash_key: item.payment_response_hash_key,
+            redirect_to_merchant_with_http_post: item.redirect_to_merchant_with_http_post,
+            webhook_details: item.webhook_details.map(Secret::new),
+            metadata: item.metadata,
+            routing_algorithm: item.routing_algorithm,
+            intent_fulfillment_time: item.intent_fulfillment_time,
+            frm_routing_algorithm: item.frm_routing_algorithm,
+            payout_routing_algorithm: item.payout_routing_algorithm,
         })
     }
 }
