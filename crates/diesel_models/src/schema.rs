bf48b29bf4cc5d1a71b566ca8d4bf1ed079e80d1
hrithikeshvm <hrithikeshmylatty@gmail.com>
2023-08-21 20:14:06 +0530

    run migration

--- crates/diesel_models/src/schema.rs
+++ crates/diesel_models/src/schema.rs
@@ -329,7 +329,7 @@ diesel::table! {
         card_fingerprint -> Varchar,
         #[max_length = 255]
         card_global_fingerprint -> Varchar,
-        #[max_length = 64]
+        #[max_length = 255]
         merchant_id -> Varchar,
         #[max_length = 255]
         card_number -> Varchar,
@@ -473,21 +473,6 @@ diesel::table! {
         merchant_id -> Varchar,
         key -> Bytea,
         created_at -> Timestamp,
-    }
-}
-
-diesel::table! {
-    use diesel::sql_types::*;
-    use crate::enums::diesel_exports::*;
-
-    onboarding_data (id) {
-        id -> Int4,
-        #[max_length = 64]
-        user_id -> Varchar,
-        onboarding_step -> Int4,
-        created_at -> Timestamp,
-        last_modified -> Timestamp,
-        subscribed -> Nullable<Bool>,
     }
 }
 
@@ -798,28 +783,6 @@ diesel::table! {
     }
 }
 
-diesel::table! {
-    use diesel::sql_types::*;
-    use crate::enums::diesel_exports::*;
-
-    users (id) {
-        id -> Int4,
-        #[max_length = 64]
-        user_id -> Varchar,
-        #[max_length = 255]
-        email -> Varchar,
-        #[max_length = 255]
-        name -> Varchar,
-        #[max_length = 255]
-        password -> Varchar,
-        #[max_length = 64]
-        merchant_id -> Nullable<Varchar>,
-        is_verified -> Bool,
-        created_at -> Timestamp,
-        last_modified -> Timestamp,
-    }
-}
-
 diesel::allow_tables_to_appear_in_same_query!(
     address,
     api_keys,
@@ -838,7 +801,6 @@ diesel::allow_tables_to_appear_in_same_query!(
     merchant_account,
     merchant_connector_account,
     merchant_key_store,
-    onboarding_data,
     payment_attempt,
     payment_intent,
     payment_methods,
@@ -847,5 +809,4 @@ diesel::allow_tables_to_appear_in_same_query!(
     process_tracker,
     refund,
     reverse_lookup,
-    users,
 );
