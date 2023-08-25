850cfb7201f01dd036b41b94f2a8efee1e383da2
Nishant Joshi <nishant.joshi@juspay.in>
2023-08-25 13:35:02 +0530

    chore: make 90s as default timeout

--- crates/router/src/services/api/client.rs
+++ crates/router/src/services/api/client.rs
@@ -24,11 +24,9 @@ fn get_client_builder(
 ) -> CustomResult<reqwest::ClientBuilder, ApiClientError> {
     let mut client_builder = reqwest::Client::builder()
         .redirect(reqwest::redirect::Policy::none())
-        .pool_idle_timeout(
-            proxy_config
-                .idle_pool_connection_timeout
-                .map(std::time::Duration::from_secs),
-        );
+        .pool_idle_timeout(std::time::Duration::from_secs(
+            proxy_config.idle_pool_connection_timeout.unwrap_or(90),
+        ));
 
     if should_bypass_proxy {
         return Ok(client_builder);
