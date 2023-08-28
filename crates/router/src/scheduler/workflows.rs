2b6d3f323b8fb0daf48cea5ec8822ebdc9e15d0f
Kritik Modi <kritik.modi@juspay.in>
2023-08-28 19:40:00 +0530

    chore: update autorefunds workflow

--- crates/router/src/scheduler/workflows.rs
+++ crates/router/src/scheduler/workflows.rs
@@ -10,10 +10,10 @@ use crate::{
 };
 #[cfg(feature = "email")]
 pub mod api_key_expiry;
+pub mod create_auto_refund;
 pub mod payment_sync;
 pub mod refund_router;
 pub mod tokenized_data;
-pub mod create_auto_refund;
 
 macro_rules! runners {
     ($(#[$attr:meta] $body:tt),*) => {
--- crates/router/src/scheduler/workflows/create_auto_refund.rs
+++ crates/router/src/scheduler/workflows/create_auto_refund.rs
@@ -1,10 +1,26 @@
-use api_models::refunds::{RefundRequest, RefundType};
+use api_models::{
+    enums::EventType,
+    refunds::{self, RefundRequest, RefundType},
+    webhooks::OutgoingWebhookContent,
+};
 use common_utils::ext_traits::ValueExt;
-use diesel_models::payment_intent::PaymentIntent;
+use diesel_models::{
+    enums::{EventClass, EventObjectType},
+    payment_intent::PaymentIntent,
+};
+use error_stack::{IntoReport, ResultExt};
 
-use super::{ProcessTrackerWorkflow,AutoRefundWorkflow};
+use super::{AutoRefundWorkflow, ProcessTrackerWorkflow};
 use crate::{
-    errors, logger::error, routes::AppState, types::storage, core::{refunds::refund_create_core, errors::ProcessTrackerError},
+    core::{
+        errors::ProcessTrackerError,
+        refunds::refund_create_core,
+        webhooks::{create_event_and_trigger_outgoing_webhook, types::OutgoingWebhookType},
+    },
+    errors,
+    logger::error,
+    routes::AppState,
+    types::storage,
 };
 
 #[async_trait::async_trait]
@@ -13,33 +29,52 @@ impl ProcessTrackerWorkflow for AutoRefundWorkflow {
         &'a self,
         state: &'a AppState,
         process: storage::ProcessTracker,
-    ) -> Result<() , ProcessTrackerError> {
-            let db = &*state.store;
-            let tracking_data: PaymentIntent = process
-                .tracking_data
-                .clone()
-                .parse_value("PaymentIntent")?;
-            let key_store = state
-                .store
-                .get_merchant_key_store_by_merchant_id(
-                    tracking_data.merchant_id.as_str(),
-                    &state.store.get_master_key().to_vec().into(),
+    ) -> Result<(), ProcessTrackerError> {
+        let db = &*state.store;
+        let tracking_data: PaymentIntent =
+            process.tracking_data.clone().parse_value("PaymentIntent")?;
+        let key_store = state
+            .store
+            .get_merchant_key_store_by_merchant_id(
+                tracking_data.merchant_id.as_str(),
+                &state.store.get_master_key().to_vec().into(),
+            )
+            .await?;
+        let merchant_account = db
+            .find_merchant_account_by_merchant_id(tracking_data.merchant_id.as_str(), &key_store)
+            .await?;
+        let ref_req = RefundRequest {
+            refund_id: None,
+            payment_id: tracking_data.payment_id,
+            merchant_id: Some(tracking_data.merchant_id),
+            amount: None,
+            reason: Some("Auto Refund".to_string()),
+            refund_type: Some(RefundType::Scheduled),
+            metadata: None,
+            merchant_connector_details: None,
+        };
+        let refund_response =
+            refund_create_core(state, merchant_account.clone(), key_store, ref_req).await?;
+        match refund_response {
+            crate::services::ApplicationResponse::Json(refund_details) => {
+                create_event_and_trigger_outgoing_webhook::<OutgoingWebhookType>(
+                    *state,
+                    merchant_account,
+                    EventType::RefundSucceeded,
+                    EventClass::Refunds,
+                    None,
+                    "".to_string(),
+                    EventObjectType::RefundDetails,
+                    OutgoingWebhookContent::RefundDetails(refund_details),
                 )
                 .await?;
-            let merchant_account = db
-                .find_merchant_account_by_merchant_id(tracking_data.merchant_id.as_str(), &key_store)
-                .await?;
-            let ref_req = RefundRequest {
-                refund_id: None,
-                payment_id: tracking_data.payment_id,
-                merchant_id: Some(tracking_data.merchant_id),
-                amount: None,
-                reason: Some("Auto Refund".to_string()),
-                refund_type: Some(RefundType::Scheduled),
-                metadata: None,
-                merchant_connector_details: None,
-            };
-            let refund_response = refund_create_core(state, merchant_account.clone(), key_store, ref_req,).await;
+            }
+            _ => {
+                self.error_handler(state, process, ProcessTrackerError::DeserializationFailed)
+                    .await;
+            }
+        };
+        Ok(())
     }
 
     async fn error_handler<'a>(
