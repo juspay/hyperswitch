d804b2328274189cf5ddab9aac5bee56838618da
DEEPANSHU BANSAL <41580413+deepanshu-iiitu@users.noreply.github.com>
2023-08-29 11:54:02 +0530

    feat(connector): [HELCIM] Add template code for Helcim (#2019)

--- crates/router/src/configs/settings.rs
+++ crates/router/src/configs/settings.rs
@@ -489,6 +489,7 @@ pub struct Connectors {
     pub forte: ConnectorParams,
     pub globalpay: ConnectorParams,
     pub globepay: ConnectorParams,
+    pub helcim: ConnectorParams,
     pub iatapay: ConnectorParams,
     pub klarna: ConnectorParams,
     pub mollie: ConnectorParams,
