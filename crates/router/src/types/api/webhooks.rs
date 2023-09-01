1881739e2cc66ff2f53b7aa2be8474c1676746de
Sangamesh <sangamesh.kulkarni@juspay.in>
2023-09-01 12:07:10 +0530

    chore: small code refactor

--- crates/router/src/types/api/webhooks.rs
+++ crates/router/src/types/api/webhooks.rs
@@ -106,7 +106,7 @@ pub trait IncomingWebhook: ConnectorCommon + Sync {
             )
             .await;
 
-        let (merchant_secret, additional_secret) = match merchant_connector_account_result {
+        let connector_webhook_secrets = match merchant_connector_account_result {
             Ok(mca) => match mca.connector_webhook_details {
                 Some(merchant_connector_webhook_details) => {
                     let connector_webhook_details = merchant_connector_webhook_details
@@ -122,12 +122,18 @@ pub trait IncomingWebhook: ConnectorCommon + Sync {
                                 debug_suffix
                             )
                         })?;
-                    (
-                        connector_webhook_details.merchant_secret.expose(),
-                        connector_webhook_details.additional_secret,
-                    )
+                    api_models::webhooks::ConnectorWebhookSecrets {
+                        secret: connector_webhook_details
+                            .merchant_secret
+                            .expose()
+                            .into_bytes(),
+                        additional_secret: connector_webhook_details.additional_secret,
+                    }
                 }
-                None => (default_secret, None),
+                None => api_models::webhooks::ConnectorWebhookSecrets {
+                    secret: default_secret.into_bytes(),
+                    additional_secret: None,
+                },
             },
             Err(err) => {
                 logger::error!(
@@ -135,7 +141,10 @@ pub trait IncomingWebhook: ConnectorCommon + Sync {
                     debug_suffix
                 );
                 logger::error!("DB error = {:?}", err);
-                (default_secret, None)
+                api_models::webhooks::ConnectorWebhookSecrets {
+                    secret: default_secret.into_bytes(),
+                    additional_secret: None,
+                }
             }
         };
 
@@ -143,10 +152,7 @@ pub trait IncomingWebhook: ConnectorCommon + Sync {
 
         //If merchant has not set the secret for webhook source verification, "default_secret" is returned.
         //So it will fail during verification step and goes to psync flow.
-        Ok(api_models::webhooks::ConnectorWebhookSecrets {
-            secret: merchant_secret.into_bytes(),
-            additional_secret,
-        })
+        Ok(connector_webhook_secrets)
     }
 
     fn get_webhook_source_verification_signature(
