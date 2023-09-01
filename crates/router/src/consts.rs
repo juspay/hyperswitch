ba901cf01ea62dcad7e3ca01c57f0c973d76e966
Shankar Singh C <shankar.singh@juspay.in>
2023-09-01 14:57:33 +0530

    address pr comments

--- crates/router/src/consts.rs
+++ crates/router/src/consts.rs
@@ -42,6 +42,6 @@ pub(crate) const QR_IMAGE_DATA_SOURCE_STRING: &str = "data:image/png;base64";
 
 // OID (Object Identifier) for the merchant ID field extension.
 #[cfg(feature = "kms")]
-pub(crate) const MERCHANT_ID_FIELD_OID: &str = "1.2.840.113635.100.6.32";
+pub(crate) const MERCHANT_ID_FIELD_EXTENSION_ID: &str = "1.2.840.113635.100.6.32";
 
 pub(crate) const APPLE_PAY_MERCHANT_ID: &str = "merchant.com.noon.juspay";
--- crates/router/src/core/payments/helpers.rs
+++ crates/router/src/core/payments/helpers.rs
@@ -3072,7 +3072,7 @@ impl ApplePayData {
         let apple_pay_m_id = certificate
             .extensions()
             .iter()
-            .find(|extension| extension.oid.to_string().eq(consts::MERCHANT_ID_FIELD_OID))
+            .find(|extension| extension.oid.to_string().eq(consts::MERCHANT_ID_FIELD_EXTENSION_ID))
             .map(|ext| {
                 let merchant_id = String::from_utf8_lossy(ext.value)
                     .trim()
