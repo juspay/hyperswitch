81a98bd13078d5a07cae2e274cfd62cb320d1704
Shankar Singh C <shankar.singh@juspay.in>
2023-09-05 11:56:08 +0530

    address failing checks

--- crates/router/src/core/payments/flows/approve_flow.rs
+++ crates/router/src/core/payments/flows/approve_flow.rs
@@ -4,7 +4,7 @@ use super::{ConstructFlowSpecificData, Feature};
 use crate::{
     core::{
         errors::{api_error_response::NotImplementedMessage, ApiErrorResponse, RouterResult},
-        payments::{self, access_token, transformers, PaymentData},
+        payments::{self, access_token, transformers, PaymentData, helpers},
     },
     routes::AppState,
     services,
@@ -23,6 +23,7 @@ impl
         merchant_account: &domain::MerchantAccount,
         key_store: &domain::MerchantKeyStore,
         customer: &Option<domain::Customer>,
+        merchant_connector_account: &helpers::MerchantConnectorAccountType,
     ) -> RouterResult<types::PaymentsApproveRouterData> {
         transformers::construct_payment_router_data::<api::Approve, types::PaymentsApproveData>(
             state,
@@ -31,6 +32,7 @@ impl
             merchant_account,
             key_store,
             customer,
+            merchant_connector_account,
         )
         .await
     }
