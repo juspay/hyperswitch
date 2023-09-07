604471de83425c57f24f1e890b9a58c94f9dc411
AkshayaFoiger <akshaya.shankar@juspay.in>
2023-09-07 23:15:37 +0530

    resolve merge conflict

--- crates/router/src/connector/square.rs
+++ crates/router/src/connector/square.rs
@@ -822,6 +822,7 @@ impl api::IncomingWebhook for Square {
     fn get_webhook_source_verification_signature(
         &self,
         request: &api::IncomingWebhookRequestDetails<'_>,
+        _connector_webhook_secrets: &api_models::webhooks::ConnectorWebhookSecrets,
     ) -> CustomResult<Vec<u8>, errors::ConnectorError> {
         let encoded_signature =
             super_utils::get_header_key_value("x-square-hmacsha256-signature", request.headers)?;
@@ -836,7 +837,7 @@ impl api::IncomingWebhook for Square {
         &self,
         request: &api::IncomingWebhookRequestDetails<'_>,
         _merchant_id: &str,
-        _secret: &[u8],
+        _connector_webhook_secrets: &api_models::webhooks::ConnectorWebhookSecrets,
     ) -> CustomResult<Vec<u8>, errors::ConnectorError> {
         Ok(format!("{}{}", request.uri, String::from_utf8_lossy(request.body)).into_bytes())
     }
