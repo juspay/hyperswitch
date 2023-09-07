e293f96884423f1e9b273b4d4969f363e3a19aa2
Hudson C Dalpra <dalpra.hcd@gmail.com>
2023-09-07 22:01:56 +1200

    feat(db): implement BusinessProfileInterface for MockDb
    Implement the missing implementation of MockDb for business profiles.
    
    Closes #2039

--- crates/diesel_models/src/business_profile.rs
+++ crates/diesel_models/src/business_profile.rs
@@ -70,3 +70,28 @@ pub struct BusinessProfileUpdateInternal {
     pub payout_routing_algorithm: Option<serde_json::Value>,
     pub is_recon_enabled: Option<bool>,
 }
+
+impl BusinessProfileUpdateInternal {
+    pub fn apply_changeset(self, source: BusinessProfile) -> BusinessProfile {
+        BusinessProfile {
+            profile_name: self.profile_name.unwrap_or(source.profile_name),
+            modified_at: self.modified_at.unwrap_or(source.modified_at),
+            return_url: self.return_url,
+            enable_payment_response_hash: self
+                .enable_payment_response_hash
+                .unwrap_or(source.enable_payment_response_hash),
+            payment_response_hash_key: self.payment_response_hash_key,
+            redirect_to_merchant_with_http_post: self
+                .redirect_to_merchant_with_http_post
+                .unwrap_or(source.redirect_to_merchant_with_http_post),
+            webhook_details: self.webhook_details,
+            metadata: self.metadata,
+            routing_algorithm: self.routing_algorithm,
+            intent_fulfillment_time: self.intent_fulfillment_time,
+            frm_routing_algorithm: self.frm_routing_algorithm,
+            payout_routing_algorithm: self.payout_routing_algorithm,
+            is_recon_enabled: self.is_recon_enabled.unwrap_or(source.is_recon_enabled),
+            ..source
+        }
+    }
+}
--- crates/router/src/db/business_profile.rs
+++ crates/router/src/db/business_profile.rs
@@ -114,38 +114,118 @@ impl BusinessProfileInterface for Store {
 impl BusinessProfileInterface for MockDb {
     async fn insert_business_profile(
         &self,
-        _business_profile: business_profile::BusinessProfileNew,
+        business_profile: business_profile::BusinessProfileNew,
     ) -> CustomResult<business_profile::BusinessProfile, errors::StorageError> {
-        Err(errors::StorageError::MockDbError)?
+        let mut business_profiles = self.business_profiles.lock().await;
+
+        let business_profile_insert = business_profile::BusinessProfile {
+            profile_id: business_profile.profile_id,
+            merchant_id: business_profile.merchant_id,
+            profile_name: business_profile.profile_name,
+            created_at: business_profile.created_at,
+            modified_at: business_profile.modified_at,
+            return_url: business_profile.return_url,
+            enable_payment_response_hash: business_profile.enable_payment_response_hash,
+            payment_response_hash_key: business_profile.payment_response_hash_key,
+            redirect_to_merchant_with_http_post: business_profile
+                .redirect_to_merchant_with_http_post,
+            webhook_details: business_profile.webhook_details,
+            metadata: business_profile.metadata,
+            routing_algorithm: business_profile.routing_algorithm,
+            intent_fulfillment_time: business_profile.intent_fulfillment_time,
+            frm_routing_algorithm: business_profile.frm_routing_algorithm,
+            payout_routing_algorithm: business_profile.payout_routing_algorithm,
+            is_recon_enabled: business_profile.is_recon_enabled,
+        };
+
+        business_profiles.push(business_profile_insert.clone());
+
+        Ok(business_profile_insert)
     }
 
     async fn find_business_profile_by_profile_id(
         &self,
-        _profile_id: &str,
+        profile_id: &str,
     ) -> CustomResult<business_profile::BusinessProfile, errors::StorageError> {
-        Err(errors::StorageError::MockDbError)?
+        match self
+            .business_profiles
+            .lock()
+            .await
+            .iter()
+            .find(|business_profile| business_profile.profile_id == profile_id)
+        {
+            Some(business_profile) => Ok(business_profile.clone()),
+            None => {
+                return Err(errors::StorageError::ValueNotFound(
+                    "business profile not found".to_string(),
+                )
+                .into())
+            }
+        }
     }
 
     async fn update_business_profile_by_profile_id(
         &self,
-        _current_state: business_profile::BusinessProfile,
-        _business_profile_update: business_profile::BusinessProfileUpdateInternal,
+        current_state: business_profile::BusinessProfile,
+        business_profile_update: business_profile::BusinessProfileUpdateInternal,
     ) -> CustomResult<business_profile::BusinessProfile, errors::StorageError> {
-        Err(errors::StorageError::MockDbError)?
+        match self
+            .business_profiles
+            .lock()
+            .await
+            .iter_mut()
+            .find(|bp| bp.profile_id == current_state.profile_id)
+            .map(|bp| {
+                let business_profile_updated =
+                    business_profile_update.apply_changeset(current_state);
+                *bp = business_profile_updated.clone();
+                business_profile_updated
+            }) {
+            Some(business_profile) => Ok(business_profile),
+            None => {
+                return Err(errors::StorageError::ValueNotFound(
+                    "cannot find business profile to update".to_string(),
+                )
+                .into())
+            }
+        }
     }
 
     async fn delete_business_profile_by_profile_id_merchant_id(
         &self,
-        _profile_id: &str,
-        _merchant_id: &str,
+        profile_id: &str,
+        merchant_id: &str,
     ) -> CustomResult<bool, errors::StorageError> {
-        Err(errors::StorageError::MockDbError)?
+        let mut business_profiles = self.business_profiles.lock().await;
+
+        match business_profiles
+            .iter()
+            .position(|bp| bp.profile_id == profile_id && bp.merchant_id == merchant_id)
+        {
+            Some(index) => {
+                _ = business_profiles.remove(index);
+                Ok(true)
+            }
+            None => Err(errors::StorageError::ValueNotFound(
+                "cannot find business profile to delete".to_string(),
+            )
+            .into()),
+        }
     }
 
     async fn list_business_profile_by_merchant_id(
         &self,
-        _merchant_id: &str,
+        merchant_id: &str,
     ) -> CustomResult<Vec<business_profile::BusinessProfile>, errors::StorageError> {
-        Err(errors::StorageError::MockDbError)?
+        let business_profile_by_merchant_id = self
+            .business_profiles
+            .lock()
+            .await
+            .iter()
+            .filter(|business_profile| business_profile.merchant_id == merchant_id)
+            .cloned()
+            .collect();
+
+        Ok(business_profile_by_merchant_id)
     }
 }
