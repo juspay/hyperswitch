3949298550d93356c46f858e0eb6a544008d990c
prajjwalkumar17 <prajjwal.kumar@juspay.in>
2023-09-08 17:52:39 +0530

    refactor(router): formatting

--- crates/api_models/src/admin.rs
+++ crates/api_models/src/admin.rs
@@ -1076,7 +1076,7 @@ pub struct BusinessProfileResponse {
         deserialize_with = "payout_routing_algorithm::deserialize_option"
     )]
     pub payout_routing_algorithm: Option<serde_json::Value>,
-    
+
     /// Verified applepay domains for a particular profile
     pub applepay_verified_domains: Option<Vec<String>>,
 }
@@ -1133,7 +1133,7 @@ pub struct BusinessProfileUpdate {
         deserialize_with = "payout_routing_algorithm::deserialize_option"
     )]
     pub payout_routing_algorithm: Option<serde_json::Value>,
-    
+
     /// Verified applepay domains for a particular profile
     pub applepay_verified_domains: Option<Vec<String>>,
 }
