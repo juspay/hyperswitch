74f00643dda2d7d7f264f1341fc3631097388d47
jeeva <jeevaprakashdr@gmail.com>
2023-09-08 17:36:25 +0100

    implemented thiserror

--- crates/router/src/core/errors/customers_error_response.rs
+++ crates/router/src/core/errors/customers_error_response.rs
@@ -7,30 +7,19 @@ pub enum CustomersErrorType {
     InternalServerError,
 }
 
-#[derive(Debug, Clone, router_derive::ApiError)]
-#[error(error_type_enum = CustomersErrorType)]
+#[derive(Debug, thiserror::Error)]
 pub enum CustomersErrorResponse {
-    #[error(error_type = CustomersErrorType::InvalidRequestError, code = "CE_01", message = "Customer has already been redacted")]
+    #[error("Customer has already been redacted")]
     CustomerRedacted,
 
-    #[error(error_type = CustomersErrorType::InternalServerError, code = "CE_02", message = "Something went wrong")]
+    #[error("Something went wrong")]
     InternalServerError,
 
-    #[error(error_type = CustomersErrorType::InvalidRequestError, code = "CE_03", message = "Customer has already been redacted")]
+    #[error("Customer has already been redacted")]
     MandateActive,
 
-    #[error(error_type = CustomersErrorType::ObjectNotFound, code = "CE_04", message = "Customer does not exist in our records")]
+    #[error("Customer does not exist in our records")]
     CustomerNotFound,
-}
-
-impl std::fmt::Display for CustomersErrorResponse {
-    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            fmt,
-            r#"{{"error":{}}}"#,
-            serde_json::to_string(self).unwrap_or_else(|_| "API error response".to_string())
-        )
-    }
 }
 
 impl actix_web::ResponseError for CustomersErrorResponse {
