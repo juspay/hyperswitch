bc1a71f93834df13f8eab7473f86985a920b57ab
Narayan Bhat <narayan.bhat@juspay.in>
2023-09-09 22:14:55 +0530

    fix: merge fixes

--- crates/diesel_models/src/query/merchant_connector_account.rs
+++ crates/diesel_models/src/query/merchant_connector_account.rs
@@ -55,20 +55,20 @@ impl MerchantConnectorAccount {
         .await
     }
 
-    #[instrument(skip(conn))]
-    pub async fn find_by_merchant_id_connector(
-        conn: &PgPooledConn,
-        merchant_id: &str,
-        connector_label: &str,
-    ) -> StorageResult<Self> {
-        generics::generic_find_one::<<Self as HasTable>::Table, _, _>(
-            conn,
-            dsl::merchant_id
-                .eq(merchant_id.to_owned())
-                .and(dsl::connector_label.eq(connector_label.to_owned())),
-        )
-        .await
-    }
+    // #[instrument(skip(conn))]
+    // pub async fn find_by_merchant_id_connector(
+    //     conn: &PgPooledConn,
+    //     merchant_id: &str,
+    //     connector_label: &str,
+    // ) -> StorageResult<Self> {
+    //     generics::generic_find_one::<<Self as HasTable>::Table, _, _>(
+    //         conn,
+    //         dsl::merchant_id
+    //             .eq(merchant_id.to_owned())
+    //             .and(dsl::connector_label.eq(connector_label.to_owned())),
+    //     )
+    //     .await
+    // }
 
     #[instrument(skip(conn))]
     pub async fn find_by_profile_id_connector_name(
