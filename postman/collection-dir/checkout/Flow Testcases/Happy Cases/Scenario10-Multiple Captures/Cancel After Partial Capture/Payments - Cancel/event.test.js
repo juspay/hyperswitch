54a406c98c2957ef783bd9a35c61a50ba36719b0
pixincreate@work <69745008+pixincreate@users.noreply.github.com>
2023-09-13 21:06:56 +0530

    Add validation for amount capturable

--- postman/collection-dir/checkout/Flow Testcases/Happy Cases/Scenario10-Multiple Captures/Cancel After Partial Capture/Payments - Cancel/event.test.js
+++ postman/collection-dir/checkout/Flow Testcases/Happy Cases/Scenario10-Multiple Captures/Cancel After Partial Capture/Payments - Cancel/event.test.js
@@ -59,3 +59,10 @@ if (jsonData?.status) {
     },
   );
 }
+
+// Response body should have value "0" for "amount_capturable"
+if (jsonData?.amount){
+  pm.test("[post]:://payments/:id/capture - Content check if value for 'amount_capturable' matches 'amount - 0'", function() {
+  pm.expect(jsonData.amount_capturable).to.eql(0);
+  } )
+} 
