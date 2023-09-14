13663838edbb716131f76f9f7ac4b590c69b1f75
Prasunna Soppa <prasunna.soppa@juspay.in>
2023-09-14 13:22:06 +0530

    refactor psync for cryptopay

--- crates/router/src/connector/cryptopay.rs
+++ crates/router/src/connector/cryptopay.rs
@@ -285,16 +285,11 @@ impl ConnectorIntegration<api::PSync, types::PaymentsSyncData, types::PaymentsRe
         req: &types::PaymentsSyncRouterData,
         connectors: &settings::Connectors,
     ) -> CustomResult<String, errors::ConnectorError> {
-        let connector_id = req
-            .request
-            .connector_transaction_id
-            .get_connector_transaction_id()
-            .change_context(errors::ConnectorError::MissingConnectorTransactionID)?;
-
+        let custom_id = req.connector_request_reference_id.clone();
         Ok(format!(
-            "{}/api/invoices/{}",
+            "{}/api/invoices/custom_id/{}",
             self.base_url(connectors),
-            connector_id
+            custom_id
         ))
     }
 
