4d8b36b37cb414de35de79a1baaa5c3f242be7c7
prajjwalkumar17 <prajjwal.kumar@juspay.in>
2023-09-14 11:14:27 +0530

    refactor(router): added get route for apple_pay_verification

--- crates/api_models/src/admin.rs
+++ crates/api_models/src/admin.rs
@@ -633,8 +633,6 @@ pub struct MerchantConnectorCreate {
     pub connector_webhook_details: Option<MerchantConnectorWebhookDetails>,
     /// Identifier for the business profile, if not provided default will be chosen from merchant account
     pub profile_id: Option<String>,
-    /// identifier for the verified domains of a particular connector account
-    pub applepay_verified_domains: Option<Vec<String>>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
@@ -808,8 +806,6 @@ pub struct MerchantConnectorUpdate {
     }))]
     pub connector_webhook_details: Option<MerchantConnectorWebhookDetails>,
 
-    /// Verified applepay domains for a particular profile
-    pub applepay_verified_domains: Option<Vec<String>>,
 }
 
 ///Details of FrmConfigs are mentioned here... it should be passed in payment connector create api call, and stored in merchant_connector_table
@@ -1019,8 +1015,6 @@ pub struct BusinessProfileCreate {
     )]
     pub payout_routing_algorithm: Option<serde_json::Value>,
 
-    /// Verified applepay domains for a particular profile
-    pub applepay_verified_domains: Option<Vec<String>>,
 }
 
 #[derive(Clone, Debug, ToSchema, Serialize)]
@@ -1140,6 +1134,4 @@ pub struct BusinessProfileUpdate {
     )]
     pub payout_routing_algorithm: Option<serde_json::Value>,
 
-    /// Verified applepay domains for a particular profile
-    pub applepay_verified_domains: Option<Vec<String>>,
 }
