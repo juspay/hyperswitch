51fd53999db61c757756ba6f41daeb01c5ed276f
Sarthak Soni <sarthak.soni@juspay.in>
2023-09-16 16:20:11 +0530

    fix(mca): Updated mca query

--- migrations/2023-09-12-132509_add_pm_auth_config_mca/up.sql
+++ migrations/2023-09-12-132509_add_pm_auth_config_mca/up.sql
@@ -1,2 +1,3 @@
 -- Your SQL goes here
 ALTER TABLE merchant_connector_account ADD COLUMN IF NOT EXISTS pm_auth_config JSONB DEFAULT NULL;
+ALTER TYPE "ConnectorType" ADD VALUE IF NOT EXISTS "payment_method_auth";
