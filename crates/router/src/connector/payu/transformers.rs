c4cdf97355138be06bf359c5c26577b3327f34a1
Harshal <37841724+harshalkh@users.noreply.github.com>
2023-10-05 21:26:45 +0530

    feat(connector): [PayU] Use connector_response_response_id as reference

--- crates/router/src/connector/payu/transformers.rs
+++ crates/router/src/connector/payu/transformers.rs
@@ -199,7 +199,10 @@ impl<F, T>
                 mandate_reference: None,
                 connector_metadata: None,
                 network_txn_id: None,
-                connector_response_reference_id: item.response.ext_order_id.or(Some(item.response.order_id)),
+                connector_response_reference_id: item
+                     .response
+                     .ext_order_id
+                     .or(Some(item.response.order_id)),
             }),
             amount_captured: None,
             ..item.data
