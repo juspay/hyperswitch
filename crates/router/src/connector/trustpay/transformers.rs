210ae65954ccd7941a503e8eb368c086ec3a6bed
Cioraz <123grover222@gmail.com>
2023-10-07 16:22:13 +0530

    Updated

--- crates/router/src/connector/trustpay/transformers.rs
+++ crates/router/src/connector/trustpay/transformers.rs
@@ -445,9 +445,10 @@ impl TryFrom<&TrustpayRouterData<&types::PaymentsAuthorizeRouterData>> for Trust
             | api::PaymentMethodData::Reward
             | api::PaymentMethodData::Upi(_)
             | api::PaymentMethodData::Voucher(_)
-            | api::PaymentMethodData::GiftCard(_) => {
-                Err(errors::ConnectorError::NotImplemented("Payment methods".to_string()).into())
-            }
+            | api::PaymentMethodData::GiftCard(_) => Err(errors::ConnectorError::NotImplemented(
+                utils::get_unimplemented_payment_method_error_message("trustpay"),
+            )
+            .into()),
         }
     }
 }
