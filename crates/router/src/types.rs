352cd5916ae161eb2994001e11b852235d10a2fb
Samraat Bansal <samraat.bansal@juspay.in>
2023-10-11 19:32:35 +0530

    chore: link issue to deperecate

--- crates/router/src/types.rs
+++ crates/router/src/types.rs
@@ -477,6 +477,7 @@ pub struct PaymentsSyncData {
     pub sync_type: SyncRequestType,
     pub mandate_id: Option<api_models::payments::MandateIds>,
     //This is being added as a temporary fix, will be deprecated before or by v1.65.0
+    // #2628
     pub payment_attempt_created_at_as_utc: i64,
 }
 
