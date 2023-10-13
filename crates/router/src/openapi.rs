6a8f52a485225de41fd3cd282b53720bb2d343b0
Sahkal Poddar <sahkal.poddar@juspay.in>
2023-10-13 20:58:05 +0530

    fixed open api check errors

--- crates/router/src/openapi.rs
+++ crates/router/src/openapi.rs
@@ -180,6 +180,7 @@ Never share your secret api keys. Keep them guarded and secure.
         api_models::admin::MerchantConnectorDetailsWrap,
         api_models::admin::MerchantConnectorDetails,
         api_models::admin::MerchantConnectorWebhookDetails,
+        api_models::admin::PaymentLinkMetadata,
         api_models::disputes::DisputeResponse,
         api_models::disputes::DisputeResponsePaymentsRetrieve,
         api_models::payments::AddressDetails,
