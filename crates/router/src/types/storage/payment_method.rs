d222453cebaef0da146e30469dedde7086c84a6d
shashank_attarde <shashank.attarde@juspay.in>
2023-10-17 17:38:21 +0530

    refactor(router): make payment token data enum internally tagged and rename all variants to snake case

--- crates/router/src/types/storage/payment_method.rs
+++ crates/router/src/types/storage/payment_method.rs
@@ -22,6 +22,7 @@ pub struct GenericTokenData {
 }
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
+#[serde(tag = "kind", rename_all = "snake_case")]
 pub enum PaymentTokenData {
     // The variants 'Temporary' and 'Permanent' are added for backwards compatibility
     // with any tokenized data present in Redis at the time of deployment of this change
