444b809019db81962875b611ff411570899416a8
Sampras lopes <lsampras@pm.me>
2023-10-18 14:42:44 +0530

    feat(events): add swappable logger implementations

--- crates/router/src/events.rs
+++ crates/router/src/events.rs
@@ -3,7 +3,15 @@ use serde::Serialize;
 pub mod event_logger;
 
 pub trait EventHandler: Sync + Send + dyn_clone::DynClone {
-    fn log_event<T: Event>(&self, event: T, previous: Option<T>);
+    fn log_event(&self, event: RawEvent, previous: Option<RawEvent>);
+}
+
+dyn_clone::clone_trait_object!(EventHandler);
+
+pub struct RawEvent {
+    event_type: EventType,
+    key: String,
+    payload: serde_json::Value,
 }
 
 #[derive(Debug, Serialize)]
