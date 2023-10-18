2edbd6123512a6f2f4d51d5c2d1ed8b6ee502813
Abhishek Marrivagu <68317979+Abhicodes-crypto@users.noreply.github.com>
2023-10-18 19:04:26 +0530

    feat(core): replace temp locker with redis (#2594)

--- crates/router/src/routes/app.rs
+++ crates/router/src/routes/app.rs
@@ -128,6 +128,11 @@ impl AppState {
         #[allow(clippy::expect_used)]
         let kms_secrets = settings::ActiveKmsSecrets {
             jwekey: conf.jwekey.clone().into(),
+            redis_temp_locker_encryption_key: conf
+                .locker
+                .redis_temp_locker_encryption_key
+                .clone()
+                .into(),
         }
         .decrypt_inner(kms_client)
         .await
@@ -135,6 +140,7 @@ impl AppState {
 
         #[cfg(feature = "email")]
         let email_client = Arc::new(AwsSes::new(&conf.email).await);
+
         Self {
             flow_name: String::from("default"),
             store,
