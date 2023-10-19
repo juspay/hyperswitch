b338e3df4528511a62b883a22c6f0ed679aa5884
Abhishek Marrivagu <abhi.codes10@gmail.com>
2023-10-19 19:20:39 +0530

    fix: down.sql

--- migrations/2023-10-19-102636_back_fill_n_remove_connector_response/down.sql
+++ migrations/2023-10-19-102636_back_fill_n_remove_connector_response/down.sql
@@ -1,6 +1,4 @@
 -- This file should undo anything in `up.sql`
-
-
 CREATE TABLE connector_response (
     id SERIAL PRIMARY KEY,
     payment_id VARCHAR(255) NOT NULL,
@@ -17,7 +15,7 @@ CREATE TABLE connector_response (
 CREATE UNIQUE INDEX connector_response_id_index ON connector_response (payment_id, merchant_id, txn_id);
 
 ALTER TABLE connector_response ALTER COLUMN connector_name DROP NOT NULL;
-
+ALTER TABLE connector_response RENAME COLUMN txn_id TO attempt_id;
 ALTER TABLE connector_response
     ALTER COLUMN payment_id TYPE VARCHAR(64),
     ALTER COLUMN merchant_id TYPE VARCHAR(64),
@@ -25,7 +23,7 @@ ALTER TABLE connector_response
     ALTER COLUMN connector_name TYPE VARCHAR(64),
     ALTER COLUMN connector_transaction_id TYPE VARCHAR(128);
 
-ALTER TABLE connector_response RENAME COLUMN txn_id TO attempt_id;
+
 
 ALTER TABLE connector_response
 ALTER COLUMN modified_at DROP DEFAULT;
