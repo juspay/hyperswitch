115e3f4550e993e20c564a331453e547e695982d
tanbirali <tanbirali2004@gmail.com>
2023-10-24 16:08:52 +0530

    refactor(connector): BankRedirectData default case handled individually

--- crates/router/src/connector/aci/transformers.rs
+++ crates/router/src/connector/aci/transformers.rs
@@ -252,7 +252,6 @@ impl
             | api_models::payments::BankRedirectData::OpenBankingUk { .. }
             | api_models::payments::BankRedirectData::Przelewy24 { .. }
             | api_models::payments::BankRedirectData::Sofort { .. }
-            | api_models::payments::BankRedirectData::Trustly { .. }
             | api_models::payments::BankRedirectData::BancontactCard { .. }
             | api_models::payments::BankRedirectData::Trustly { .. } => Err(
                 errors::ConnectorError::NotImplemented("Payment method".to_string()),
