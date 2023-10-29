ea6ccfb16386283ffa2b4b7c96d4528461f9ae7f
Saksham Misra <122719607+vinfinity7@users.noreply.github.com>
2023-10-30 00:37:46 +0530

    Update transformers.rs

--- crates/router/src/connector/rapyd/transformers.rs
+++ crates/router/src/connector/rapyd/transformers.rs
@@ -53,6 +53,7 @@ pub struct RapydPaymentsRequest {
     pub description: Option<String>,
     pub complete_payment_url: Option<String>,
     pub error_payment_url: Option<String>,
+    pub merchant_order_id: Option<String>,
 }
 
 #[derive(Default, Debug, Serialize)]
@@ -173,6 +174,7 @@ impl TryFrom<&RapydRouterData<&types::PaymentsAuthorizeRouterData>> for RapydPay
             description: None,
             error_payment_url: Some(return_url.clone()),
             complete_payment_url: Some(return_url),
+            merchant_order_id: item.connector_request_reference_id.clone(),
         })
     }
 }
