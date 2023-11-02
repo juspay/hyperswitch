1b1d917cd35b7fa42c06d0f20b6af256f845fb04
Harshwardhan9431 <wardhanharsh732@gmail.com>
2023-11-02 19:02:09 +0530

    feat(connector): Made some changes

--- crates/router/src/connector/aci/transformers.rs
+++ crates/router/src/connector/aci/transformers.rs
@@ -554,7 +554,7 @@ fn get_transaction_details(
     let auth = AciAuthType::try_from(&item.router_data.connector_auth_type)?;
     Ok(TransactionDetails {
         entity_id: auth.entity_id,
-        amount: item.amount.to_string(),
+        amount: item.amount.to_owned(),
         currency: item.router_data.request.currency.to_string(),
         payment_type: AciPaymentType::Debit,
     })
@@ -750,7 +750,7 @@ pub struct AciRefundRequest {
 impl<F> TryFrom<&AciRouterData<&types::RefundsRouterData<F>>> for AciRefundRequest {
     type Error = error_stack::Report<errors::ConnectorError>;
     fn try_from(item: &AciRouterData<&types::RefundsRouterData<F>>) -> Result<Self, Self::Error> {
-        let amount = item.amount.to_string();
+        let amount = item.amount.to_owned();
         let currency = item.router_data.request.currency;
         let payment_type = AciPaymentType::Refund;
         let auth = AciAuthType::try_from(&item.router_data.connector_auth_type)?;
