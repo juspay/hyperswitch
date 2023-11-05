3a54ff3b928ed50872bcbb15e0a73faea35579e8
pixincreate@work <69745008+pixincreate@users.noreply.github.com>
2023-11-05 17:48:31 +0530

    Simplify it again

--- .github/workflows/test.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-if git diff --quiet && git diff --staged --quiet; then
-            echo "No changes detected."
-          else 
-            if git add postman && ! git diff --staged --quiet postman; then
-              git commit -am 'test(postman): update postman collection files'
-              echo "Changes detected and commited."
-          fi
