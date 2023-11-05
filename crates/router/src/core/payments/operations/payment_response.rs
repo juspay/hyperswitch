370b81c8606aaf9a60bb0825a37ae36c469957c9
akshay.s <akshay.s@juspay.in>
2023-11-05 22:14:57 +0530

    #3059 group connector_response, payment_attempt update calls with payment_intent

--- crates/router/src/core/payments/operations/payment_response.rs
+++ crates/router/src/core/payments/operations/payment_response.rs
@@ -29,6 +29,8 @@ use crate::{
     },
     utils,
 };
+use storage_impl::DataModelExt;
+use data_models::payments::payment_attempt::PaymentAttempt;
 
 #[derive(Debug, Clone, Copy, router_derive::PaymentOperation)]
 #[operation(
@@ -589,6 +591,12 @@ async fn payment_response_update_tracker<F: Clone, T: types::Capturable>(
     let m_db = state.clone().store;
     let m_payment_attempt_update = payment_attempt_update.clone();
     let m_payment_attempt = payment_attempt.clone();
+
+    let payment_attempt = match payment_attempt_update{
+        Some(payment_attempt_update) => PaymentAttempt::from_storage_model(payment_attempt_update.to_storage_model().apply_changeset(payment_attempt.clone().to_storage_model())),
+        None => payment_attempt,
+    };
+
     let payment_attempt_fut = tokio::spawn(
         async move {
             Box::pin(async move {
@@ -602,7 +610,7 @@ async fn payment_response_update_tracker<F: Clone, T: types::Capturable>(
                             )
                             .await
                             .to_not_found_response(errors::ApiErrorResponse::PaymentNotFound)?,
-                        None => payment_attempt,
+                        None => m_payment_attempt,
                     },
                 )
             })
@@ -614,6 +622,12 @@ async fn payment_response_update_tracker<F: Clone, T: types::Capturable>(
     let m_db = state.clone().store;
     let m_connector_response_update = connector_response_update.clone();
     let m_connector_response = connector_response.clone();
+
+    let connector_response = match connector_response_update{
+        Some(connector_response_update) => connector_response_update.apply_changeset(connector_response.clone()),
+        None => connector_response,
+    };
+    
     let connector_response_fut = tokio::spawn(
         async move {
             Box::pin(async move {
@@ -627,7 +641,7 @@ async fn payment_response_update_tracker<F: Clone, T: types::Capturable>(
                             )
                             .await
                             .to_not_found_response(errors::ApiErrorResponse::PaymentNotFound)?,
-                        None => connector_response,
+                        None => m_connector_response,
                     },
                 )
             })
@@ -636,10 +650,6 @@ async fn payment_response_update_tracker<F: Clone, T: types::Capturable>(
         .in_current_span(),
     );
 
-    let (payment_attempt, connector_response) = futures::try_join!(
-        utils::flatten_join_error(payment_attempt_fut),
-        utils::flatten_join_error(connector_response_fut)
-    )?;
     payment_data.payment_attempt = payment_attempt;
     payment_data.connector_response = connector_response;
 
@@ -701,12 +711,14 @@ async fn payment_response_update_tracker<F: Clone, T: types::Capturable>(
         .in_current_span(),
     );
 
-    let (payment_intent, _) = futures::try_join!(
+    let (payment_intent, _, _, _) = futures::try_join!(
         utils::flatten_join_error(payment_intent_fut),
-        utils::flatten_join_error(mandate_update_fut)
+        utils::flatten_join_error(mandate_update_fut),
+        utils::flatten_join_error(payment_attempt_fut),
+        utils::flatten_join_error(connector_response_fut)
     )?;
-    payment_data.payment_intent = payment_intent;
 
+    payment_data.payment_intent = payment_intent;
     Ok(payment_data)
 }
 
