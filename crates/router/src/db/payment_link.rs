eec8f33bb5faf9e858104ed5fc108c0cca7a4eb2
Sahkal Poddar <sahkal.poddar@juspay.in>
2023-11-08 13:32:34 +0530

    refactor(router): fixed spell check issue

--- crates/router/src/db/payment_link.rs
+++ crates/router/src/db/payment_link.rs
@@ -57,10 +57,10 @@ impl PaymentLinkInterface for Store {
     async fn find_payment_link_by_merchant_id(
         &self,
         merchant_id: &str,
-        payment_link_contraints: api_models::payments::PaymentLinkListConstraints,
+        payment_link_constraints: api_models::payments::PaymentLinkListConstraints,
     ) -> CustomResult<Vec<storage::PaymentLink>, errors::StorageError> {
         let conn = connection::pg_connection_read(self).await?;
-        storage::PaymentLink::filter_by_constraints(&conn, merchant_id, payment_link_contraints)
+        storage::PaymentLink::filter_by_constraints(&conn, merchant_id, payment_link_constraints)
             .await
             .map_err(Into::into)
             .into_report()
