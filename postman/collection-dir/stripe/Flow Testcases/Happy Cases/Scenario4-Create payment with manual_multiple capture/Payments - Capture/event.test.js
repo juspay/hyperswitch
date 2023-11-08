6f535e3fbf6421b3c73d1956fde680df0088b97c
hrithikeshvm <hrithikeshmylatty@gmail.com>
2023-11-08 13:16:43 +0530

    add postman test cases

--- postman/collection-dir/stripe/Flow Testcases/Happy Cases/Scenario4-Create payment with manual_multiple capture/Payments - Capture/event.test.js
+++ postman/collection-dir/stripe/Flow Testcases/Happy Cases/Scenario4-Create payment with manual_multiple capture/Payments - Capture/event.test.js
@@ -1,17 +1,20 @@
 // Validate status 2xx
-pm.test("[GET]::/payments/:id - Status code is 2xx", function () {
+pm.test("[POST]::/payments/:id/capture - Status code is 2xx", function () {
   pm.response.to.be.success;
 });
 
 // Validate if response header has matching content-type
-pm.test("[GET]::/payments/:id - Content-Type is application/json", function () {
-  pm.expect(pm.response.headers.get("Content-Type")).to.include(
-    "application/json",
-  );
-});
+pm.test(
+  "[POST]::/payments/:id/capture - Content-Type is application/json",
+  function () {
+    pm.expect(pm.response.headers.get("Content-Type")).to.include(
+      "application/json",
+    );
+  },
+);
 
 // Validate if response has JSON Body
-pm.test("[GET]::/payments/:id - Response has JSON Body", function () {
+pm.test("[POST]::/payments/:id/capture - Response has JSON Body", function () {
   pm.response.to.have.jsonBody();
 });
 
@@ -60,20 +63,15 @@ if (jsonData?.client_secret) {
   );
 }
 
-// Response body should have value "Succeeded" for "status"
+// Response body should have value "succeeded" for "status"
 if (jsonData?.status) {
   pm.test(
-    "[POST]::/payments/:id - Content check if value for 'status' matches 'succeeded'",
+    "[POST]:://payments/:id/capture - Content check if value for 'status' matches 'partially_captured'",
     function () {
-      pm.expect(jsonData.status).to.eql("succeeded");
+      pm.expect(jsonData.status).to.eql("partially_captured");
     },
   );
 }
-
-// Validate the connector
-pm.test("[POST]::/payments - connector", function () {
-  pm.expect(jsonData.connector).to.eql("stripe");
-});
 
 // Response body should have value "6540" for "amount"
 if (jsonData?.amount) {
@@ -88,19 +86,9 @@ if (jsonData?.amount) {
 // Response body should have value "6000" for "amount_received"
 if (jsonData?.amount_received) {
   pm.test(
-    "[POST]::/payments:id/capture - Content check if value for 'amount_received' matches '6540'",
+    "[POST]::/payments:id/capture - Content check if value for 'amount_received' matches '6000'",
     function () {
-      pm.expect(jsonData.amount_received).to.eql(6540);
-    },
-  );
-}
-
-// Response body should have value "6540" for "amount_capturable"
-if (jsonData?.amount) {
-  pm.test(
-    "[post]:://payments/:id/capture - Content check if value for 'amount_capturable' matches 'amount - 540'",
-    function () {
-      pm.expect(jsonData.amount_capturable).to.eql(0);
+      pm.expect(jsonData.amount_received).to.eql(6000);
     },
   );
 }
