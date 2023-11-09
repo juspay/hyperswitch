6bcd874b145c6010eb501e2d1ebd24e762da7085
Hrithikesh <61539176+hrithikesh026@users.noreply.github.com>
2023-11-09 13:24:18 +0530

    Update migrations/2023-11-06-153840_introduce_new_attempt_and_intent_status/up.sql
    Co-authored-by: Narayan Bhat <48803246+Narayanbhat166@users.noreply.github.com>

--- migrations/2023-11-06-153840_introduce_new_attempt_and_intent_status/up.sql
+++ migrations/2023-11-06-153840_introduce_new_attempt_and_intent_status/up.sql
@@ -1,3 +1,3 @@
 -- Your SQL goes here
-ALTER TYPE "IntentStatus" ADD VALUE 'partially_captured_and_capturable' after 'partially_captured';
-ALTER TYPE "AttemptStatus" ADD VALUE 'partial_charged_and_chargeable' after 'partial_charged';
+ALTER TYPE "IntentStatus" ADD VALUE IF NOT EXISTS 'partially_captured_and_capturable'
+ALTER TYPE "AttemptStatus" ADD VALUE IF NOT EXISTS 'partial_charged_and_chargeable';
