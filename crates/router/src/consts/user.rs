54d6171d6fb47069e3f51392bc1ab6d0869ee57b
Mani Chandra Dulam <mani.dchandra@juspay.in>
2023-11-24 14:24:23 +0530

    fix: hack

--- crates/router/src/consts/user.rs
+++ crates/router/src/consts/user.rs
@@ -4,5 +4,4 @@ pub const MAX_NAME_LENGTH: usize = 70;
 pub const MAX_COMPANY_NAME_LENGTH: usize = 70;
 
 // USER ROLES
-#[cfg(any(feature = "olap", feature = "oltp"))]
 pub const ROLE_ID_ORGANIZATION_ADMIN: &str = "org_admin";
