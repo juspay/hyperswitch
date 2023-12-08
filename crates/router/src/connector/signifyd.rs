b7e6a97e02cf8c1fece3b16700a660e63cf2ad61
Sampras lopes <lsampras@pm.me>
2023-12-08 18:00:59 +0530

    chore(lints): fix hack lints

--- crates/router/src/connector/signifyd.rs
+++ crates/router/src/connector/signifyd.rs
@@ -1,6 +1,7 @@
 pub mod transformers;
 use std::fmt::Debug;
 
+#[cfg(feature = "frm")]
 use common_utils::request::RequestContent;
 use error_stack::{IntoReport, ResultExt};
 use masking::PeekInterface;
