bb456952950fce12a9a9e46d8ba2916624c3c112
hrithikeshvm <hrithikeshmylatty@gmail.com>
2023-12-18 10:48:57 +0530

    address comments

--- migrations/2023-12-15-062816__net_amount_in_payment_attempt/up.sql
+++ migrations/2023-12-15-062816__net_amount_in_payment_attempt/up.sql
@@ -1,8 +1,7 @@
-
 ALTER TABLE payment_attempt
-ADD COLUMN IF NOT EXISTS net_amount BIGINT;
+ADD COLUMN IF NOT EXISTS net_amount BIGINT NOT NULL DEFAULT 0;
 
+// Backfill
 UPDATE payment_attempt pa
 SET net_amount = pa.amount + COALESCE(pa.surcharge_amount, 0) + COALESCE(pa.tax_amount, 0);
 
-ALTER TABLE payment_attempt ALTER COLUMN net_amount SET NOT NULL; 
