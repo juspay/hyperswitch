b98e53d5cba5a5af04ada9bd83fa7bd2e27462d9
Chethan Rao <70657455+Chethan-rao@users.noreply.github.com>
2023-12-20 15:52:02 +0530

    refactor(payment_methods): make the card_holder_name as an empty string if not sent (#3173)

--- crates/router/src/connector/aci/transformers.rs
+++ crates/router/src/connector/aci/transformers.rs
@@ -256,7 +256,7 @@ impl TryFrom<api_models::payments::Card> for PaymentDetails {
             card_number: card_data.card_number,
             card_holder: card_data
                 .card_holder_name
-                .ok_or_else(utils::missing_field_err("card_holder_name"))?,
+                .unwrap_or(Secret::new("".to_string())),
             card_expiry_month: card_data.card_exp_month,
             card_expiry_year: card_data.card_exp_year,
             card_cvv: card_data.card_cvc,
