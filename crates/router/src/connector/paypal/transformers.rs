58c5fe5b66728e4c1ae13e675e4f52b520af6337
MridulY <mridulyadav3173@gmail.com>
2023-12-24 19:48:01 +0530

    [REFACTOR] : [Paypal] Error Message For Connector Implementation

--- crates/router/src/connector/paypal/transformers.rs
+++ crates/router/src/connector/paypal/transformers.rs
@@ -379,11 +379,10 @@ fn get_payment_source(
         | BankRedirectData::Trustly { .. }
         | BankRedirectData::OnlineBankingFpx { .. }
         | BankRedirectData::OnlineBankingThailand { .. } => {
-            Err(errors::ConnectorError::NotSupported {
-                message: utils::SELECTED_PAYMENT_METHOD.to_string(),
-                connector: "Paypal",
-            }
-            .into())
+            Err(errors::ConnectorError::NotImplemented(
+                utils::get_unimplemented_payment_method_error_message("Paypal"),
+            )
+            .into())            
         }
     }
 }
@@ -527,10 +526,10 @@ impl TryFrom<&PaypalRouterData<&types::PaymentsAuthorizeRouterData>> for PaypalP
                 | api_models::payments::WalletData::WeChatPayQr(_)
                 | api_models::payments::WalletData::CashappQr(_)
                 | api_models::payments::WalletData::SwishQr(_) => {
-                    Err(errors::ConnectorError::NotSupported {
-                        message: utils::SELECTED_PAYMENT_METHOD.to_string(),
-                        connector: "Paypal",
-                    })?
+                    Err(errors::ConnectorError::NotImplemented(
+                        utils::get_unimplemented_payment_method_error_message("Paypal"),
+                    )
+                    .into())                    
                 }
             },
             api::PaymentMethodData::BankRedirect(ref bank_redirection_data) => {
@@ -594,11 +593,10 @@ impl TryFrom<&PaypalRouterData<&types::PaymentsAuthorizeRouterData>> for PaypalP
             | api_models::payments::PaymentMethodData::Crypto(_)
             | api_models::payments::PaymentMethodData::Upi(_)
             | api_models::payments::PaymentMethodData::CardToken(_) => {
-                Err(errors::ConnectorError::NotSupported {
-                    message: utils::SELECTED_PAYMENT_METHOD.to_string(),
-                    connector: "Paypal",
-                }
-                .into())
+                Err(errors::ConnectorError::NotImplemented(
+                    utils::get_unimplemented_payment_method_error_message("Paypal"),
+                )
+                .into())                
             }
         }
     }
@@ -612,11 +610,10 @@ impl TryFrom<&api_models::payments::CardRedirectData> for PaypalPaymentsRequest 
             | api_models::payments::CardRedirectData::Benefit {}
             | api_models::payments::CardRedirectData::MomoAtm {}
             | api_models::payments::CardRedirectData::CardRedirect {} => {
-                Err(errors::ConnectorError::NotSupported {
-                    message: utils::SELECTED_PAYMENT_METHOD.to_string(),
-                    connector: "Paypal",
-                }
-                .into())
+                Err(errors::ConnectorError::NotImplemented(
+                    utils::get_unimplemented_payment_method_error_message("Paypal"),
+                )
+                .into())                
             }
         }
     }
@@ -634,11 +631,10 @@ impl TryFrom<&api_models::payments::PayLaterData> for PaypalPaymentsRequest {
             | api_models::payments::PayLaterData::WalleyRedirect {}
             | api_models::payments::PayLaterData::AlmaRedirect {}
             | api_models::payments::PayLaterData::AtomeRedirect {} => {
-                Err(errors::ConnectorError::NotSupported {
-                    message: utils::SELECTED_PAYMENT_METHOD.to_string(),
-                    connector: "Paypal",
-                }
-                .into())
+                Err(errors::ConnectorError::NotImplemented(
+                    utils::get_unimplemented_payment_method_error_message("Paypal"),
+                )
+                .into())                
             }
         }
     }
@@ -652,11 +648,10 @@ impl TryFrom<&api_models::payments::BankDebitData> for PaypalPaymentsRequest {
             | api_models::payments::BankDebitData::SepaBankDebit { .. }
             | api_models::payments::BankDebitData::BecsBankDebit { .. }
             | api_models::payments::BankDebitData::BacsBankDebit { .. } => {
-                Err(errors::ConnectorError::NotSupported {
-                    message: utils::SELECTED_PAYMENT_METHOD.to_string(),
-                    connector: "Paypal",
-                }
-                .into())
+                Err(errors::ConnectorError::NotImplemented(
+                    utils::get_unimplemented_payment_method_error_message("Paypal"),
+                )
+                .into())                
             }
         }
     }
@@ -679,11 +674,10 @@ impl TryFrom<&api_models::payments::BankTransferData> for PaypalPaymentsRequest 
             | api_models::payments::BankTransferData::MandiriVaBankTransfer { .. }
             | api_models::payments::BankTransferData::Pix {}
             | api_models::payments::BankTransferData::Pse {} => {
-                Err(errors::ConnectorError::NotSupported {
-                    message: utils::SELECTED_PAYMENT_METHOD.to_string(),
-                    connector: "Paypal",
-                }
-                .into())
+                Err(errors::ConnectorError::NotImplemented(
+                    utils::get_unimplemented_payment_method_error_message("Paypal"),
+                )
+                .into())                
             }
         }
     }
@@ -707,11 +701,10 @@ impl TryFrom<&api_models::payments::VoucherData> for PaypalPaymentsRequest {
             | api_models::payments::VoucherData::FamilyMart(_)
             | api_models::payments::VoucherData::Seicomart(_)
             | api_models::payments::VoucherData::PayEasy(_) => {
-                Err(errors::ConnectorError::NotSupported {
-                    message: utils::SELECTED_PAYMENT_METHOD.to_string(),
-                    connector: "Paypal",
-                }
-                .into())
+                Err(errors::ConnectorError::NotImplemented(
+                    utils::get_unimplemented_payment_method_error_message("Paypal"),
+                )
+                .into())                
             }
         }
     }
@@ -723,11 +716,10 @@ impl TryFrom<&api_models::payments::GiftCardData> for PaypalPaymentsRequest {
         match value {
             api_models::payments::GiftCardData::Givex(_)
             | api_models::payments::GiftCardData::PaySafeCard {} => {
-                Err(errors::ConnectorError::NotSupported {
-                    message: utils::SELECTED_PAYMENT_METHOD.to_string(),
-                    connector: "Paypal",
-                }
-                .into())
+                Err(errors::ConnectorError::NotImplemented(
+                    utils::get_unimplemented_payment_method_error_message("Paypal"),
+                )
+                .into())                
             }
         }
     }
