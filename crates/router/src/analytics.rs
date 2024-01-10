009f16771c35acf6868283a40dce23bb1440baa5
harsh_sharma_juspay <harsh.sharma@juspay.in>
2024-01-10 20:10:53 +0530

    added outgoing webhook events flow

--- crates/router/src/analytics.rs
+++ crates/router/src/analytics.rs
@@ -4,7 +4,7 @@ pub mod routes {
     use actix_web::{web, Responder, Scope};
     use analytics::{
         api_event::api_events_core, errors::AnalyticsError, lambda_utils::invoke_lambda,
-        sdk_events::sdk_events_core,
+        outgoing_webhook_event::outgoing_webhook_events_core, sdk_events::sdk_events_core,
     };
     use api_models::analytics::{
         GenerateReportRequest, GetApiEventFiltersRequest, GetApiEventMetricRequest,
@@ -321,16 +321,18 @@ pub mod routes {
     pub async fn get_outgoing_webhook_events(
         state: web::Data<AppState>,
         req: actix_web::HttpRequest,
-        json_payload: web::Query<api_models::analytics::api_event::ApiLogsRequest>,
+        json_payload: web::Query<
+            api_models::analytics::outgoing_webhook_event::OutgoingWebhookLogsRequest,
+        >,
     ) -> impl Responder {
-        let flow = AnalyticsFlow::GetApiEvents;
+        let flow = AnalyticsFlow::GetOutgoingWebhookEvents;
         Box::pin(api::server_wrap(
             flow,
             state,
             &req,
             json_payload.into_inner(),
             |state, auth: AuthenticationData, req| async move {
-                api_events_core(&state.pool, req, auth.merchant_account.merchant_id)
+                outgoing_webhook_events_core(&state.pool, req, auth.merchant_account.merchant_id)
                     .await
                     .map(ApplicationResponse::Json)
             },
