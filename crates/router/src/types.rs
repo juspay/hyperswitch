db3665c64f5fadeff40c10e2670e096266f4d7d1
DEEPANSHU BANSAL <deepanshubansal515@gmail.com>
2024-01-10 20:16:58 +0530

    chore: Resolve Pr Comments

--- crates/router/src/types.rs
+++ crates/router/src/types.rs
@@ -420,6 +420,7 @@ pub struct PaymentsCaptureData {
     pub connector_meta: Option<serde_json::Value>,
     pub browser_info: Option<BrowserInformation>,
     pub metadata: Option<pii::SecretSerdeValue>,
+    // This metadata is used to store the metadata shared during the payment intent request.
 }
 
 #[derive(Debug, Clone, Default)]
@@ -535,6 +536,7 @@ pub struct PaymentsCancelData {
     pub connector_meta: Option<serde_json::Value>,
     pub browser_info: Option<BrowserInformation>,
     pub metadata: Option<pii::SecretSerdeValue>,
+    // This metadata is used to store the metadata shared during the payment intent request.
 }
 
 #[derive(Debug, Default, Clone)]
