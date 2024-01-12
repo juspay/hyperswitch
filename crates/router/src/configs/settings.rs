c1c304e49271a0572ed2560a58b9c62c90ee53cb
Chethan <chethan.rao@juspay.in>
2024-01-12 16:30:00 +0530

    refactor: rename s3 feature flag to aws_s3

--- crates/router/src/configs/settings.rs
+++ crates/router/src/configs/settings.rs
@@ -88,7 +88,7 @@ pub struct Settings {
     pub api_keys: ApiKeys,
     #[cfg(feature = "kms")]
     pub kms: kms::KmsConfig,
-    #[cfg(feature = "s3")]
+    #[cfg(feature = "aws_s3")]
     pub file_upload_config: FileUploadConfig,
     pub tokenization: TokenizationConfig,
     pub connector_customer: ConnectorCustomer,
@@ -716,7 +716,7 @@ pub struct ApiKeys {
     pub expiry_reminder_days: Vec<u8>,
 }
 
-#[cfg(feature = "s3")]
+#[cfg(feature = "aws_s3")]
 #[derive(Debug, Deserialize, Clone, Default)]
 #[serde(default)]
 pub struct FileUploadConfig {
@@ -848,7 +848,7 @@ impl Settings {
         self.kms
             .validate()
             .map_err(|error| ApplicationError::InvalidConfigurationValueError(error.into()))?;
-        #[cfg(feature = "s3")]
+        #[cfg(feature = "aws_s3")]
         self.file_upload_config.validate()?;
         self.lock_settings.validate()?;
         self.events.validate()?;
