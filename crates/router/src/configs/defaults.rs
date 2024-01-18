cb87d754697a156af4f214393405ffcbb7865093
AkshayaFoiger <131388445+AkshayaFoiger@users.noreply.github.com>
2024-01-18 16:32:25 +0530

    Update crates/router/src/configs/defaults.rs
    Co-authored-by: Prasunna Soppa <70575890+prasunna09@users.noreply.github.com>

--- crates/router/src/configs/defaults.rs
+++ crates/router/src/configs/defaults.rs
@@ -4914,7 +4914,7 @@ impl Default for super::settings::RequiredFields {
                                             RequiredFieldInfo {
                                                 required_field: "payment_method_data.bank_redirect.sofort.billing_details.billing_name".to_string(),
                                                 display_name: "billing_name".to_string(),
-                                                field_type: enums::FieldType::UserFullName,
+                                                field_type: enums::FieldType::UserBillingName,
                                                 value: None,
                                             }
                                         )
