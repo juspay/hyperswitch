58d3a23ae3235efde7214e1adc198c9ccac814b5
Sampras lopes <lsampras@pm.me>
2024-01-22 18:09:48 +0530

    fix(clickhouse): update source table names

--- crates/analytics/src/clickhouse.rs
+++ crates/analytics/src/clickhouse.rs
@@ -356,11 +356,11 @@ impl ToSql<ClickhouseClient> for AnalyticsCollection {
         match self {
             Self::Payment => Ok("payment_attempts".to_string()),
             Self::Refund => Ok("refunds".to_string()),
-            Self::SdkEvents => Ok("sdk_events".to_string()),
-            Self::ApiEvents => Ok("api_events".to_string()),
+            Self::SdkEvents => Ok("sdk_events_audit".to_string()),
+            Self::ApiEvents => Ok("api_events_audit".to_string()),
             Self::PaymentIntent => Ok("payment_intents".to_string()),
-            Self::ConnectorEvents => Ok("connector_events".to_string()),
-            Self::OutgoingWebhookEvent => Ok("outgoing_webhook_events".to_string()),
+            Self::ConnectorEvents => Ok("connector_events_audit".to_string()),
+            Self::OutgoingWebhookEvent => Ok("outgoing_webhook_events_audit".to_string()),
         }
     }
 }
