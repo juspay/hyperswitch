d827c9af29b8516f379e648e00f4ab307ae1a34d
Branislav Kontur <bkontur@gmail.com>
2024-01-25 10:48:02 +0100

    fix(connector): Use `ConnectorError::InvalidConnectorConfig` for an invalid `CoinbaseConnectorMeta` (#3168)
    Co-authored-by: chikke srujan <121822803+srujanchikke@users.noreply.github.com>

--- crates/router/src/connector/utils.rs
+++ crates/router/src/connector/utils.rs
@@ -1222,23 +1222,6 @@ where
     json.parse_value(std::any::type_name::<T>()).switch()
 }
 
-pub fn to_connector_meta_from_secret_with_required_field<T>(
-    connector_meta: Option<Secret<serde_json::Value>>,
-    error_message: &'static str,
-) -> Result<T, Error>
-where
-    T: serde::de::DeserializeOwned,
-{
-    let connector_error = errors::ConnectorError::MissingRequiredField {
-        field_name: error_message,
-    };
-    let parsed_meta = to_connector_meta_from_secret(connector_meta).ok();
-    match parsed_meta {
-        Some(meta) => Ok(meta),
-        _ => Err(connector_error.into()),
-    }
-}
-
 pub fn to_connector_meta_from_secret<T>(
     connector_meta: Option<Secret<serde_json::Value>>,
 ) -> Result<T, Error>
