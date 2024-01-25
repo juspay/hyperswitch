47fc42112fb9de11fbe4c3c3316cee9e04f6c10f
Sk Sakil Mostak <skmahim71@gmail.com>
2024-01-25 15:59:43 +0530

    refactor: comments

--- crates/router/src/connector/wise.rs
+++ crates/router/src/connector/wise.rs
@@ -595,7 +595,7 @@ impl
         _connectors: &settings::Connectors,
     ) -> CustomResult<Option<services::Request>, errors::ConnectorError> {
         // Eligiblity check for cards is not implemented
-        Err(errors::ConnectorError::NotImplemented("Payout Cards for Wise".to_string()).into())
+        Err(errors::ConnectorError::NotImplemented("Payout Eligibility for Wise".to_string()).into())
     }
 }
 
