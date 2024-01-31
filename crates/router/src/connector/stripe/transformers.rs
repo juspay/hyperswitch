8ff55991ee680050ef29d89a600be58ece41cccd
AkshayaFoiger <akshaya.shankar@juspay.in>
2024-01-31 17:52:44 +0530

    change the psync error mapping

--- crates/router/src/connector/stripe/transformers.rs
+++ crates/router/src/connector/stripe/transformers.rs
@@ -2520,7 +2520,7 @@ impl<F, T>
 
         let response = if connector_util::is_payment_failure(status) {
             types::PaymentsResponseData::try_from((
-                &item.response.last_payment_error,
+                &item.response.payment_intent_fields.last_payment_error,
                 item.http_code,
                 item.response.id.clone(),
             ))
@@ -3897,43 +3897,6 @@ mod test_validate_shipping_address_against_payment_method {
             state: Some(Secret::new(String::from("state"))),
             phone: Some(Secret::new(String::from("pbone number"))),
         }
-    }
-}
-
-impl TryFrom<(&Option<LastPaymentError>, u16, String)> for types::PaymentsResponseData {
-    type Error = types::ErrorResponse;
-    fn try_from(
-        (response, http_code, response_id): (&Option<LastPaymentError>, u16, String),
-    ) -> Result<Self, Self::Error> {
-        let (code, error_message) = match response {
-            Some(error_details) => (
-                error_details.code.to_owned(),
-                error_details.message.to_owned(),
-            ),
-            None => (
-                consts::NO_ERROR_CODE.to_string(),
-                consts::NO_ERROR_MESSAGE.to_string(),
-            ),
-        };
-
-        Err(types::ErrorResponse {
-            code,
-            message: error_message.clone(),
-            reason: response.as_ref().and_then(|res| {
-                res.decline_code
-                    .clone()
-                    .map(|decline_code| {
-                        format!(
-                            "message - {}, decline_code - {}",
-                            error_message, decline_code
-                        )
-                    })
-                    .or(Some(error_message.clone()))
-            }),
-            status_code: http_code,
-            attempt_status: None,
-            connector_transaction_id: Some(response_id),
-        })
     }
 }
 
