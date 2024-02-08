58c47523a6fddbdad3ce4ea2537cc01cb87c803a
Chethan <chethan.rao@juspay.in>
2024-02-08 22:30:22 +0530

    chore: address Rust 1.76 clippy lints

--- crates/router/src/services/api.rs
+++ crates/router/src/services/api.rs
@@ -1083,7 +1083,7 @@ where
     let start_instant = Instant::now();
     logger::info!(tag = ?Tag::BeginRequest, payload = ?payload);
 
-    let res = match metrics::request::record_request_time_metric(
+    let server_wrap_util_res = metrics::request::record_request_time_metric(
         server_wrap_util(
             &flow,
             state.clone(),
@@ -1099,7 +1099,9 @@ where
     .map(|response| {
         logger::info!(api_response =? response);
         response
-    }) {
+    });
+
+    let res = match server_wrap_util_res {
         Ok(ApplicationResponse::Json(response)) => match serde_json::to_string(&response) {
             Ok(res) => http_response_json(res),
             Err(_) => http_response_err(
