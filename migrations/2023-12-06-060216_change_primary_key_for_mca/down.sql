88fc392ad194bdf33098461d95ec65cb9087cddd
Narayan Bhat <narayan.bhat@juspay.in>
2024-02-19 11:51:18 +0530

    chore: make migrations to be rerun

--- migrations/2023-12-06-060216_change_primary_key_for_mca/down.sql
+++ migrations/2023-12-06-060216_change_primary_key_for_mca/down.sql
@@ -1,4 +1,4 @@
 -- This file should undo anything in `up.sql`
-CREATE UNIQUE INDEX "merchant_connector_account_profile_id_connector_id_index" ON merchant_connector_account (profile_id, connector_name);
+CREATE UNIQUE INDEX IF NOT EXISTS "merchant_connector_account_profile_id_connector_id_index" ON merchant_connector_account (profile_id, connector_name);
 
-ALTER TABLE merchant_connector_account DROP CONSTRAINT "merchant_connector_account_profile_id_connector_label_key";
+ALTER TABLE merchant_connector_account DROP CONSTRAINT IF EXISTS "merchant_connector_account_profile_id_connector_label_key";
