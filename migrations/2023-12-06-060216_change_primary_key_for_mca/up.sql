6ec48f751e88f633e5fb7bd858644b3336198cd1
Narayan Bhat <narayan.bhat@juspay.in>
2024-02-19 19:30:51 +0530

    chore: create index before deleting previous index

--- migrations/2023-12-06-060216_change_primary_key_for_mca/up.sql
+++ migrations/2023-12-06-060216_change_primary_key_for_mca/up.sql
@@ -1,5 +1,5 @@
 -- Your SQL goes here
-DROP INDEX IF EXISTS "merchant_connector_account_profile_id_connector_id_index";
-
 ALTER TABLE merchant_connector_account
 ADD UNIQUE (profile_id, connector_label);
+
+DROP INDEX IF EXISTS "merchant_connector_account_profile_id_connector_id_index";
