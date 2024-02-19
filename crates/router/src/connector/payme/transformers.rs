b9c219ede006d8ff44484524e84daeff5dea3e81
Sk Sakil Mostak <skmahim71@gmail.com>
2024-02-19 21:29:47 +0530

    refactor: resolve comments

--- crates/router/src/connector/payme/transformers.rs
+++ crates/router/src/connector/payme/transformers.rs
@@ -983,7 +983,7 @@ impl TryFrom<SaleStatus> for enums::RefundStatus {
 #[derive(Debug, Deserialize, Serialize)]
 pub struct PaymeRefundResponse {
     sale_status: SaleStatus,
-    payme_transaction_id: Option<String>,
+    payme_transaction_id: String,
     status_error_code: i64,
 }
 
