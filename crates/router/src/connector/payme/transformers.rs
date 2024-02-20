5a35f97b3a487444c7b771975a495e7e711e4a9f
Sk Sakil Mostak <skmahim71@gmail.com>
2024-02-20 15:03:53 +0530

    refactor: resolve cherry pick

--- crates/router/src/connector/payme/transformers.rs
+++ crates/router/src/connector/payme/transformers.rs
@@ -1003,14 +1003,11 @@ impl TryFrom<types::RefundsResponseRouterData<api::Execute, PaymeRefundResponse>
                 reason: Some(payme_response.status_error_code.to_string()),
                 status_code: item.http_code,
                 attempt_status: None,
-                connector_transaction_id: payme_response.payme_transaction_id.clone(),
+                connector_transaction_id: Some(payme_response.payme_transaction_id.clone()),
             })
         } else {
             Ok(types::RefundsResponseData {
-                connector_refund_id: item
-                    .response
-                    .payme_transaction_id
-                    .ok_or(errors::ConnectorError::MissingConnectorRefundID)?,
+                connector_refund_id: item.response.payme_transaction_id,
                 refund_status,
             })
         };
