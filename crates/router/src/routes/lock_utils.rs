bbb3d3d1e26f303964a495606dece7958f6c40fc
Tanbir Ali <124070023+tanbirali@users.noreply.github.com>
2024-02-22 16:55:10 +0530

    feat(router): added api for the deleting config key (#3554)
    Co-authored-by: Kartikeya Hegde <karthihegde010@gmail.com>
    Co-authored-by: dracarys18 <karthikey.hegde@juspay.in>

--- crates/router/src/routes/lock_utils.rs
+++ crates/router/src/routes/lock_utils.rs
@@ -74,6 +74,7 @@ impl From<Flow> for ApiIdentifier {
             Flow::ConfigKeyCreate
             | Flow::ConfigKeyFetch
             | Flow::ConfigKeyUpdate
+            | Flow::ConfigKeyDelete
             | Flow::CreateConfigKey => Self::Configs,
 
             Flow::CustomersCreate
