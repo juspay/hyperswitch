decf0ea2654bb51b9700f742f274f3fba444c440
Mani Chandra Dulam <mani.dchandra@juspay.in>
2024-02-26 13:32:18 +0530

    refactor: maintain previous order of api models

--- crates/api_models/src/user_role/role.rs
+++ crates/api_models/src/user_role/role.rs
@@ -1,17 +1,23 @@
 use super::Permission;
 use common_enums::{PermissionGroup, RoleScope};
 
-#[derive(Debug, serde::Serialize)]
-pub struct ListRolesWithPermissionsResponse(pub Vec<RoleInfoWithPermissionsResponse>);
-
-#[derive(Debug, serde::Serialize)]
-pub struct ListRolesWithGroupsResponse(pub Vec<RoleInfoWithGroupsResponse>);
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+pub struct CreateRoleRequest {
+    pub role_name: String,
+    pub groups: Vec<PermissionGroup>,
+    pub role_scope: RoleScope,
+}
 
 #[derive(Debug, serde::Serialize)]
 #[serde(tag = "response_type", rename_all = "snake_case")]
 pub enum ListRolesResponse {
     Permissions(Vec<RoleInfoWithPermissionsResponse>),
     Groups(Vec<RoleInfoWithGroupsResponse>),
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct GetGroupsQueryParam {
+    pub groups: Option<bool>,
 }
 
 #[derive(Debug, serde::Serialize)]
@@ -34,18 +40,6 @@ pub struct RoleInfoWithGroupsResponse {
     pub role_id: String,
     pub groups: Vec<PermissionGroup>,
     pub role_name: String,
-    pub role_scope: RoleScope,
-}
-
-#[derive(Debug, serde::Deserialize)]
-pub struct GetGroupsQueryParam {
-    pub groups: Option<bool>,
-}
-
-#[derive(Debug, serde::Deserialize, serde::Serialize)]
-pub struct CreateRoleRequest {
-    pub role_name: String,
-    pub groups: Vec<PermissionGroup>,
     pub role_scope: RoleScope,
 }
 
