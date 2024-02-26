a29a244e4c274f30d9ec8a2c308748292c619609
Ivor Dsouza <ivor.dsouza@juspay.in>
2024-02-26 17:11:18 +0530

    feat(global-search): opensearch client req

--- crates/analytics/src/search.rs
+++ crates/analytics/src/search.rs
@@ -1,27 +1,43 @@
-use actix_web::web::Json;
 use api_models::analytics::{GetGlobalSearchRequest, GetSearchRequest, SearchIndex};
 use common_utils::errors::CustomResult;
+use opensearch::http::transport::Transport;
 use opensearch::{http::request::JsonBody, MsearchParts, OpenSearch, SearchParts};
 use serde_json::{json, Value};
 use strum::IntoEnumIterator;
 
 use crate::errors::AnalyticsError;
-use crate::AnalyticsProvider;
+
+#[derive(Debug, thiserror::Error)]
+pub enum OpensearchError {
+    #[error("Opensearch connection error")]
+    ConnectionError,
+    #[error("Opensearch NON-200 response content: '{0}'")]
+    ResponseNotOK(String),
+    #[error("Opensearch response error")]
+    ResponseError,
+}
+
+async fn get_opensearch_client(url: String) -> Result<OpenSearch, OpensearchError> {
+    let transport = Transport::single_node(&url).map_err(|_| OpensearchError::ConnectionError)?;
+    Ok(OpenSearch::new(transport))
+}
 
 async fn msearch_results(
-    pool: &AnalyticsProvider,
     req: GetGlobalSearchRequest,
     merchant_id: &String,
+    url: String,
 ) -> CustomResult<(), AnalyticsError> {
-    let client = OpenSearch::default();
+    let client = get_opensearch_client(url)
+        .await
+        .map_err(|_| AnalyticsError::UnknownError)?;
 
     let mut msearch_vector: Vec<JsonBody<Value>> = vec![];
-    SearchIndex::iter().map(|index| {
+    for index in SearchIndex::iter() {
         msearch_vector.push(json!({"index": index.to_string()}).into());
         msearch_vector.push(json!({"query": {"bool": {"must": {"query_string": {"query": req.query}}, "filter": {"match_phrase": {"merchant_id": merchant_id}}}}}).into());
-    });
+    }
 
-    let mut response = client
+    let response = client
         .msearch(MsearchParts::None)
         .body(msearch_vector)
         .send()
@@ -37,12 +53,13 @@ async fn msearch_results(
 }
 
 async fn search_results(
-    pool: &AnalyticsProvider,
     req: GetSearchRequest,
     merchant_id: &String,
     index: SearchIndex,
 ) -> CustomResult<(), AnalyticsError> {
-    let client = OpenSearch::default();
+    let client = get_opensearch_client(url)
+        .await
+        .map_err(|_| AnalyticsError::UnknownError)?;
 
     let mut response = client
         .search(SearchParts::Index(&[&index.to_string()]))
