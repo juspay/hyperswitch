032d58cdbbf388cf25cbf2e43b0117b83f7d076d
Sagar naik <sagar.naik@juspay.in>
2024-02-28 19:10:20 +0530

    feat(analytics): add force retrieve call for force retrieve calls (#3565)
    Co-authored-by: harsh-sharma-juspay <125131007+harsh-sharma-juspay@users.noreply.github.com>

--- crates/router/src/routes/lock_utils.rs
+++ crates/router/src/routes/lock_utils.rs
@@ -103,6 +103,7 @@ impl From<Flow> for ApiIdentifier {
 
             Flow::PaymentsCreate
             | Flow::PaymentsRetrieve
+            | Flow::PaymentsRetrieveForceSync
             | Flow::PaymentsUpdate
             | Flow::PaymentsConfirm
             | Flow::PaymentsCapture
@@ -124,6 +125,7 @@ impl From<Flow> for ApiIdentifier {
 
             Flow::RefundsCreate
             | Flow::RefundsRetrieve
+            | Flow::RefundsRetrieveForceSync
             | Flow::RefundsUpdate
             | Flow::RefundsList => Self::Refunds,
 
