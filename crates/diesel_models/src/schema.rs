bb3ee588a90ed128618fdf4509d9ad3f5c6901c6
hrithikesh026 <hrithikesh.vm@juspay.in>
2024-02-28 13:32:33 +0530

    chore: generate schema.rs changes

--- crates/diesel_models/src/schema.rs
+++ crates/diesel_models/src/schema.rs
@@ -62,6 +62,35 @@ diesel::table! {
     use diesel::sql_types::*;
     use crate::enums::diesel_exports::*;
 
+    authentication (authentication_id) {
+        #[max_length = 64]
+        authentication_id -> Varchar,
+        #[max_length = 64]
+        merchant_id -> Varchar,
+        #[max_length = 64]
+        authentication_connector -> Varchar,
+        #[max_length = 64]
+        authentication_connector_id -> Nullable<Varchar>,
+        authentication_data -> Nullable<Jsonb>,
+        #[max_length = 64]
+        payment_method_id -> Varchar,
+        authentication_type -> Nullable<DecoupledAuthenticationType>,
+        authentication_status -> AuthenticationStatus,
+        authentication_lifecycle_status -> AuthenticationLifecycleStatus,
+        created_at -> Timestamp,
+        modified_at -> Timestamp,
+        #[max_length = 64]
+        error_message -> Nullable<Varchar>,
+        #[max_length = 64]
+        error_code -> Nullable<Varchar>,
+        connector_metadata -> Nullable<Jsonb>,
+    }
+}
+
+diesel::table! {
+    use diesel::sql_types::*;
+    use crate::enums::diesel_exports::*;
+
     blocklist (id) {
         id -> Int4,
         #[max_length = 64]
@@ -1101,6 +1130,7 @@ diesel::table! {
 diesel::allow_tables_to_appear_in_same_query!(
     address,
     api_keys,
+    authentication,
     blocklist,
     blocklist_fingerprint,
     blocklist_lookup,
