390258b60584a8a1f8d3fbfeec7182df30771884
Mani Chandra Dulam <mani.dchandra@juspay.in>
2024-02-29 15:53:25 +0530

    refactor: add more checks to create and update role and change the response

--- crates/api_models/src/events/user_role.rs
+++ crates/api_models/src/events/user_role.rs
@@ -3,7 +3,7 @@ use common_utils::events::{ApiEventMetric, ApiEventsType};
 use crate::user_role::{
     role::{
         CreateRoleRequest, GetRoleRequest, ListRolesResponse, RoleInfoResponse,
-        RoleInfoWithPermissionsResponse, UpdateRoleRequest,
+        RoleInfoWithGroupsResponse, RoleInfoWithPermissionsResponse, UpdateRoleRequest,
     },
     AcceptInvitationRequest, AuthorizationInfoResponse, DeleteUserRoleRequest,
     TransferOrgOwnershipRequest, UpdateUserRoleRequest,
@@ -20,5 +20,6 @@ common_utils::impl_misc_api_event_type!(
     CreateRoleRequest,
     UpdateRoleRequest,
     ListRolesResponse,
-    RoleInfoResponse
+    RoleInfoResponse,
+    RoleInfoWithGroupsResponse
 );
--- crates/router/src/utils/user_role.rs
+++ crates/router/src/utils/user_role.rs
@@ -1,10 +1,12 @@
 use api_models::user_role as user_role_api;
+use common_enums::PermissionGroup;
 use error_stack::ResultExt;
 
 use crate::{
     core::errors::{UserErrors, UserResult},
     routes::AppState,
-    services::authorization::permissions::Permission,
+    services::authorization::{permissions::Permission, roles},
+    types::domain,
 };
 
 impl From<Permission> for user_role_api::Permission {
@@ -58,5 +60,50 @@ pub async fn is_role_name_already_present_for_merchant(
     if role_name_list.contains(&role_name.to_string()) {
         return Err(UserErrors::RoleNameAlreadyExists.into());
     }
+    Ok(())
+}
+
+pub fn validate_role_groups(groups: &Vec<PermissionGroup>) -> UserResult<()> {
+    if groups.is_empty() {
+        return Err(UserErrors::InvalidRoleOperation.into())
+            .attach_printable("Role groups cannot be empty");
+    }
+
+    if groups.contains(&PermissionGroup::OrganizationManage) {
+        return Err(UserErrors::InvalidRoleOperation.into())
+            .attach_printable("Organization manage group cannot be added to role");
+    }
+
+    Ok(())
+}
+
+pub async fn validate_role_name(
+    state: &AppState,
+    role_name: &domain::RoleName,
+    merchant_id: &str,
+    org_id: &str,
+) -> UserResult<()> {
+    let role_name_str = role_name.clone().get_role_name();
+
+    let is_present_in_predefined_roles = roles::predefined_roles::PREDEFINED_ROLES
+        .iter()
+        .find(|(_, role_info)| role_info.get_role_name() == &role_name_str)
+        .is_some();
+
+    // TODO: Create and use find_by_role_name to make this efficient
+    let is_present_in_custom_roles = state
+        .store
+        .list_all_roles(merchant_id, org_id)
+        .await
+        .change_context(UserErrors::InternalServerError)?
+        .iter()
+        .map(|role| &role.role_name)
+        .find(|name| name == &role_name_str.as_str())
+        .is_some();
+
+    if is_present_in_predefined_roles || is_present_in_custom_roles {
+        return Err(UserErrors::RoleNameAlreadyExists.into());
+    }
+
     Ok(())
 }
