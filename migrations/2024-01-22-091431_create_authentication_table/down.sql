71bb2e09f74731cae7688d8663ab0c642764fe7d
hrithikesh026 <hrithikesh.vm@juspay.in>
2024-03-01 12:28:40 +0530

    remove drop query for authentication enums

--- migrations/2024-01-22-091431_create_authentication_table/down.sql
+++ migrations/2024-01-22-091431_create_authentication_table/down.sql
@@ -1,5 +1,2 @@
 -- This file should undo anything in `up.sql`
 DROP TABLE IF EXISTS authentication;
-DROP TYPE "DecoupledAuthenticationType";
-DROP TYPE "AuthenticationStatus";
-DROP TYPE "AuthenticationLifecycleStatus";
