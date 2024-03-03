47e7d5c56bc64e03bdd074456c5f90e202f61801
Riahiamirreza <riahiamirreza79@gmail.com>
2024-03-03 17:42:33 +0330

    feat(api_model): add offset field to DisputeListConstraints struct

--- crates/api_models/src/disputes.rs
+++ crates/api_models/src/disputes.rs
@@ -106,6 +106,8 @@ pub struct DisputeEvidenceBlock {
 #[derive(Clone, Debug, Deserialize, ToSchema, Serialize)]
 #[serde(deny_unknown_fields)]
 pub struct DisputeListConstraints {
+    /// used to skip certain number of objects
+    pub offset: Option<i64>,
     /// limit on the number of objects to return
     pub limit: Option<i64>,
     /// The identifier for business profile
