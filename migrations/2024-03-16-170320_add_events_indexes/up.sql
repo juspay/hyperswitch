2cd3a5482646e88157f32b9ce1523c3460029d44
Sanchith Hegde <sanchith.hegde@juspay.in>
2024-03-21 17:35:03 +0530

    chore(migrations): remove unused `events_business_profile_id_event_id_index` index

--- migrations/2024-03-16-170320_add_events_indexes/up.sql
+++ migrations/2024-03-16-170320_add_events_indexes/up.sql
@@ -4,8 +4,6 @@ CREATE INDEX events_merchant_id_initial_attempt_id_index ON events (merchant_id,
 
 CREATE INDEX events_merchant_id_initial_events_index ON events (merchant_id, (event_id = initial_attempt_id));
 
-CREATE UNIQUE INDEX events_business_profile_id_event_id_index ON events (business_profile_id, event_id);
-
 CREATE INDEX events_business_profile_id_initial_attempt_id_index ON events (business_profile_id, initial_attempt_id);
 
 CREATE INDEX events_business_profile_id_initial_events_index ON events (
