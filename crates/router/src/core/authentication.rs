131e487c662985737e9b50a8e62295ed9d23ca83
Sai Harsha Vardhan <56996463+sai-harsha-vardhan@users.noreply.github.com>
2024-04-24 20:00:52 +0530

    feat(router): handle authorize redirection after webhook processing for external 3ds flow (#4452)

--- crates/router/src/core/authentication.rs
+++ crates/router/src/core/authentication.rs
@@ -11,7 +11,6 @@ use masking::{ExposeInterface, PeekInterface};
 
 use super::errors;
 use crate::{
-    consts::POLL_ID_TTL,
     core::{errors::ApiErrorResponse, payments as payments_core},
     routes::AppState,
     types::{self as core_types, api, authentication::AuthenticationResponseData, storage},
@@ -156,31 +155,6 @@ pub async fn perform_post_authentication<F: Clone + Send>(
             //If authentication is not successful, skip the payment connector flows and mark the payment as failure
             if !(authentication_status == api_models::enums::AuthenticationStatus::Success) {
                 *should_continue_confirm_transaction = false;
-            }
-            // When authentication status is non-terminal, Set poll_id in redis to allow the fetch status of poll through retrieve_poll_status api from client
-            if !authentication_status.is_terminal_status() {
-                let req_poll_id = super::utils::get_external_authentication_request_poll_id(
-                    &payment_data.payment_intent.payment_id,
-                );
-                let poll_id = super::utils::get_poll_id(
-                    business_profile.merchant_id.clone(),
-                    req_poll_id.clone(),
-                );
-                let redis_conn = state
-                    .store
-                    .get_redis_conn()
-                    .change_context(errors::ApiErrorResponse::InternalServerError)
-                    .attach_printable("Failed to get redis connection")?;
-                redis_conn
-                    .set_key_with_expiry(
-                        &poll_id,
-                        api_models::poll::PollStatus::Pending.to_string(),
-                        POLL_ID_TTL,
-                    )
-                    .await
-                    .change_context(errors::StorageError::KVError)
-                    .change_context(errors::ApiErrorResponse::InternalServerError)
-                    .attach_printable("Failed to add poll_id in redis")?;
             }
         }
         types::PostAuthenthenticationFlowInput::PaymentMethodAuthNFlow { other_fields: _ } => {
