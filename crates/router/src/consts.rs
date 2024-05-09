404f587c0b5fa196ced815517ad676cf7becb36e
Narayan Bhat <narayan.bhat@juspay.in>
2024-05-09 23:41:24 +0530

    refactor: add a buffer to reduce the access token expiry

--- crates/router/src/consts.rs
+++ crates/router/src/consts.rs
@@ -115,3 +115,6 @@ pub const POLL_ID_TTL: i64 = 900;
 // Default Poll Config
 pub const DEFAULT_POLL_DELAY_IN_SECS: i8 = 2;
 pub const DEFAULT_POLL_FREQUENCY: i8 = 5;
+
+// Number of seconds to subtract from access token expiry
+pub(crate) const REDUCE_ACCESS_TOKEN_EXPIRY_TIME: u8 = 30;
