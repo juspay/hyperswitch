cdcdab4c36e39df877b73dc0344238d9c45ddbdd
vrishab.srivatsa] <vrishab.srivatsa@juspay.in>
2024-05-15 22:10:07 +0530

    feat: added client source and client version columns in payment analytics

--- crates/analytics/src/sqlx.rs
+++ crates/analytics/src/sqlx.rs
@@ -260,6 +260,15 @@ impl<'a> FromRow<'a, PgRow> for super::payments::metrics::PaymentMetricRow {
                 ColumnNotFound(_) => Ok(Default::default()),
                 e => Err(e),
             })?;
+        let client_source: Option<String> = row.try_get("client_source").or_else(|e| match e {
+            ColumnNotFound(_) => Ok(Default::default()),
+            e => Err(e),
+        })?;
+        let client_version: Option<String> =
+            row.try_get("client_version").or_else(|e| match e {
+                ColumnNotFound(_) => Ok(Default::default()),
+                e => Err(e),
+            })?;
         let total: Option<bigdecimal::BigDecimal> = row.try_get("total").or_else(|e| match e {
             ColumnNotFound(_) => Ok(Default::default()),
             e => Err(e),
@@ -282,6 +291,8 @@ impl<'a> FromRow<'a, PgRow> for super::payments::metrics::PaymentMetricRow {
             authentication_type,
             payment_method,
             payment_method_type,
+            client_source,
+            client_version,
             total,
             count,
             start_bucket,
@@ -321,6 +332,15 @@ impl<'a> FromRow<'a, PgRow> for super::payments::distribution::PaymentDistributi
                 ColumnNotFound(_) => Ok(Default::default()),
                 e => Err(e),
             })?;
+        let client_source: Option<String> = row.try_get("client_source").or_else(|e| match e {
+            ColumnNotFound(_) => Ok(Default::default()),
+            e => Err(e),
+        })?;
+        let client_version: Option<String> =
+            row.try_get("client_version").or_else(|e| match e {
+                ColumnNotFound(_) => Ok(Default::default()),
+                e => Err(e),
+            })?;
         let total: Option<bigdecimal::BigDecimal> = row.try_get("total").or_else(|e| match e {
             ColumnNotFound(_) => Ok(Default::default()),
             e => Err(e),
@@ -347,6 +367,8 @@ impl<'a> FromRow<'a, PgRow> for super::payments::distribution::PaymentDistributi
             authentication_type,
             payment_method,
             payment_method_type,
+            client_source,
+            client_version,
             total,
             count,
             error_message,
@@ -387,6 +409,15 @@ impl<'a> FromRow<'a, PgRow> for super::payments::filters::FilterRow {
                 ColumnNotFound(_) => Ok(Default::default()),
                 e => Err(e),
             })?;
+        let client_source: Option<String> = row.try_get("client_source").or_else(|e| match e {
+            ColumnNotFound(_) => Ok(Default::default()),
+            e => Err(e),
+        })?;
+        let client_version: Option<String> =
+            row.try_get("client_version").or_else(|e| match e {
+                ColumnNotFound(_) => Ok(Default::default()),
+                e => Err(e),
+            })?;
         Ok(Self {
             currency,
             status,
@@ -394,6 +425,8 @@ impl<'a> FromRow<'a, PgRow> for super::payments::filters::FilterRow {
             authentication_type,
             payment_method,
             payment_method_type,
+            client_source,
+            client_version,
         })
     }
 }
