2e403a67154a444e1482b208dfe2b3d37f443218
Shankar Singh C <shankar.singh@juspay.in>
2024-05-23 16:41:01 +0530

    include connector name in the logs

--- crates/router/src/core/payments/helpers.rs
+++ crates/router/src/core/payments/helpers.rs
@@ -3889,13 +3889,15 @@ pub fn validate_customer_access(
 }
 
 pub fn is_apple_pay_simplified_flow(
-    connector_metadata: Option<pii::SecretSerdeValue>,
+    connector_metadata: domain::MerchantConnectorAccount,
+    connector_name: Option<String>,
 ) -> CustomResult<bool, errors::ApiErrorResponse> {
     let option_apple_pay_metadata = get_applepay_metadata(connector_metadata)
-        .map_err(|err| {
+        .map_err(|error| {
             logger::info!(
-                "Apple pay metadata parsing in is_apple_pay_simplified_flow {:?}",
-                err
+                "Apple pay metadata parsing for {:?} in is_apple_pay_simplified_flow {:?}",
+                connector_name,
+                error
             )
         })
         .ok();
@@ -3967,7 +3969,7 @@ where
             field_name: "profile_id",
         })?;
 
-    let merchant_connector_account = get_merchant_connector_account(
+    let merchant_connector_account_type = get_merchant_connector_account(
         &state,
         merchant_account.merchant_id.as_str(),
         payment_data.creds_identifier.to_owned(),
@@ -3976,12 +3978,14 @@ where
         &decided_connector_data.connector_name.to_string(),
         merchant_connector_id,
     )
-    .await?
-    .get_metadata();
+    .await?;
 
     println!("aaa {:?}", merchant_connector_account);
 
-    let connector_data_list = if is_apple_pay_simplified_flow(merchant_connector_account)? {
+    let connector_data_list = if is_apple_pay_simplified_flow(
+        merchant_connector_account_type.get_metadata(),
+        merchant_connector_account_type.get_connector_name(),
+    )? {
         let merchant_connector_account_list = state
             .store
             .find_merchant_connector_account_by_merchant_id_and_disabled_list(
@@ -3995,7 +3999,10 @@ where
         let mut connector_data_list = vec![decided_connector_data.clone()];
 
         for merchant_connector_account in merchant_connector_account_list {
-            if is_apple_pay_simplified_flow(merchant_connector_account.metadata)? {
+            if is_apple_pay_simplified_flow(
+                merchant_connector_account.metadata,
+                Some(merchant_connector_account.connector_name),
+            )? {
                 let connector_data = api::ConnectorData::get_connector_by_name(
                     &state.conf.connectors,
                     &merchant_connector_account.connector_name.to_string(),
