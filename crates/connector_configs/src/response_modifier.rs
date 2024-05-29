a6570b6a06917be27e543cb35bb210df4fc6391e
Swangi Kumari <85639103+swangi-kumari@users.noreply.github.com>
2024-05-29 15:39:10 +0530

    refactor(connector): [Klarna] Refactor Authorize call and configs for prod (#4750)
    Co-authored-by: Samraat Bansal <samraat.bansal@juspay.in>

--- crates/connector_configs/src/response_modifier.rs
+++ crates/connector_configs/src/response_modifier.rs
@@ -322,6 +322,7 @@ impl From<ApiModelMetaData> for DashboardMetaData {
             three_ds_requestor_id: api_model.three_ds_requestor_id,
             pull_mechanism_for_external_3ds_enabled: api_model
                 .pull_mechanism_for_external_3ds_enabled,
+            klarna_region: api_model.klarna_region,
         }
     }
 }
