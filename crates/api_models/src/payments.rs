bf9393a4dcbeeb380621352018bb1e43500ca008
Sahkal Poddar <sahkal.poddar@juspay.in>
2024-05-30 17:50:54 +0530

    refactor(connector):added amount framework to charged object

--- crates/api_models/src/payments.rs
+++ crates/api_models/src/payments.rs
@@ -487,7 +487,8 @@ pub struct PaymentChargeRequest {
     pub charge_type: api_enums::PaymentChargeType,
 
     /// Platform fees to be collected on the payment
-    pub fees: i64,
+    #[schema(value_type = i64, example = 6540)]
+    pub fees: MinorUnit,
 
     /// Identifier for the reseller's account to send the funds to
     pub transfer_account_id: String,
@@ -3504,7 +3505,8 @@ pub struct PaymentChargeResponse {
     pub charge_type: api_enums::PaymentChargeType,
 
     /// Platform fees collected on the payment
-    pub application_fees: i64,
+    #[schema(value_type = i64, example = 6540)]
+    pub application_fees: MinorUnit,
 
     /// Identifier for the reseller's account where the funds were transferred
     pub transfer_account_id: String,
