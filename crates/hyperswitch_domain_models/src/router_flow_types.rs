4c173026a2f8cefa3a2596af3846727ab7f0b680
hrithikesh026 <hrithikesh.vm@juspay.in>
2024-05-30 07:52:44 +0530

    avoid generics where not necessary

--- crates/hyperswitch_domain_models/src/router_flow_types.rs
+++ crates/hyperswitch_domain_models/src/router_flow_types.rs
@@ -6,3 +6,12 @@ pub mod payments;
 pub mod payouts;
 pub mod refunds;
 pub mod webhooks;
+
+pub use access_token_auth::*;
+pub use dispute::*;
+pub use files::*;
+pub use fraud_check::*;
+pub use payments::*;
+pub use payouts::*;
+pub use refunds::*;
+pub use webhooks::*;
