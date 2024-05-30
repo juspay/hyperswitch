f9b71d1e739027318c42da8145433e505f2bb507
kiran.kummari <kiran.kummari@juspay.in>
2024-05-30 18:43:53 +0530

    drop the table

--- migrations/2024-05-15-113543_add_additional_fields_to_authentication_table/down.sql
+++
@@ -1,3 +0,0 @@
--- This file should undo anything in `up.sql`
-ALTER TABLE authentication DROP COLUMN three_ds_requestor_trans_id,
-DROP COLUMN authentication_url;
--- migrations/2024-05-15-113543_add_additional_fields_to_authentication_table/up.sql
+++
@@ -1,3 +0,0 @@
--- Your SQL goes here
-ALTER TABLE authentication ADD COLUMN three_ds_requestor_trans_id VARCHAR(64),
-ADD COLUMN authentication_url VARCHAR;
