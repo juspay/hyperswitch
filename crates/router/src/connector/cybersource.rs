ebfdaa09bfc85a3b3852e4134cabb85fc7c099ee
AkshayaFoiger <akshaya.shankar@juspay.in>
2024-06-04 15:36:48 +0530

    undo error_code change

--- crates/router/src/connector/cybersource.rs
+++ crates/router/src/connector/cybersource.rs
@@ -135,7 +135,7 @@ impl ConnectorCommon for Cybersource {
                 event_builder.map(|i| i.set_error_response_body(&response));
                 router_env::logger::info!(connector_response=?response);
 
-                let (error_code, error_message, connector_reason) = match response.error_information
+                let (code, error_message, connector_reason) = match response.error_information
                 {
                     Some(ref error_info) => {
                         let detailed_error_info = error_info.details.as_ref().map(|details| {
@@ -184,7 +184,7 @@ impl ConnectorCommon for Cybersource {
 
                 Ok(types::ErrorResponse {
                     status_code: res.status_code,
-                    code: error_code,
+                    code,
                     message: error_message,
                     reason: connector_reason,
                     attempt_status: None,
