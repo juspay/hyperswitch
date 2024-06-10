1cb24859f4f450ea7a41f9b85aaba684bf8d4cd1
Narayan Bhat <narayan.bhat@juspay.in>
2024-06-10 11:27:14 +0530

    chore: cargo clippy

--- migrations/2024-06-03-090859_make_id_as_optional_for_payments/down.sql
+++ migrations/2024-06-03-090859_make_id_as_optional_for_payments/down.sql
@@ -1,16 +1,18 @@
 -- This file should undo anything in `up.sql`
 ALTER TABLE payment_attempt
 ALTER COLUMN id
-SET NOT NULL:
-ALTER TABLE payment_attempt DROP payment_attempt_pkey;
+SET NOT NULL;
+
+ALTER TABLE payment_attempt DROP CONSTRAINT payment_attempt_pkey;
 
 ALTER TABLE payment_attempt
 ADD PRIMARY KEY (id);
 
 ALTER TABLE payment_intent
 ALTER COLUMN id
-SET NOT NULL:
-ALTER TABLE payment_intent DROP payment_intent_pkey;
+SET NOT NULL;
+
+ALTER TABLE payment_intent DROP CONSTRAINT payment_intent_pkey;
 
 ALTER TABLE payment_intent
 ADD PRIMARY KEY (id);
