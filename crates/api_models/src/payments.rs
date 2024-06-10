8fccf36da54f008534f3e99eea98912881979fe1
Shankar Singh C <shankar.singh@juspay.in>
2024-06-10 15:22:25 +0530

    revert testing commit

--- crates/api_models/src/payments.rs
+++ crates/api_models/src/payments.rs
@@ -4067,9 +4067,6 @@ pub struct GpayTokenParameters {
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, serde::Serialize, serde::Deserialize, ToSchema)]
-pub struct CiTesting {}
-
-#[derive(Debug, Clone, Eq, PartialEq, serde::Serialize, serde::Deserialize, ToSchema)]
 pub struct GpayTokenizationSpecification {
     /// The token specification type(ex: PAYMENT_GATEWAY)
     #[serde(rename = "type")]
