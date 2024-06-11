6be03253a89c16e2affe26dc5ec8c36911f87295
Srujan chikke <chikke.srujan@juspay.in>
2024-06-11 16:28:07 +0530

    fix validate openapi spec

--- crates/openapi/src/openapi.rs
+++ crates/openapi/src/openapi.rs
@@ -468,6 +468,7 @@ Never share your secret api keys. Keep them guarded and secure.
         api_models::payouts::PayoutRetrieveRequest,
         api_models::payouts::PayoutMethodData,
         api_models::payouts::Bank,
+        api_models::payouts::PayoutCreatePayoutLinkConfig,
         api_models::enums::PayoutEntityType,
         api_models::enums::PayoutSendPriority,
         api_models::enums::PayoutStatus,
