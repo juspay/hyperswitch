41939aa947b8da142561cd9b2e7bbca1b188fa46
PiX <69745008+pixincreate@users.noreply.github.com>
2024-06-13 16:01:01 +0530

    re-organise

--- cypress-tests/cypress/e2e/PayoutTest/00004-BankTransfer.cy.js
+++ cypress-tests/cypress/e2e/PayoutTest/00004-BankTransfer.cy.js
@@ -4,6 +4,58 @@ import * as utils from "../PayoutUtils/utils";
 
 let globalState;
 
+// TODO: Add test for Bank Transfer - ACH
+describe("[Payout] [Bank Transfer - ACH]", () => {
+  let should_continue = true; // variable that will be used to skip tests if a previous test fails
+
+  before("seed global state", () => {
+    cy.task("getGlobalState").then((state) => {
+      globalState = new State(state);
+
+      // Check if the connector supports card payouts (based on the connector configuration in creds)
+      if (!globalState.get("payoutsExecution")) {
+        should_continue = false;
+      }
+    });
+  });
+
+  after("flush global state", () => {
+    cy.task("setGlobalState", globalState.data);
+  });
+
+  beforeEach(function () {
+    if (!should_continue) {
+      this.skip();
+    }
+  });
+});
+
+// TODO: Add test for Bank Transfer - BACS
+describe("[Payout] [Bank Transfer - BACS]", () => {
+  let should_continue = true; // variable that will be used to skip tests if a previous test fails
+
+  before("seed global state", () => {
+    cy.task("getGlobalState").then((state) => {
+      globalState = new State(state);
+
+      // Check if the connector supports card payouts (based on the connector configuration in creds)
+      if (!globalState.get("payoutsExecution")) {
+        should_continue = false;
+      }
+    });
+  });
+
+  after("flush global state", () => {
+    cy.task("setGlobalState", globalState.data);
+  });
+
+  beforeEach(function () {
+    if (!should_continue) {
+      this.skip();
+    }
+  });
+});
+
 describe("[Payout] [Bank Transfer - SEPA]", () => {
   let should_continue = true; // variable that will be used to skip tests if a previous test fails
 
@@ -135,57 +187,5 @@ describe("[Payout] [Bank Transfer - SEPA]", () => {
     it("retrieve-payout-call-test", () => {
       cy.retrievePayoutCallTest(globalState);
     });
-  });
-});
-
-// TODO: Add test for Bank Transfer - ACH
-describe("[Payout] [Bank Transfer - ACH]", () => {
-  let should_continue = true; // variable that will be used to skip tests if a previous test fails
-
-  before("seed global state", () => {
-    cy.task("getGlobalState").then((state) => {
-      globalState = new State(state);
-
-      // Check if the connector supports card payouts (based on the connector configuration in creds)
-      if (!globalState.get("payoutsExecution")) {
-        should_continue = false;
-      }
-    });
-  });
-
-  after("flush global state", () => {
-    cy.task("setGlobalState", globalState.data);
-  });
-
-  beforeEach(function () {
-    if (!should_continue) {
-      this.skip();
-    }
-  });
-});
-
-// TODO: Add test for Bank Transfer - BACS
-describe("[Payout] [Bank Transfer - BACS]", () => {
-  let should_continue = true; // variable that will be used to skip tests if a previous test fails
-
-  before("seed global state", () => {
-    cy.task("getGlobalState").then((state) => {
-      globalState = new State(state);
-
-      // Check if the connector supports card payouts (based on the connector configuration in creds)
-      if (!globalState.get("payoutsExecution")) {
-        should_continue = false;
-      }
-    });
-  });
-
-  after("flush global state", () => {
-    cy.task("setGlobalState", globalState.data);
-  });
-
-  beforeEach(function () {
-    if (!should_continue) {
-      this.skip();
-    }
   });
 });
