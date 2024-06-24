080fde07929538e81b24e737b22fb60a7be65e6d
Narayan Bhat <narayan.bhat@juspay.in>
2024-06-24 12:08:30 +0530

    chore: fix typo

--- crates/router_env/src/logger/types.rs
+++ crates/router_env/src/logger/types.rs
@@ -68,7 +68,7 @@ pub enum Flow {
     MerchantAccountCreateV2,
     /// Merchants account retrieve flow.
     MerchantsAccountRetrieve,
-    /// Merhcant account retrieve flow for v2
+    /// Merchant account retrieve flow for v2
     #[cfg(feature = "v2")]
     MerchantAccountRetrieveV2,
     /// Merchants account update flow.
