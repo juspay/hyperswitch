542fa521772119d7e5cbf0189ace2d2185493b18
GORAKHNATH YADAV <gorakhcodes@gmail.com>
2024-06-26 01:14:26 +0530

    Update crates/api_models/src/payments.rs
    Co-authored-by: Sanchith Hegde <22217505+SanchithHegde@users.noreply.github.com>

--- crates/api_models/src/payments.rs
+++ crates/api_models/src/payments.rs
@@ -3440,7 +3440,7 @@ pub struct PaymentsResponse {
     #[schema(value_type = Option<PaymentExperience>, example = "redirect_to_url")]
     pub payment_experience: Option<api_enums::PaymentExperience>,
 
-    /// To select Payment Method Type
+    /// Can be used to specify the Payment Method Type
     #[schema(value_type = Option<PaymentMethodType>, example = "gpay")]
     pub payment_method_type: Option<api_enums::PaymentMethodType>,
 
