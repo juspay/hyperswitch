dbde6ecc76791f02e637bca512b97960e010876a
prajjwalkumar17 <prajjwal.kumar@juspay.in>
2024-06-27 18:37:17 +0530

    chore: addition of fallback for the customer data

--- crates/router/src/core/payments/helpers.rs
+++ crates/router/src/core/payments/helpers.rs
@@ -1584,6 +1584,20 @@ pub async fn create_customer_if_not_exist<'a, F: Clone, R>(
         .get_required_value("customer")
         .change_context(errors::StorageError::ValueNotFound("customer".to_owned()))?;
 
+    let temp_customer_data = if request_customer_details.name.is_some() ||
+        request_customer_details.email.is_some() ||
+        request_customer_details.phone.is_some() ||
+        request_customer_details.phone_country_code.is_some() {
+            Some(CustomerData { 
+                name: request_customer_details.name.clone(),
+                email: request_customer_details.email.clone(),
+                phone: request_customer_details.phone.clone(),
+                phone_country_code: request_customer_details.phone_country_code.clone(),
+            })
+        } else {
+            None
+        };
+
     // Updation of Customer Details for the cases where both customer_id and specific customer
     // details are provided in Payment Update Request
     let raw_customer_details = payment_data
@@ -1591,38 +1605,25 @@ pub async fn create_customer_if_not_exist<'a, F: Clone, R>(
         .customer_details
         .clone()
         .map(|customer_details_encrypted| {
-            customer_details_encrypted
-                .into_inner()
-                .expose()
-                .parse_value::<CustomerData>("CustomerData")
-        })
-        .transpose()
-        .change_context(errors::StorageError::DeserializationFailed)
-        .attach_printable("Failed to parse customer data from payment intent")?
-        .map(|parsed_customer_data| CustomerData {
-            name: request_customer_details
-                .name
-                .clone()
-                .or(parsed_customer_data.name.clone()),
-            email: request_customer_details
-                .email
-                .clone()
-                .or(parsed_customer_data.email.clone()),
-            phone: request_customer_details
-                .phone
-                .clone()
-                .or(parsed_customer_data.phone.clone()),
-            phone_country_code: request_customer_details
-                .phone_country_code
-                .clone()
-                .or(parsed_customer_data.phone_country_code.clone()),
-        })
-        .or(Some(CustomerData {
-            name: request_customer_details.name.clone(),
-            email: request_customer_details.email.clone(),
-            phone: request_customer_details.phone.clone(),
-            phone_country_code: request_customer_details.phone_country_code.clone(),
-        }));
+        customer_details_encrypted.into_inner()
+            .expose()
+            .parse_value::<CustomerData>("CustomerData")
+    })
+    .transpose()
+    .change_context(errors::StorageError::DeserializationFailed)
+    .attach_printable("Failed to parse customer data from payment intent")?
+        .map(|parsed_customer_data| {
+            CustomerData {
+                name: request_customer_details.name.clone()
+                    .or(parsed_customer_data.name.clone()),
+                email: request_customer_details.email.clone()
+                    .or(parsed_customer_data.email.clone()),
+                phone: request_customer_details.phone.clone()
+                    .or(parsed_customer_data.phone.clone()),
+                phone_country_code: request_customer_details.phone_country_code.clone()
+                    .or(parsed_customer_data.phone_country_code.clone()),
+            }
+        }).or(temp_customer_data);
 
     payment_data.payment_intent.customer_details = raw_customer_details
         .clone()
