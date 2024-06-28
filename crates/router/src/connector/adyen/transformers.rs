6a10c47b99d94c69c75fadbbd74ac68377dd563a
sai-harsha-vardhan <harsha111hero@gmail.com>
2024-06-28 12:31:19 +0530

    send browser_info for all the googlepay payments

--- crates/router/src/connector/adyen/transformers.rs
+++ crates/router/src/connector/adyen/transformers.rs
@@ -1672,6 +1672,7 @@ fn get_browser_info(
         || item.payment_method == storage_enums::PaymentMethod::Card
         || item.payment_method == storage_enums::PaymentMethod::BankRedirect
         || item.request.payment_method_type == Some(storage_enums::PaymentMethodType::GoPay)
+        || item.request.payment_method_type == Some(storage_enums::PaymentMethodType::GooglePay)
     {
         let info = item.request.get_browser_info()?;
         Ok(Some(AdyenBrowserInfo {
