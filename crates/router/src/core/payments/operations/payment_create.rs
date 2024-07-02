6f078050f9a419e1aed552bc6cf8c8016cfa1e9a
Sahkal Poddar <sahkalplanet@gmail.com>
2024-07-02 13:49:45 +0530

    refactor(router): changed payment method token TTL to api contract based config from const value (#5115)
    Co-authored-by: hyperswitch-bot[bot] <148525504+hyperswitch-bot[bot]@users.noreply.github.com>

--- crates/router/src/core/payments/operations/payment_create.rs
+++ crates/router/src/core/payments/operations/payment_create.rs
@@ -502,6 +502,7 @@ impl<F: Clone + Send> Domain<F, api::PaymentsRequest> for PaymentCreate {
         storage_scheme: enums::MerchantStorageScheme,
         merchant_key_store: &domain::MerchantKeyStore,
         customer: &Option<domain::Customer>,
+        business_profile: Option<&diesel_models::business_profile::BusinessProfile>,
     ) -> RouterResult<(
         BoxedOperation<'a, F, api::PaymentsRequest>,
         Option<api::PaymentMethodData>,
@@ -514,6 +515,7 @@ impl<F: Clone + Send> Domain<F, api::PaymentsRequest> for PaymentCreate {
             merchant_key_store,
             customer,
             storage_scheme,
+            business_profile,
         )
         .await
     }
