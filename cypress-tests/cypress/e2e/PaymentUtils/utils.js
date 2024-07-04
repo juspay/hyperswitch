959592c12d05074cc79343358426abe171d55522
Pa1NarK <69745008+pixincreate@users.noreply.github.com>
2024-07-04 15:48:21 +0530

    feat(cypress): make tests forcefully skippable (#5176)

--- cypress-tests/cypress/e2e/PaymentUtils/utils.js
+++ cypress-tests/cypress/e2e/PaymentUtils/utils.js
@@ -77,6 +77,10 @@ export function getValueByKey(jsonObject, key) {
 }
 
 export const should_continue_further = (res_data) => {
+  if (res_data.trigger_skip !== undefined) {
+    return !res_data.trigger_skip;
+  }
+
   if (
     res_data.body.error !== undefined ||
     res_data.body.error_code !== undefined ||
