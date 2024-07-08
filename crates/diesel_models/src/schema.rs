77a13fb9278adff41837cddd868d1e2885ae073f
Narayan Bhat <narayan.bhat@juspay.in>
2024-07-08 20:40:08 +0530

    fix: schema file

--- crates/diesel_models/src/schema.rs
+++ crates/diesel_models/src/schema.rs
@@ -727,16 +727,6 @@ diesel::table! {
     use diesel::sql_types::*;
     use crate::enums::diesel_exports::*;
 
-    my_table (id) {
-        id -> Int4,
-        data -> Nullable<Jsonb>,
-    }
-}
-
-diesel::table! {
-    use diesel::sql_types::*;
-    use crate::enums::diesel_exports::*;
-
     organization (org_id) {
         #[max_length = 32]
         org_id -> Varchar,
@@ -1327,7 +1317,6 @@ diesel::allow_tables_to_appear_in_same_query!(
     merchant_account,
     merchant_connector_account,
     merchant_key_store,
-    my_table,
     organization,
     payment_attempt,
     payment_intent,
