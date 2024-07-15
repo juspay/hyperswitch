7889156ec9e2235aa9ec5fa3850cb402a816e0e7
sai-harsha-vardhan <harsha111hero@gmail.com>
2024-07-15 22:22:04 +0530

    change primary key for dispute and mandate

---
+++ migrations/2024-07-15-120121_change_primary_key_for_dispute/up.sql
@@ -0,0 +1,9 @@
+-- Your SQL goes here
+-- The below query will lock the dispute table
+-- Running this query is not necessary on higher environments
+-- as the application will work fine without these queries being run
+-- This query is necessary for the application to not use id in update of dispute
+ALTER TABLE dispute DROP CONSTRAINT dispute_pkey;
+
+-- Use the `dispute_id` column as primary key
+ALTER TABLE dispute ADD PRIMARY KEY (dispute_id);
---
+++ migrations/2024-07-15-143920_change_primary_key_for_mandate/up.sql
@@ -0,0 +1,9 @@
+-- Your SQL goes here
+-- The below query will lock the mandate table
+-- Running this query is not necessary on higher environments
+-- as the application will work fine without these queries being run
+-- This query is necessary for the application to not use id in update of mandate
+ALTER TABLE mandate DROP CONSTRAINT mandate_pkey;
+
+-- Use the `mandate_id` column as primary key
+ALTER TABLE mandate ADD PRIMARY KEY (mandate_id);
