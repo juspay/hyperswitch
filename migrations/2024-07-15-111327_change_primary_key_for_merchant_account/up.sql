168915721bd3aff8b9644d5fffac98c8cd35886e
Narayan Bhat <narayan.bhat@juspay.in>
2024-07-17 18:34:42 +0530

    chore: add comment on migration

--- migrations/2024-07-15-111327_change_primary_key_for_merchant_account/up.sql
+++ migrations/2024-07-15-111327_change_primary_key_for_merchant_account/up.sql
@@ -3,6 +3,7 @@
 -- Running this query is not necessary on higher environments
 -- as the application will work fine without these queries being run
 -- This query is necessary for the application to not use id in update of merchant_account
+-- This query should be run after the new version of application is deployed
 ALTER TABLE merchant_account DROP CONSTRAINT merchant_account_pkey;
 
 -- Use the `merchant_id` column as primary key
