dd6603c87a79e540e91afe898da9d471dc68d425
kiran.kummari <kiran.kummari@juspay.in>
2024-07-18 15:14:51 +0530

    clippy issue resolved

--- crates/router/src/types/api.rs
+++ crates/router/src/types/api.rs
@@ -393,7 +393,7 @@ impl ConnectorData {
                 enums::Connector::Datatrans => {
                     Ok(ConnectorEnum::Old(Box::new(connector::Datatrans::new())))
                 }
-                enums::Connector::Dlocal => Ok(ConnectorEnum::Old(Box::new(&connector::Dlocal))),
+                enums::Connector::Dlocal => Ok(ConnectorEnum::Old(Box::new(connector::Dlocal::new()))),
                 #[cfg(feature = "dummy_connector")]
                 enums::Connector::DummyConnector1 => Ok(ConnectorEnum::Old(Box::new(
                     &connector::DummyConnector::<1>,
