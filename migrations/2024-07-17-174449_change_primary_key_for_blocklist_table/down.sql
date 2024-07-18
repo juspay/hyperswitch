472d88d096b8c31fae91f409642cdc7e043a747d
Prajjwal Kumar <prajjwal.kumar@juspay.in>
2024-07-18 16:01:46 +0530

    chore: update comments

--- migrations/2024-07-17-174449_change_primary_key_for_blocklist_table/down.sql
+++ migrations/2024-07-17-174449_change_primary_key_for_blocklist_table/down.sql
@@ -2,6 +2,7 @@
 -- Use the `merchant_id, fingerprint_id` columns as primary key
 -- These are already unique, not null columns
 -- So this query should not fail for not null or duplicate value reasons
+-- This query should be run after the new version of application is deployed
 ALTER TABLE blocklist DROP CONSTRAINT blocklist_pkey;
 
 ALTER TABLE blocklist
