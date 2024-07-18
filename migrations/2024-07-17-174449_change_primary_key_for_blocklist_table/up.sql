a3db89ad94e71c7137fe27e6cf120dab5fa4bbea
prajjwalkumar17 <prajjwal.kumar@juspay.in>
2024-07-18 16:30:49 +0530

    chore: comments beautification

--- migrations/2024-07-17-174449_change_primary_key_for_blocklist_table/up.sql
+++ migrations/2024-07-17-174449_change_primary_key_for_blocklist_table/up.sql
@@ -1,7 +1,11 @@
 -- Your SQL goes here
+-- The below query will lock the blocklist table
+-- Use the `merchant_id, fingerprint_id` columns as primary key
+-- These are already unique, not null columns
+-- So this query should not fail for not null or duplicate value reasons
 -- Running this query is not necessary on higher environments
 -- as the application will work fine without these queries being run
--- This query is necessary for the application to not use id of blocklist in any operation
+
 ALTER TABLE blocklist DROP CONSTRAINT blocklist_pkey;
 
 ALTER TABLE blocklist
