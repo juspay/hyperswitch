1dc54d6ee196634890b8e6cf0dd504e2ff08ed3b
Narayan Bhat <narayan.bhat@juspay.in>
2024-07-18 23:44:06 +0530

    refactor: refactor script to increase readability

--- scripts/ci-checks.sh
+++ scripts/ci-checks.sh
@@ -65,30 +65,34 @@ all_commands=()
 # Process the metadata to generate the cargo check commands for crates which have v1 features
 # We need to always have the v1 feature with each feature
 # This is because, no crate should be run without any features
-while IFS=' ' read -r crate features; do
-  command="cargo check --all-targets --package \"${crate}\" --no-default-features --features \"${features}\""
-  all_commands+=("$command")
-done < <(jq --monochrome-output --raw-output \
-  --argjson crates_with_features "${crates_with_features}" \
-  --null-input \
-  '$crates_with_features[] 
+crates_with_v1_feature="$(
+  jq --monochrome-output --raw-output \
+    --argjson crates_with_features "${crates_with_features}" \
+    --null-input \
+    '$crates_with_features[] 
     | select( IN("v1"; .features[]))  # Select crates with `v1` feature
     | { name, features: (.features - ["v1", "v2", "default", "payment_v2", "merchant_account_v2"]) }  # Remove specific features to generate feature combinations
     | { name, features: ( .features | map([., "v1"] | join(",")) ) }  # Add `v1` to remaining features and join them by comma
     | .name as $name | .features[] | { $name, features: . }  # Expand nested features object to have package - features combinations
-    | "\(.name) \(.features)"') # Print out package name and features separated by space
-
-echo $all_commands
+    | "\(.name) \(.features)" # Print out package name and features separated by space'
+)"
+while IFS=' ' read -r crate features; do
+  command="cargo check --all-targets --package \"${crate}\" --no-default-features --features \"${features}\""
+  all_commands+=("$command")
+done <<< "${crates_with_v1_feature}"
 
 # For crates which do not have v1 feature, we can run the usual cargo hack command
-while IFS=' ' read -r crate; do
+crates_without_v1_feature="$(
+  jq --monochrome-output --raw-output \
+    --argjson crates_with_features "${crates_with_features}" \
+    --null-input \
+    '$crates_with_features[] | select(IN("v1"; .features[]) | not ) # Select crates without `v1` feature
+    | "\(.name)" # Print out package name'
+)"
+while IFS= read -r crate; do
   command="cargo hack check --all-targets --each-feature --package \"${crate}\""
   all_commands+=("$command")
-done < <(jq --monochrome-output --raw-output \
-  --argjson crates_with_features "${crates_with_features}" \
-  --null-input \
-  '$crates_with_features[] | select(IN("v1"; .features[]) | not ) # Select crates without `v1` feature
-  | "\(.name)" # Print out package name')
+done <<< "${crates_without_v1_feature}"
 
 echo "The list of commands that will be executed:"
 printf "%s\n" "${all_commands[@]}"
