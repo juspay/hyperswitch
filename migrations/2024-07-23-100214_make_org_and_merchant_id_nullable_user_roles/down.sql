7dd650164787f104e8feecb5b3041d82df7af455
Mani Chandra Dulam <mani.dchandra@juspay.in>
2024-07-23 16:23:32 +0530

    refactor: format SQL files

--- migrations/2024-07-23-100214_make_org_and_merchant_id_nullable_user_roles/down.sql
+++ migrations/2024-07-23-100214_make_org_and_merchant_id_nullable_user_roles/down.sql
@@ -1,7 +1,6 @@
 -- This file should undo anything in `up.sql`
 ALTER TABLE user_roles DROP CONSTRAINT user_roles_pkey;
-ALTER TABLE user_roles
-ADD PRIMARY KEY (user_id, merchant_id);
+ALTER TABLE user_roles ADD PRIMARY KEY (user_id, merchant_id);
 
 ALTER TABLE user_roles ALTER COLUMN org_id SET NOT NULL;
 ALTER TABLE user_roles ALTER COLUMN merchant_id SET NOT NULL;
--- migrations/2024-07-23-100214_make_org_and_merchant_id_nullable_user_roles/up.sql
+++ migrations/2024-07-23-100214_make_org_and_merchant_id_nullable_user_roles/up.sql
@@ -1,7 +1,6 @@
 -- Your SQL goes here
 ALTER TABLE user_roles DROP CONSTRAINT user_roles_pkey;
-ALTER TABLE user_roles
-ADD PRIMARY KEY (id);
+ALTER TABLE user_roles ADD PRIMARY KEY (id);
 
 ALTER TABLE user_roles ALTER COLUMN org_id DROP NOT NULL;
 ALTER TABLE user_roles ALTER COLUMN merchant_id DROP NOT NULL;
