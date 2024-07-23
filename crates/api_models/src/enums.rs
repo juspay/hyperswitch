afae5906a8d6ceab136393c7588bfc447e822ddc
Sakil Mostak <73734619+Sakilmostak@users.noreply.github.com>
2024-07-23 20:54:56 +0530

    refactor(connector): [Itaubank] add dynamic fields for pix (#5419)
    Co-authored-by: hyperswitch-bot[bot] <148525504+hyperswitch-bot[bot]@users.noreply.github.com>

--- crates/api_models/src/enums.rs
+++ crates/api_models/src/enums.rs
@@ -476,6 +476,9 @@ pub enum FieldType {
     UserDateOfBirth,
     UserVpaId,
     LanguagePreference { options: Vec<String> },
+    UserPixKey,
+    UserCpf,
+    UserCnpj,
 }
 
 impl FieldType {
@@ -562,6 +565,9 @@ impl PartialEq for FieldType {
             ) => options_self.eq(options_other),
             (Self::UserDateOfBirth, Self::UserDateOfBirth) => true,
             (Self::UserVpaId, Self::UserVpaId) => true,
+            (Self::UserPixKey, Self::UserPixKey) => true,
+            (Self::UserCpf, Self::UserCpf) => true,
+            (Self::UserCnpj, Self::UserCnpj) => true,
             (Self::LanguagePreference { .. }, Self::LanguagePreference { .. }) => true,
             _unused => false,
         }
