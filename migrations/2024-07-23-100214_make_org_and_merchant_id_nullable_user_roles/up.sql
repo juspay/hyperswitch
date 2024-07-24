11786a4abbfd1312196ee955e4f3f09e7eca639c
Mani Chandra Dulam <mani.dchandra@juspay.in>
2024-07-24 19:03:15 +0530

    fix: change profile_id and entity_id length in user_roles to 64

--- migrations/2024-07-23-100214_make_org_and_merchant_id_nullable_user_roles/up.sql
+++ migrations/2024-07-23-100214_make_org_and_merchant_id_nullable_user_roles/up.sql
@@ -12,8 +12,8 @@ ALTER TABLE user_roles ADD PRIMARY KEY (id);
 ALTER TABLE user_roles ALTER COLUMN org_id DROP NOT NULL;
 ALTER TABLE user_roles ALTER COLUMN merchant_id DROP NOT NULL;
 
-ALTER TABLE user_roles ADD COLUMN profile_id VARCHAR(255);
-ALTER TABLE user_roles ADD COLUMN entity_id VARCHAR(255);
+ALTER TABLE user_roles ADD COLUMN profile_id VARCHAR(64);
+ALTER TABLE user_roles ADD COLUMN entity_id VARCHAR(64);
 ALTER TABLE user_roles ADD COLUMN entity_type VARCHAR(64);
 
 CREATE TYPE "UserRoleVersion" AS ENUM('v1', 'v2');
