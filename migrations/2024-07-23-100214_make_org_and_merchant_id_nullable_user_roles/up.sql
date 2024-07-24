f56846435dea1bbeb4fa170abe9b8eea179d4c8b
Mani Chandra Dulam <mani.dchandra@juspay.in>
2024-07-24 18:18:39 +0530

    refactor: add safety comment in migration

--- migrations/2024-07-23-100214_make_org_and_merchant_id_nullable_user_roles/up.sql
+++ migrations/2024-07-23-100214_make_org_and_merchant_id_nullable_user_roles/up.sql
@@ -1,5 +1,12 @@
 -- Your SQL goes here
+-- The below query will lock the user_roles table
+-- Running this query is not necessary on higher environments
+-- as the application will work fine without these queries being run
+-- This query should be run after the new version of application is deployed
 ALTER TABLE user_roles DROP CONSTRAINT user_roles_pkey;
+-- Use the `id` column as primary key
+-- This is serial and a not null column
+-- So this query should not fail for not null or duplicate value reasons
 ALTER TABLE user_roles ADD PRIMARY KEY (id);
 
 ALTER TABLE user_roles ALTER COLUMN org_id DROP NOT NULL;
