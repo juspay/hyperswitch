e3401c09bf0a42179047e73dd4abeef9f1a422d2
sai-harsha-vardhan <harsha111hero@gmail.com>
2024-07-29 01:02:34 +0530

    fix migrations

--- v2_migrations/2024-07-24-065721_modify_id_mca/down.sql
+++ v2_migrations/2024-07-24-065721_modify_id_mca/down.sql
@@ -3,6 +3,8 @@ ALTER TABLE merchant_connector_account DROP CONSTRAINT merchant_connector_accoun
 
 DROP INDEX IF EXISTS merchant_connector_account_id_index;
 
+UPDATE merchant_connector_account SET merchant_connector_id = id;
+
 ALTER TABLE merchant_connector_account DROP COLUMN IF EXISTS id;
 
 ALTER TABLE merchant_connector_account ADD COLUMN IF NOT EXISTS id SERIAL;
