8784cb67f5c67ffa3d1bba2f091460c5dd5f5f82
likhinbopanna <likhin.bopanna@juspay.in>
2024-07-29 17:58:21 +0530

    ci(cypress): Added shipping address test cases for PML

--- cypress-tests/cypress/e2e/PaymentMethodListUtils/Commons.js
+++ cypress-tests/cypress/e2e/PaymentMethodListUtils/Commons.js
@@ -114,7 +114,8 @@ export const bank_redirect_ideal_and_credit_enabled = [
 
 export const create_payment_body_with_currency_country = (
   currency,
-  billingCountry
+  billingCountry,
+  shippingCountry
 ) => ({
   currency: currency,
   amount: 6500,
@@ -142,6 +143,24 @@ export const create_payment_body_with_currency_country = (
     },
     phone: {
       number: "8056594427",
+      country_code: "+91",
+    },
+    email: "example@example.com",
+  },
+  shipping: {
+    address: {
+      line1: "130",
+      line2: "Koramangala",
+      line3: "Ramnagar",
+      city: "Bengaluru",
+      state: "Karnataka",
+      zip: "560011",
+      country: shippingCountry, // Shipping country from parameter
+      first_name: "John",
+      last_name: "Joseph",
+    },
+    phone: {
+      number: "9888888888",
       country_code: "+91",
     },
     email: "example@example.com",
