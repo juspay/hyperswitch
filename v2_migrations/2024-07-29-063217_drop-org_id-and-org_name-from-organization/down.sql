7fe38c70d55a3b49c33f4344e7eede5ebbcd3a54
hrithikesh026 <hrithikesh.vm@juspay.in>
2024-07-29 15:01:33 +0530

    update down.sql for v2 migration

--- v2_migrations/2024-07-29-063217_drop-org_id-and-org_name-from-organization/down.sql
+++ v2_migrations/2024-07-29-063217_drop-org_id-and-org_name-from-organization/down.sql
@@ -1,1 +1,9 @@
--- This file should undo anything in `up.sql`
+-- Alter queries
+ALTER TABLE organization
+DROP CONSTRAINT pk_id;
+
+ALTER TABLE organization
+ADD COLUMN org_id VARCHAR(32) PRIMARY KEY NOT NULL;
+
+ALTER TABLE organization
+ADD COLUMN org_name TEXT;
