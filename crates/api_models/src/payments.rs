be9347b8d56c0a6cf0d04cf51c75dd6426d3a21a
Sakil Mostak <73734619+Sakilmostak@users.noreply.github.com>
2024-07-30 18:52:38 +0530

    fix(connector): [Pix] convert data type of pix fields  (#5476)

--- crates/api_models/src/payments.rs
+++ crates/api_models/src/payments.rs
@@ -2473,11 +2473,11 @@ pub enum BankTransferData {
         #[schema(value_type = Option<String>, example = "a1f4102e-a446-4a57-bcce-6fa48899c1d1")]
         pix_key: Option<Secret<String>>,
         /// CPF is a Brazilian tax identification number
-        #[schema(value_type = Option<i64>, example = "10599054689")]
-        cpf: Option<Secret<i64>>,
+        #[schema(value_type = Option<String>, example = "10599054689")]
+        cpf: Option<Secret<String>>,
         /// CNPJ is a Brazilian company tax identification number
-        #[schema(value_type = Option<i64>, example = "74469027417312")]
-        cnpj: Option<Secret<i64>>,
+        #[schema(value_type = Option<String>, example = "74469027417312")]
+        cnpj: Option<Secret<String>>,
     },
     Pse {},
     LocalBankTransfer {
