07196660c8e8ec604cda2d46dc1471633d89797c
sai-harsha-vardhan <harsha111hero@gmail.com>
2024-08-02 01:29:45 +0530

    remove redundant migrations

--- v2_migrations/2024-07-23-140609_remove_business_and_test_mode_columns_mca/down.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE merchant_connector_account ADD COLUMN IF NOT EXISTS business_country "CountryAlpha2";
-ALTER TABLE merchant_connector_account ADD COLUMN IF NOT EXISTS business_label VARCHAR(255);
-ALTER TABLE merchant_connector_account ADD COLUMN IF NOT EXISTS business_sub_label VARCHAR(64);
-ALTER TABLE merchant_connector_account ADD COLUMN IF NOT EXISTS test_mode BOOLEAN;
--- v2_migrations/2024-07-23-140609_remove_business_and_test_mode_columns_mca/up.sql
+++
@@ -1,6 +0,0 @@
--- Your SQL goes here
--- This migration is to remove the business_country, business_label, business_sub_label, and test_mode columns from the merchant_connector_account table
-ALTER TABLE merchant_connector_account DROP COLUMN IF EXISTS business_country;
-ALTER TABLE merchant_connector_account DROP COLUMN IF EXISTS business_label;
-ALTER TABLE merchant_connector_account DROP COLUMN IF EXISTS business_sub_label;
-ALTER TABLE merchant_connector_account DROP COLUMN IF EXISTS test_mode;
