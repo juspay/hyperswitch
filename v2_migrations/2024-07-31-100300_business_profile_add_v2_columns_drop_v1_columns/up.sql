76b504e8d22fd6e9551b1ea3865dbec3f9971718
Sanchith Hegde <sanchith.hegde@juspay.in>
2024-08-01 13:07:38 +0530

    refactor(v2_migrations): merge all migrations related to business profiles

--- v2_migrations/2024-07-31-100300_business_profile_add_v2_columns_drop_v1_columns/up.sql
+++ v2_migrations/2024-07-31-100300_business_profile_add_v2_columns_drop_v1_columns/up.sql
@@ -7,3 +7,11 @@ ALTER TABLE business_profile
     ADD COLUMN frm_routing_algorithm_id VARCHAR(64) DEFAULT NULL,
     ADD COLUMN payout_routing_algorithm_id VARCHAR(64) DEFAULT NULL,
     ADD COLUMN default_fallback_routing JSONB DEFAULT NULL;
+
+-- Note: This query should not be run on higher environments as this leads to data loss.
+-- The application will work fine even without these queries being run.
+ALTER TABLE business_profile
+    DROP COLUMN routing_algorithm,
+    DROP COLUMN intent_fulfillment_time,
+    DROP COLUMN frm_routing_algorithm,
+    DROP COLUMN payout_routing_algorithm;
