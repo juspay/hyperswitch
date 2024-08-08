e8579f07f8c1cf8bf6a0b916059f71d8b06ae0a4
Kashif <kashif.dev@protonmail.com>
2024-08-08 15:29:34 +0530

    chore: add default fallback locale

--- crates/router/src/locale.rs
+++ crates/router/src/locale.rs
@@ -2,4 +2,4 @@
 
 // allowing clippy indexing_slicing due to this
 // https://github.com/longbridgeapp/rust-i18n/blob/v3.0.1/crates/macro/src/lib.rs#L225
-i18n!("locales");
+i18n!("locales", fallback = "en");
