8e0ba855d61c867e5789b637489cc81ef9f4bd91
PiX <69745008+pixincreate@users.noreply.github.com>
2024-08-09 12:42:35 +0530

    refactor(cypress): rename screenshots after capture w.r.t connector name

--- cypress-tests/cypress.config.js
+++ cypress-tests/cypress.config.js
@@ -1,6 +1,12 @@
-let globalState;
+const { defineConfig } = require("cypress");
+const fs = require("fs-extra");
+const path = require("path");
 
-module.exports = {
+let globalState;
+// Fetch connector name from environment variable
+const connectorId = process.env.CYPRESS_CONNECTOR;
+
+module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       on("task", {
@@ -15,6 +21,30 @@ module.exports = {
           console.log(message);
           return null;
         },
+      });
+      on("after:screenshot", (details) => {
+        // Full path to the screenshot file
+        const screenshotPath = details.path;
+
+        // Extract filename without extension
+        const name = path.basename(
+          screenshotPath,
+          path.extname(screenshotPath)
+        );
+
+        // Define a new name with a connectorId
+        const newName = `[${connectorId}] ${name}.png`;
+        const newPath = path.join(path.dirname(screenshotPath), newName);
+
+        return fs
+          .rename(screenshotPath, newPath)
+          .then(() => {
+            console.log("Screenshot renamed successfully");
+            return { path: newPath };
+          })
+          .catch((err) => {
+            console.error("Failed to rename screenshot:", err);
+          });
       });
     },
     experimentalRunAllSpecs: true,
@@ -31,5 +61,4 @@ module.exports = {
   chromeWebSecurity: false,
   defaultCommandTimeout: 10000,
   pageLoadTimeout: 20000,
-  trashAssetsBeforeRuns: false,
-};
+});
