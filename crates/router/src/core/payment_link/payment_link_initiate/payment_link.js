7a27c7fa296dc3b7971e17579ab24774b94f8be2
Sk Sakil Mostak <skmahim71@gmail.com>
2024-08-12 15:39:41 +0530

    refactor: resolve comments

--- crates/router/src/core/payment_link/payment_link_initiate/payment_link.js
+++ crates/router/src/core/payment_link/payment_link_initiate/payment_link.js
@@ -617,28 +617,36 @@ function renderDynamicMerchantDetails(paymentDetails) {
 }
 
 function appendMerchantDetails(paymentDetails, merchantDynamicDetails) {
-  if (!(typeof paymentDetails.transaction_details === "string" && paymentDetails.transaction_details.length > 0 && paymentDetails.transaction_details !== "{}")) {
+  if (!(typeof paymentDetails.transaction_details === "string" && paymentDetails.transaction_details.length > 0)) {
     return;
   }
 
-  // render a horizontal line above dynamic merchant details
-  let horizontalLineContainer = document.getElementById("hyper-checkout-payment-horizontal-line-container");
-  let horizontalLine = document.createElement("hr");
-  horizontalLine.className = "hyper-checkout-payment-horizontal-line";
-  horizontalLineContainer.append(horizontalLine);
+  try {
+    let merchantDetailsObject = JSON.parse(paymentDetails.transaction_details);
 
-  // max number of items to show in the merchant details
-  let maxItemsInDetails = 5;
-  let merchantDetailsObject = JSON.parse(paymentDetails.transaction_details);
-  for(const key in merchantDetailsObject) {
-    var merchantData = document.createElement("div");
-    merchantData.className = "hyper-checkout-payment-merchant-dynamic-data";
-    merchantData.innerHTML = key+": "+merchantDetailsObject[key].bold();
-
-    merchantDynamicDetails.append(merchantData);
-    if(--maxItemsInDetails === 0) {
-      break;
+    if(Object.keys(merchantDetailsObject).length !== 0) {
+      // render a horizontal line above dynamic merchant details
+      let horizontalLineContainer = document.getElementById("hyper-checkout-payment-horizontal-line-container");
+      let horizontalLine = document.createElement("hr");
+      horizontalLine.className = "hyper-checkout-payment-horizontal-line";
+      horizontalLineContainer.append(horizontalLine);
     }
+
+    // max number of items to show in the merchant details
+    let maxItemsInDetails = 5;
+    for(const key in merchantDetailsObject) {
+      var merchantData = document.createElement("div");
+      merchantData.className = "hyper-checkout-payment-merchant-dynamic-data";
+      merchantData.innerHTML = key+": "+merchantDetailsObject[key].bold();
+
+      merchantDynamicDetails.append(merchantData);
+      if(--maxItemsInDetails === 0) {
+        break;
+      }
+    }
+  }
+  catch (error) {
+    console.error("Error parsing merchant details", error);
   }
 }
 
