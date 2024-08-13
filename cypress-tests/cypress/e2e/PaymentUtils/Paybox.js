dd5817e1b11ee8cc3b2c251479e72de216846a67
kiran.kummari <kiran.kummari@juspay.in>
2024-08-13 16:21:08 +0530

    add cypress tests

--- cypress-tests/cypress/e2e/PaymentUtils/Paybox.js
+++ cypress-tests/cypress/e2e/PaymentUtils/Paybox.js
@@ -6,48 +6,6 @@ const successfulNo3DSCardDetails = {
     card_cvc: "222",
 };
 
-// const successfulThreeDSTestCardDetails = {
-//     card_number: "4000000000001091",
-//     card_exp_month: "01",
-//     card_exp_year: "25",
-//     card_holder_name: "joseph Doe",
-//     card_cvc: "123",
-// };
-
-// const singleUseMandateData = {
-//     customer_acceptance: {
-//         acceptance_type: "offline",
-//         accepted_at: "1963-05-03T04:07:52.723Z",
-//         online: {
-//             ip_address: "125.0.0.1",
-//             user_agent: "amet irure esse",
-//         },
-//     },
-//     mandate_type: {
-//         single_use: {
-//             amount: 8000,
-//             currency: "EUR",
-//         },
-//     },
-// };
-
-// const multiUseMandateData = {
-//     customer_acceptance: {
-//         acceptance_type: "offline",
-//         accepted_at: "1963-05-03T04:07:52.723Z",
-//         online: {
-//             ip_address: "125.0.0.1",
-//             user_agent: "amet irure esse",
-//         },
-//     },
-//     mandate_type: {
-//         multi_use: {
-//             amount: 8000,
-//             currency: "EUR",
-//         },
-//     },
-// };
-
 export const connectorDetails = {
     card_pm: {
         PaymentIntent: {
@@ -67,43 +25,6 @@ export const connectorDetails = {
                 },
             },
         },
-        // "3DSManualCapture": {
-        //     Request: {
-        //         payment_method: "card",
-        //         payment_method_data: {
-        //             card: successfulThreeDSTestCardDetails,
-        //         },
-        //         currency: "EUR",
-        //         customer_acceptance: null,
-        //         setup_future_usage: "on_session",
-        //     },
-        //     Response: {
-        //         status: 200,
-        //         trigger_skip: true,
-        //         body: {
-        //             status: "requires_capture",
-        //         },
-        //     },
-        // },
-        // "3DSAutoCapture": {
-        //     Request: {
-        //         payment_method: "card",
-        //         payment_method_data: {
-        //             card: successfulThreeDSTestCardDetails,
-        //         },
-        //         currency: "EUR",
-        //         customer_acceptance: null,
-        //         setup_future_usage: "on_session",
-        //     },
-        //     Response: {
-        //         status: 200,
-        //         trigger_skip: true,
-
-        //         body: {
-        //             status: "requires_customer_action",
-        //         },
-        //     },
-        // },
         No3DSManualCapture: {
             Request: {
                 payment_method: "card",
@@ -117,7 +38,7 @@ export const connectorDetails = {
             Response: {
                 status: 200,
                 body: {
-                    status: "requires_capture",
+                    status: "processing",
                 },
             },
         },
@@ -134,7 +55,7 @@ export const connectorDetails = {
             Response: {
                 status: 200,
                 body: {
-                    status: "succeeded",
+                    status: "processing",
                 },
             },
         },
@@ -150,10 +71,10 @@ export const connectorDetails = {
             Response: {
                 status: 200,
                 body: {
-                    status: "succeeded",
+                    status: "processing",
                     amount: 6500,
-                    amount_capturable: 0,
-                    amount_received: 6500,
+                    amount_capturable: 6500,
+                    amount_received: null,
                 },
             },
         },
@@ -162,19 +83,10 @@ export const connectorDetails = {
             Response: {
                 status: 200,
                 body: {
-                    status: "partially_captured",
+                    status: "processing",
                     amount: 6500,
-                    amount_capturable: 0,
-                    amount_received: 100,
-                },
-            },
-        },
-        Void: {
-            Request: {},
-            Response: {
-                status: 200,
-                body: {
-                    status: "cancelled",
+                    amount_capturable: 6500,
+                    amount_received: null,
                 },
             },
         },
@@ -222,307 +134,9 @@ export const connectorDetails = {
             Response: {
                 status: 200,
                 body: {
-                    status: "pending",
+                    status: "succeeded",
                 },
             },
         },
-        // MandateSingleUse3DSAutoCapture: {
-        //     Request: {
-        //         payment_method: "card",
-        //         payment_method_data: {
-        //             card: successfulThreeDSTestCardDetails,
-        //         },
-        //         currency: "EUR",
-        //         mandate_data: singleUseMandateData,
-        //     },
-        //     Response: {
-        //         status: 200,
-        //         trigger_skip: true,
-
-        //         body: {
-        //             status: "succeeded",
-        //         },
-        //     },
-        // },
-        // MandateSingleUse3DSManualCapture: {
-        //     Request: {
-        //         payment_method: "card",
-        //         payment_method_data: {
-        //             card: successfulThreeDSTestCardDetails,
-        //         },
-        //         currency: "EUR",
-        //         mandate_data: singleUseMandateData,
-        //     },
-        //     Response: {
-        //         status: 200,
-        //         trigger_skip: true,
-
-        //         body: {
-        //             status: "requires_customer_action",
-        //         },
-        //     },
-        // },
-        // MandateSingleUseNo3DSAutoCapture: {
-        //     Request: {
-        //         payment_method: "card",
-        //         payment_method_data: {
-        //             card: successfulNo3DSCardDetails,
-        //         },
-        //         currency: "EUR",
-        //         mandate_data: singleUseMandateData,
-        //     },
-        //     Response: {
-        //         status: 200,
-        //         body: {
-        //             status: "succeeded",
-        //         },
-        //     },
-        // },
-        // MandateSingleUseNo3DSManualCapture: {
-        //     Request: {
-        //         payment_method: "card",
-        //         payment_method_data: {
-        //             card: successfulNo3DSCardDetails,
-        //         },
-        //         currency: "EUR",
-        //         mandate_data: singleUseMandateData,
-        //     },
-        //     Response: {
-        //         status: 200,
-        //         body: {
-        //             status: "requires_capture",
-        //         },
-        //     },
-        // },
-        // MandateMultiUseNo3DSAutoCapture: {
-        //     Request: {
-        //         payment_method: "card",
-        //         payment_method_data: {
-        //             card: successfulNo3DSCardDetails,
-        //         },
-        //         currency: "EUR",
-        //         mandate_data: multiUseMandateData,
-        //     },
-        //     Response: {
-        //         status: 200,
-        //         body: {
-        //             status: "succeeded",
-        //         },
-        //     },
-        // },
-        // MandateMultiUseNo3DSManualCapture: {
-        //     Request: {
-        //         payment_method: "card",
-        //         payment_method_data: {
-        //             card: successfulNo3DSCardDetails,
-        //         },
-        //         currency: "EUR",
-        //         mandate_data: multiUseMandateData,
-        //     },
-        //     Response: {
-        //         status: 200,
-        //         body: {
-        //             status: "requires_capture",
-        //         },
-        //     },
-        // },
-        // MandateMultiUse3DSAutoCapture: {
-        //     Request: {
-        //         payment_method: "card",
-        //         payment_method_data: {
-        //             card: successfulThreeDSTestCardDetails,
-        //         },
-        //         currency: "EUR",
-        //         mandate_data: multiUseMandateData,
-        //     },
-        //     Response: {
-        //         status: 200,
-        //         trigger_skip: true,
-
-        //         body: {
-        //             status: "requires_capture",
-        //         },
-        //     },
-        // },
-        // MandateMultiUse3DSManualCapture: {
-        //     Request: {
-        //         payment_method: "card",
-        //         payment_method_data: {
-        //             card: successfulThreeDSTestCardDetails,
-        //         },
-        //         currency: "EUR",
-        //         mandate_data: multiUseMandateData,
-        //     },
-        //     Response: {
-        //         status: 200,
-        //         trigger_skip: true,
-
-        //         body: {
-        //             status: "requires_capture",
-        //         },
-        //     },
-        // },
-        // ZeroAuthMandate: {
-        //     Request: {
-        //         payment_method: "card",
-        //         payment_method_data: {
-        //             card: successfulNo3DSCardDetails,
-        //         },
-        //         currency: "EUR",
-        //         mandate_data: singleUseMandateData,
-        //     },
-        //     Response: {
-        //         status: 200,
-        //         body: {
-        //             status: "succeeded",
-        //         },
-        //     },
-        // },
-        // SaveCardUseNo3DSAutoCapture: {
-        //     Request: {
-        //         payment_method: "card",
-        //         payment_method_data: {
-        //             card: successfulNo3DSCardDetails,
-        //         },
-        //         currency: "EUR",
-        //         setup_future_usage: "on_session",
-        //         customer_acceptance: {
-        //             acceptance_type: "offline",
-        //             accepted_at: "1963-05-03T04:07:52.723Z",
-        //             online: {
-        //                 ip_address: "127.0.0.1",
-        //                 user_agent: "amet irure esse",
-        //             },
-        //         },
-        //     },
-        //     Response: {
-        //         status: 200,
-        //         body: {
-        //             status: "succeeded",
-        //         },
-        //     },
-        // },
-        // SaveCardUseNo3DSManualCapture: {
-        //     Request: {
-        //         payment_method: "card",
-        //         payment_method_data: {
-        //             card: successfulNo3DSCardDetails,
-        //         },
-        //         currency: "EUR",
-        //         setup_future_usage: "on_session",
-        //         customer_acceptance: {
-        //             acceptance_type: "offline",
-        //             accepted_at: "1963-05-03T04:07:52.723Z",
-        //             online: {
-        //                 ip_address: "127.0.0.1",
-        //                 user_agent: "amet irure esse",
-        //             },
-        //         },
-        //     },
-        //     Response: {
-        //         status: 200,
-        //         body: {
-        //             status: "requires_capture",
-        //         },
-        //     },
-        // },
-        // PaymentMethodIdMandateNo3DSAutoCapture: {
-        //     Request: {
-        //         payment_method: "card",
-        //         payment_method_data: {
-        //             card: successfulNo3DSCardDetails,
-        //         },
-        //         currency: "EUR",
-        //         mandate_data: null,
-        //         customer_acceptance: {
-        //             acceptance_type: "offline",
-        //             accepted_at: "1963-05-03T04:07:52.723Z",
-        //             online: {
-        //                 ip_address: "125.0.0.1",
-        //                 user_agent: "amet irure esse",
-        //             },
-        //         },
-        //     },
-        //     Response: {
-        //         status: 200,
-        //         body: {
-        //             status: "succeeded",
-        //         },
-        //     },
-        // },
-        // PaymentMethodIdMandateNo3DSManualCapture: {
-        //     Request: {
-        //         payment_method: "card",
-        //         payment_method_data: {
-        //             card: successfulNo3DSCardDetails,
-        //         },
-        //         currency: "EUR",
-        //         mandate_data: null,
-        //         customer_acceptance: {
-        //             acceptance_type: "offline",
-        //             accepted_at: "1963-05-03T04:07:52.723Z",
-        //             online: {
-        //                 ip_address: "125.0.0.1",
-        //                 user_agent: "amet irure esse",
-        //             },
-        //         },
-        //     },
-        //     Response: {
-        //         status: 200,
-        //         body: {
-        //             status: "requires_capture",
-        //         },
-        //     },
-        // },
-        // PaymentMethodIdMandate3DSAutoCapture: {
-        //     Request: {
-        //         payment_method: "card",
-        //         payment_method_data: {
-        //             card: successfulThreeDSTestCardDetails,
-        //         },
-        //         currency: "EUR",
-        //         mandate_data: null,
-        //         authentication_type: "three_ds",
-        //         customer_acceptance: {
-        //             acceptance_type: "offline",
-        //             accepted_at: "1963-05-03T04:07:52.723Z",
-        //             online: {
-        //                 ip_address: "125.0.0.1",
-        //                 user_agent: "amet irure esse",
-        //             },
-        //         },
-        //     },
-        //     Response: {
-        //         status: 200,
-        //         trigger_skip: true,
-        //         body: {
-        //             status: "requires_customer_action",
-        //         },
-        //     },
-        // },
-        // PaymentMethodIdMandate3DSManualCapture: {
-        //     Request: {
-        //         payment_method: "card",
-        //         payment_method_data: {
-        //             card: successfulThreeDSTestCardDetails,
-        //         },
-        //         mandate_data: null,
-        //         authentication_type: "three_ds",
-        //         customer_acceptance: {
-        //             acceptance_type: "offline",
-        //             accepted_at: "1963-05-03T04:07:52.723Z",
-        //             online: {
-        //                 ip_address: "125.0.0.1",
-        //                 user_agent: "amet irure esse",
-        //             },
-        //         },
-        //     },
-        //     Response: {
-        //         status: 200,
-        //         body: {
-        //             status: "requires_customer_action",
-        //         },
-        //     },
-        // },
     },
 };
