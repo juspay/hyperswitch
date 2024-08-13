a9d1b683bdb7faffb3d6d29c54d8ae61667ef76c
Kashif <mohammed.kashif@juspay.in>
2024-08-13 12:25:07 +0530

    refactor: hack fixes

--- crates/storage_impl/src/payouts/payouts.rs
+++ crates/storage_impl/src/payouts/payouts.rs
@@ -2,7 +2,9 @@
 use api_models::enums::PayoutConnectors;
 #[cfg(feature = "olap")]
 use async_bb8_diesel::{AsyncConnection, AsyncRunQueryDsl};
-use common_utils::{errors::ReportSwitchExt, ext_traits::Encode};
+#[cfg(feature = "payouts")]
+use common_utils::errors::ReportSwitchExt;
+use common_utils::ext_traits::Encode;
 #[cfg(all(
     feature = "olap",
     any(feature = "v1", feature = "v2"),
