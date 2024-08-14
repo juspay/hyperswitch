5d089943d09442c791c453eb435b7f949510e62c
kiran.kummari <kiran.kummari@juspay.in>
2024-08-14 16:04:26 +0530

    resolve pix comments

--- cypress-tests/cypress/e2e/PaymentUtils/Paybox.js
+++ cypress-tests/cypress/e2e/PaymentUtils/Paybox.js
@@ -31,7 +31,6 @@ export const connectorDetails = {
                 payment_method_data: {
                     card: successfulNo3DSCardDetails,
                 },
-                currency: "EUR",
                 customer_acceptance: null,
                 setup_future_usage: "on_session",
             },
