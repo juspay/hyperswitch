ff1faaaf513a67f00abfdb420868802f3591e19d
sai-harsha-vardhan <harsha111hero@gmail.com>
2024-08-14 16:07:25 +0530

    fix migrations

--- v2_migrations/2024-08-12-075913_make_profile_id_mandatory_in_mca/up.sql
+++ v2_migrations/2024-08-12-075913_make_profile_id_mandatory_in_mca/up.sql
@@ -1,7 +1,3 @@
 -- Your SQL goes here
-
--- Backfill the profile_id column with the default value for the one record which was created before the profile_id was added in create seed data
-UPDATE merchant_connector_account SET profile_id = 'default_profile_id' WHERE profile_id IS NULL;
-
 -- This migration is to make profile_id mandatory in mca table
 ALTER TABLE merchant_connector_account ALTER COLUMN profile_id SET NOT NULL;
