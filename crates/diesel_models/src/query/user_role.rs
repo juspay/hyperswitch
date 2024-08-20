f3e8d4636c1c016b08ce2a80368de37ec58b97d1
Mani Chandra Dulam <mani.dchandra@juspay.in>
2024-08-20 19:50:30 +0530

    refactor: remove old update queries

--- crates/diesel_models/src/query/user_role.rs
+++ crates/diesel_models/src/query/user_role.rs
@@ -41,47 +41,6 @@ impl UserRole {
         .await
     }
 
-    pub async fn update_by_user_id_merchant_id(
-        conn: &PgPooledConn,
-        user_id: String,
-        merchant_id: id_type::MerchantId,
-        update: UserRoleUpdate,
-        version: UserRoleVersion,
-    ) -> StorageResult<Self> {
-        generics::generic_update_with_unique_predicate_get_result::<
-            <Self as HasTable>::Table,
-            _,
-            _,
-            _,
-        >(
-            conn,
-            dsl::user_id
-                .eq(user_id)
-                .and(dsl::merchant_id.eq(merchant_id))
-                .and(dsl::version.eq(version)),
-            UserRoleUpdateInternal::from(update),
-        )
-        .await
-    }
-
-    pub async fn update_by_user_id_org_id(
-        conn: &PgPooledConn,
-        user_id: String,
-        org_id: id_type::OrganizationId,
-        update: UserRoleUpdate,
-        version: UserRoleVersion,
-    ) -> StorageResult<Vec<Self>> {
-        generics::generic_update_with_results::<<Self as HasTable>::Table, _, _, _>(
-            conn,
-            dsl::user_id
-                .eq(user_id)
-                .and(dsl::org_id.eq(org_id))
-                .and(dsl::version.eq(version)),
-            UserRoleUpdateInternal::from(update),
-        )
-        .await
-    }
-
     pub async fn list_by_user_id(
         conn: &PgPooledConn,
         user_id: String,
