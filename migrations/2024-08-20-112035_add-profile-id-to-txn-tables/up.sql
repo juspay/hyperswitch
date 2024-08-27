b21578af81e27a712507d3c666b4299253e7152a
Sampras Lopes <sampras.lopes@juspay.in>
2024-08-27 17:50:59 +0530

    feat(migrations): add backfill scripts in the up sql file

--- migrations/2024-08-20-112035_add-profile-id-to-txn-tables/up.sql
+++ migrations/2024-08-20-112035_add-profile-id-to-txn-tables/up.sql
@@ -17,3 +17,25 @@ ADD COLUMN IF NOT EXISTS organization_id VARCHAR(32) NOT NULL DEFAULT 'default_o
 -- Add organization_id to dispute table
 ALTER TABLE dispute
 ADD COLUMN IF NOT EXISTS organization_id VARCHAR(32) NOT NULL DEFAULT 'default_org';
+
+UPDATE payment_attempt pa
+SET organization_id = ma.organization_id
+FROM merchant_account ma
+WHERE pa.merchant_id = ma.merchant_id;
+
+UPDATE payment_intent pi
+SET organization_id = ma.organization_id
+FROM merchant_account ma
+WHERE pi.merchant_id = ma.merchant_id;
+
+UPDATE refund r
+SET organization_id = ma.organization_id
+FROM merchant_account ma
+WHERE r.merchant_id = ma.merchant_id;
+
+UPDATE payment_attempt pa
+SET profile_id = pi.profile_id
+FROM payment_intent pi
+WHERE pa.payment_id = pi.payment_id
+  AND pa.merchant_id = pi.merchant_id
+  AND pi.profile_id IS NOT NULL;
