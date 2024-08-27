c3c9b2740b8e47ad75c74c886c30472a747e7119
dg-212 <debarati.ghatak@juspay.in>
2024-08-27 12:23:30 +0530

    feat(connector): [NOVALNET] Add template code (#5670)

--- crates/hyperswitch_connectors/src/connectors.rs
+++ crates/hyperswitch_connectors/src/connectors.rs
@@ -3,10 +3,11 @@ pub mod bitpay;
 pub mod fiserv;
 pub mod fiservemea;
 pub mod helcim;
+pub mod novalnet;
 pub mod stax;
 pub mod taxjar;
 
 pub use self::{
     bambora::Bambora, bitpay::Bitpay, fiserv::Fiserv, fiservemea::Fiservemea, helcim::Helcim,
-    stax::Stax, taxjar::Taxjar,
+    novalnet::Novalnet, stax::Stax, taxjar::Taxjar,
 };
