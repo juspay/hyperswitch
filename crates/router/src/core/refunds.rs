fb6fc2a11ad36c6777e18d2eb5c01a10d79cfe24
gitanjli525 <gitanjli525@gmail.com>
2024-09-12 12:35:19 +0530

    chore: addresses comments

--- crates/router/src/core/refunds.rs
+++ crates/router/src/core/refunds.rs
@@ -1045,7 +1045,8 @@ pub async fn get_aggregates_for_refunds(
     let refund_status_with_count = db
         .get_refund_status_with_count(merchant.get_id(), &time_range, merchant.storage_scheme)
         .await
-        .change_context(errors::ApiErrorResponse::InternalServerError)?;
+        .change_context(errors::ApiErrorResponse::InternalServerError)
+        .attach_printable("Failed to find status count")?;
     let mut status_map: HashMap<enums::RefundStatus, i64> =
         refund_status_with_count.into_iter().collect();
     for status in enums::RefundStatus::iter() {
