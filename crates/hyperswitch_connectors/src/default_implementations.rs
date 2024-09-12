05a4d34045eb9924966b870f6c07d13c74d07cd5
Mrudul Vajpayee <mrudul.vajpayee@mrudulvajpayee-XJWXCWP7HF.local>
2024-09-12 13:54:56 +0530

    add nexixpay in defualt_implemtations

--- crates/hyperswitch_connectors/src/default_implementations.rs
+++ crates/hyperswitch_connectors/src/default_implementations.rs
@@ -379,7 +379,6 @@ default_imp_for_pre_processing_steps!(
     connectors::Globepay,
     connectors::Helcim,
     connectors::Novalnet,
-    connectors::Nexixpay,
     connectors::Powertranz,
     connectors::Mollie,
     connectors::Stax,
