7cfb12f25e0ca34256c6b799986da507ec0c0901
Aprabhat19 <amishaprabhat@gmail.com>
2024-09-18 16:16:03 +0530

    fix cargo hack

--- crates/external_services/build.rs
+++ crates/external_services/build.rs
@@ -1,17 +1,19 @@
 #[cfg(feature = "dynamic_routing")]
 use std::{env, path::PathBuf};
 
-#[cfg(feature = "dynamic_routing")]
 #[allow(clippy::expect_used)]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Get the directory of the current crate
-    let crate_dir = env::var("CARGO_MANIFEST_DIR")?;
-    let proto_file = PathBuf::from(crate_dir)
-        .join("..")
-        .join("..")
-        .join("proto")
-        .join("success_rate.proto");
-    // Compile the .proto file
-    tonic_build::compile_protos(proto_file).expect("Failed to compile protos ");
+    #[cfg(feature = "dynamic_routing")]
+    {
+        // Get the directory of the current crate
+        let crate_dir = env::var("CARGO_MANIFEST_DIR")?;
+        let proto_file = PathBuf::from(crate_dir)
+            .join("..")
+            .join("..")
+            .join("proto")
+            .join("success_rate.proto");
+        // Compile the .proto file
+        tonic_build::compile_protos(proto_file).expect("Failed to compile protos ");
+    }
     Ok(())
 }
