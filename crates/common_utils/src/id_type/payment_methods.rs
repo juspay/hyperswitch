799ab9032bd1683556b9571cea2856a5b364a383
Sarthak Soni <sarthak.soni@juspay.in>
2024-09-18 19:36:23 +0530

    fix: Fixed errors

--- crates/common_utils/src/id_type/payment_methods.rs
+++ crates/common_utils/src/id_type/payment_methods.rs
@@ -31,8 +31,10 @@ impl GlobalPaymentMethodId {
         &self.0
     }
     /// Create a new GlobalPaymentMethodId from celll id information
-    pub fn generate(cell_id: &str) -> error_stack::Result<Self, errors::ValidationError> {
-        let cell_id = CellId::from_string(cell_id.to_string())?;
+    pub fn generate(cell_id: &str) -> error_stack::Result<Self, GlobalPaymentMethodIdError> {
+        let cell_id = CellId::from_str(cell_id)
+        .change_context(GlobalPaymentMethodIdError::ConstructionError)
+        .attach_printable("Failed to construct CellId from str")?;
         let global_id = GlobalId::generate(cell_id, GlobalEntity::PaymentMethod);
         Ok(Self(global_id))
     }
