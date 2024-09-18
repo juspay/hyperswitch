32a463089575423a226364b64b02e1774f545cb4
Aprabhat19 <amishaprabhat@gmail.com>
2024-09-18 17:03:04 +0530

    naming changes

--- crates/external_services/build.rs
+++ crates/external_services/build.rs
@@ -2,11 +2,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     #[cfg(feature = "dynamic_routing")]
     {
-        /// Get the directory of the current crate
+        // Get the directory of the current crate
         let proto_file = router_env::workspace_path()
             .join("proto")
             .join("success_rate.proto");
-        /// Compile the .proto file
+        // Compile the .proto file
         tonic_build::compile_protos(proto_file).expect("Failed to compile protos ");
     }
     Ok(())
