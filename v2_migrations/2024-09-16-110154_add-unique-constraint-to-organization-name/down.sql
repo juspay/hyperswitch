d95362776f27825e1b09e43e0cc5e940ece0004c
hrithikesh026 <hrithikesh.vm@juspay.in>
2024-09-19 16:19:14 +0530

    refactor: move sql migrations to existing v2_migrations folder

--- v2_migrations/2024-09-16-110154_add-unique-constraint-to-organization-name/down.sql
+++
@@ -1,2 +0,0 @@
--- This file should undo anything in `up.sql`
-ALTER TABLE organization DROP CONSTRAINT organization_organization_name_key;
--- v2_migrations/2024-09-16-110154_add-unique-constraint-to-organization-name/up.sql
+++
@@ -1,3 +0,0 @@
--- Your SQL goes here
-ALTER TABLE organization
-ADD CONSTRAINT organization_organization_name_key UNIQUE (organization_name);
