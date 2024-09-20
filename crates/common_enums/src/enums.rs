30a8f8cdb6999e0fb286b7d007486237f39f341e
Gorakhnath Yadav <gorakhcodes@gmail.com>
2024-09-20 16:19:00 +0530

    Added terms and conditions

--- crates/common_enums/src/enums.rs
+++ crates/common_enums/src/enums.rs
@@ -1356,7 +1356,9 @@ pub enum IntentStatus {
     PartiallyCapturedAndCapturable,
 }
 
-/// Indicates that you intend to make future payments with the payment methods used for this Payment. Providing this parameter will attach the payment method to the Customer, if present, after the Payment is confirmed and any required actions from the user are complete.
+/// Indicates that you intend to make future payments with the payment methods used for this Payment. Providing this parameter will attach the payment method to the Customer, if present, after the Payment is confirmed and any required actions from the user are complete. 
+/// - On_session - Payment method saved only at hyperswitch when consent is provided by the user. CVV will asked during the returning user payment
+/// - Off_session - Payment method saved at both hyperswitch and Processor when consent is provided by the user. No input is required during the returning user payment.
 #[derive(
     Clone,
     Copy,
