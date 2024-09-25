cda690bf39bc1c26634ed8ba07539196bed59257
Pa1NarK <69745008+pixincreate@users.noreply.github.com>
2024-09-25 16:22:09 +0530

    fix(api_key): fix api key `list` and `update` endpoints for v2 (#5980)

--- crates/openapi/src/openapi_v2.rs
+++ crates/openapi/src/openapi_v2.rs
@@ -109,6 +109,7 @@ Never share your secret api keys. Keep them guarded and secure.
         routes::api_keys::api_key_retrieve,
         routes::api_keys::api_key_update,
         routes::api_keys::api_key_revoke,
+        routes::api_keys::api_key_list,
 
         //Routes for customers
         routes::customers::customers_create,
