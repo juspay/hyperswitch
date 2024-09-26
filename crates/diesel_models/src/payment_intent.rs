1b2791516357aa665a4876eb5e10a3bf08d8bd38
swangi-kumari <swangi.12015941@lpu.in>
2024-09-26 14:58:28 +0530

    refactor: add pmt alias

--- crates/diesel_models/src/payment_intent.rs
+++ crates/diesel_models/src/payment_intent.rs
@@ -197,6 +197,7 @@ common_utils::impl_to_sql_from_sql_json!(TaxDetails);
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct PaymentMethodTypeTax {
     pub order_tax_amount: MinorUnit,
+    #[serde(alias = "pmt")]
     pub payment_method_type: PaymentMethodType,
 }
 
