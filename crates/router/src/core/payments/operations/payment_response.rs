8c128624f6421584482df9a23c24ecb6833386d7
Amisha Prabhat <55580080+Aprabhat19@users.noreply.github.com>
2024-09-26 18:44:59 +0530

    fix(core): Fix connector mandate details for setup mandate (#6096)

--- crates/router/src/core/payments/operations/payment_response.rs
+++ crates/router/src/core/payments/operations/payment_response.rs
@@ -924,7 +924,9 @@ impl<F: Clone> PostUpdateTracker<F, PaymentData<F>, types::SetupMandateRequestDa
             })?;
         let merchant_connector_id = payment_data.payment_attempt.merchant_connector_id.clone();
         let tokenization::SavePaymentMethodDataResponse {
-            payment_method_id, ..
+            payment_method_id,
+            mandate_reference_id,
+            ..
         } = Box::pin(tokenization::save_payment_method(
             state,
             connector_name,
@@ -954,6 +956,10 @@ impl<F: Clone> PostUpdateTracker<F, PaymentData<F>, types::SetupMandateRequestDa
         .await?;
         payment_data.payment_attempt.payment_method_id = payment_method_id;
         payment_data.payment_attempt.mandate_id = mandate_id;
+        payment_data.set_mandate_id(api_models::payments::MandateIds {
+            mandate_id: None,
+            mandate_reference_id,
+        });
         Ok(())
     }
 }
