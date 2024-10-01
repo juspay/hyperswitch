1bf62506611b64508febd68a1e68f7d1469bd5d2
hrithikesh026 <hrithikesh.vm@juspay.in>
2024-10-01 18:06:13 +0530

    chore: address rust 1.81.0 clippy lints

--- crates/router/src/connector/globalpay.rs
+++ crates/router/src/connector/globalpay.rs
@@ -1017,11 +1017,12 @@ impl api::IncomingWebhook for Globalpay {
         &self,
         request: &api::IncomingWebhookRequestDetails<'_>,
     ) -> CustomResult<Box<dyn masking::ErasedMaskSerialize>, errors::ConnectorError> {
-        let details = std::str::from_utf8(request.body)
-            .change_context(errors::ConnectorError::WebhookBodyDecodingFailed)?;
-        Ok(Box::new(serde_json::from_str(details).change_context(
-            errors::ConnectorError::WebhookResourceObjectNotFound,
-        )?))
+        Ok(Box::new(
+            request
+                .body
+                .parse_struct::<GlobalpayPaymentsResponse>("GlobalpayPaymentsResponse")
+                .change_context(errors::ConnectorError::WebhookBodyDecodingFailed)?,
+        ))
     }
 }
 
