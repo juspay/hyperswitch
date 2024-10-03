9531f3df94d0977738e67f03a6743d27728e86d1
Hrithikesh <61539176+hrithikesh026@users.noreply.github.com>
2024-10-03 12:37:41 +0530

    chore: intoduce GenericError enum variant in enum ConnectorError (#6189)

--- crates/router/src/core/errors/utils.rs
+++ crates/router/src/core/errors/utils.rs
@@ -216,7 +216,8 @@ impl<T> ConnectorErrorExt<T> for error_stack::Result<T, errors::ConnectorError> 
             | errors::ConnectorError::RequestTimeoutReceived
             | errors::ConnectorError::CurrencyNotSupported { .. }
             | errors::ConnectorError::InvalidConnectorConfig { .. }
-            | errors::ConnectorError::AmountConversionFailed { .. } => {
+            | errors::ConnectorError::AmountConversionFailed { .. }
+            | errors::ConnectorError::GenericError { .. } => {
                 err.change_context(errors::ApiErrorResponse::RefundFailed { data: None })
             }
         })
@@ -311,6 +312,7 @@ impl<T> ConnectorErrorExt<T> for error_stack::Result<T, errors::ConnectorError> 
                 errors::ConnectorError::InSufficientBalanceInPaymentMethod |
                 errors::ConnectorError::RequestTimeoutReceived |
                 errors::ConnectorError::ProcessingStepFailed(None)|
+                errors::ConnectorError::GenericError {..} |
                 errors::ConnectorError::AmountConversionFailed => errors::ApiErrorResponse::InternalServerError
             };
             err.change_context(error)
@@ -402,7 +404,8 @@ impl<T> ConnectorErrorExt<T> for error_stack::Result<T, errors::ConnectorError> 
                 | errors::ConnectorError::RequestTimeoutReceived
                 | errors::ConnectorError::CurrencyNotSupported { .. }
                 | errors::ConnectorError::ProcessingStepFailed(None)
-                | errors::ConnectorError::AmountConversionFailed { .. } => {
+                | errors::ConnectorError::AmountConversionFailed { .. }
+                | errors::ConnectorError::GenericError { .. } => {
                     logger::error!(%error,"Setup Mandate flow failed");
                     errors::ApiErrorResponse::PaymentAuthorizationFailed { data: None }
                 }
