1123d9eacd0a779d63e6939dabafd75ed0373b5c
hrithikesh026 <hrithikesh.vm@juspay.in>
2024-10-03 15:15:58 +0530

    remove client_secret field from PaymentsSessionRequest for v2

--- crates/api_models/src/payments.rs
+++ crates/api_models/src/payments.rs
@@ -4763,11 +4763,7 @@ pub struct RedirectResponse {
 
 #[cfg(feature = "v2")]
 #[derive(Debug, serde::Deserialize, serde::Serialize, Clone, ToSchema)]
-pub struct PaymentsSessionRequest {
-    /// This is a token which expires after 15 minutes, used from the client to authenticate and create sessions from the SDK
-    #[schema(value_type = String)]
-    pub client_secret: ClientSecret,
-}
+pub struct PaymentsSessionRequest {}
 
 #[cfg(feature = "v1")]
 #[derive(Debug, serde::Deserialize, serde::Serialize, Clone, ToSchema)]
