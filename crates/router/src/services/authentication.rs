7648eea6c997788c494ab3ae653aeb6f500967f6
hrithikesh026 <hrithikesh.vm@juspay.in>
2024-10-08 19:17:20 +0530

    use FromStr impl to get id_type from headers

--- crates/router/src/services/authentication.rs
+++ crates/router/src/services/authentication.rs
@@ -383,12 +383,9 @@ where
                 .attach_printable("API key is empty");
         }
 
-        let profile_id = get_header_value_by_key_and_deserialize(
-            headers::X_PROFILE_ID.to_string(),
-            request_headers,
-            "ProfileId",
-        )?
-        .get_required_value(headers::X_PROFILE_ID)?;
+        let profile_id =
+            get_id_type_by_key_from_headers(headers::X_PROFILE_ID.to_string(), request_headers)?
+                .get_required_value(headers::X_PROFILE_ID)?;
 
         let api_key = api_keys::PlaintextApiKey::from(api_key);
         let hash_key = {
@@ -661,12 +658,9 @@ where
             .0
             .authenticate_and_fetch(request_headers, state)
             .await?;
-        let profile_id = get_header_value_by_key_and_deserialize(
-            headers::X_PROFILE_ID.to_string(),
-            request_headers,
-            "ProfileId",
-        )?
-        .get_required_value(headers::X_PROFILE_ID)?;
+        let profile_id =
+            get_id_type_by_key_from_headers(headers::X_PROFILE_ID.to_string(), request_headers)?
+                .get_required_value(headers::X_PROFILE_ID)?;
         let key_manager_state = &(&state.session_state()).into();
         let profile = state
             .store()
@@ -1791,12 +1785,9 @@ where
             return Err(errors::ApiErrorResponse::InvalidJwtToken.into());
         }
 
-        let profile_id = get_header_value_by_key_and_deserialize(
-            headers::X_PROFILE_ID.to_string(),
-            request_headers,
-            "ProfileId",
-        )?
-        .get_required_value(headers::X_PROFILE_ID)?;
+        let profile_id =
+            get_id_type_by_key_from_headers(headers::X_PROFILE_ID.to_string(), request_headers)?
+                .get_required_value(headers::X_PROFILE_ID)?;
 
         let role_info = authorization::get_role_info(state, &payload).await?;
         authorization::check_permission(&self.permission, &role_info)?;
@@ -2214,18 +2205,18 @@ pub fn get_header_value_by_key(key: String, headers: &HeaderMap) -> RouterResult
         })
         .transpose()
 }
-use common_utils::ext_traits::ByteSliceExt;
-pub fn get_header_value_by_key_and_deserialize<'a, T: masking::Deserialize<'a>>(
+pub fn get_id_type_by_key_from_headers<'a, T: std::str::FromStr>(
     key: String,
     headers: &'a HeaderMap,
-    type_name: &'static str,
 ) -> RouterResult<Option<T>> {
-    get_header_value_by_key(key, headers)?
-        .map(|str_value| str_value.as_bytes().parse_struct(type_name))
+    get_header_value_by_key(key.clone(), headers)?
+        .map(|str_value| T::from_str(str_value))
         .transpose()
-        .change_context(errors::ApiErrorResponse::InvalidDataFormat {
-            field_name: "X-Profile-Id header".to_string(),
-            expected_format: "Valid ProfileId".to_string(),
+        .map_err(|_err| {
+            error_stack::report!(errors::ApiErrorResponse::InvalidDataFormat {
+                field_name: key,
+                expected_format: "Valid Id String".to_string(),
+            })
         })
 }
 
