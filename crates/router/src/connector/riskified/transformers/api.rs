8e25c510d02dc235a4da77adf3fdcd1058931c60
swangi-kumari <swangi.12015941@lpu.in>
2024-10-24 19:14:41 +0530

    refactor: resolve comments

--- crates/router/src/connector/riskified/transformers/api.rs
+++ crates/router/src/connector/riskified/transformers/api.rs
@@ -163,7 +163,7 @@ impl TryFrom<&frm_types::FrmCheckoutRouterData> for RiskifiedPaymentsCheckoutReq
                     .get_order_details()?
                     .iter()
                     .map(|order_detail| LineItem {
-                        price: order_detail.amount.get_amount_as_i64(), // This should be changed to MiniorUnit when we implement amount conversion for this connector. Additionally, the function get_amount_as_i64() should be avoided in the future.
+                        price: order_detail.amount.get_amount_as_i64(), // This should be changed to MinorUnit when we implement amount conversion for this connector. Additionally, the function get_amount_as_i64() should be avoided in the future.
                         quantity: i32::from(order_detail.quantity),
                         title: order_detail.product_name.clone(),
                         product_type: order_detail.product_type.clone(),
