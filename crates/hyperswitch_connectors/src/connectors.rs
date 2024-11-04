72ee434003eef744d516343a2f803264f226d92a
spritianeja03 <146620839+spritianeja03@users.noreply.github.com>
2024-11-04 20:20:39 +0530

    refactor(connector): [AIRWALLEX, MULTISAFEPAY, RAZORPAY, SHIFT4, WORLDPAY, ZSL] Move connectors from `router` to `hyperswitch_connectors` crate (#6369)
    Co-authored-by: Spriti Aneja <spriti.aneja@juspay.in>
    Co-authored-by: hyperswitch-bot[bot] <148525504+hyperswitch-bot[bot]@users.noreply.github.com>

--- crates/hyperswitch_connectors/src/connectors.rs
+++ crates/hyperswitch_connectors/src/connectors.rs
@@ -1,3 +1,4 @@
+pub mod airwallex;
 pub mod bambora;
 pub mod billwerk;
 pub mod bitpay;
@@ -14,12 +15,15 @@ pub mod forte;
 pub mod globepay;
 pub mod helcim;
 pub mod mollie;
+pub mod multisafepay;
 pub mod nexinets;
 pub mod nexixpay;
 pub mod novalnet;
 pub mod payeezy;
 pub mod payu;
 pub mod powertranz;
+pub mod razorpay;
+pub mod shift4;
 pub mod square;
 pub mod stax;
 pub mod taxjar;
@@ -27,14 +31,17 @@ pub mod thunes;
 pub mod tsys;
 pub mod volt;
 pub mod worldline;
+pub mod worldpay;
 pub mod zen;
+pub mod zsl;
 
 pub use self::{
-    bambora::Bambora, billwerk::Billwerk, bitpay::Bitpay, cashtocode::Cashtocode,
-    coinbase::Coinbase, cryptopay::Cryptopay, deutschebank::Deutschebank,
+    airwallex::Airwallex, bambora::Bambora, billwerk::Billwerk, bitpay::Bitpay,
+    cashtocode::Cashtocode, coinbase::Coinbase, cryptopay::Cryptopay, deutschebank::Deutschebank,
     digitalvirgo::Digitalvirgo, dlocal::Dlocal, fiserv::Fiserv, fiservemea::Fiservemea, fiuu::Fiuu,
-    forte::Forte, globepay::Globepay, helcim::Helcim, mollie::Mollie, nexinets::Nexinets,
-    nexixpay::Nexixpay, novalnet::Novalnet, payeezy::Payeezy, payu::Payu, powertranz::Powertranz,
-    square::Square, stax::Stax, taxjar::Taxjar, thunes::Thunes, tsys::Tsys, volt::Volt,
-    worldline::Worldline, zen::Zen,
+    forte::Forte, globepay::Globepay, helcim::Helcim, mollie::Mollie, multisafepay::Multisafepay,
+    nexinets::Nexinets, nexixpay::Nexixpay, novalnet::Novalnet, payeezy::Payeezy, payu::Payu,
+    powertranz::Powertranz, razorpay::Razorpay, shift4::Shift4, square::Square, stax::Stax,
+    taxjar::Taxjar, thunes::Thunes, tsys::Tsys, volt::Volt, worldline::Worldline,
+    worldpay::Worldpay, zen::Zen, zsl::Zsl,
 };
