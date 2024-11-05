adbc29c55e8b6ab09f1d23f10a1929d8476f477e
Sk Sakil Mostak <skmahim71@gmail.com>
2024-11-05 13:03:19 +0530

    refactor: replace newline with space

--- crates/router/src/connector/cybersource/transformers.rs
+++ crates/router/src/connector/cybersource/transformers.rs
@@ -4064,7 +4064,7 @@ pub trait RemoveNewLine {
 impl RemoveNewLine for Option<Secret<String>> {
     fn remove_new_line(&self) -> Self {
         self.clone().map(|masked_value| {
-            let new_string = masked_value.expose().replace("\n", "");
+            let new_string = masked_value.expose().replace("\n", " ");
             Secret::new(new_string)
         })
     }
@@ -4072,6 +4072,6 @@ impl RemoveNewLine for Option<Secret<String>> {
 
 impl RemoveNewLine for Option<String> {
     fn remove_new_line(&self) -> Self {
-        self.clone().map(|value| value.replace("\n", ""))
+        self.clone().map(|value| value.replace("\n", " "))
     }
 }
