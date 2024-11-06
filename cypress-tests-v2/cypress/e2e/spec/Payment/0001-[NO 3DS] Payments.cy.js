9fcabc510f7e8fd4c05ea427eea880c04d5902d3
PiX <69745008+pixincreate@users.noreply.github.com>
2024-11-06 17:51:14 +0530

    feat: rename tests and add example payment intent tests
    0001 contains an example for payment intent create call, confirm call
    and retrieve call. and of course, these does not work at present and
    hence, skipped intentionally from being run.
    
    add more number contexts within describe to address all the cases
    mentioned at the top of the file.
    
    configs needs to be introduced. but before that, we need to do some
    structural changes to change the behavior

--- cypress-tests-v2/cypress/e2e/spec/Payment/0001-[NO 3DS] Payments.cy.js
+++
@@ -1,8 +0,0 @@
-/*
-No 3DS Auto capture with Confirm True
-No 3DS Auto capture with Confirm False
-No 3DS Manual capture with Confirm True
-No 3DS Manual capture with Confirm False
-No 3DS Manual multiple capture with Confirm True
-No 3DS Manual multiple capture with Confirm False
-*/
