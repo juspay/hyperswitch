0389ae74e112dedd9d98314906820f78e4b89380
Sanchith Hegde <22217505+SanchithHegde@users.noreply.github.com>
2024-11-08 19:20:14 +0530

    refactor(payment_methods): refactor customer payment methods list v2 code to follow better code practices (#6433)

--- crates/router/src/services/authentication.rs
+++ crates/router/src/services/authentication.rs
@@ -2525,6 +2525,7 @@ pub fn get_auth_type_and_flow<A: SessionStateInfo + Sync + Send>(
     Ok((Box::new(HeaderAuth(ApiKeyAuth)), api::AuthFlow::Merchant))
 }
 
+#[cfg(feature = "v1")]
 pub fn check_client_secret_and_get_auth<T>(
     headers: &HeaderMap,
     payload: &impl ClientSecretFetch,
@@ -2560,6 +2561,44 @@ where
     Ok((Box::new(HeaderAuth(ApiKeyAuth)), api::AuthFlow::Merchant))
 }
 
+#[cfg(feature = "v2")]
+pub fn check_client_secret_and_get_auth<T>(
+    headers: &HeaderMap,
+    payload: &impl ClientSecretFetch,
+) -> RouterResult<(
+    Box<dyn AuthenticateAndFetch<AuthenticationData, T>>,
+    api::AuthFlow,
+)>
+where
+    T: SessionStateInfo + Sync + Send,
+    ApiKeyAuth: AuthenticateAndFetch<AuthenticationData, T>,
+    PublishableKeyAuth: AuthenticateAndFetch<AuthenticationData, T>,
+{
+    let api_key = get_api_key(headers)?;
+    if api_key.starts_with("pk_") {
+        payload
+            .get_client_secret()
+            .check_value_present("client_secret")
+            .map_err(|_| errors::ApiErrorResponse::MissingRequiredField {
+                field_name: "client_secret",
+            })?;
+        return Ok((
+            Box::new(HeaderAuth(PublishableKeyAuth)),
+            api::AuthFlow::Client,
+        ));
+    }
+
+    if payload.get_client_secret().is_some() {
+        return Err(errors::ApiErrorResponse::InvalidRequestData {
+            message: "client_secret is not a valid parameter".to_owned(),
+        }
+        .into());
+    }
+
+    Ok((Box::new(HeaderAuth(ApiKeyAuth)), api::AuthFlow::Merchant))
+}
+
+#[cfg(feature = "v1")]
 pub async fn get_ephemeral_or_other_auth<T>(
     headers: &HeaderMap,
     is_merchant_flow: bool,
@@ -2592,6 +2631,7 @@ where
     }
 }
 
+#[cfg(feature = "v1")]
 pub fn is_ephemeral_auth<A: SessionStateInfo + Sync + Send>(
     headers: &HeaderMap,
 ) -> RouterResult<Box<dyn AuthenticateAndFetch<AuthenticationData, A>>> {
@@ -2602,6 +2642,13 @@ pub fn is_ephemeral_auth<A: SessionStateInfo + Sync + Send>(
     } else {
         Ok(Box::new(EphemeralKeyAuth))
     }
+}
+
+#[cfg(feature = "v2")]
+pub fn is_ephemeral_auth<A: SessionStateInfo + Sync + Send>(
+    headers: &HeaderMap,
+) -> RouterResult<Box<dyn AuthenticateAndFetch<AuthenticationData, A>>> {
+    todo!()
 }
 
 pub fn is_jwt_auth(headers: &HeaderMap) -> bool {
