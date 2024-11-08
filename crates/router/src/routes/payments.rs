cc542845c7b66328b421c8cc38f0330cba82d36d
Narayan Bhat <narayan.bhat@juspay.in>
2024-11-08 15:44:58 +0530

    chore: cargo clippy_v2

--- crates/router/src/routes/payments.rs
+++ crates/router/src/routes/payments.rs
@@ -2166,7 +2166,7 @@ pub async fn payment_status(
         state,
         &req,
         internal_payload,
-        |state, auth: auth::AuthenticationDataV2, req, req_state| async {
+        |state, auth: auth::AuthenticationData, req, req_state| async {
             let payment_id = req.global_payment_id;
             let request = req.payload;
 
