20a3a1c2d6bb93fb4dae7f7eb669ebd85e631c96
Suman Maji <77887221+sumanmaji4@users.noreply.github.com>
2024-11-12 12:27:52 +0530

    feat(connector): [NOMUPAY] Add template code (#6382)
    Co-authored-by: hyperswitch-bot[bot] <148525504+hyperswitch-bot[bot]@users.noreply.github.com>

--- crates/hyperswitch_interfaces/src/configs.rs
+++ crates/hyperswitch_interfaces/src/configs.rs
@@ -57,6 +57,7 @@ pub struct Connectors {
     pub nexinets: ConnectorParams,
     pub nexixpay: ConnectorParams,
     pub nmi: ConnectorParams,
+    pub nomupay: ConnectorParams,
     pub noon: ConnectorParamsWithModeType,
     pub novalnet: ConnectorParams,
     pub nuvei: ConnectorParams,
