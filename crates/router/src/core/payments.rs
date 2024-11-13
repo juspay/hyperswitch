d0a041c361668d0eff6c9b0dde67351b6ed43d19
Sakil Mostak <73734619+Sakilmostak@users.noreply.github.com>
2024-11-14 01:19:59 +0530

    feat(core): add Mobile Payment (Direct Carrier Billing) as a payment method (#6196)
    Co-authored-by: hyperswitch-bot[bot] <148525504+hyperswitch-bot[bot]@users.noreply.github.com>

--- crates/router/src/core/payments.rs
+++ crates/router/src/core/payments.rs
@@ -1765,6 +1765,7 @@ impl PaymentRedirectFlow for PaymentRedirectCompleteAuthorize {
                         api_models::payments::NextActionData::WaitScreenInformation{..} => None,
                         api_models::payments::NextActionData::ThreeDsInvoke{..} => None,
                         api_models::payments::NextActionData::InvokeSdkClient{..} => None,
+                        api_models::payments::NextActionData::CollectOtp{ .. } => None,
                     })
                     .ok_or(errors::ApiErrorResponse::InternalServerError)
 
