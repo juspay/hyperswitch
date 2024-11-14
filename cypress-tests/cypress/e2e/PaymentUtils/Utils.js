dd20645e0ec3e18af3ca4410f8ea00d883ba809e
likhinbopanna <likhin.bopanna@juspay.in>
2024-11-14 17:51:59 +0530

    ci(fix): revert lints as requested

--- cypress-tests/cypress/e2e/PaymentUtils/Utils.js
+++ cypress-tests/cypress/e2e/PaymentUtils/Utils.js
@@ -1,7 +1,6 @@
 import { connectorDetails as adyenConnectorDetails } from "./Adyen.js";
 import { connectorDetails as bankOfAmericaConnectorDetails } from "./BankOfAmerica.js";
 import { connectorDetails as bluesnapConnectorDetails } from "./Bluesnap.js";
-import { connectorDetails as checkoutConnectorDetails } from "./Checkout.js";
 import {
   connectorDetails as CommonConnectorDetails,
   updateDefaultStatusCode,
@@ -9,7 +8,6 @@ import {
 import { connectorDetails as cybersourceConnectorDetails } from "./Cybersource.js";
 import { connectorDetails as datatransConnectorDetails } from "./Datatrans.js";
 import { connectorDetails as fiservemeaConnectorDetails } from "./Fiservemea.js";
-import { connectorDetails as fiuuConnectorDetails } from "./Fiuu.js";
 import { connectorDetails as iatapayConnectorDetails } from "./Iatapay.js";
 import { connectorDetails as itaubankConnectorDetails } from "./ItauBank.js";
 import { connectorDetails as nexixpayConnectorDetails } from "./Nexixpay.js";
@@ -20,7 +18,9 @@ import { connectorDetails as paypalConnectorDetails } from "./Paypal.js";
 import { connectorDetails as stripeConnectorDetails } from "./Stripe.js";
 import { connectorDetails as trustpayConnectorDetails } from "./Trustpay.js";
 import { connectorDetails as wellsfargoConnectorDetails } from "./WellsFargo.js";
+import { connectorDetails as fiuuConnectorDetails } from "./Fiuu.js";
 import { connectorDetails as worldpayConnectorDetails } from "./WorldPay.js";
+import { connectorDetails as checkoutConnectorDetails } from "./Checkout.js";
 
 const connectorDetails = {
   adyen: adyenConnectorDetails,
@@ -51,8 +51,7 @@ export default function getConnectorDetails(connectorId) {
 }
 
 export function getConnectorFlowDetails(connectorData, commonData, key) {
-  let data =
-    connectorData[key] === undefined ? commonData[key] : connectorData[key];
+  let data = connectorData[key] === undefined ? commonData[key] : connectorData[key];
   return data;
 }
 
