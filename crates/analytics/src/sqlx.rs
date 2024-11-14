55ad65f63e83b906214933887e933726f8af1f4d
uzair khan <uzair.khan@juspay.in>
2024-11-14 18:00:32 +0530

    feat(analytics): add sessionizer_dispute table mapping for ckh and sqlx

--- crates/analytics/src/sqlx.rs
+++ crates/analytics/src/sqlx.rs
@@ -927,6 +927,9 @@ impl ToSql<SqlxClient> for AnalyticsCollection {
             Self::OutgoingWebhookEvent => Err(error_stack::report!(ParsingError::UnknownError)
                 .attach_printable("OutgoingWebhookEvents table is not implemented for Sqlx"))?,
             Self::Dispute => Ok("dispute".to_string()),
+            Self::DisputeSessionized => Err(error_stack::report!(ParsingError::UnknownError)
+                .attach_printable("DisputeSessionized table is not implemented for Sqlx"))?,
+
         }
     }
 }
