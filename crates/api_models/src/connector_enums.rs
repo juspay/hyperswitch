5bfef5f4bd4137a62e484b812eaf702271696c6b
sumanmaji4 <suman.maji@juspay.in>
2024-11-20 10:50:42 +0530

    Nomupay flows

--- crates/api_models/src/connector_enums.rs
+++ crates/api_models/src/connector_enums.rs
@@ -96,7 +96,7 @@ pub enum Connector {
     Nexinets,
     Nexixpay,
     Nmi,
-    // Nomupay,
+    Nomupay,
     Noon,
     Novalnet,
     Nuvei,
@@ -146,6 +146,7 @@ impl Connector {
             (Self::Paypal, Some(PayoutType::Wallet))
                 | (_, Some(PayoutType::Card))
                 | (Self::Adyenplatform, _)
+                | (Self::Nomupay, _)
         )
     }
     #[cfg(feature = "payouts")]
@@ -165,8 +166,8 @@ impl Connector {
         matches!((self, payout_method), (Self::Paypal, _))
     }
     #[cfg(feature = "payouts")]
-    pub fn supports_vendor_disburse_account_create_for_payout(self) -> bool {
-        matches!(self, Self::Stripe)
+    pub fn supports_vendor_disburse_account_create_for_payout(&self) -> bool {
+        matches!(self, Self::Stripe | Self::Nomupay)
     }
     pub fn supports_access_token(self, payment_method: PaymentMethod) -> bool {
         matches!(
@@ -240,7 +241,7 @@ impl Connector {
             | Self::Multisafepay
             | Self::Nexinets
             | Self::Nexixpay
-            // | Self::Nomupay
+            | Self::Nomupay
             | Self::Novalnet
             | Self::Nuvei
             | Self::Opennode
