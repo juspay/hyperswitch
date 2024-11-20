f988b668c34a869c90a69ae30790755aaf02cf81
Prasunna Soppa <prasunna.soppa@juspay.in>
2024-11-20 16:00:56 +0530

    fix failing checks

--- crates/api_models/src/events/payment.rs
+++ crates/api_models/src/events/payment.rs
@@ -219,11 +219,24 @@ impl ApiEventMetric for PaymentMethodResponse {
 }
 
 impl ApiEventMetric for PaymentMethodMigrateResponse {
+    #[cfg(all(
+        any(feature = "v1", feature = "v2"),
+        not(feature = "payment_methods_v2")
+    ))]
     fn get_api_event_type(&self) -> Option<ApiEventsType> {
         Some(ApiEventsType::PaymentMethod {
             payment_method_id: self.payment_method_response.payment_method_id.clone(),
             payment_method: self.payment_method_response.payment_method,
             payment_method_type: self.payment_method_response.payment_method_type,
+        })
+    }
+
+    #[cfg(all(feature = "v2", feature = "payment_methods_v2"))]
+    fn get_api_event_type(&self) -> Option<ApiEventsType> {
+        Some(ApiEventsType::PaymentMethod {
+            payment_method_id: self.payment_method_response.payment_method_id.clone(),
+            payment_method: self.payment_method_response.payment_method_type,
+            payment_method_type: self.payment_method_response.payment_method_subtype,
         })
     }
 }
