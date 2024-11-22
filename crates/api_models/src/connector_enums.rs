11b872614a2fc2250559e2a42a9eb9bc4536e3f1
Amey Wale <amey.wale@juspay.in>
2024-11-22 19:26:23 +0530

    Xendit Tokenization flow raw

--- crates/api_models/src/connector_enums.rs
+++ crates/api_models/src/connector_enums.rs
@@ -178,6 +178,7 @@ impl Connector {
                 | (Self::Iatapay, _)
                 | (Self::Volt, _)
                 | (Self::Itaubank, _)
+                | (Self::Xendit, _)
         )
     }
     pub fn supports_file_storage_module(&self) -> bool {
