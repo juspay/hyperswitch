5a98ed65a94a6e8204a3ea34f834033654fdbaa7
Kashif <kashif.dev@protonmail.com>
2024-11-28 19:53:20 +0530

    feat(connector): worldpay - add dynamic fields and update terminal status mapping (#6468)

--- crates/router/src/db/kafka_store.rs
+++ crates/router/src/db/kafka_store.rs
@@ -1454,7 +1454,7 @@ impl PaymentAttemptInterface for KafkaStore {
     #[cfg(feature = "v1")]
     async fn find_payment_attempt_by_connector_transaction_id_payment_id_merchant_id(
         &self,
-        connector_transaction_id: &str,
+        connector_transaction_id: &common_utils::types::ConnectorTransactionId,
         payment_id: &id_type::PaymentId,
         merchant_id: &id_type::MerchantId,
         storage_scheme: MerchantStorageScheme,
