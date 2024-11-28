1c6de3239de71084dace1313b1771a41d41ac6e7
Chikke Srujan <chikke.srujan@Chikke-Srujan-N7WRTY72X7.local>
2024-11-28 22:12:49 +0530

    add support for hideCardNicknameField for open payment lins

--- crates/router/src/core/payment_link/payment_link_initiate/payment_link_initiator.js
+++ crates/router/src/core/payment_link/payment_link_initiate/payment_link_initiator.js
@@ -39,7 +39,7 @@ function initializeSDK() {
     paymentDetails.sdk_layout === "accordion"
       ? "accordion"
       : paymentDetails.sdk_layout;
-
+  var hideCardNicknameField = paymentDetails.hide_card_nickname_field;
   var unifiedCheckoutOptions = {
     displaySavedPaymentMethodsCheckbox: false,
     displaySavedPaymentMethods: false,
@@ -56,7 +56,7 @@ function initializeSDK() {
         height: 55,
       },
     },
-    hideCardNicknameField: false,
+    hideCardNicknameField: hideCardNicknameField,
   };
   // @ts-ignore
   unifiedCheckout = widgets.create("payment", unifiedCheckoutOptions);
@@ -82,5 +82,10 @@ function redirectToStatus() {
   arr.splice(0, 2);
   arr.unshift("status");
   arr.unshift("payment_link");
-  window.location.href = window.location.origin + "/" + arr.join("/") + "?locale=" + paymentDetails.locale;
+  window.location.href =
+    window.location.origin +
+    "/" +
+    arr.join("/") +
+    "?locale=" +
+    paymentDetails.locale;
 }
