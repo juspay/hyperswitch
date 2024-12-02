982b26a8c2851266b6e8615699479a05ab62e519
Pa1NarK <69745008+pixincreate@users.noreply.github.com>
2024-12-02 15:28:54 +0530

    ci: update ntid card expiry (#6714)

--- cypress-tests/cypress/e2e/PaymentUtils/Novalnet.js
+++ cypress-tests/cypress/e2e/PaymentUtils/Novalnet.js
@@ -1,7 +1,7 @@
 const successfulNo3DSCardDetails = {
   card_number: "4200000000000000",
   card_exp_month: "12",
-  card_exp_year: "25",
+  card_exp_year: "50",
   card_holder_name: "Max Mustermann",
   card_cvc: "123",
 };
@@ -9,7 +9,7 @@ const successfulNo3DSCardDetails = {
 const successfulThreeDSTestCardDetails = {
   card_number: "4000000000001091",
   card_exp_month: "12",
-  card_exp_year: "25",
+  card_exp_year: "50",
   card_holder_name: "Max Mustermann",
   card_cvc: "123",
 };
