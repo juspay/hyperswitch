f254aa184c076491b0a8a6cb0d3406b623169afd
Malay Awasthi <malay.awasthi@juspay.in>
2024-12-04 16:50:44 +0530

    fix/pmd_getting_overrided

--- crates/router/src/core/payments/operations/payment_create.rs
+++ crates/router/src/core/payments/operations/payment_create.rs
@@ -532,13 +532,12 @@ impl<F: Send + Clone> GetTracker<F, PaymentData<F>, api::PaymentsRequest> for Pa
             None
         };
         // Only set `payment_attempt.payment_method_data` if `additional_pm_data_from_locker` is not None
-        if additional_pm_data_from_locker.is_some() {
-            payment_attempt.payment_method_data = additional_pm_data_from_locker
-                .as_ref()
-                .map(Encode::encode_to_value)
-                .transpose()
-                .change_context(errors::ApiErrorResponse::InternalServerError)
-                .attach_printable("Failed to encode additional pm data")?;
+        if let Some(additional_pm_data) = additional_pm_data_from_locker.as_ref() {
+            payment_attempt.payment_method_data = Some(
+                Encode::encode_to_value(additional_pm_data)
+                    .change_context(errors::ApiErrorResponse::InternalServerError)
+                    .attach_printable("Failed to encode additional pm data")?,
+            );
         }
         let amount = payment_attempt.get_total_amount().into();
 
