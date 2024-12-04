086115f47379b06185a1753979fc6dd9dc945afd
likhinbopanna <131246334+likhinbopanna@users.noreply.github.com>
2024-12-04 21:33:12 +0530

    ci(cypress): Add Dynamic fields Test Cases (#6703)
    Co-authored-by: hyperswitch-bot[bot] <148525504+hyperswitch-bot[bot]@users.noreply.github.com>

--- cypress-tests/cypress/e2e/PaymentUtils/Cybersource.js
+++ cypress-tests/cypress/e2e/PaymentUtils/Cybersource.js
@@ -931,4 +931,255 @@ export const connectorDetails = {
       },
     },
   },
+  pm_list: {
+    PmListResponse: {
+      PmListNull: {
+        payment_methods: [],
+      },
+      pmListDynamicFieldWithoutBilling: {
+        payment_methods: [
+          {
+            payment_method: "card",
+            payment_method_types: [
+              {
+                payment_method_type: "credit",
+                card_networks: [
+                  {
+                    eligible_connectors: ["cybersource"],
+                  },
+                ],
+                required_fields: {
+                  "billing.address.first_name": {
+                    required_field:
+                      "payment_method_data.billing.address.first_name",
+                    display_name: "card_holder_name",
+                    field_type: "user_full_name",
+                    value: null,
+                  },
+                  "payment_method_data.card.card_number": {
+                    required_field: "payment_method_data.card.card_number",
+                    display_name: "card_number",
+                    field_type: "user_card_number",
+                    value: null,
+                  },
+                  "payment_method_data.card.card_cvc": {
+                    required_field: "payment_method_data.card.card_cvc",
+                    display_name: "card_cvc",
+                    field_type: "user_card_cvc",
+                    value: null,
+                  },
+
+                  "payment_method_data.card.card_exp_year": {
+                    required_field: "payment_method_data.card.card_exp_year",
+                    display_name: "card_exp_year",
+                    field_type: "user_card_expiry_year",
+                    value: null,
+                  },
+                  "billing.address.last_name": {
+                    required_field:
+                      "payment_method_data.billing.address.last_name",
+                    display_name: "card_holder_name",
+                    field_type: "user_full_name",
+                    value: null,
+                  },
+                  "billing.address.state": {
+                    required_field: "payment_method_data.billing.address.state",
+                    display_name: "state",
+                    field_type: "user_address_state",
+                    value: null,
+                  },
+                  "billing.email": {
+                    required_field: "payment_method_data.billing.email",
+                    display_name: "email",
+                    field_type: "user_email_address",
+                    value: "hyperswitch_sdk_demo_id@gmail.com",
+                  },
+                  "billing.address.zip": {
+                    required_field: "payment_method_data.billing.address.zip",
+                    display_name: "zip",
+                    field_type: "user_address_pincode",
+                    value: null,
+                  },
+                  "payment_method_data.card.card_exp_month": {
+                    required_field: "payment_method_data.card.card_exp_month",
+                    display_name: "card_exp_month",
+                    field_type: "user_card_expiry_month",
+                    value: null,
+                  },
+                  "billing.address.line1": {
+                    required_field: "payment_method_data.billing.address.line1",
+                    display_name: "line1",
+                    field_type: "user_address_line1",
+                    value: null,
+                  },
+                  "billing.address.city": {
+                    required_field: "payment_method_data.billing.address.city",
+                    display_name: "city",
+                    field_type: "user_address_city",
+                    value: null,
+                  },
+                },
+              },
+            ],
+          },
+        ],
+      },
+      pmListDynamicFieldWithBilling: {
+        payment_methods: [
+          {
+            payment_method: "card",
+            payment_method_types: [
+              {
+                payment_method_type: "credit",
+                card_networks: [
+                  {
+                    eligible_connectors: ["cybersource"],
+                  },
+                ],
+                required_fields: {
+                  "billing.address.city": {
+                    required_field: "payment_method_data.billing.address.city",
+                    display_name: "city",
+                    field_type: "user_address_city",
+                    value: "San Fransico",
+                  },
+                  "billing.address.state": {
+                    required_field: "payment_method_data.billing.address.state",
+                    display_name: "state",
+                    field_type: "user_address_state",
+                    value: "CA",
+                  },
+                  "billing.address.zip": {
+                    required_field: "payment_method_data.billing.address.zip",
+                    display_name: "zip",
+                    field_type: "user_address_pincode",
+                    value: "94122",
+                  },
+                  "billing.address.country": {
+                    required_field:
+                      "payment_method_data.billing.address.country",
+                    display_name: "country",
+                    field_type: {
+                      user_address_country: {
+                        options: ["ALL"],
+                      },
+                    },
+                    value: "PL",
+                  },
+                  "billing.address.first_name": {
+                    required_field:
+                      "payment_method_data.billing.address.first_name",
+                    display_name: "card_holder_name",
+                    field_type: "user_full_name",
+                    value: "joseph",
+                  },
+                  "billing.address.last_name": {
+                    required_field:
+                      "payment_method_data.billing.address.last_name",
+                    display_name: "card_holder_name",
+                    field_type: "user_full_name",
+                    value: "Doe",
+                  },
+                  "billing.email": {
+                    required_field: "payment_method_data.billing.email",
+                    display_name: "email",
+                    field_type: "user_email_address",
+                    value: "hyperswitch.example@gmail.com",
+                  },
+                  "payment_method_data.card.card_cvc": {
+                    required_field: "payment_method_data.card.card_cvc",
+                    display_name: "card_cvc",
+                    field_type: "user_card_cvc",
+                    value: null,
+                  },
+                  "billing.address.line1": {
+                    required_field: "payment_method_data.billing.address.line1",
+                    display_name: "line1",
+                    field_type: "user_address_line1",
+                    value: "1467",
+                  },
+                  "payment_method_data.card.card_exp_month": {
+                    required_field: "payment_method_data.card.card_exp_month",
+                    display_name: "card_exp_month",
+                    field_type: "user_card_expiry_month",
+                    value: null,
+                  },
+                  "payment_method_data.card.card_number": {
+                    required_field: "payment_method_data.card.card_number",
+                    display_name: "card_number",
+                    field_type: "user_card_number",
+                    value: null,
+                  },
+                  "payment_method_data.card.card_exp_year": {
+                    required_field: "payment_method_data.card.card_exp_year",
+                    display_name: "card_exp_year",
+                    field_type: "user_card_expiry_year",
+                    value: null,
+                  },
+                },
+              },
+            ],
+          },
+        ],
+      },
+      pmListDynamicFieldWithNames: {
+        payment_methods: [
+          {
+            payment_method: "card",
+            payment_method_types: [
+              {
+                payment_method_type: "credit",
+                card_networks: [
+                  {
+                    eligible_connectors: ["cybersource"],
+                  },
+                ],
+                required_fields: {
+                  "billing.address.last_name": {
+                    required_field:
+                      "payment_method_data.billing.address.last_name",
+                    display_name: "card_holder_name",
+                    field_type: "user_full_name",
+                    value: "Doe",
+                  },
+                  "billing.address.first_name": {
+                    required_field:
+                      "payment_method_data.billing.address.first_name",
+                    display_name: "card_holder_name",
+                    field_type: "user_full_name",
+                    value: "joseph",
+                  },
+                },
+              },
+            ],
+          },
+        ],
+      },
+      pmListDynamicFieldWithEmail: {
+        payment_methods: [
+          {
+            payment_method: "card",
+            payment_method_types: [
+              {
+                payment_method_type: "credit",
+                card_networks: [
+                  {
+                    eligible_connectors: ["cybersource"],
+                  },
+                ],
+                required_fields: {
+                  "billing.email": {
+                    required_field: "payment_method_data.billing.email",
+                    display_name: "email",
+                    field_type: "user_email_address",
+                    value: "hyperswitch.example@gmail.com",
+                  },
+                },
+              },
+            ],
+          },
+        ],
+      },
+    },
+  },
 };
