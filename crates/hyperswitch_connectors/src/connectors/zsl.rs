0c8421a30bb5f86dc8be1281a115be959fcfa1e8
AkshayaFoiger <akshaya.shankar@juspay.in>
2024-12-10 14:59:01 +0530

    refactor(router): remove validate_capture_method from zsl

--- crates/hyperswitch_connectors/src/connectors/zsl.rs
+++ crates/hyperswitch_connectors/src/connectors/zsl.rs
@@ -128,24 +128,6 @@ impl ConnectorCommon for Zsl {
 }
 
 impl ConnectorValidation for Zsl {
-    fn validate_capture_method(
-        &self,
-        capture_method: Option<enums::CaptureMethod>,
-        _payment_method: enums::PaymentMethod,
-        _pmt: Option<enums::PaymentMethodType>,
-    ) -> CustomResult<(), errors::ConnectorError> {
-        let capture_method = capture_method.unwrap_or_default();
-        match capture_method {
-            enums::CaptureMethod::Automatic | enums::CaptureMethod::SequentialAutomatic => Ok(()),
-            enums::CaptureMethod::Manual
-            | enums::CaptureMethod::ManualMultiple
-            | enums::CaptureMethod::Scheduled => Err(construct_not_supported_error_report(
-                capture_method,
-                self.id(),
-            )),
-        }
-    }
-
     fn is_webhook_source_verification_mandatory(&self) -> bool {
         true
     }
