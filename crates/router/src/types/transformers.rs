2cf186dedd2f20dd6daa6c1a86eef66af497631c
Kashif <kashif.dev@protonmail.com>
2024-12-12 20:50:33 +0530

    feat(core): payment links - add support for custom background image and layout in details section (#6725)
    Co-authored-by: hyperswitch-bot[bot] <148525504+hyperswitch-bot[bot]@users.noreply.github.com>
    (cherry picked from commit d11d87408d0c4195bbe2c4c51df50f24c1d332c6)

--- crates/router/src/types/transformers.rs
+++ crates/router/src/types/transformers.rs
@@ -1912,6 +1912,7 @@ impl ForeignFrom<api_models::admin::BusinessPaymentLinkConfig>
                     .collect()
             }),
             allowed_domains: item.allowed_domains,
+            branding_visibility: item.branding_visibility,
         }
     }
 }
@@ -1929,6 +1930,7 @@ impl ForeignFrom<diesel_models::business_profile::BusinessPaymentLinkConfig>
                     .collect()
             }),
             allowed_domains: item.allowed_domains,
+            branding_visibility: item.branding_visibility,
         }
     }
 }
@@ -1946,6 +1948,10 @@ impl ForeignFrom<api_models::admin::PaymentLinkConfigRequest>
             enabled_saved_payment_method: item.enabled_saved_payment_method,
             hide_card_nickname_field: item.hide_card_nickname_field,
             show_card_form_by_default: item.show_card_form_by_default,
+            details_layout: item.details_layout,
+            background_image: item
+                .background_image
+                .map(|background_image| background_image.foreign_into()),
         }
     }
 }
@@ -1964,6 +1970,36 @@ impl ForeignFrom<diesel_models::business_profile::PaymentLinkConfigRequest>
             hide_card_nickname_field: item.hide_card_nickname_field,
             show_card_form_by_default: item.show_card_form_by_default,
             transaction_details: None,
+            details_layout: item.details_layout,
+            background_image: item
+                .background_image
+                .map(|background_image| background_image.foreign_into()),
+        }
+    }
+}
+
+impl ForeignFrom<diesel_models::business_profile::PaymentLinkBackgroundImageConfig>
+    for api_models::admin::PaymentLinkBackgroundImageConfig
+{
+    fn foreign_from(
+        item: diesel_models::business_profile::PaymentLinkBackgroundImageConfig,
+    ) -> Self {
+        Self {
+            url: item.url,
+            position: item.position,
+            size: item.size,
+        }
+    }
+}
+
+impl ForeignFrom<api_models::admin::PaymentLinkBackgroundImageConfig>
+    for diesel_models::business_profile::PaymentLinkBackgroundImageConfig
+{
+    fn foreign_from(item: api_models::admin::PaymentLinkBackgroundImageConfig) -> Self {
+        Self {
+            url: item.url,
+            position: item.position,
+            size: item.size,
         }
     }
 }
