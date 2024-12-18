dfaa51f1ad9d210e69696ab9ad99a3f968d34562
prajjwalkumar17 <prajjwal.kumar@juspay.in>
2024-12-18 18:47:26 +0530

    add sql query

--- migrations/2024-12-18-124527_add_new_value_in_success_based_routing_conclusive_state/down.sql
+++ migrations/2024-12-18-124527_add_new_value_in_success_based_routing_conclusive_state/down.sql
@@ -1,6 +1,2 @@
 -- This file should undo anything in `up.sql`
-DELETE FROM pg_enum
-WHERE enumlabel = 'non_deterministic'
-AND enumtypid = (
-  SELECT oid FROM pg_type WHERE typname = 'SuccessBasedRoutingConclusiveState'
-)
+SELECT 1;
