a7b3b6e179cb871d69edca08fe14906970335bb0
Sarthak Soni <sarthak.soni@juspay.in>
2024-12-19 16:31:32 +0530

    fix: Fixed errors

--- crates/api_models/src/payment_methods.rs
+++ crates/api_models/src/payment_methods.rs
@@ -815,6 +815,7 @@ pub struct PaymentMethodResponse {
     pub last_used_at: Option<time::PrimitiveDateTime>,
 
     /// For Client based calls
+    #[schema(value_type=Option<String>)]
     pub ephemeral_key: Option<masking::Secret<String>>,
 
     pub payment_method_data: Option<PaymentMethodResponseData>,
