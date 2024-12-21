7102076d3fe8ea884b1b139481762ff3ea0ee488
Rachit Naithani <rachitdev45@gmail.com>
2024-12-22 00:14:04 +0530

    stack size and v2

--- crates/router/build.rs
+++ crates/router/build.rs
@@ -2,7 +2,7 @@ fn main() {
     // Set thread stack size to 8 MiB for debug builds
     // Reference: https://doc.rust-lang.org/std/thread/#stack-size
     #[cfg(debug_assertions)]
-    println!("cargo:rustc-env=RUST_MIN_STACK=12582912"); // 12 * 1024 * 1024 = 12 MiB
+    println!("cargo:rustc-env=RUST_MIN_STACK=14680064"); // 14 * 1024 * 1024 = 12 MiB
 
     #[cfg(feature = "vergen")]
     router_env::vergen::generate_cargo_instructions();
