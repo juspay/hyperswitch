f97f6a976e002130572e4a74f8e2830b112d1f0d
Mani Chandra Dulam <mani.dchandra@juspay.in>
2024-12-26 13:11:23 +0530

    feat: add v2 schema

--- crates/diesel_models/src/schema_v2.rs
+++ crates/diesel_models/src/schema_v2.rs
@@ -1318,6 +1318,8 @@ diesel::table! {
         allow_signup -> Bool,
         created_at -> Timestamp,
         last_modified_at -> Timestamp,
+        #[max_length = 64]
+        email_domain -> Varchar,
     }
 }
 
