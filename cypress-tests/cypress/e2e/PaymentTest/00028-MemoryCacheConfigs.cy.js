f28708024607f635c5d50d106e9c120ef9b29923
Sumit Kumar <sumit.kumar@Sumit-Kumar-D9GXRYR75W.local>
2024-12-30 17:11:16 +0530

    fix: address lints

--- cypress-tests/cypress/e2e/PaymentTest/00028-MemoryCacheConfigs.cy.js
+++ cypress-tests/cypress/e2e/PaymentTest/00028-MemoryCacheConfigs.cy.js
@@ -2,43 +2,35 @@ import State from "../../utils/State";
 
 let globalState;
 
-function logRequestId(xRequestId) {
-  if (xRequestId) {
-    cy.task("cli_log", "x-request-id -> " + xRequestId);
-  } else {
-    cy.task("cli_log", "x-request-id is not available in the response headers");
-  }
-}
-
 describe("In Memeory Cache Test", () => {
 
-    before("seed global state", () => {
-      cy.task("getGlobalState").then((state) => {
-        globalState = new State(state);
-      });
+  before("seed global state", () => {
+    cy.task("getGlobalState").then((state) => {
+      globalState = new State(state);
+    });
+  });
+
+  after("flush global state", () => {
+    cy.task("setGlobalState", globalState.data);
+  });
+
+  context("Config flows", () => {
+    const key = "test-key";
+    const value = "test value";
+    const newValue = "new test value";
+
+    it("Create Configs", () => {
+      cy.createConfigs(globalState, key, value);
+      cy.fetchConfigs(globalState, key, value);
     });
 
-    after("flush global state", () => {
-      cy.task("setGlobalState", globalState.data);
+    it("Update Configs", () => {
+      cy.updateConfigs(globalState, key, newValue);
+      cy.fetchConfigs(globalState, key, newValue);
     });
 
-    context("Config flows", () => {
-      const key = "test-key";
-      const value = "test value";
-      const newValue = "new test value";
-
-      it("Create Configs", () => {
-        cy.createConfigs(globalState, key, value);
-        cy.fetchConfigs(globalState, key, value);
-      });
-
-      it("Update Configs", () => {
-        cy.updateConfigs(globalState, key, newValue);
-        cy.fetchConfigs(globalState, key, newValue);
-      });
-
-      it("delete configs", () => {
-        cy.deleteConfigs(globalState, key, newValue);
-      })
-    });
+    it("delete configs", () => {
+      cy.deleteConfigs(globalState, key, newValue);
+    })
+  });
 })
