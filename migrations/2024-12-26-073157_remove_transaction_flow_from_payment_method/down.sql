dfb2a6f29a777c13b053cd503913a4fcb833ebc2
sumanmaji4 <suman.maji@juspay.in>
2025-01-02 20:30:24 +0530

    retrieve call fix and comment resolve

--- migrations/2024-12-26-073157_remove_transaction_flow_from_payment_method/down.sql
+++
@@ -1,7 +0,0 @@
--- This file should undo anything in `up.sql`
-CREATE TYPE "PaymentDirection" AS ENUM (
-  'payin',
-  'payout'
-);
-
-ALTER TABLE payment_methods ADD COLUMN IF NOT EXISTS transaction_flow "PaymentDirection";
