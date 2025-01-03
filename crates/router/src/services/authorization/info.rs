27ee7d2da7fddaa9f5f3156f9ddbb0ad096ee419
Kashif <kashif.dev@protonmail.com>
2025-01-03 17:53:54 +0530

    chore(roles): update PermissionGroup descriptions for recon

--- crates/router/src/services/authorization/info.rs
+++ crates/router/src/services/authorization/info.rs
@@ -40,10 +40,10 @@ fn get_group_description(group: PermissionGroup) -> &'static str {
         PermissionGroup::MerchantDetailsView | PermissionGroup::AccountView => "View Merchant Details",
         PermissionGroup::MerchantDetailsManage | PermissionGroup::AccountManage => "Create, modify and delete Merchant Details like api keys, webhooks, etc",
         PermissionGroup::OrganizationManage => "Manage organization level tasks like create new Merchant accounts, Organization level roles, etc",
-        PermissionGroup::ReconReportsView => "View and access reconciliation reports and analytics",
-        PermissionGroup::ReconReportsManage => "Manage reconciliation reports",
-        PermissionGroup::ReconOpsView => "View and access reconciliation operations",
-        PermissionGroup::ReconOpsManage | PermissionGroup::ReconOps => "Manage reconciliation operations",
+        PermissionGroup::ReconReportsView => "View recon reports and analytics",
+        PermissionGroup::ReconReportsManage => "Manage recon reports",
+        PermissionGroup::ReconOpsView => "View all recon operations",
+        PermissionGroup::ReconOpsManage | PermissionGroup::ReconOps => "Manage all recon operations",
     }
 }
 
