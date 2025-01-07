931d5c070934eef1fb63060c7b4f4427b3c9de59
PiX <69745008+pixincreate@users.noreply.github.com>
2025-01-07 15:56:45 +0530

    fix: session call webpack failure

--- cypress-tests/cypress/e2e/spec/Payment/00025-SessionCall.cy.js
+++ cypress-tests/cypress/e2e/spec/Payment/00025-SessionCall.cy.js
@@ -1,6 +1,6 @@
-import * as fixtures from "../../fixtures/imports";
-import State from "../../utils/State";
-import getConnectorDetails, * as utils from "../PaymentUtils/Utils";
+import * as fixtures from "../../../fixtures/imports";
+import State from "../../../utils/State";
+import getConnectorDetails, * as utils from "../../configs/Payment/Utils";
 
 let globalState;
 
