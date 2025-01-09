9ad430bb09f9be50f531aa14395c0f9eec17bee2
PiX <69745008+pixincreate@users.noreply.github.com>
2025-01-09 15:40:09 +0530

    chore: remove crap

--- cypress-tests/cypress/support/e2e.js
+++ cypress-tests/cypress/support/e2e.js
@@ -16,6 +16,3 @@
 // Import commands.js using ES2015 syntax:
 import "cypress-mochawesome-reporter/register";
 import "./commands";
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
