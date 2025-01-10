3b6364d8591f8114d263411846353ef43b2a5af0
Kashif <kashif.dev@protonmail.com>
2025-01-10 13:26:26 +0530

    chore: update down query

--- migrations/2025-01-09-135057_increase_connector_transaction_data_length/down.sql
+++ migrations/2025-01-09-135057_increase_connector_transaction_data_length/down.sql
@@ -1,4 +1,51 @@
-ALTER TABLE payment_attempt ALTER COLUMN connector_transaction_data TYPE VARCHAR(512);
-ALTER TABLE refund ALTER COLUMN connector_refund_data TYPE VARCHAR(512);
-ALTER TABLE refund ALTER COLUMN connector_transaction_data TYPE VARCHAR(512);
-ALTER TABLE captures ALTER COLUMN connector_capture_data TYPE VARCHAR(512);
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_name = 'payment_attempt'
+          AND column_name = 'connector_transaction_data'
+    ) THEN
+        ALTER TABLE payment_attempt 
+        ALTER COLUMN connector_transaction_data TYPE VARCHAR(512);
+    END IF;
+END $$;
+
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_name = 'refund'
+          AND column_name = 'connector_refund_data'
+    ) THEN
+        ALTER TABLE refund 
+        ALTER COLUMN connector_refund_data TYPE VARCHAR(512);
+    END IF;
+END $$;
+
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_name = 'refund'
+          AND column_name = 'connector_transaction_data'
+    ) THEN
+        ALTER TABLE refund 
+        ALTER COLUMN connector_transaction_data TYPE VARCHAR(512);
+    END IF;
+END $$;
+
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_name = 'captures'
+          AND column_name = 'connector_capture_data'
+    ) THEN
+        ALTER TABLE captures 
+        ALTER COLUMN connector_capture_data TYPE VARCHAR(512);
+    END IF;
+END $$;
