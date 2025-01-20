60b90dd730be1d4344b08614f9571c63d17324f0
Kashif <kashif.dev@protonmail.com>
2025-01-20 08:23:48 +0530

    chore: revert

--- crates/router/src/core/payment_link/payment_link_initiate/payment_link_initiator.js
+++ crates/router/src/core/payment_link/payment_link_initiate/payment_link_initiator.js
@@ -27,7 +27,7 @@ function initializeSDK() {
   // @ts-ignore
   hyper = window.Hyper(pub_key, {
     isPreloadEnabled: false,
-    shouldUseTopRedirection: isFramed,
+    shouldUseTopRedirection: true,
   });
   // @ts-ignore
   widgets = hyper.widgets({
