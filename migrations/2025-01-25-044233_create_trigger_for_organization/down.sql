288688890439331a571a5d3e7f531146eff670b8
hrithikesh026 <hrithikesh.vm@juspay.in>
2025-01-27 15:15:47 +0530

    refactor: update sql up and down

--- migrations/2025-01-25-044233_create_trigger_for_organization/down.sql
+++ migrations/2025-01-25-044233_create_trigger_for_organization/down.sql
@@ -1,4 +1,8 @@
 -- This file should undo anything in `up.sql`
-DROP TRIGGER IF EXISTS before_insert_trigger ON organization;
+DROP TRIGGER IF EXISTS trigger_set_org_id_org_name ON organization;
 
-DROP FUNCTION IF EXISTS set_not_null_field();
+DROP FUNCTION IF EXISTS set_org_id_org_name();
+
+DROP TRIGGER IF EXISTS trigger_update_org_name ON organization;
+
+DROP FUNCTION IF EXISTS update_org_name();
--- migrations/2025-01-25-044233_create_trigger_for_organization/up.sql
+++ migrations/2025-01-25-044233_create_trigger_for_organization/up.sql
@@ -1,16 +1,35 @@
--- Your SQL goes here
-CREATE OR REPLACE FUNCTION set_not_null_field()
+-- Create function
+CREATE OR REPLACE FUNCTION set_org_id_org_name()
 RETURNS TRIGGER AS $$
 BEGIN
     -- If 'org_id' is NULL, default it to 'id'
     IF NEW.org_id IS NULL THEN
         NEW.org_id := NEW.id;
+        NEW.org_name := NEW.organization_name;
     END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER before_insert_trigger
+-- Create trigger to call the function before insert
+CREATE TRIGGER trigger_set_org_id_org_name
 BEFORE INSERT ON organization
 FOR EACH ROW
-EXECUTE FUNCTION set_not_null_field();
+EXECUTE FUNCTION set_org_id_org_name();
+
+-- Create function
+CREATE OR REPLACE FUNCTION update_org_name()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.organization_name IS DISTINCT FROM OLD.org_name THEN
+        NEW.org_name = NEW.organization_name;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Create trigger to call the function before update
+CREATE TRIGGER trigger_update_org_name
+BEFORE UPDATE ON organization
+FOR EACH ROW
+EXECUTE FUNCTION update_org_name();
