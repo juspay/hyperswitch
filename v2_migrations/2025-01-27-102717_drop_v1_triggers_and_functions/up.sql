d8bb5d93b06f38507ff7633f883945b922313493
hrithikesh026 <hrithikesh.vm@juspay.in>
2025-01-27 16:14:20 +0530

    chore: drop v1_triggers in v2

---
+++ v2_migrations/2025-01-27-102717_drop_v1_triggers_and_functions/up.sql
@@ -0,0 +1,8 @@
+-- Your SQL goes here
+DROP TRIGGER IF EXISTS trigger_set_org_id_org_name ON organization;
+
+DROP FUNCTION IF EXISTS set_org_id_org_name();
+
+DROP TRIGGER IF EXISTS trigger_update_org_name ON organization;
+
+DROP FUNCTION IF EXISTS update_org_name();
