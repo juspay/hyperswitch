958a302664a5c5a0333e1a0bf51a34dd60a1caec
hrithikesh026 <hrithikesh.vm@juspay.in>
2025-02-03 16:28:27 +0530

    update sql queries

--- migrations/2025-01-25-044233_create_trigger_for_organization/down.sql
+++ migrations/2025-01-25-044233_create_trigger_for_organization/down.sql
@@ -1,8 +1,8 @@
 -- This file should undo anything in `up.sql`
-DROP TRIGGER IF EXISTS trigger_set_org_id_org_name ON organization;
+DROP TRIGGER IF EXISTS trigger_set_id_organization_name ON organization;
 
-DROP FUNCTION IF EXISTS set_org_id_org_name();
+DROP FUNCTION IF EXISTS set_id_organization_name();
 
-DROP TRIGGER IF EXISTS trigger_update_org_name ON organization;
+DROP TRIGGER IF EXISTS trigger_update_organization_name ON organization;
 
-DROP FUNCTION IF EXISTS update_org_name();
+DROP FUNCTION IF EXISTS update_organization_name();
--- migrations/2025-01-25-044233_create_trigger_for_organization/up.sql
+++ migrations/2025-01-25-044233_create_trigger_for_organization/up.sql
@@ -1,35 +1,35 @@
 -- Create function
-CREATE OR REPLACE FUNCTION set_org_id_org_name()
+CREATE OR REPLACE FUNCTION set_id_organization_name()
 RETURNS TRIGGER AS $$
 BEGIN
-    -- If 'org_id' is NULL, default it to 'id'
-    IF NEW.org_id IS NULL THEN
-        NEW.org_id := NEW.id;
-        NEW.org_name := NEW.organization_name;
+    -- If 'id' is NULL, default it to 'org_id'
+    IF NEW.id IS NULL THEN
+        NEW.id := NEW.org_id;
+        NEW.organization_name := NEW.org_name;
     END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 -- Create trigger to call the function before insert
-CREATE TRIGGER trigger_set_org_id_org_name
+CREATE TRIGGER trigger_set_id_organization_name
 BEFORE INSERT ON organization
 FOR EACH ROW
-EXECUTE FUNCTION set_org_id_org_name();
+EXECUTE FUNCTION set_id_organization_name();
 
 -- Create function
-CREATE OR REPLACE FUNCTION update_org_name()
+CREATE OR REPLACE FUNCTION update_organization_name()
 RETURNS TRIGGER AS $$
 BEGIN
-    IF NEW.organization_name IS DISTINCT FROM OLD.org_name THEN
-        NEW.org_name = NEW.organization_name;
+    IF NEW.org_name IS DISTINCT FROM OLD.organization_name THEN
+        NEW.organization_name = NEW.org_name;
     END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 -- Create trigger to call the function before update
-CREATE TRIGGER trigger_update_org_name
+CREATE TRIGGER trigger_update_organization_name
 BEFORE UPDATE ON organization
 FOR EACH ROW
-EXECUTE FUNCTION update_org_name();
+EXECUTE FUNCTION update_organization_name();
