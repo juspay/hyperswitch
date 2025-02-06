06995e99229c488ee04b14c61b763db55011d7a2
Malay Awasthi <malay.awasthi@juspay.in>
2025-02-06 11:41:07 +0530

    template-pr

--- crates/router/src/types/transformers.rs
+++ crates/router/src/types/transformers.rs
@@ -228,6 +228,7 @@ impl ForeignTryFrom<api_enums::Connector> for common_enums::RoutableConnectors {
             // api_enums::Connector::Chargebee => Self::Chargebee,
             api_enums::Connector::Checkout => Self::Checkout,
             api_enums::Connector::Coinbase => Self::Coinbase,
+			// api_enums::Connector::Coingate => Self::Coingate,
             api_enums::Connector::Cryptopay => Self::Cryptopay,
             api_enums::Connector::CtpMastercard => {
                 Err(common_utils::errors::ValidationError::InvalidValue {
