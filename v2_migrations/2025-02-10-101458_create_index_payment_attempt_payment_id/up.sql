c1bb435c521d21e88209e5358df0d4b0c116b9f2
Chikke Srujan <chikke.srujan@Chikke-Srujan-N7WRTY72X7.local>
2025-02-10 21:08:20 +0530

    ci fix

--- v2_migrations/2025-02-10-101458_create_index_payment_attempt_payment_id/up.sql
+++ v2_migrations/2025-02-10-101458_create_index_payment_attempt_payment_id/up.sql
@@ -1,4 +1,3 @@
 -- Your SQL goes here
-CREATE INDEX IF NOT EXISTS payment_attempt_payment_id_index ON payment_attempt (payment_id);
-
 DROP INDEX IF EXISTS payment_attempt_payment_id_merchant_id_index;
+CREATE INDEX IF NOT EXISTS payment_attempt_payment_id_index ON payment_attempt (payment_id);
