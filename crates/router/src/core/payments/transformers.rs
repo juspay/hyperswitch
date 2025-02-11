6338c0a6ee8cc06635174b0fff0e5d6a50abe8e9
AkshayaFoiger <akshaya.shankar@juspay.in>
2025-02-11 12:56:14 +0530

    fix clippy error

--- crates/router/src/core/payments/transformers.rs
+++ crates/router/src/core/payments/transformers.rs
@@ -468,6 +468,7 @@ pub async fn construct_payment_router_data_for_capture<'a>(
         browser_info: None,
         metadata: payment_data.payment_intent.metadata.expose_option(),
         integrity_object: None,
+        split_payments: None,
     };
 
     // TODO: evaluate the fields in router data, if they are required or not
@@ -3262,6 +3263,7 @@ impl<F: Clone> TryFrom<PaymentAdditionalData<'_, F>> for types::PaymentsCaptureD
             browser_info: None,
             metadata: payment_data.payment_intent.metadata.expose_option(),
             integrity_object: None,
+            split_payments: None,
         })
     }
 }
