4829f2e8efb87cc7b4d6d69a7eeabcd42faf4f07
hrithikesh026 <hrithikesh.vm@juspay.in>
2025-02-11 13:32:37 +0530

    chore: remove triggers since it is already being handled in code

--- migrations/2025-01-25-044233_create_trigger_for_organization/down.sql
+++
@@ -1,8 +0,0 @@
--- This file should undo anything in `up.sql`
-DROP TRIGGER IF EXISTS trigger_set_id_organization_name ON organization;
-
-DROP FUNCTION IF EXISTS set_id_organization_name();
-
-DROP TRIGGER IF EXISTS trigger_update_organization_name ON organization;
-
-DROP FUNCTION IF EXISTS update_organization_name();
