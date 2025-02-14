12ef8ee0fc63829429697c42b98f4c773f12cade
Mani Chandra <84711804+ThisIsMani@users.noreply.github.com>
2025-02-14 14:48:12 +0530

    refactor(payments): Add platform merchant account checks for payment intent (#7204)

--- crates/router/src/core/payments/operations/payment_confirm_intent.rs
+++ crates/router/src/core/payments/operations/payment_confirm_intent.rs
@@ -171,6 +171,8 @@ impl<F: Send + Clone + Sync> GetTracker<F, PaymentConfirmData<F>, PaymentsConfir
             .await
             .to_not_found_response(errors::ApiErrorResponse::PaymentNotFound)?;
 
+        // TODO (#7195): Add platform merchant account validation once publishable key auth is solved
+
         self.validate_status_for_operation(payment_intent.status)?;
         let client_secret = header_payload
             .client_secret
