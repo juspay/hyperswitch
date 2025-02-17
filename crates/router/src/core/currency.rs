0d4854147d9800afdcde5b970af90d55e6975cb0
shivansh.mathur <shivansh.mathur@juspay.in>
2025-02-17 13:05:21 +0530

    Retry counter for forex call

--- crates/router/src/core/currency.rs
+++ crates/router/src/core/currency.rs
@@ -2,6 +2,7 @@ use analytics::errors::AnalyticsError;
 use common_utils::errors::CustomResult;
 use currency_conversion::types::ExchangeRates;
 use error_stack::ResultExt;
+use router_env::logger;
 
 use crate::{
     core::errors::ApiErrorResponse,
@@ -48,9 +49,27 @@ pub async fn get_forex_exchange_rates(
     state: SessionState,
 ) -> CustomResult<ExchangeRates, AnalyticsError> {
     let forex_api = state.conf.forex_api.get_inner();
-    let rates = get_forex_rates(&state, forex_api.call_delay)
-        .await
-        .change_context(AnalyticsError::ForexFetchFailed)?;
-
-    Ok((*rates.data).clone())
+    let max_attempts = 3;
+    let mut attempt = 1;
+    
+    logger::info!("Starting forex exchange rates fetch");
+    loop {
+        logger::info!("Attempting to fetch forex rates - Attempt {attempt} of {max_attempts}");
+        
+        match get_forex_rates(&state, forex_api.call_delay).await {
+            Ok(rates) => {
+                logger::info!("Successfully fetched forex rates");
+                return Ok((*rates.data).clone())
+            },
+            Err(error) => {
+                if attempt >= max_attempts {
+                    logger::error!("Failed to fetch forex rates after {max_attempts} attempts");
+                    return Err(error.change_context(AnalyticsError::ForexFetchFailed));
+                }
+                logger::warn!("Forex rates fetch failed, retrying in {attempt} seconds");
+                tokio::time::sleep(std::time::Duration::from_secs(attempt)).await;
+                attempt += 1;
+            }
+        }
+    }
 }
