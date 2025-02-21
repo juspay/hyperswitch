420b91e0604b2a24b71ed549b3d77d2be098385b
sumanmaji4 <suman.maji@juspay.in>
2025-02-21 16:12:12 +0530

    connectors added in mandates supported list

--- crates/hyperswitch_connectors/src/connectors/payeezy.rs
+++ crates/hyperswitch_connectors/src/connectors/payeezy.rs
@@ -616,6 +616,25 @@ lazy_static! {
             },
         );
 
+        payeezy_supported_payment_methods.add(
+            PaymentMethod::Card,
+            PaymentMethodType::Debit,
+            PaymentMethodDetails {
+                mandates: common_enums::enums::FeatureStatus::Supported,
+                refunds: common_enums::enums::FeatureStatus::Supported,
+                supported_capture_methods: supported_capture_methods.clone(),
+                specific_features: Some(
+                    api_models::feature_matrix::PaymentMethodSpecificFeatures::Card({
+                        api_models::feature_matrix::CardSpecificFeatures {
+                            three_ds: common_enums::FeatureStatus::NotSupported,
+                            no_three_ds: common_enums::FeatureStatus::Supported,
+                            supported_card_networks: supported_card_network.clone(),
+                        }
+                    }),
+                ),
+            },
+        );
+
         payeezy_supported_payment_methods
     };
     static ref PAYEEZY_CONNECTOR_INFO: ConnectorInfo = ConnectorInfo {
