83fe8d7e19233251c83c4e5b8af04a6b7ed0239e
PiX <69745008+pixincreate@users.noreply.github.com>
2025-02-24 11:48:21 +0530

    refactor: do not check for connector validation since feature matrix is enabled (core does not call validation if feature matrix is implemented)

--- crates/hyperswitch_connectors/src/connectors/coinbase.rs
+++ crates/hyperswitch_connectors/src/connectors/coinbase.rs
@@ -139,24 +139,7 @@ impl ConnectorCommon for Coinbase {
     }
 }
 
-impl ConnectorValidation for Coinbase {
-    fn validate_connector_against_payment_request(
-        &self,
-        capture_method: Option<enums::CaptureMethod>,
-        _payment_method: enums::PaymentMethod,
-        _pmt: Option<enums::PaymentMethodType>,
-    ) -> CustomResult<(), errors::ConnectorError> {
-        let capture_method = capture_method.unwrap_or_default();
-        match capture_method {
-            enums::CaptureMethod::Automatic
-            | enums::CaptureMethod::Manual
-            | enums::CaptureMethod::SequentialAutomatic => Ok(()),
-            enums::CaptureMethod::ManualMultiple | enums::CaptureMethod::Scheduled => Err(
-                utils::construct_not_supported_error_report(capture_method, self.id()),
-            ),
-        }
-    }
-}
+impl ConnectorValidation for Coinbase {}
 
 impl ConnectorIntegration<PaymentMethodToken, PaymentMethodTokenizationData, PaymentsResponseData>
     for Coinbase
--- crates/hyperswitch_connectors/src/connectors/square.rs
+++ crates/hyperswitch_connectors/src/connectors/square.rs
@@ -162,24 +162,7 @@ impl ConnectorCommon for Square {
     }
 }
 
-impl ConnectorValidation for Square {
-    fn validate_connector_against_payment_request(
-        &self,
-        capture_method: Option<enums::CaptureMethod>,
-        _payment_method: enums::PaymentMethod,
-        _pmt: Option<enums::PaymentMethodType>,
-    ) -> CustomResult<(), errors::ConnectorError> {
-        let capture_method = capture_method.unwrap_or_default();
-        match capture_method {
-            enums::CaptureMethod::Automatic
-            | enums::CaptureMethod::Manual
-            | enums::CaptureMethod::SequentialAutomatic => Ok(()),
-            enums::CaptureMethod::ManualMultiple | enums::CaptureMethod::Scheduled => Err(
-                utils::construct_not_implemented_error_report(capture_method, self.id()),
-            ),
-        }
-    }
-}
+impl ConnectorValidation for Square {}
 
 impl ConnectorIntegration<Session, PaymentsSessionData, PaymentsResponseData> for Square {
     //TODO: implement sessions flow
