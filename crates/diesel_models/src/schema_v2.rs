0f1238c3b88a0dc3da3ec35fba5045423517e8c1
Amey Wale <amey.wale@juspay.in>
2025-02-24 12:13:00 +0530

    refactor(webhook): add is_overall_delivery_successful column in v2 schema

--- crates/diesel_models/src/schema_v2.rs
+++ crates/diesel_models/src/schema_v2.rs
@@ -483,6 +483,7 @@ diesel::table! {
         response -> Nullable<Bytea>,
         delivery_attempt -> Nullable<WebhookDeliveryAttempt>,
         metadata -> Nullable<Jsonb>,
+        is_overall_delivery_successful -> Bool,
     }
 }
 
