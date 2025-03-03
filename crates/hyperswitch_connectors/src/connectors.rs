af8778e0091fb4dd92bac55c3972c1ca99303aff
CHALLA NISHANTH BABU <115225644+NISHANTH1221@users.noreply.github.com>
2025-03-03 15:04:51 +0530

    feat(connector): add template code for stripebilling (#7228)
    Co-authored-by: Nishanth Challa <nishanth.challa@Nishanth-Challa-C0WGKCFHLF.local>
    Co-authored-by: hyperswitch-bot[bot] <148525504+hyperswitch-bot[bot]@users.noreply.github.com>

--- crates/hyperswitch_connectors/src/connectors.rs
+++ crates/hyperswitch_connectors/src/connectors.rs
@@ -57,6 +57,7 @@ pub mod redsys;
 pub mod shift4;
 pub mod square;
 pub mod stax;
+pub mod stripebilling;
 pub mod taxjar;
 pub mod thunes;
 pub mod tsys;
@@ -83,8 +84,9 @@ pub use self::{
     nexinets::Nexinets, nexixpay::Nexixpay, nomupay::Nomupay, novalnet::Novalnet, nuvei::Nuvei,
     paybox::Paybox, payeezy::Payeezy, paystack::Paystack, payu::Payu, placetopay::Placetopay,
     powertranz::Powertranz, prophetpay::Prophetpay, rapyd::Rapyd, razorpay::Razorpay,
-    redsys::Redsys, shift4::Shift4, square::Square, stax::Stax, taxjar::Taxjar, thunes::Thunes,
-    tsys::Tsys, unified_authentication_service::UnifiedAuthenticationService, volt::Volt,
+    redsys::Redsys, shift4::Shift4, square::Square, stax::Stax, stripebilling::Stripebilling,
+    taxjar::Taxjar, thunes::Thunes, tsys::Tsys,
+    unified_authentication_service::UnifiedAuthenticationService, volt::Volt,
     wellsfargo::Wellsfargo, worldline::Worldline, worldpay::Worldpay, xendit::Xendit, zen::Zen,
     zsl::Zsl,
 };
