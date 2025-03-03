272cb0edb7b88ec6699a7d41fa15c3e840779ea4
Amey Wale <amey.wale@juspay.in>
2025-03-03 12:30:47 +0530

    refactor(webhook): Ensure db migration consistency with v2_schema

--- crates/diesel_models/src/schema_v2.rs
+++ crates/diesel_models/src/schema_v2.rs
@@ -483,7 +483,7 @@ diesel::table! {
         response -> Nullable<Bytea>,
         delivery_attempt -> Nullable<WebhookDeliveryAttempt>,
         metadata -> Nullable<Jsonb>,
-        is_overall_delivery_successful -> Bool,
+        is_overall_delivery_successful -> Nullable<Bool>,
     }
 }
 
