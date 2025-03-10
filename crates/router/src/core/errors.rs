3ec52a17b9ce019ee43a903fe72a3ce567d94790
Prasunna Soppa <prasunna.soppa@juspay.in>
2025-03-10 18:57:45 +0530

    fix spell check

--- crates/router/src/core/errors.rs
+++ crates/router/src/core/errors.rs
@@ -483,6 +483,6 @@ pub enum RevenueRecoveryError {
     PaymentIntentCreateFailed,
     #[error("Source verification failed for billing connector")]
     WebhookAuthenticationFailed,
-    #[error("Payment merchant connector account not found using acccount reference id")]
+    #[error("Payment merchant connector account not found using account reference id")]
     PaymentMerchantConnectorAccountNotFound,
 }
