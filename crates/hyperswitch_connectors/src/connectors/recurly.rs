1407bac587e6ec1c26f7539da85e2e3b635f3d40
Aniket Burman <aniket.burman@192.168.1.5>
2025-03-11 14:41:07 +0530

    Resolved error

--- crates/hyperswitch_connectors/src/connectors/recurly.rs
+++ crates/hyperswitch_connectors/src/connectors/recurly.rs
@@ -1,7 +1,4 @@
 pub mod transformers;
-
-use std::collections::HashMap;
-
 use common_utils::{
     errors::CustomResult,
     ext_traits::BytesExt,
@@ -578,9 +575,9 @@ impl webhooks::IncomingWebhook for Recurly {
         request: &webhooks::IncomingWebhookRequestDetails<'_>,
         _connector_webhook_secrets: &api_models::webhooks::ConnectorWebhookSecrets,
     ) -> CustomResult<Vec<u8>, errors::ConnectorError> {
-        /// The `recurly-signature` header consists of a Unix timestamp (in milliseconds) followed by one or more HMAC-SHA256 signatures, separated by commas.  
-        /// Multiple signatures exist when a secret key is regenerated, with the old key remaining active for 24 hours.  
-        let header_values = Recurly::get_signature_elements_from_header(request.headers)?;
+        // The `recurly-signature` header consists of a Unix timestamp (in milliseconds) followed by one or more HMAC-SHA256 signatures, separated by commas.  
+        // Multiple signatures exist when a secret key is regenerated, with the old key remaining active for 24 hours.  
+        let header_values = Self::get_signature_elements_from_header(request.headers)?;
         let signature = header_values
             .get(1)
             .ok_or(errors::ConnectorError::WebhookSignatureNotFound)?;
@@ -593,13 +590,13 @@ impl webhooks::IncomingWebhook for Recurly {
         _merchant_id: &common_utils::id_type::MerchantId,
         _connector_webhook_secrets: &api_models::webhooks::ConnectorWebhookSecrets,
     ) -> CustomResult<Vec<u8>, errors::ConnectorError> {
-        let header_values = Recurly::get_signature_elements_from_header(request.headers)?;
+        let header_values = Self::get_signature_elements_from_header(request.headers)?;
         let timestamp = header_values
-            .get(0)
+            .first()
             .ok_or(errors::ConnectorError::WebhookSignatureNotFound)?;
         Ok(format!(
             "{}.{}",
-            String::from_utf8_lossy(&timestamp),
+            String::from_utf8_lossy(timestamp),
             String::from_utf8_lossy(request.body)
         )
         .into_bytes())
