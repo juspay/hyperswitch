9f3123b3f2ca008603f9b61415559fbf8acdd4ec
Sahkal Poddar <sahkal.poddar@juspay.in>
2025-03-13 15:17:08 +0530

    fixed open api specs

--- crates/api_models/src/admin.rs
+++ crates/api_models/src/admin.rs
@@ -113,7 +113,7 @@ pub struct MerchantAccountCreate {
     pub pm_collect_link_config: Option<BusinessCollectLinkConfig>,
 
     /// Product Type of this merchant account
-    #[schema(value_type = Option<api_enums::MerchantProductType>)]
+    #[schema(value_type = Option<MerchantProductType>)]
     pub product_type: Option<api_enums::MerchantProductType>,
 }
 
@@ -560,7 +560,7 @@ pub struct MerchantAccountResponse {
     pub pm_collect_link_config: Option<BusinessCollectLinkConfig>,
 
     /// Product Type of this merchant account
-    #[schema(value_type = Option<api_enums::MerchantProductType>)]
+    #[schema(value_type = Option<MerchantProductType>)]
     pub product_type: Option<api_enums::MerchantProductType>,
 }
 
@@ -596,7 +596,7 @@ pub struct MerchantAccountResponse {
     pub recon_status: api_enums::ReconStatus,
 
     /// Product Type of this merchant account
-    #[schema(value_type = Option<api_enums::MerchantProductType>)]
+    #[schema(value_type = Option<MerchantProductType>)]
     pub product_type: Option<api_enums::MerchantProductType>,
 }
 
