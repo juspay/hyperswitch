c775ae22f9e1e62cd44f7b9b95c9fd0740abd3bb
Sayak Bhattacharya <sayak.b@Sayak-Bhattacharya-G092THXJ34.local>
2025-03-18 14:40:16 +0530

    feat(router): add local card networks and a field in payment method data in domain models

--- crates/router/tests/connectors/airwallex.rs
+++ crates/router/tests/connectors/airwallex.rs
@@ -72,6 +72,7 @@ fn get_default_payment_info() -> Option<utils::PaymentInfo> {
 fn payment_method_details() -> Option<types::PaymentsAuthorizeData> {
     Some(types::PaymentsAuthorizeData {
         payment_method_data: domain::PaymentMethodData::Card(domain::Card {
+            secondary_card_networks: None,
             card_number: cards::CardNumber::from_str("4035501000000008").unwrap(),
             card_exp_month: Secret::new("02".to_string()),
             card_exp_year: Secret::new("2035".to_string()),
