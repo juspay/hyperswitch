d3dfcd8c0c0714b16032e262ad00c420ebffa8c3
Aprabhat19 <amishaprabhat@gmail.com>
2025-03-23 23:53:23 +0530

    untracked files

---
+++ crates/api_models/src/process_tracker/revenue_recovery.rs
@@ -0,0 +1,18 @@
+use crate::enums;
+use common_utils::id_type;
+use serde::{Deserialize, Serialize};
+use time::PrimitiveDateTime;
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct RevenueRecoveryResponse {
+    pub id: String,
+    pub name: Option<String>,
+    pub schedule_time_for_payment: Option<PrimitiveDateTime>,
+    pub schedule_time_for_psync: Option<PrimitiveDateTime>,
+    pub status: enums::ProcessTrackerStatus,
+    pub business_status: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct RevenueRecoveryId {
+    pub revenue_recovery_id: id_type::GlobalPaymentId,
+}
