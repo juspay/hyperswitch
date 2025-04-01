fc937246c09ccc706390a7a10c87c1a8c7823146
hrithikesh026 <hrithikesh.vm@juspay.in>
2025-04-01 14:20:44 +0530

    add comments

--- crates/common_types/src/payment_methods.rs
+++ crates/common_types/src/payment_methods.rs
@@ -11,6 +11,11 @@ use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
 /// Details of all the payment methods enabled for the connector for the given merchant account
+
+// sql_type for this can be json instead of jsonb. This is because validation at database is not required since it will always be written by the application.
+// This is a performance optimization to avoid json validation at database level.
+// jsonb enables faster querying on json columns, but it doesn't justify here since we are not querying on this column.
+// https://docs.rs/diesel/latest/diesel/sql_types/struct.Jsonb.html
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema, AsExpression)]
 #[serde(deny_unknown_fields)]
 #[diesel(sql_type = Json)]
