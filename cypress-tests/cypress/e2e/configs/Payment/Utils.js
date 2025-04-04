a4ea6b5a6a5ae1088d96228d0c7510de3096d90a
gbague <guillaume.bague@flowbird.group>
2025-04-04 16:13:16 +0200

    fix(cypress): Archipel connector

--- cypress-tests/cypress/e2e/configs/Payment/Utils.js
+++ cypress-tests/cypress/e2e/configs/Payment/Utils.js
@@ -326,7 +326,7 @@ export function updateBusinessProfile(
 export const CONNECTOR_LISTS = {
   // Exclusion lists (skip these connectors)
   EXCLUDE: {
-    CONNECTOR_AGNOSTIC_NTID: ["bankofamerica", "braintree", "fiuu", "paypal"],
+    CONNECTOR_AGNOSTIC_NTID: ["bankofamerica", "braintree", "fiuu", "paypal", "archipel"],
     // Add more exclusion lists
   },
 
