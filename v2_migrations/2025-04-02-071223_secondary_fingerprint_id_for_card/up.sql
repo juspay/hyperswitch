37fda204a857ca59ae3dc2f7577f041af1ce5a22
Shivansh Mathur <104988143+ShivanshMathurJuspay@users.noreply.github.com>
2025-04-15 11:01:27 +0530

    Update up.sql

--- v2_migrations/2025-04-02-071223_secondary_fingerprint_id_for_card/up.sql
+++ v2_migrations/2025-04-02-071223_secondary_fingerprint_id_for_card/up.sql
@@ -1,2 +1,2 @@
 -- Your SQL goes here
-ALTER TABLE payment_methods ADD COLUMN secondary_fingerprint_id VARCHAR(64);
+ALTER TABLE payment_methods ADD COLUMN secondary_fingerprint_id VARCHAR(64) DEFAULT NULL;
