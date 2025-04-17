c53b310c8732552f9daaa0b5c0b58288a23d0e21
Nishanth Challa <nishanth.challa@Nishanth-Challa-C0WGKCFHLF.local>
2025-04-17 12:22:34 +0530

    recurly invoice sync connector inetgration support

--- crates/hyperswitch_connectors/src/connectors/recurly/transformers.rs
+++ crates/hyperswitch_connectors/src/connectors/recurly/transformers.rs
@@ -494,3 +494,95 @@ impl
         })
     }
 }
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct RecurlyInvoiceSyncResponse {
+    pub id : String,
+    pub total: FloatMajorUnit,
+    pub currency : common_enums::Currency,
+    pub address : Option<RecurlyInvoiceBillingAdddress>,
+    pub lines : Vec<RecurlyLineItems>,
+    pub transactions : Vec<RecurlyInvoiceTransactionsStatus>
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct RecurlyInvoiceBillingAdddress {
+    pub street1: Option<Secret<String>>,
+    pub street2: Option<Secret<String>>,
+    pub region: Option<Secret<String>>,
+    pub country: Option<enums::CountryAlpha2>,
+    pub postal_code: Option<Secret<String>>,
+    pub city: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct RecurlyLineItems {
+    #[serde(rename="type")]
+    pub invoice_type : RecurlyInvoiceLineItemType,
+    #[serde(with = "common_utils::custom_serde::iso8601")]
+    pub start_date : PrimitiveDateTime,
+    #[serde(with = "common_utils::custom_serde::iso8601")]
+    pub end_date : PrimitiveDateTime
+} 
+
+#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
+#[serde(rename_all = "snake_case")]
+pub enum RecurlyInvoiceLineItemType {
+    Credit,
+    Charge
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
+#[serde(rename_all = "snake_case")]
+pub struct RecurlyInvoiceTransactionsStatus {
+    pub status : String
+}
+
+impl TryFrom<
+    ResponseRouterData<
+            recovery_router_flows::BillingConnectorInvoiceSync,
+            RecurlyInvoiceSyncResponse,
+            recovery_request_types::BillingConnectorInvoiceSyncRequest,
+            recovery_response_types::BillingConnectorInvoiceSyncResponse
+    >
+> for recovery_router_data_types::BillingConnectorInvoiceSyncRouterData {
+    type Error = errors::ConnectorError;
+    fn try_from(item :
+        ResponseRouterData<
+        recovery_router_flows::BillingConnectorInvoiceSync,
+        RecurlyInvoiceSyncResponse,
+        recovery_request_types::BillingConnectorInvoiceSyncRequest,
+        recovery_response_types::BillingConnectorInvoiceSyncResponse
+>) ->  Result<Self, Self::Error> {
+        #[allow(clippy::as_conversions)]
+        // No of retries never exceeds u16 in recurly. So its better to supress the clippy warning
+        let retry_count = item.response.transactions.len() as u16; 
+        Ok(Self{
+            response : Ok(recovery_response_types::BillingConnectorInvoiceSyncResponse{
+                amount : item.response.total.into(),
+                currency: item.response.currency.into(),
+                merchant_reference_id: item.response.id.into(),
+                retry_count : Some(retry_count),
+                billing_address : Some(api_models::payments::Address{
+                    address : Some(api_models::payments::AddressDetails{
+                        city: item.response.address.and_then(|address| address.city),
+                        state: item.response.address.and_then(|address| address.region),
+                        country: item.response.address.and_then(|address| address.country),
+                        line1: item.response.address.and_then(|address| address.street1),
+                        line2: item.response.address.and_then(|address| address.street2),
+                        line3: None,
+                        zip: item.response.address.and_then(|address| address.postal_code),
+                        first_name : None,
+                        last_name: None
+                    }),
+                    phone : None,
+                    email: None
+                }),
+                created_at: item.response.lines.get(0).and_then(|line| Some(line.start_date)),
+                ends_at: item.response.lines.get(0).and_then(|line| Some(line.end_date))
+            }),
+            ..item.data
+        })
+
+    }
+}
