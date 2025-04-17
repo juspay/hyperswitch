d061e0a7c55c518cf76047eb1cbe506b0faed0bf
Sakil Mostak <73734619+Sakilmostak@users.noreply.github.com>
2025-04-17 17:08:18 +0530

    feat(payment_method): add logic for setup_future_usage downgrade and add filter based on zero mandate config (#7775)
    Co-authored-by: hyperswitch-bot[bot] <148525504+hyperswitch-bot[bot]@users.noreply.github.com>

--- crates/router/src/core/payments/helpers.rs
+++ crates/router/src/core/payments/helpers.rs
@@ -4500,6 +4500,7 @@ impl AttemptType {
             extended_authorization_applied: None,
             capture_before: None,
             card_discovery: None,
+            setup_future_usage_applied: None,
         }
     }
 
