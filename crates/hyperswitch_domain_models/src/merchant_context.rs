4de4acc32b360c20d0703b8a8822791e02535594
uzair khan <uzair.khan@juspay.in>
2025-04-23 12:09:21 +0530

    doc(merchant_context): add documentation to the enum struct of merchant_context

--- crates/hyperswitch_domain_models/src/merchant_context.rs
+++ crates/hyperswitch_domain_models/src/merchant_context.rs
@@ -1,10 +1,18 @@
 pub use crate::{merchant_account::MerchantAccount, merchant_key_store::MerchantKeyStore};
 
+/// `MerchantContext` represents the authentication and operational context for a merchant.
+///
+/// This enum encapsulates the merchant's account information and cryptographic keys
+/// needed for secure operations. Currently supports only normal merchant operations,
+/// but the enum structure allows for future expansion to different merchant types for example a
+/// **platform** context.
 #[derive(Clone, Debug)]
 pub enum MerchantContext {
+    /// Represents a normal operation merchant context.
     NormalMerchant(Box<Context>),
 }
 
+/// `Context` holds the merchant account details and cryptographic key store.
 #[derive(Clone, Debug)]
 pub struct Context(pub MerchantAccount, pub MerchantKeyStore);
 
