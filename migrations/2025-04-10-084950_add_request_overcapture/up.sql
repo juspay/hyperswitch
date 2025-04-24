38f0791c7ca6f491f1aa05c2c33de7fce6b3b057
AkshayaFoiger <akshaya.shankar@juspay.in>
2025-04-24 13:42:08 +0530

    Remove default null

--- migrations/2025-04-10-084950_add_request_overcapture/up.sql
+++ migrations/2025-04-10-084950_add_request_overcapture/up.sql
@@ -1,5 +1,5 @@
 -- Your SQL goes here
-ALTER TABLE business_profile ADD COLUMN IF NOT EXISTS always_request_overcapture BOOLEAN DEFAULT NULL;
+ALTER TABLE business_profile ADD COLUMN IF NOT EXISTS always_request_overcapture BOOLEAN;
 ALTER TABLE payment_attempt
-ADD COLUMN IF NOT EXISTS overcapture_status VARCHAR(32) DEFAULT NULL;
-ALTER TABLE payment_intent ADD COLUMN IF NOT EXISTS request_overcapture VARCHAR(32) DEFAULT NULL;
+ADD COLUMN IF NOT EXISTS overcapture_status VARCHAR(32);
+ALTER TABLE payment_intent ADD COLUMN IF NOT EXISTS request_overcapture VARCHAR(32);
