aca7aa93a14937f8848e107b9dd7d210299e40bd
sumanmaji4 <suman.maji@juspay.in>
2025-05-08 12:47:47 +0530

    pm_filter updated for boa and cybersource

--- crates/hyperswitch_connectors/src/connectors/bankofamerica.rs
+++ crates/hyperswitch_connectors/src/connectors/bankofamerica.rs
@@ -1103,6 +1103,17 @@ static BANKOFAMERICA_SUPPORTED_PAYMENT_METHODS: LazyLock<SupportedPaymentMethods
         );
 
         bankofamerica_supported_payment_methods.add(
+            enums::PaymentMethod::Wallet,
+            enums::PaymentMethodType::SamsungPay,
+            PaymentMethodDetails {
+                mandates: enums::FeatureStatus::NotSupported,
+                refunds: enums::FeatureStatus::Supported,
+                supported_capture_methods: supported_capture_methods.clone(),
+                specific_features: None,
+            },
+        );
+
+        bankofamerica_supported_payment_methods.add(
             enums::PaymentMethod::Card,
             enums::PaymentMethodType::Credit,
             PaymentMethodDetails {
