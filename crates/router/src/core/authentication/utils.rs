7b26ff2df9eadccd87f2854552245b79c6bd17a8
Sahkal Poddar <sahkalplanet@gmail.com>
2025-05-12 16:54:55 +0530

    refactor(authentication): moved cavv storing from table to temp locker (#7978)
    Co-authored-by: hyperswitch-bot[bot] <148525504+hyperswitch-bot[bot]@users.noreply.github.com>

--- crates/router/src/core/authentication/utils.rs
+++ crates/router/src/core/authentication/utils.rs
@@ -1,5 +1,7 @@
+use common_utils::ext_traits::AsyncExt;
 use error_stack::ResultExt;
 use hyperswitch_domain_models::router_data_v2::ExternalAuthenticationFlowData;
+use masking::ExposeInterface;
 
 use crate::{
     consts,
@@ -53,6 +55,7 @@ pub async fn update_trackers<F: Clone, Req>(
     router_data: RouterData<F, Req, AuthenticationResponseData>,
     authentication: storage::Authentication,
     acquirer_details: Option<super::types::AcquirerDetails>,
+    merchant_key_store: &hyperswitch_domain_models::merchant_key_store::MerchantKeyStore,
 ) -> RouterResult<storage::Authentication> {
     let authentication_update = match router_data.response {
         Ok(response) => match response {
@@ -91,10 +94,23 @@ pub async fn update_trackers<F: Clone, Req>(
                 connector_metadata,
                 ds_trans_id,
             } => {
+                authentication_value
+                    .async_map(|auth_val| {
+                        crate::core::payment_methods::vault::create_tokenize(
+                            state,
+                            auth_val.expose(),
+                            None,
+                            authentication.authentication_id.clone(),
+                            merchant_key_store.key.get_inner(),
+                        )
+                    })
+                    .await
+                    .transpose()?;
+
                 let authentication_status =
                     common_enums::AuthenticationStatus::foreign_from(trans_status.clone());
+
                 storage::AuthenticationUpdate::AuthenticationUpdate {
-                    authentication_value,
                     trans_status,
                     acs_url: authn_flow_type.get_acs_url(),
                     challenge_request: authn_flow_type.get_challenge_request(),
@@ -111,14 +127,27 @@ pub async fn update_trackers<F: Clone, Req>(
                 trans_status,
                 authentication_value,
                 eci,
-            } => storage::AuthenticationUpdate::PostAuthenticationUpdate {
-                authentication_status: common_enums::AuthenticationStatus::foreign_from(
-                    trans_status.clone(),
-                ),
-                trans_status,
-                authentication_value,
-                eci,
-            },
+            } => {
+                authentication_value
+                    .async_map(|auth_val| {
+                        crate::core::payment_methods::vault::create_tokenize(
+                            state,
+                            auth_val.expose(),
+                            None,
+                            authentication.authentication_id.clone(),
+                            merchant_key_store.key.get_inner(),
+                        )
+                    })
+                    .await
+                    .transpose()?;
+                storage::AuthenticationUpdate::PostAuthenticationUpdate {
+                    authentication_status: common_enums::AuthenticationStatus::foreign_from(
+                        trans_status.clone(),
+                    ),
+                    trans_status,
+                    eci,
+                }
+            }
             AuthenticationResponseData::PreAuthVersionCallResponse {
                 maximum_supported_3ds_version,
             } => storage::AuthenticationUpdate::PreAuthenticationVersionCallUpdate {
