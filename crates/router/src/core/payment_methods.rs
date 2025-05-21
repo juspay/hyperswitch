8be1cfa1939aabd5e480db2191f1241ee5bbc654
shivansh.mathur <shivansh.mathur@juspay.in>
2025-05-22 01:04:54 +0530

    Format error

--- crates/router/src/core/payment_methods.rs
+++ crates/router/src/core/payment_methods.rs
@@ -73,7 +73,6 @@ use crate::{
         
         payment_methods::transformers as pm_transforms, payments as payments_core,
         tokenization as tokenization_core,
-    ,
         utils as core_utils,
     },
     db::errors::ConnectorErrorExt,
