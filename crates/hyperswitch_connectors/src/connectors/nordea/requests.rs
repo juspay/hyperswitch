298806969ca6a26bb6343da4b23c6eecd1d6983b
PiX <69745008+pixincreate@users.noreply.github.com>
2025-05-24 23:52:10 +0530

    feaet: re-do request and response for nordea

--- crates/hyperswitch_connectors/src/connectors/nordea/requests.rs
+++ crates/hyperswitch_connectors/src/connectors/nordea/requests.rs
@@ -7,87 +7,295 @@ pub struct NordeaRouterData<T> {
     pub router_data: T,
 }
 
-#[derive(Default, Debug, Serialize, PartialEq)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "snake_case")]
+pub enum GrantType {
+    AuthorizationCode,
+    RefreshToken,
+}
+
+// To be passed in query parameters for OAuth scopes
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum AccessScope {
+    AccountsBasic,
+    AccountsBalances,
+    AccountsDetails,
+    AccountsTransactions,
+    PaymentsMultiple,
+    PaymentsSingleSca,
+    CardsInformation,
+    CardsTransactions,
+}
+
+#[derive(Debug, Serialize)]
+pub struct NordeaOAuthTokenExchangeRequest {
+    pub grant_type: GrantType,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub code: Option<Secret<String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub redirect_uri: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub refresh_token: Option<Secret<String>>, // For refresh_token flow
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum AccountType {
+    /// International bank account number
+    Iban,
+    /// National bank account number of Sweden
+    BbanSe,
+    /// National bank account number of Denmark
+    BbanDk,
+    /// National bank account number of Norway
+    BbanNo,
+    /// Bankgiro number
+    Bgnr,
+    /// Plusgiro number
+    Pgnr,
+    /// Creditor number (Giro) Denmark
+    GiroDk,
+    /// Any bank account number without any check-digit validations
+    BbanOther,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct AccountNumber {
+    /// Type of account number
+    #[serde(rename = "_type")]
+    pub account_type: AccountType,
+    /// Currency of the account (Mandatory for debtor, Optional for creditor)
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub currency: Option<api_models::enums::Currency>,
+    /// Actual account number
+    pub value: Secret<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct CreditorAccountReference {
+    /// RF or Invoice for FI Sepa payments, OCR for NO Kid payments and 01, 04, 15, 71, 73 or 75 for Danish Transfer Form payments.
+    #[serde(rename = "_type")]
+    pub creditor_reference_type: String,
+    /// Actual reference number
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub value: Option<Secret<String>>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct CreditorAccount {
+    /// Account number
+    pub account: AccountNumber,
+    pub country: Option<api_models::enums::CountryAlpha2>,
+    /// Message for the creditor to appear on their transaction.
+    /// Max length: FI SEPA:140; SE:12; PGNR:25; BGNR:150; DK: 40 (Instant/Express: 140); NO: 140
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub message: Option<String>,
+    /// Name of the creditor.
+    /// Max length: FI SEPA: 30; SE: 35; DK: Not use (Mandatory for Instant/Express payments: 70);
+    /// NO: 30 (mandatory for Straksbetaling/Express payments).
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub name: Option<String>,
+    /// Creditor reference number
+    pub reference: CreditorAccountReference,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct DebitorAccount {
+    /// Account number
+    pub account: AccountNumber,
+    /// Own message to be on the debtor's transaction.
+    /// Max length 20. NB: This field is not supported for SEPA and Norwegian payments and will be ignored.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub message: Option<String>,
+}
+
+#[derive(Debug, Serialize, PartialEq)]
+pub struct InstructedAmount {
+    /// Monetary amount of the payment. Max (digits+decimals): FI SEPA: (9+2); SE:(11+2); DK:(7+2); NO:(7+2)
+    amount: StringMajorUnit,
+    /// Currency code according to ISO 4217.
+    /// NB: Possible value depends on the type of the payment.
+    /// For domestic payment it should be same as debtor local currency,
+    /// for SEPA it must be EUR,
+    /// for cross border it can be Currency code according to ISO 4217.
+    currency: api_models::enums::Currency,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum RecurrenceType {
+    Daily,
+    Weekly,
+    Biweekly,
+    MonthlySameDay,
+    MonthlyEom,
+    QuartelySameDay,
+    QuartelyEom,
+    SemiAnnualySameDay,
+    SemiAnnualyEom,
+    TriAnnuallySameDay,
+    YearlySameDay,
+    YearlyEom,
+    EveryMinuteSandboxOnly,
+}
+
+#[derive(Debug, Serialize, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub enum FundsAvailabilityRequest {
+    True,
+    False,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub enum PaymentsUrgency {
+    Standard,
+    Express,
+    #[serde(rename = "sameday (Deprecated)")]
+    Sameday,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct RecurringInformation {
+    /// Number of occurrences. Not applicable for NO (use end_date instead). Format: int32.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub count: Option<i32>,
+    /// Date on which the recurrence will end. Format: YYYY-MM-DD. Applicable only for Norway. Format: date
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub end_date: Option<String>,
+    /// Repeats every interval
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub recurrence_type: Option<RecurrenceType>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum TppCategory {
+    Error,
+    Warning,
+    Info,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum TppCode {
+    Ds0a,
+    Narr,
+    Am21,
+    Am04,
+    Tm01,
+    Am12,
+    Rc06,
+    Rc07,
+    Rc04,
+    Ag06,
+    Bg06,
+    Be22,
+    Be20,
+    Ff06,
+    Be19,
+    Am03,
+    Am11,
+    Ch04,
+    Dt01,
+    Ch03,
+    Ff08,
+    Ac10,
+    Ac02,
+    Ag08,
+    Rr09,
+    Rc11,
+    Ff10,
+    Rr10,
+    Ff05,
+    Ch15,
+    Ff04,
+    Ac11,
+    Ac03,
+    Ac13,
+    Ac14,
+    Ac05,
+    Ac06,
+    Rr07,
+    Dt03,
+    Am13,
+    Ds24,
+    Fr01,
+    Am02,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct ThirdPartyMessages {
+    /// Category of the TPP message, INFO is further information, WARNING is something can be fixed, ERROR possibly non fixable issue
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub category: Option<TppCategory>,
+    /// Additional code that is combined with the text
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub code: Option<TppCode>,
+    /// Additional explaining text to the TPP
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub text: Option<String>,
+}
+
+#[derive(Debug, Serialize, PartialEq)]
 pub struct NordeaPaymentsRequest {
+    /// Creditor of the payment
+    #[serde(rename = "creditor")]
+    pub creditor_account: CreditorAccount,
+    /// Debtor of the payment
+    #[serde(rename = "debtor")]
+    pub debitor_account: DebitorAccount,
+    /// Free text reference that can be provided by the PSU.
+    /// This identification is passed on throughout the entire end-to-end chain.
+    /// Only in scope for Nordea Business DK.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub end_to_end_identification: Option<String>,
+    /// Unique identification as assigned by a partner to identify the payment.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub external_id: Option<String>,
+    /// Monetary amount
+    pub instructed_amount: InstructedAmount,
+    /// Recurring information
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub recurring: Option<RecurringInformation>,
+    /// Use as an indicator that the supplied payment (amount, currency and debtor account)
+    /// should be used to check whether the funds are available for further processing - at this moment.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub request_availability_of_funds: Option<FundsAvailabilityRequest>,
+    /// Choose a preferred execution date (or leave blank for today's date).
+    /// This should be a valid bank day, and depending on the country the date will either be
+    /// pushed to the next valid bank day, or return an error if a non-banking day date was
+    /// supplied (all dates accepted in sandbox). SEPA: max +5 years from yesterday,
+    /// Domestic: max. +1 year from yesterday. NB: Not supported for Own transfer Non-Recurring Norway.
+    /// Format:date.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub requested_execution_date: Option<String>,
+    /// Additional messages for third parties
+    #[serde(rename = "tpp_messages")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub tpp_messages: Option<Vec<ThirdPartyMessages>>,
+    /// Urgency of the payment. NB: This field is supported for
+    /// DK Domestic ('standard' and 'express')
+    /// NO Domestic bank transfer payments ('standard'). Use 'express' for Straksbetaling (Instant payment).
+    /// FI Sepa ('standard' and 'express') All other payment types ignore this input.
+    /// For further details on urgencies and cut-offs, refer to the Nordea website. Value 'sameday' is marked as deprecated and will be removed in the future.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub urgency: Option<PaymentsUrgency>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct NordeaConnectorMetadata {
+    #[serde(rename = "value")]
+    pub creditor_account_value: Secret<String>,
+    #[serde(rename = "_type")]
+    pub creditor_account_type: String,
+}
+
+//TODO: Fill the struct with respective fields
+// REFUND :
+// Type definition for RefundRequest
+#[derive(Debug, Serialize)]
+pub struct NordeaRefundRequest {
     pub amount: StringMajorUnit,
-    pub card: NordeaCard,
-}
-
-#[derive(Default, Debug, Serialize, Eq, PartialEq)]
-pub struct NordeaCard {
-    pub number: cards::CardNumber,
-    pub expiry_month: Secret<String>,
-    pub expiry_year: Secret<String>,
-    pub cvc: Secret<String>,
-    pub complete: bool,
-}
-
-// Helper struct for deserializing Nordea specific metadata from connector_details
-#[derive(Debug, Deserialize, Serialize)]
-struct NordeaConnectorMetadata {
-    #[serde(rename = "merchant_creditor_name")]
-    pub creditor_name: Option<String>,
-    #[serde(rename = "merchant_iban")]
-    pub creditor_iban: Option<Secret<String>>,
-}
-
-// Represents an Account Number (IBAN, BBAN, etc.)
-#[derive(Debug, Serialize, Clone)]
-#[serde(rename_all = "snake_case")]
-pub struct NordeaAccountNumber {
-    pub value: Secret<String>, // The actual account number
-    #[serde(rename = "_type")]
-    pub account_type: String, // Type of account number, e.g., "IBAN"
-    pub currency: Option<String>, // Currency of the account, e.g., "EUR"
-}
-
-// Represents the Debtor (Payer)
-#[derive(Debug, Serialize, Clone)]
-#[serde(rename_all = "snake_case")]
-pub struct NordeaPaymentDebtor {
-    pub account: NordeaAccountNumber,
-    pub message: Option<String>, // Optional message for the debtor's statement
-}
-
-// Represents a Creditor Reference
-#[derive(Debug, Serialize, Clone)]
-#[serde(rename_all = "snake_case")]
-pub struct NordeaReference {
-    #[serde(rename = "_type")]
-    pub reference_type: String, // Type of reference, e.g., "RF"
-    pub value: String, // The actual reference value
-}
-
-// Represents the Creditor (Beneficiary)
-#[derive(Debug, Serialize, Clone)]
-#[serde(rename_all = "snake_case")]
-pub struct NordeaPaymentCreditor {
-    pub account: NordeaAccountNumber,
-    pub name: String,
-    pub message: Option<String>, // Message for the creditor, maps to remittance_information_unstructured
-    pub reference: Option<NordeaReference>,
-}
-
-// Request structure for Nordea SEPA Credit Transfer (CreatePaymentRequest)
-#[derive(Debug, Serialize, Clone)]
-#[serde(rename_all = "snake_case")]
-pub struct NordeaSepaCreditTransferRequest {
-    // Payment details
-    pub amount: String, // Amount as a string (e.g., "123.45"), converted by transformer
-    pub currency: String, // e.g., "EUR"
-
-    // Debtor (Payer) information
-    pub debtor: NordeaPaymentDebtor,
-
-    // Creditor (Beneficiary) information
-    pub creditor: NordeaPaymentCreditor,
-
-    // Identifiers and references
-    pub end_to_end_identification: String, // Unique identifier for the transaction
-    pub external_id: Option<String>,       // Optional partner-assigned unique ID for the payment
-
-    // Optional SEPA specific fields
-    pub requested_execution_date: Option<String>, // Format YYYY-MM-DD
-    pub urgency: Option<String>,                  // e.g., "standard", "express"
-                                                  // recurring and tpp_messages are skipped for now due to complexity
 }
--- crates/hyperswitch_connectors/src/connectors/nordea/responses.rs
+++ crates/hyperswitch_connectors/src/connectors/nordea/responses.rs
@@ -1,28 +1,25 @@
+use common_enums::CountryAlpha2;
+use common_utils::types::StringMajorUnit;
+use masking::Secret;
 use serde::{Deserialize, Serialize};
-use url::Url;
 
-// PaymentsResponse
-//TODO: Append the remaining status flags
-#[derive(Debug, Clone, Default, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "lowercase")]
+use super::requests::{
+    CreditorAccount, DebitorAccount, PaymentsUrgency, RecurringInformation, ThirdPartyMessages,
+};
+
+// OAuth token response structure
+#[derive(Debug, Deserialize, Clone)]
+pub struct NordeaOAuthTokenExchangeResponse {
+    pub access_token: Option<Secret<String>>,
+    pub expires_in: Option<i64>,
+    pub refresh_token: Option<Secret<String>>,
+    pub token_type: Option<String>,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "PascalCase")]
 pub enum NordeaPaymentStatus {
-    Succeeded,
-    Failed,
     #[default]
-    Processing,
-}
-
-//TODO: Fill the struct with respective fields
-#[derive(Default, Debug, Clone, Serialize, Deserialize, PartialEq)]
-pub struct NordeaPaymentsResponse {
-    pub status: NordeaPaymentStatus,
-    pub id: String,
-}
-
-// Enum for Nordea Payment Status from Swagger definition
-#[derive(Debug, Deserialize, Clone, PartialEq, Eq)]
-// The enum variants in the Swagger are PascalCase. Serde handles this by default for Rust PascalCase enums.
-pub enum NordeaSwaggerPaymentStatus {
     PendingConfirmation,
     PendingSecondConfirmation,
     PendingUserApproval,
@@ -35,19 +32,121 @@ pub enum NordeaSwaggerPaymentStatus {
     UserApprovalFailed,
     UserApprovalTimeout,
     UserApprovalCancelled,
-    #[serde(other)] // Catch-all for unknown variants not explicitly defined
     Unknown,
 }
 
-//TODO: Fill the struct with respective fields
-// REFUND :
-// Type definition for RefundRequest
-#[derive(Default, Debug, Serialize)]
-pub struct NordeaRefundRequest {
-    pub amount: StringMinorUnit,
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct NordeaGroupHeader {
+    /// Response creation time. Format: date-time.
+    pub creation_date_time: Option<String>,
+    /// HTTP code for response. Format: int32.
+    pub http_code: Option<i32>,
+    /// Original request id for correlation purposes
+    pub message_identification: Option<String>,
+    /// Details of paginated response
+    pub message_pagination: Option<MessagePagination>,
 }
 
-// Type definition for Refund Response
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct NordeaResponseLinks {
+    /// Describes the nature of the link, e.g. 'details' for a link to the detailed information of a listed resource.
+    rel: Option<String>,
+    /// Relative path to the linked resource
+    href: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct BankFee {
+    /// Example 'domestic_transaction' only for DK domestic payments
+    #[serde(rename = "_type")]
+    pub bank_fee_type: Option<String>,
+    /// Country code according to ISO Alpha-2
+    pub country_code: Option<CountryAlpha2>,
+    /// Currency code according to ISO 4217
+    pub currency_code: Option<api_models::enums::Currency>,
+    /// Value of the fee.
+    pub value: Option<StringMajorUnit>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum ChargeBearer {
+    Shar,
+    Debt,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct NordeaResponseBody {
+    /// Unique payment identifier assigned for new payment
+    #[serde(rename = "_id")]
+    pub payment_id: String,
+    /// HATEOAS inspired links: 'rel' and 'href'. Context specific link (only GET supported)
+    #[serde(rename = "_links")]
+    pub links: Option<Vec<NordeaResponseLinks>>,
+    pub amount: StringMajorUnit,
+    /// The indicator shows whether the funds are available to process the current payment - at this moment.
+    pub availability_of_funds: Option<bool>,
+    /// BankIdSe authentication session id. Needed when you call /v4/payments/domestic/confirm/bankidse/{bankIdSeAuthenticationId}
+    pub bank_id_se_authentication_id: Option<String>,
+    /// Purpose code of money transfer as defined by Swedish Tax Authority/Skattewerket
+    /// Purpose code of money transfer, mandatory for transfers above 100.000 NOK or the
+    /// equivalent in other currencies, have to be reported to the Norwegian Customs and Excise (Tollvesenet)
+    pub central_bank_reporting_code: Option<String>,
+    /// Supplementary information about the transfer purpose. Mandatory when central_bank_reporting_code is informed
+    pub central_bank_reporting_supplementary_text: Option<String>,
+    /// Bearer of charges. shar = The Debtor (sender of the payment) will pay all fees charged by the sending bank.
+    /// The Creditor (recipient of the payment) will pay all fees charged by the receiving bank.
+    /// debt = The Debtor (sender of the payment) will bear all of the payment transaction fees.
+    /// The creditor (beneficiary) will receive the full amount of the payment.
+    pub charge_bearer: Option<ChargeBearer>,
+    /// Creditor of the payment
+    #[serde(rename = "creditor")]
+    pub creditor_account: Option<CreditorAccount>,
+    pub currency: Option<api_models::enums::Currency>,
+    /// Debtor of the payment
+    #[serde(rename = "debtor")]
+    pub debitor_account: Option<DebitorAccount>,
+    end_to_end_identification: Option<String>,
+    /// Timestamp of payment creation. ISO 8601 format yyyy-mm-ddThh:mm:ss.fffZ. Format:date-time.
+    pub entry_date_time: Option<String>,
+    /// Unique identification as assigned by a partner to identify the payment.
+    pub external_id: Option<String>,
+    /// An amount the bank will charge for executing the payment
+    pub fee: Option<BankFee>,
+    /// OTP Challenge
+    pub otp_challenge: Option<String>,
+    /// Status of the payment
+    pub payment_status: NordeaPaymentStatus,
+    /// Planned execution date will indicate the day the payment will be finalized. If the payment has been pushed due to cut-off, it will be indicated in planned execution date. Format:date.
+    pub planned_execution_date: Option<String>,
+    /// Recurring information
+    pub recurring: Option<RecurringInformation>,
+    /// Choose a preferred execution date (or leave blank for today's date).
+    /// This should be a valid bank day, and depending on the country the date will either be pushed to the next valid bank day,
+    /// or return an error if a non-banking day date was supplied (all dates accepted in sandbox).
+    /// SEPA: max +5 years from yesterday, Domestic: max. +1 year from yesterday. NB: Not supported for Own transfer Non-Recurring Norway.
+    /// Format:date.
+    pub requested_execution_date: Option<String>,
+    /// Timestamp of payment creation. ISO 8601 format yyyy-mm-ddThh:mm:ss.fffZ Format:date-time.
+    pub timestamp: Option<String>,
+    /// Additional messages for third parties
+    pub tpp_messages: Option<Vec<ThirdPartyMessages>>,
+    /// Urgency of the payment. NB: This field is supported for DK Domestic ('standard' and 'express') and NO Domestic bank transfer payments ('standard' and 'express').
+    /// Use 'express' for Straksbetaling (Instant payment).
+    /// All other payment types ignore this input.
+    /// For further details on urgencies and cut-offs, refer to the Nordea website.
+    /// Value 'sameday' is marked as deprecated and will be removed in the future.
+    pub urgency: Option<PaymentsUrgency>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct NordeaPaymentsResponse {
+    /// Payment information
+    #[serde(rename = "response")]
+    pub payments_response: Option<NordeaResponseBody>,
+    /// External response header
+    pub group_header: Option<NordeaGroupHeader>,
+}
 
 #[allow(dead_code)]
 #[derive(Debug, Default, Deserialize, Clone, Serialize)]
@@ -58,45 +157,54 @@ pub enum NordeaRefundStatus {
     Processing,
 }
 
-//TODO: Fill the struct with respective fields
 #[derive(Default, Debug, Clone, Serialize, Deserialize)]
 pub struct NordeaRefundResponse {
     pub id: String,
     pub status: NordeaRefundStatus,
 }
 
-//TODO: Fill the struct with respective fields
-#[derive(Default, Debug, Serialize, Deserialize, PartialEq)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct NordeaOriginalRequest {
+    /// Original request url
+    #[serde(rename = "url")]
+    nordea_url: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct NordeaFailures {
+    /// Failure code
+    pub code: Option<String>,
+    /// Failure description
+    pub description: Option<String>,
+    /// JSON path of the failing element if applicable
+    pub path: Option<String>,
+    /// Type of the validation error, e.g. NotNull
+    #[serde(rename = "type")]
+    pub failure_type: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct NordeaErrorBody {
+    // Serde JSON because connector returns an `(item)` object in failures array object
+    /// More details on the occurred error: Validation error
+    #[serde(rename = "failures")]
+    nordea_failures: Option<Vec<NordeaFailures>>,
+    /// Original request information
+    #[serde(rename = "request")]
+    nordea_request: Option<NordeaOriginalRequest>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct MessagePagination {
+    /// Resource listing may return a continuationKey if there's more results available.
+    /// Request may be retried with the continuationKey, but otherwise same parameters, in order to get more results.
+    pub continuation_key: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct NordeaErrorResponse {
-    pub status_code: u16,
-    pub code: String,
-    pub message: String,
-    pub reason: Option<String>,
-}
-
-// Corresponds to the 'Payment' object within the API response (Swagger definition)
-#[derive(Debug, Deserialize, Clone)]
-#[serde(rename_all = "snake_case")] // Fields in the 'Payment' object are snake_case
-pub struct NordeaPaymentDetails {
-    #[serde(rename = "_id")] // Maps from "_id" in JSON to "payment_id" in struct
-    pub payment_id: String,
-    pub payment_status: NordeaSwaggerPaymentStatus,
-    // Other fields from the 'Payment' object can be added here if needed by the application
-    // For example:
-    // pub amount: f64,
-    // pub currency: String,
-    // pub creditor: Option<SomeCreditorStruct>, // Define if needed
-    // pub debtor: Option<SomeDebtorStruct>,     // Define if needed
-    // pub requested_execution_date: Option<String>,
-    // pub planned_execution_date: Option<String>,
-}
-
-// Top-level response structure for SEPA Credit Transfer (corresponds to 'PaymentResponse' in Swagger)
-// This is what the connector's handle_response will parse the entire API JSON response into.
-#[derive(Debug, Deserialize, Clone)]
-#[serde(rename_all = "snake_case")] // Fields in 'PaymentResponse' are snake_case
-pub struct NordeaSepaCreditTransferResponse {
-    // The 'response' field in the API's JSON contains the actual payment details
-    pub response: NordeaPaymentDetails,
-    // pub group_header: Option<serde_json::Value>, // Represents 'ResponseHeader', can be added if needed
+    /// Error response body
+    pub error: Option<NordeaErrorBody>,
+    /// External response header
+    pub group_header: Option<NordeaGroupHeader>,
 }
