4ba65eb957fdb1c87909fbf6c3fdf5e73b31a314
AkshayaFoiger <131388445+AkshayaFoiger@users.noreply.github.com>
2025-05-26 17:22:53 +0530

    feat(connector): [Worldpayxml] add card payment (#8139)

--- crates/common_enums/src/connector_enums.rs
+++ crates/common_enums/src/connector_enums.rs
@@ -148,7 +148,7 @@ pub enum RoutableConnectors {
     Wise,
     Worldline,
     Worldpay,
-    // Worldpayxml,
+    Worldpayxml,
     Xendit,
     Zen,
     Plaid,
@@ -301,7 +301,7 @@ pub enum Connector {
     Wise,
     Worldline,
     Worldpay,
-    // Worldpayxml,
+    Worldpayxml,
     Signifyd,
     Plaid,
     Riskified,
@@ -460,7 +460,7 @@ impl Connector {
             | Self::Wise
             | Self::Worldline
             | Self::Worldpay
-            // | Self::Worldpayxml
+            | Self::Worldpayxml
             | Self::Xendit
             | Self::Zen
             | Self::Zsl
@@ -612,7 +612,7 @@ impl From<RoutableConnectors> for Connector {
             RoutableConnectors::Wise => Self::Wise,
             RoutableConnectors::Worldline => Self::Worldline,
             RoutableConnectors::Worldpay => Self::Worldpay,
-            // RoutableConnectors::Worldpayxml => Self::Worldpayxml,
+            RoutableConnectors::Worldpayxml => Self::Worldpayxml,
             RoutableConnectors::Zen => Self::Zen,
             RoutableConnectors::Plaid => Self::Plaid,
             RoutableConnectors::Zsl => Self::Zsl,
@@ -722,7 +722,7 @@ impl TryFrom<Connector> for RoutableConnectors {
             Connector::Wise => Ok(Self::Wise),
             Connector::Worldline => Ok(Self::Worldline),
             Connector::Worldpay => Ok(Self::Worldpay),
-            // Connector::Worldpayxml => Ok(Self::Worldpayxml),
+            Connector::Worldpayxml => Ok(Self::Worldpayxml),
             Connector::Xendit => Ok(Self::Xendit),
             Connector::Zen => Ok(Self::Zen),
             Connector::Plaid => Ok(Self::Plaid),
