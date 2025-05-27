bc4bab935fa6a08e04f45b2ea5f36ac621b703c8
Amey Wale <76102448+AmeyWale@users.noreply.github.com>
2025-05-27 11:39:57 +0530

    Refactor(refunds_v2): Refactor api event metric for refunds v2 apis (#8041)

--- crates/common_utils/src/events.rs
+++ crates/common_utils/src/events.rs
@@ -29,7 +29,7 @@ pub enum ApiEventsType {
     },
     #[cfg(feature = "v2")]
     Refund {
-        payment_id: id_type::GlobalPaymentId,
+        payment_id: Option<id_type::GlobalPaymentId>,
         refund_id: id_type::GlobalRefundId,
     },
     #[cfg(feature = "v1")]
