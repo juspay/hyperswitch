48a30be07d0a57847b5dc4443b81ac217faf3e38
Sahkal Poddar <sahkal.poddar@juspay.in>
2025-06-02 16:14:03 +0530

    Added api contract comments for open-api specs

--- crates/api_models/src/profile_acquirer.rs
+++ crates/api_models/src/profile_acquirer.rs
@@ -5,46 +5,65 @@ use crate::enums;
 
 #[derive(Debug, Serialize, Deserialize, ToSchema)]
 pub struct ProfileAcquirerCreate {
+    /// The merchant id assigned by the acquirer
     #[schema(value_type= String,example = "M123456789")]
     pub acquirer_assigned_merchant_id: String,
+    /// merchant name
     #[schema(value_type= String,example = "NewAge Retailer")]
     pub merchant_name: String,
+    /// Merchant category code assigned by acquirer
     #[schema(value_type= String,example = "5812")]
     pub mcc: String,
+    /// Merchant country code assigned by acquirer
     #[schema(value_type= String,example = "US")]
     pub merchant_country_code: enums::CountryAlpha2,
+    /// Network provider
     #[schema(value_type= String,example = "VISA")]
     pub network: common_enums::enums::CardNetwork,
+    /// Acquirer bin
     #[schema(value_type= String,example = "456789")]
     pub acquirer_bin: String,
+    /// Acquirer ica provided by acquirer
     #[schema(value_type= Option<String>,example = "401288")]
     pub acquirer_ica: Option<String>,
+    /// Fraud rate for the particular acquirer configuration
     #[schema(value_type= f64,example = "0.01")]
     pub acquirer_fraud_rate: f64,
+    /// Parent profile id to link the acquirer account with
     #[schema(value_type= String,example = "pro_ky0yNyOXXlA5hF8JzE5q")]
     pub profile_id: common_utils::id_type::ProfileId,
 }
 
 #[derive(Debug, Serialize, Deserialize, ToSchema)]
 pub struct ProfileAcquirerResponse {
+    /// The unique identifier of the profile acquirer
     #[schema(value_type= String,example = "pro_acq_LCRdERuylQvNQ4qh3QE0")]
     pub profile_acquirer_id: common_utils::id_type::ProfileAcquirerId,
+    /// The merchant id assigned by the acquirer
     #[schema(value_type= String,example = "M123456789")]
     pub acquirer_assigned_merchant_id: String,
+    /// Merchant name
     #[schema(value_type= String,example = "NewAge Retailer")]
     pub merchant_name: String,
+    /// Merchant category code assigned by acquirer
     #[schema(value_type= String,example = "5812")]
     pub mcc: String,
+    /// Merchant country code assigned by acquirer
     #[schema(value_type= String,example = "US")]
     pub merchant_country_code: enums::CountryAlpha2,
+    /// Network provider
     #[schema(value_type= String,example = "VISA")]
     pub network: common_enums::enums::CardNetwork,
+    /// Acquirer bin
     #[schema(value_type= String,example = "456789")]
     pub acquirer_bin: String,
+    /// Acquirer ica provided by acquirer
     #[schema(value_type= Option<String>,example = "401288")]
     pub acquirer_ica: Option<String>,
+    /// Fraud rate for the particular acquirer configuration
     #[schema(value_type= f64,example = "0.01")]
     pub acquirer_fraud_rate: f64,
+    /// Parent profile id to link the acquirer account with
     #[schema(value_type= String,example = "pro_ky0yNyOXXlA5hF8JzE5q")]
     pub profile_id: common_utils::id_type::ProfileId,
 }
