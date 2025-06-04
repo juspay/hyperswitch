835a425ded2cf26b9a3f69f29c3a26a41603b2de
Chethan Rao <70657455+Chethan-rao@users.noreply.github.com>
2025-06-04 13:42:24 +0530

    refactor(dynamic_routing): add logic for creating merchant account in decision engine (#8191)
    Co-authored-by: Shankar Singh C <shankar.singh@juspay.in>
    Co-authored-by: hyperswitch-bot[bot] <148525504+hyperswitch-bot[bot]@users.noreply.github.com>

--- crates/api_models/src/routing.rs
+++ crates/api_models/src/routing.rs
@@ -608,6 +608,8 @@ pub struct DynamicRoutingAlgorithmRef {
     pub dynamic_routing_volume_split: Option<u8>,
     pub elimination_routing_algorithm: Option<EliminationRoutingAlgorithm>,
     pub contract_based_routing: Option<ContractRoutingAlgorithm>,
+    #[serde(default)]
+    pub is_merchant_created_in_decision_engine: bool,
 }
 
 pub trait DynamicRoutingAlgoAccessor {
@@ -715,6 +717,10 @@ impl DynamicRoutingAlgorithmRef {
 
     pub fn update_volume_split(&mut self, volume: Option<u8>) {
         self.dynamic_routing_volume_split = volume
+    }
+
+    pub fn update_merchant_creation_status_in_decision_engine(&mut self, is_created: bool) {
+        self.is_merchant_created_in_decision_engine = is_created;
     }
 
     pub fn is_success_rate_routing_enabled(&self) -> bool {
