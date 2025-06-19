84933e2ecba69a48bdd04c632ea77fbeb583f60d
Sakil Mostak <73734619+Sakilmostak@users.noreply.github.com>
2025-06-19 19:23:36 +0530

    feat(payment-method): create payment_token for session confirm call (#8391)

--- crates/api_models/src/payment_methods.rs
+++ crates/api_models/src/payment_methods.rs
@@ -2981,7 +2981,7 @@ pub struct PaymentMethodSessionResponse {
 
     /// The payment method that was created using this payment method session
     #[schema(value_type = Option<Vec<String>>)]
-    pub associated_payment_methods: Option<Vec<id_type::GlobalPaymentMethodId>>,
+    pub associated_payment_methods: Option<Vec<String>>,
 
     /// The token-id created if there is tokenization_data present
     #[schema(value_type = Option<String>, example = "12345_tok_01926c58bc6e77c09e809964e72af8c8")]
