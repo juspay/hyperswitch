97e1ccc9d94c4f0919c2bf2aaeae0b057d4cabcd
Aditya <kumaaraditya47@gmail.com>
2025-06-20 11:29:08 +0530

    racfactoring build

--- crates/external_services/build.rs
+++ crates/external_services/build.rs
@@ -45,11 +45,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         tonic_build::configure()
             .out_dir(&out_dir)
             .compile_well_known_types(true)
-            .extern_path(".google.protobuf.Timestamp", "::prost_types::Timestamp")
-            .type_attribute(
-                "google.protobuf.Timestamp",
-                "#[derive(serde::Serialize, serde::Deserialize)]",
-            )
             .compile(&dr_proto_files, &[&proto_base_path])
             .expect("Failed to compile dynamic routing proto files");
     }
