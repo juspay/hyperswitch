8df08955416045fe602295c30a5ad92acd528ff9
Akshay S <akshay.s@juspay.in>
2025-06-24 10:30:21 +0530

    fixed clause

--- crates/storage_impl/src/payments/payment_attempt.rs
+++ crates/storage_impl/src/payments/payment_attempt.rs
@@ -1241,7 +1241,10 @@ impl<T: DatabaseStore> PaymentAttemptInterface for KVRouterStore<T> {
                         payment_attempts.sort_by(|a, b| b.modified_at.cmp(&a.modified_at));
                         payment_attempts
                             .iter()
-                            .find(|&pa| pa.status == diesel_models::enums::AttemptStatus::Charged)
+                            .find(|&pa|
+                                pa.status == diesel_models::enums::AttemptStatus::Charged ||
+                                pa.status == diesel_models::enums::AttemptStatus::PartialCharged
+                            )
                             .cloned()
                             .ok_or(error_stack::report!(
                                 redis_interface::errors::RedisError::NotFound
