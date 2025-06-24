70e37f1c3a74b10324722cac61ebf673769a16aa
gnanasundarig <gnanasundari.gopal@juspay.in>
2025-06-24 19:09:36 +0530

    ci(cypress): Changed filename

--- cypress-tests/cypress/e2e/configs/Payment/Utils.js
+++ cypress-tests/cypress/e2e/configs/Payment/Utils.js
@@ -21,7 +21,7 @@ import { connectorDetails as fiservConnectorDetails } from "./Fiserv.js";
 import { connectorDetails as fiservemeaConnectorDetails } from "./Fiservemea.js";
 import { connectorDetails as fiuuConnectorDetails } from "./Fiuu.js";
 import { connectorDetails as getnetConnectorDetails } from "./Getnet.js";
-import { connectorDetails as globalpayConnectorDetails } from "./GlobalPay.js";
+import { connectorDetails as globalpayConnectorDetails } from "./Globalpay.js";
 import { connectorDetails as hipayConnectorDetails } from "./Hipay.js";
 import { connectorDetails as iatapayConnectorDetails } from "./Iatapay.js";
 import { connectorDetails as itaubankConnectorDetails } from "./ItauBank.js";
