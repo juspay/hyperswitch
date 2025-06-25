00209a0e6f60ceb11a1fc260b96cae4ddede49a3
debarshi.gupta <debarshi.gupta@juspay.in>
2025-06-25 12:24:55 +0530

    added prost::Message trait impl for Secret<String>

--- crates/masking/src/secret.rs
+++ crates/masking/src/secret.rs
@@ -430,3 +430,36 @@ mod tests {
         );
     }
 }
+
+
+#[cfg(feature = "tonic")]
+impl prost::Message for Secret<String, crate::WithType> {
+    fn encode_raw(&self, buf: &mut impl bytes::BufMut) {
+        prost::encoding::string::encode(1, self.peek(), buf);
+    }
+
+    fn merge_field(
+        &mut self,
+        tag: u32,
+        wire_type: prost::encoding::WireType,
+        buf: &mut impl bytes::Buf,
+        ctx: prost::encoding::DecodeContext,
+    ) -> Result<(), prost::DecodeError> {
+        if tag == 1 {
+            let mut value = String::new();
+            prost::encoding::string::merge(wire_type, &mut value, buf, ctx)?;
+            *self = Secret::new(value);
+            Ok(())
+        } else {
+            prost::encoding::skip_field(wire_type, tag, buf, ctx)
+        }
+    }
+
+    fn encoded_len(&self) -> usize {
+        prost::encoding::string::encoded_len(1, self.peek())
+    }
+
+    fn clear(&mut self) {
+        *self = Secret::new(String::new());
+    }
+}
