f66f16e5ba2b7747170a4f8f574b56aa168aafc1
Sweta-Kumari-Sharma <ss6175983@gmail.com>
2025-06-26 10:47:12 +0530

    2 digit state code for ach

--- crates/hyperswitch_connectors/src/connectors/adyen/transformers.rs
+++ crates/hyperswitch_connectors/src/connectors/adyen/transformers.rs
@@ -3018,11 +3018,26 @@ impl
             .clone()
             .and_then(get_device_fingerprint);
 
-        let billing_address =
+        let mut billing_address =
             get_address_info(item.router_data.get_optional_billing()).and_then(Result::ok);
         let delivery_address =
             get_address_info(item.router_data.get_optional_shipping()).and_then(Result::ok);
         let telephone_number = item.router_data.get_optional_billing_phone_number();
+        if let BankDebitData::AchBankDebit { .. } = bank_debit_data {
+            if let Some(addr) = billing_address.as_mut() {
+                match item.router_data.get_billing_state_code() {
+                    Ok(state) => {
+                        addr.state_or_province = Some(state);
+                    }
+                    Err(_) => {
+                        return Err(errors::ConnectorError::MissingRequiredField {
+                            field_name: "billing.state_or_province",
+                        }
+                        .into());
+                    }
+                }
+            }
+        }
 
         let request = AdyenPaymentRequest {
             amount,
