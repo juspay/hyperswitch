f4f187ba3ab45fdfabe71b9a39e26e418823acd3
Sweta-Kumari-Sharma <ss6175983@gmail.com>
2025-06-27 11:25:00 +0530

    boleto, trustly, alipay, wechatpay added in shift4

--- crates/hyperswitch_connectors/src/connectors/shift4.rs
+++ crates/hyperswitch_connectors/src/connectors/shift4.rs
@@ -896,7 +896,29 @@ lazy_static! {
 
         shift4_supported_payment_methods.add(
             enums::PaymentMethod::BankRedirect,
+            enums::PaymentMethodType::Trustly,
+            PaymentMethodDetails {
+                mandates: enums::FeatureStatus::Supported,
+                refunds: enums::FeatureStatus::Supported,
+                supported_capture_methods: supported_capture_methods.clone(),
+                specific_features: None,
+            },
+        );
+
+        shift4_supported_payment_methods.add(
+            enums::PaymentMethod::BankRedirect,
             enums::PaymentMethodType::Sofort,
+            PaymentMethodDetails{
+                mandates: enums::FeatureStatus::NotSupported,
+                refunds: enums::FeatureStatus::Supported,
+                supported_capture_methods: supported_capture_methods.clone(),
+                specific_features: None
+            }
+        );
+
+        shift4_supported_payment_methods.add(
+            enums::PaymentMethod::Voucher,
+            enums::PaymentMethodType::Boleto,
             PaymentMethodDetails{
                 mandates: enums::FeatureStatus::NotSupported,
                 refunds: enums::FeatureStatus::Supported,
@@ -941,6 +963,28 @@ lazy_static! {
                     }),
                 ),
             }
+        );
+
+        shift4_supported_payment_methods.add(
+            enums::PaymentMethod::Wallet,
+            enums::PaymentMethodType::AliPay,
+            PaymentMethodDetails {
+                mandates: enums::FeatureStatus::NotSupported,
+                refunds: enums::FeatureStatus::Supported,
+                supported_capture_methods: supported_capture_methods.clone(),
+                specific_features: None,
+            },
+        );
+
+        shift4_supported_payment_methods.add(
+            enums::PaymentMethod::Wallet,
+            enums::PaymentMethodType::WeChatPay,
+            PaymentMethodDetails {
+                mandates: enums::FeatureStatus::NotSupported,
+                refunds: enums::FeatureStatus::Supported,
+                supported_capture_methods: supported_capture_methods.clone(),
+                specific_features: None,
+            },
         );
 
         shift4_supported_payment_methods
