cdfccc2ebfd4d2ff8ac7afcc9a8915332a0a6213
Sarthak Soni <sarthakasoni@gmail.com>
2025-07-01 13:37:25 +0530

    chore: Resolved comments

--- migrations/2025-06-27-120507_update_routing_approach/down.sql
+++ migrations/2025-06-27-120507_update_routing_approach/down.sql
@@ -1,18 +1,2 @@
 -- This file should undo anything in `up.sql`
-ALTER TABLE payment_attempt
-DROP COLUMN IF EXISTS routing_approach;
-
-DROP TYPE "RoutingApproach";
-
-CREATE TYPE "RoutingApproach" AS ENUM (
-  'success_rate_exploitation',
-  'success_rate_exploration',
-  'contract_based_routing',
-  'debit_routing',
-  'rule_based_routing',
-  'volume_based_routing',
-  'default_fallback'
-);
-
-ALTER TABLE payment_attempt
-ADD COLUMN IF NOT EXISTS routing_approach "RoutingApproach";
+SELECT 1;
