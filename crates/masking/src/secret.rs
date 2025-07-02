ad29631c537c92fb11f1827fb3f6737cdc741f0f
Debarshi Gupta <debarshi.gupta@juspay.in>
2025-07-02 12:12:45 +0530

    feat(masking): implement `prost::Message` trait for `Secret` and `StrongSecret` types (#8458)
    Co-authored-by: hyperswitch-bot[bot] <148525504+hyperswitch-bot[bot]@users.noreply.github.com>

--- crates/masking/src/secret.rs
+++ crates/masking/src/secret.rs
@@ -248,6 +248,38 @@ impl Strategy<serde_json::Value> for JsonMaskStrategy {
     }
 }
 
+#[cfg(feature = "proto_tonic")]
+impl<T> prost::Message for Secret<T, crate::WithType>
+where
+    T: prost::Message + Default + Clone,
+{
+    fn encode_raw(&self, buf: &mut impl bytes::BufMut) {
+        self.peek().encode_raw(buf);
+    }
+
+    fn merge_field(
+        &mut self,
+        tag: u32,
+        wire_type: prost::encoding::WireType,
+        buf: &mut impl bytes::Buf,
+        ctx: prost::encoding::DecodeContext,
+    ) -> Result<(), prost::DecodeError> {
+        if tag == 1 {
+            self.peek_mut().merge_field(tag, wire_type, buf, ctx)
+        } else {
+            prost::encoding::skip_field(wire_type, tag, buf, ctx)
+        }
+    }
+
+    fn encoded_len(&self) -> usize {
+        self.peek().encoded_len()
+    }
+
+    fn clear(&mut self) {
+        self.peek_mut().clear();
+    }
+}
+
 #[cfg(test)]
 #[cfg(feature = "serde")]
 mod tests {
