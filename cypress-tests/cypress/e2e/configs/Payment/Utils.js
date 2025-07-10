3a85b23a3ae4997dc44014c3d98a6def23c9abc8
Arindam Sahoo <arindams2002@gmail.com>
2025-07-10 09:34:22 +0530

    ci(cypress): add archipel in the incremental auth inclusion list in utils

--- cypress-tests/cypress/e2e/configs/Payment/Utils.js
+++ cypress-tests/cypress/e2e/configs/Payment/Utils.js
@@ -388,7 +388,7 @@ export const CONNECTOR_LISTS = {
   // Inclusion lists (only run for these connectors)
   INCLUDE: {
     MANDATES_USING_NTID_PROXY: ["cybersource"],
-    INCREMENTAL_AUTH: ["cybersource"],
+    INCREMENTAL_AUTH: ["archipel", "cybersource"],
     // Add more inclusion lists
   },
 };
