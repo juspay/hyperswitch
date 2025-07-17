6d5813801a6bad4d2adab684aad2276978dce96b
Nikhil Singh <nik.singh710@gmail.com>
2025-07-17 13:25:08 +0530

    chore(nix): Add `test-euclid_wasm` devShell for building on local
    Added `crate.nix` for `euclid_wasm` for the support of `nix build`

--- nix/modules/flake/devshell.nix
+++ nix/modules/flake/devshell.nix
@@ -1,6 +1,6 @@
 {
   debug = true;
-  perSystem = { config, pkgs, self', ... }:
+  perSystem = { config, pkgs, lib, self', ... }:
     let
       rustMsrv = config.rust-project.cargoToml.workspace.package.rust-version;
       rustStablePkg = config.rust-project.toolchain;
@@ -53,6 +53,51 @@
             parallel
           ];
         };
+
+        # FIXME: Once `euclid_wasm` is buildable in devShell env we can move this to `devShells.dev`
+        test-euclid_wasm =
+          let
+            clangUnwrapped = pkgs.llvmPackages_latest.clang-unwrapped;
+            bintoolsUnwrapped = pkgs.llvmPackages_latest.bintools-unwrapped;
+            wasm32 = "wasm32-unknown-unknown";
+          in
+          pkgs.mkShell {
+            name = "hyperswitch-shell-euclid_wasm";
+            meta.description = "Test shell env for euclid_wasm";
+            inputsFrom = [ self'.devShells.dev ];
+            packages = with pkgs; [
+              wasm-pack
+              wasm-bindgen-cli
+              clangUnwrapped
+              bintoolsUnwrapped
+              lld
+              libpq
+            ] ++ lib.optionals pkgs.stdenv.isDarwin (with pkgs;
+              [
+                libiconv
+                apple-sdk_11
+              ]);
+
+            shellHook = ''
+              rustfmt --version
+              cargo --version
+              echo 1>&2 ""
+              echo 1>&2 "Note: This shell is for testing \`euclid_wasm\` in a local build environment."
+              echo 1>&2 "Basically the env is a rough pick from \`crates/euclid_wasm/crate.nix\` though that is also broken at the moment."
+            '';
+
+            CC_wasm32_unknown_unknown = lib.getExe' clangUnwrapped "clang";
+            CXX_wasm32_unknown_unknown = lib.getExe' clangUnwrapped "clang++";
+            AR_wasm32_unknown_unknown = lib.getExe' bintoolsUnwrapped "llvm-ar";
+            AS_wasm32_unknown_unknown = lib.getExe' bintoolsUnwrapped "llvm-as";
+            STRIP_wasm32_unknown_unknown = lib.getExe' bintoolsUnwrapped "llvm-strip";
+
+            CARGO_BUILD_TARGET = wasm32;
+            CPATH = "${lib.getLib clangUnwrapped}/lib/clang/${lib.versions.major (lib.getVersion clangUnwrapped)}/include";
+            RUSTFLAGS = "-C linker=rust-lld";
+
+          };
+
       };
     };
 }
--- nix/modules/flake/rust.nix
+++ nix/modules/flake/rust.nix
@@ -4,24 +4,49 @@
     inputs.rust-flake.flakeModules.default
     inputs.rust-flake.flakeModules.nixpkgs
   ];
-  perSystem = { pkgs, ... }: {
-    rust-project.toolchain = pkgs.rust-bin.fromRustupToolchain {
-      channel = "1.88.0";
-      components = [
-        # Picked from: <https://rust-lang.github.io/rustup/concepts/components.html>
-        # TODO: only keep required after euclid_wasm is buildable in devShell env
-        "cargo"
-        "clippy"
-        "llvm-bitcode-linker"
-        "llvm-tools"
-        "rust-analyzer"
-        "rust-docs"
-        "rust-src"
-        "rust-std"
-        "rustc-dev"
-      ];
-      profile = "minimal"; # Has to be minimal to avoid overriding nightly `rustfmt`
-      targets = [ "wasm32-unknown-unknown" ];
+  perSystem = { pkgs, lib, config, ... }: {
+    rust-project = {
+      toolchain = pkgs.rust-bin.fromRustupToolchain {
+        channel = "1.88.0";
+        components = [
+          # Picked from: <https://rust-lang.github.io/rustup/concepts/components.html>
+          # TODO: only keep required after euclid_wasm is buildable in devShell env
+          "cargo"
+          "clippy"
+          "llvm-bitcode-linker"
+          "llvm-tools"
+          "rust-analyzer"
+          "rust-docs"
+          "rust-src"
+          "rust-std"
+          "rustc-dev"
+        ];
+        profile = "minimal"; # Has to be minimal to avoid overriding nightly `rustfmt`
+        targets = [ "wasm32-unknown-unknown" ];
+      };
+      crateNixFile = "crate.nix";
+      src = lib.cleanSourceWith {
+        src =
+          let
+            # Like crane's filterCargoSources, but doesn't blindly include all TOML files!
+            filterCargoSources = path: type:
+              config.rust-project.crane-lib.filterCargoSources path type
+              && !(lib.hasSuffix ".toml" path && !lib.hasSuffix "Cargo.toml" path);
+          in
+          # TODO: Put a more thorough filter here
+          lib.cleanSourceWith {
+            src = inputs.self;
+            filter = path: type:
+              filterCargoSources path type
+              || lib.hasSuffix "crate.nix" path
+              || "${inputs.self}/flake.nix" == path
+              || "${inputs.self}/flake.lock" == path
+              || "${inputs.self}/README.md" == path
+              || lib.hasPrefix "${inputs.self}/nix/" path
+              || lib.hasPrefix "${inputs.self}/crates/" path
+            ;
+          };
+      };
     };
   };
 }
