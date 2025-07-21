c4452e2972f2e8564df8bf6d244c036e963d6c95
Sarthak Soni <76486416+Sarthak1799@users.noreply.github.com>
2025-07-21 16:37:17 +0530

    fix: remove straight through routing from routing approach (#8694)

--- crates/common_enums/src/enums.rs
+++ crates/common_enums/src/enums.rs
@@ -8542,7 +8542,6 @@ impl RoutingApproach {
             "SR_SELECTION_V3_ROUTING" => Self::SuccessRateExploitation,
             "SR_V3_HEDGING" => Self::SuccessRateExploration,
             "NTW_BASED_ROUTING" => Self::DebitRouting,
-            "DEFAULT" => Self::StraightThroughRouting,
             _ => Self::DefaultFallback,
         }
     }
