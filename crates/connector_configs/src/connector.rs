b5ee048be9bcf2ee0a797f5b34f7bf1953b9614e
Kuntimaddi Manideep <kuntimaddi.manideep@juspay.in>
2025-07-23 15:27:33 +0530

    feat(connector): [MPGS] template pr

--- crates/connector_configs/src/connector.rs
+++ crates/connector_configs/src/connector.rs
@@ -235,6 +235,7 @@ pub struct ConnectorConfig {
     pub mifinity: Option<ConnectorTomlConfig>,
     pub mollie: Option<ConnectorTomlConfig>,
     pub moneris: Option<ConnectorTomlConfig>,
+    pub mpgs: Option<ConnectorTomlConfig>,
     pub multisafepay: Option<ConnectorTomlConfig>,
     pub nexinets: Option<ConnectorTomlConfig>,
     pub nexixpay: Option<ConnectorTomlConfig>,
