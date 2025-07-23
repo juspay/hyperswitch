990eed1af55d1b65fd1c17ad1dfe99f611806bc1
Nikhil Singh <nik.singh710@gmail.com>
2025-07-23 15:55:30 +0530

    chore(nix): devShell cleanup

--- nix/modules/flake/devshell.nix
+++ nix/modules/flake/devshell.nix
@@ -10,39 +10,59 @@
     in
     {
       devShells = {
-        default =
-          pkgs.mkShell {
-            name = "hyperswitch-shell";
-            meta.description = "Environment for Hyperswitch development";
-            inputsFrom = [ config.pre-commit.devShell ];
-            packages = with pkgs; [
-              diesel-cli
-              just
-              jq
-              openssl
-              pkg-config
-              postgresql # for libpq
-              protobuf
-            ];
-          };
-        dev = pkgs.mkShell {
-          name = "hyperswitch-dev-shell";
-          meta.description = "Environment for Hyperswitch development with additional tools";
-          inputsFrom = [ self'.devShells.default ];
+        default = pkgs.mkShell {
+          name = "hyperswitch-shell";
+          meta.description = "Environment for Hyperswitch development";
+          inputsFrom = [ config.pre-commit.devShell ];
           packages = with pkgs; [
-            cargo-watch
-            nixd
-            swagger-cli
-
-            # The order in which these rust package appear is important
-            rustStablePkg
-            rustFmtNightlyPkg
+            diesel-cli
+            just
+            jq
+            openssl
+            pkg-config
+            postgresql # for libpq
+            protobuf
           ];
-          shellHook = ''
-            echo 1>&2 "Ready to work on hyperswitch!"
-            rustc --version
-          '';
         };
+        dev =
+          let
+            clangPKG = pkgs.llvmPackages_latest.clang-unwrapped;
+          in
+          pkgs.mkShell {
+            name = "hyperswitch-dev-shell";
+            meta.description = "Environment for Hyperswitch development with additional tools";
+            inputsFrom = [ self'.devShells.default ];
+
+            packages = with pkgs; [
+              cargo-watch
+              nixd
+              swagger-cli
+
+              # The order in which these rust package appear is important
+              rustStablePkg
+              rustFmtNightlyPkg
+
+              # euclid_wasm dependencies
+              clangPKG
+              pkgconf
+              wasm-pack
+            ];
+
+            # To avoid issues if someone is having `homebrew's` openssl installed
+            OPENSSL_DIR = "${pkgs.openssl.dev}";
+            OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+
+            PATH = "${clangPKG}/bin:$PATH";
+
+            TARGET_CC = lib.getExe' clangPKG "clang";
+            CPATH = "${lib.getLib clangPKG}/lib/clang/${lib.versions.major (lib.getVersion clangPKG)}/include";
+
+            shellHook = ''
+              echo 1>&2 "Ready to work on hyperswitch!"
+              rustc --version
+              rustfmt --version
+            '';
+          };
         qa = pkgs.mkShell {
           name = "hyperswitch-qa-shell";
           meta.description = "Environment for Hyperswitch QA";
@@ -53,51 +73,6 @@
             parallel
           ];
         };
-
-        # FIXME: Once `euclid_wasm` is buildable in devShell env we can move this to `devShells.dev`
-        test-euclid_wasm =
-          let
-            clangUnwrapped = pkgs.llvmPackages_latest.clang-unwrapped;
-            bintoolsUnwrapped = pkgs.llvmPackages_latest.bintools-unwrapped;
-            wasm32 = "wasm32-unknown-unknown";
-          in
-          pkgs.mkShell {
-            name = "hyperswitch-shell-euclid_wasm";
-            meta.description = "Test shell env for euclid_wasm";
-            inputsFrom = [ self'.devShells.dev ];
-            packages = with pkgs; [
-              wasm-pack
-              wasm-bindgen-cli
-              clangUnwrapped
-              bintoolsUnwrapped
-              lld
-              libpq
-            ] ++ lib.optionals pkgs.stdenv.isDarwin (with pkgs;
-              [
-                libiconv
-                apple-sdk_11
-              ]);
-
-            shellHook = ''
-              rustfmt --version
-              cargo --version
-              echo 1>&2 ""
-              echo 1>&2 "Note: This shell is for testing \`euclid_wasm\` in a local build environment."
-              echo 1>&2 "Basically the env is a rough pick from \`crates/euclid_wasm/crate.nix\` though that is also broken at the moment."
-            '';
-
-            CC_wasm32_unknown_unknown = lib.getExe' clangUnwrapped "clang";
-            CXX_wasm32_unknown_unknown = lib.getExe' clangUnwrapped "clang++";
-            AR_wasm32_unknown_unknown = lib.getExe' bintoolsUnwrapped "llvm-ar";
-            AS_wasm32_unknown_unknown = lib.getExe' bintoolsUnwrapped "llvm-as";
-            STRIP_wasm32_unknown_unknown = lib.getExe' bintoolsUnwrapped "llvm-strip";
-
-            CARGO_BUILD_TARGET = wasm32;
-            CPATH = "${lib.getLib clangUnwrapped}/lib/clang/${lib.versions.major (lib.getVersion clangUnwrapped)}/include";
-            RUSTFLAGS = "-C linker=rust-lld";
-
-          };
-
       };
     };
 }
--- nix/modules/flake/rust.nix
+++ nix/modules/flake/rust.nix
@@ -4,7 +4,7 @@
     inputs.rust-flake.flakeModules.default
     inputs.rust-flake.flakeModules.nixpkgs
   ];
-  perSystem = { pkgs, lib, config, ... }: {
+  perSystem = { pkgs, ... }: {
     rust-project = {
       toolchain = pkgs.rust-bin.fromRustupToolchain {
         channel = "1.88.0";
@@ -23,29 +23,6 @@
         ];
         profile = "minimal"; # Has to be minimal to avoid overriding nightly `rustfmt`
         targets = [ "wasm32-unknown-unknown" ];
-      };
-      crateNixFile = "crate.nix";
-      src = lib.cleanSourceWith {
-        src =
-          let
-            # Like crane's filterCargoSources, but doesn't blindly include all TOML files!
-            filterCargoSources = path: type:
-              config.rust-project.crane-lib.filterCargoSources path type
-              && !(lib.hasSuffix ".toml" path && !lib.hasSuffix "Cargo.toml" path);
-          in
-          # TODO: Put a more thorough filter here
-          lib.cleanSourceWith {
-            src = inputs.self;
-            filter = path: type:
-              filterCargoSources path type
-              || lib.hasSuffix "crate.nix" path
-              || "${inputs.self}/flake.nix" == path
-              || "${inputs.self}/flake.lock" == path
-              || "${inputs.self}/README.md" == path
-              || lib.hasPrefix "${inputs.self}/nix/" path
-              || lib.hasPrefix "${inputs.self}/crates/" path
-            ;
-          };
       };
     };
   };
