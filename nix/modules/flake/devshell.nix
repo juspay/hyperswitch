b89cf15f99cadd9e5bf64e0d5627bd8e359095ad
Nikhil Singh <nik.singh710@gmail.com>
2025-07-25 00:18:05 +0530

    chore(nix): devShell Env variable cleanup

--- nix/modules/flake/devshell.nix
+++ nix/modules/flake/devshell.nix
@@ -27,6 +27,8 @@
         dev =
           let
             clangPKG = pkgs.llvmPackages_latest.clang-unwrapped;
+            clangLib = lib.getLib clangPKG;
+            majorVer = lib.versions.major (lib.getVersion clangPKG);
           in
           pkgs.mkShell {
             name = "hyperswitch-dev-shell";
@@ -44,23 +46,34 @@
 
               # euclid_wasm dependencies
               clangPKG
-              pkgconf
+              pkg-config
               wasm-pack
+              libpq
             ];
 
             # To avoid issues if someone is having `homebrew's` openssl installed
             OPENSSL_DIR = "${pkgs.openssl.dev}";
             OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
 
-            PATH = "${clangPKG}/bin:$PATH";
-
+            CPATH = "${clangLib}/lib/clang/${majorVer}/include";
             TARGET_CC = lib.getExe' clangPKG "clang";
-            CPATH = "${lib.getLib clangPKG}/lib/clang/${lib.versions.major (lib.getVersion clangPKG)}/include";
 
             shellHook = ''
               echo 1>&2 "Ready to work on hyperswitch!"
               rustc --version
               rustfmt --version
+
+              # FIXME: This is a workaround for the error `rust-lld: error: unable to find library -lpq`
+              # Have tried setting variables like `LD_LIBRARY_PATH`, `LIBRARY_PATH`, `PKG_CONFIG_PATH` but nothing worked
+              # Also like LD_FLAGS, LDFLAGS, CFLAGS, CXXFLAGS, etc. didn't work
+              # `PATH` has to be exported in the shellHook as Nix doesn't modify user's Environment variables
+              # homebrew's libpq bin path includes `pg_config` and `ecpg` as extra binaries from nix's postgresql
+              # pg_config in nix is at `pkgs.postgresql.pg_config` and `ecpg` is at `pkgs.postgresql.dev`
+              # lib dir maybe req to be compared to resolve the issue ......
+
+              # export PATH="${pkgs.postgresql.pg_config}/bin:${pkgs.postgresql.dev}/bin:${pkgs.postgresql}/bin:$PATH"
+              HBREW_LIBPQ_PATH="${lib.optionalString (pkgs.stdenv.isDarwin) "/opt/homebrew/opt/libpq/bin"}"
+              export PATH="$HBREW_LIBPQ_PATH:$PATH"
             '';
           };
         qa = pkgs.mkShell {
