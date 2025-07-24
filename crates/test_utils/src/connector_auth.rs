b521951942dd653f6a78729926d91e8e793cd999
Yashasvi Kapil <74726400+iemyashasvi@users.noreply.github.com>
2025-07-24 11:39:55 +0530

    feat(connector): [SILVERFLOW] Integrate cards non 3ds payments (#8591)
    Co-authored-by: yashasvi.kapil <yashasvi.kapil@juspay.in>
    Co-authored-by: Kuntimaddi Manideep <manideepk90@gmail.com>

--- crates/test_utils/src/connector_auth.rs
+++ crates/test_utils/src/connector_auth.rs
@@ -101,7 +101,7 @@ pub struct ConnectorAuthentication {
     pub redsys: Option<HeaderKey>,
     pub santander: Option<BodyKey>,
     pub shift4: Option<HeaderKey>,
-    pub silverflow: Option<BodyKey>,
+    pub silverflow: Option<SignatureKey>,
     pub square: Option<BodyKey>,
     pub stax: Option<HeaderKey>,
     pub stripe: Option<HeaderKey>,
