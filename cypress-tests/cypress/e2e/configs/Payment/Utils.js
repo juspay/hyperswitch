f2f7d277709eb0535f112f79c120af8b475bf540
Kashif <kashif.dev@protonmail.com>
2025-07-25 15:04:08 +0530

    chore: add remaining file

--- cypress-tests/cypress/e2e/configs/Payment/Utils.js
+++ cypress-tests/cypress/e2e/configs/Payment/Utils.js
@@ -396,8 +396,7 @@ export const CONNECTOR_LISTS = {
       // "cybersource"    // issues with MULTIPLE_CONNECTORS handling
       "paypal",
     ],
-    DDC_RACE_CONDITION_SERVER_SIDE: ["worldpay"],
-    DDC_RACE_CONDITION_CLIENT_SIDE: ["worldpay"],
+    DDC_RACE_CONDITION: ["worldpay"],
     // Add more inclusion lists
   },
 };
