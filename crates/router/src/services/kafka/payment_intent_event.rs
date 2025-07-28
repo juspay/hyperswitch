0ea0797fec0c17e74e50ec0648cf024819e6400d
Shivansh Mathur <shivansh.mathur@juspay.in>
2025-07-28 14:08:51 +0530

    serde changes for Optinal fields

--- crates/router/src/services/kafka/payment_intent_event.rs
+++ crates/router/src/services/kafka/payment_intent_event.rs
@@ -78,7 +78,7 @@ pub struct KafkaPaymentIntentEvent<'a> {
     pub created_at: OffsetDateTime,
     #[serde(with = "time::serde::timestamp::nanoseconds")]
     pub modified_at: OffsetDateTime,
-    #[serde(with = "time::serde::timestamp::nanoseconds::option")]
+    #[serde(default, with = "time::serde::timestamp::nanoseconds::option")]
     pub last_synced: Option<OffsetDateTime>,
     pub setup_future_usage: storage_enums::FutureUsage,
     pub off_session: bool,
@@ -97,7 +97,7 @@ pub struct KafkaPaymentIntentEvent<'a> {
     pub surcharge_applicable: Option<bool>,
     pub request_incremental_authorization: RequestIncrementalAuthorization,
     pub authorization_count: Option<i32>,
-    #[serde(default, with = "time::serde::timestamp::nanoseconds::option")]
+    #[serde(with = "time::serde::timestamp::nanoseconds")]
     pub session_expiry: OffsetDateTime,
     pub request_external_three_ds_authentication: common_enums::External3dsAuthenticationRequest,
     pub frm_metadata: Option<Secret<&'a Value>>,
