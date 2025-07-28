8b1ce02466b1b8ae9da7844882d1b6db5b0e567b
Anurag Singh <anurag.singh.001@MacBookPro.lan>
2025-07-28 22:00:06 +0530

    add cypress for stripe incremental authorization flow

--- cypress-tests/cypress/e2e/configs/Payment/Stripe.js
+++ cypress-tests/cypress/e2e/configs/Payment/Stripe.js
@@ -314,6 +314,15 @@ export const connectorDetails = {
           amount_received: 6000,
         },
       },
+      ResponseCustom: {
+        status: 200,
+        body: {
+          status: "succeeded",
+          amount: 8000,
+          amount_capturable: 0,
+          amount_received: 8000,
+        },
+      },
     },
     PartialCapture: {
       Request: {
@@ -387,6 +396,27 @@ export const connectorDetails = {
         status: 200,
         body: {
           status: "succeeded",
+        },
+      },
+    },
+    IncrementalAuth: {
+      Request: {
+        amount: 8000,
+      },
+      Response: {
+        status: 200,
+        body: {
+          status: "requires_capture",
+          amount: 8000,
+          amount_capturable: 8000,
+          amount_received: null,
+          incremental_authorizations: [
+            {
+              amount: 8000,
+              previously_authorized_amount: 6000,
+              status: "requires_capture",
+            },
+          ],
         },
       },
     },
--- cypress-tests/cypress/e2e/configs/Payment/Utils.js
+++ cypress-tests/cypress/e2e/configs/Payment/Utils.js
@@ -393,6 +393,7 @@ export const CONNECTOR_LISTS = {
     INCREMENTAL_AUTH: [
       // "cybersource"    // issues with MULTIPLE_CONNECTORS handling
       "paypal",
+      "stripe"
     ],
     // Add more inclusion lists
   },
