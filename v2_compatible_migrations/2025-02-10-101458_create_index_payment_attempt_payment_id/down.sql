35290fdfc0cdff3bc4af1c14420b431c629bca1f
hrithikesh026 <hrithikesh.vm@juspay.in>
2025-07-31 16:59:08 +0530

    chore: fix migrations

--- v2_compatible_migrations/2025-02-10-101458_create_index_payment_attempt_payment_id/down.sql
+++ v2_compatible_migrations/2025-02-10-101458_create_index_payment_attempt_payment_id/down.sql
@@ -1,2 +1,2 @@
 -- This file should undo anything in `up.sql`
-CREATE INDEX IF NOT EXISTS payment_attempt_payment_id_merchant_id_index ON payment_attempt (payment_id, merchant_id);
+DROP INDEX IF EXISTS payment_attempt_payment_id_index;
