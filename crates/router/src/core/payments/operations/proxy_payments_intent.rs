654c15ee196034313ae14ab6ff8a4d6499458388
Aditya Chaurasia <113281443+AdityaKumaar21@users.noreply.github.com>
2025-08-06 19:00:42 +0530

    feat(gRPC): build gRPC client interface to initiate communication with recovery-decider service (#8178)
    Co-authored-by: Nishanth Challa <nishanth.challa@Nishanth-Challa-C0WGKCFHLF.local>
    Co-authored-by: hyperswitch-bot[bot] <148525504+hyperswitch-bot[bot]@users.noreply.github.com>
    Co-authored-by: CHALLA NISHANTH BABU <115225644+NISHANTH1221@users.noreply.github.com>
    Co-authored-by: Aprabhat19 <amishaprabhat@gmail.com>

--- crates/router/src/core/payments/operations/proxy_payments_intent.rs
+++ crates/router/src/core/payments/operations/proxy_payments_intent.rs
@@ -257,6 +257,7 @@ impl<F: Send + Clone + Sync> GetTracker<F, PaymentConfirmData<F>, ProxyPaymentsR
                 ),
             ),
         };
+
         let payment_data = PaymentConfirmData {
             flow: std::marker::PhantomData,
             payment_intent,
