8bb8b2062e84e8d7116a9ca0e76c8ebd71b2b3ec
Sayak Bhattacharya <sayakofficial21@gmail.com>
2025-08-12 12:36:51 +0530

    feat(connector): [FISERV] Added ApplePay Wallet (#8670)
    Co-authored-by: Sayak Bhattacharya <sayak.b@Sayak-Bhattacharya-G092THXJ34.local>

--- crates/hyperswitch_connectors/src/connectors/fiserv.rs
+++ crates/hyperswitch_connectors/src/connectors/fiserv.rs
@@ -1000,6 +1000,17 @@ static FISERV_SUPPORTED_PAYMENT_METHODS: LazyLock<SupportedPaymentMethods> = Laz
         },
     );
 
+    fiserv_supported_payment_methods.add(
+        enums::PaymentMethod::Wallet,
+        enums::PaymentMethodType::ApplePay,
+        PaymentMethodDetails {
+            mandates: enums::FeatureStatus::NotSupported,
+            refunds: enums::FeatureStatus::Supported,
+            supported_capture_methods: supported_capture_methods.clone(),
+            specific_features: None,
+        },
+    );
+
     fiserv_supported_payment_methods
 });
 
