c1b0c69d443461e30c078086134ec5d0902cd6e8
Aprabhat19 <amishaprabhat@gmail.com>
2025-08-14 17:11:37 +0530

    add redis ttl for keys

--- crates/router/src/types/storage/revenue_recovery_redis_operation.rs
+++ crates/router/src/types/storage/revenue_recovery_redis_operation.rs
@@ -81,9 +81,13 @@ impl RedisTokenManager {
                 ))?;
 
         let lock_key = format!("customer:{connector_customer_id}:status");
-
+        let seconds = 3888000;
         let result: bool = match redis_conn
-            .set_key_if_not_exists_with_expiry(&lock_key.into(), payment_id.get_string_repr(), None)
+            .set_key_if_not_exists_with_expiry(
+                &lock_key.into(),
+                payment_id.get_string_repr(),
+                Some(seconds),
+            )
             .await
         {
             Ok(resp) => resp == SetnxReply::KeySet,
@@ -218,13 +222,13 @@ impl RedisTokenManager {
 
             serialized_payment_processor_tokens.insert(payment_processor_token_id, serialized);
         }
-
+        let seconds = 3888000;
         // Update or add tokens
         redis_conn
             .set_hash_fields(
                 &tokens_key.into(),
                 serialized_payment_processor_tokens,
-                None,
+                Some(seconds),
             )
             .await
             .change_context(errors::StorageError::RedisError(
