34ec09cd594edb36391a3082fff1dd2549330be2
Sagnik Mitra <sagnik.mitra@juspay.in>
2025-08-19 18:28:15 +0530

    error logging in connector mandate

--- crates/router/src/types/api/fraud_check.rs
+++ crates/router/src/types/api/fraud_check.rs
@@ -37,7 +37,8 @@ pub enum ConnectorCallType {
 pub struct ConnectorRoutingData {
     pub connector_data: ConnectorData,
     pub network: Option<common_enums::CardNetwork>,
-    pub action_type: Option<ActionType>, // action_type is used for mandates currently
+    // action_type is used for mandates currently
+    pub action_type: Option<ActionType>,
 }
 
 impl FraudCheckConnectorData {
