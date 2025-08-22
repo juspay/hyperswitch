fa7448dbd21b0ec0e51624fc941490638da21f6b
Malay Awasthi <malay.awasthi@juspay.in>
2025-08-22 18:35:11 +0530

    Initial commit

--- crates/hyperswitch_connectors/src/connectors/blackhawknetwork.rs
+++ crates/hyperswitch_connectors/src/connectors/blackhawknetwork.rs
@@ -465,7 +465,7 @@ impl ConnectorIntegration<Authorize, PaymentsAuthorizeData, PaymentsResponseData
             network_advice_code: None,
             network_decline_code: None,
             network_error_message: None,
-            connector_metadata:None,
+            connector_metadata: None,
         })
     }
 }
