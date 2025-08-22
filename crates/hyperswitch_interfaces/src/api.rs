c12448585de6071b6557731f8ee487037d34f4bb
Shivansh Mathur <shivansh.mathur@juspay.in>
2025-08-23 01:51:58 +0530

    comment resolution

--- crates/hyperswitch_interfaces/src/api.rs
+++ crates/hyperswitch_interfaces/src/api.rs
@@ -397,7 +397,7 @@ pub trait ConnectorSpecifications {
         false
     }
 
-    #[cfg(feature = "v1")]
+    #[cfg(not(feature = "v2"))]
     /// Generate connector request reference ID
     fn generate_connector_request_reference_id(
         &self,
