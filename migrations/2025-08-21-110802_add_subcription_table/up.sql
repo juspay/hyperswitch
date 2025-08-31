fa7348a9ebc471c94841cfdd575f8fe063391e99
Prajjwal kumar <write2prajjwal@gmail.com>
2025-09-01 05:04:44 +0530

    refactor: add mca_id and client_secret cols

--- migrations/2025-08-21-110802_add_subcription_table/up.sql
+++ migrations/2025-08-21-110802_add_subcription_table/up.sql
@@ -3,6 +3,8 @@ CREATE TABLE subscription (
   subscription_id VARCHAR(128),
   billing_processor VARCHAR(128),
   payment_method_id VARCHAR(128),
+  mca_id VARCHAR(128),
+  client_secret VARCHAR(128),
   merchant_id VARCHAR(64) NOT NULL,
   customer_id VARCHAR(64) NOT NULL,
   metadata JSONB,
