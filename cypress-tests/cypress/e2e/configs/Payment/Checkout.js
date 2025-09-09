7567a392cce3d1ba506e9b837cc869aa1652ce8c
Vani Gupta <vani.gupta@juspay.in>
2025-09-09 19:05:48 +0530

    fix:cypress

--- cypress-tests/cypress/e2e/configs/Payment/Checkout.js
+++ cypress-tests/cypress/e2e/configs/Payment/Checkout.js
@@ -104,23 +104,6 @@ export const connectorDetails = {
         },
       },
     },
-    PaymentMethodIdMandateNo3DSAutoCapture: {
-      Request: {
-        payment_method: "card",
-        payment_method_data: {
-          card: successfulNo3DSCardDetails,
-        },
-        currency: "USD",
-        mandate_data: null,
-        customer_acceptance: customerAcceptance,
-      },
-      Response: {
-        status: 200,
-        body: {
-          status: "succeeded",
-        },
-      },
-    },
     PaymentMethodIdMandateNo3DSManualCapture: {
       Request: {
         payment_method: "card",
