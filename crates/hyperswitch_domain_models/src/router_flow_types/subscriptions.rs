2156e36e0a19ed8e0a731b1fa7874997414310d1
Spriti Aneja <spriti.aneja@juspay.in>
2025-09-17 16:11:07 +0530

    delete same files added in 2 different PRs

--- crates/hyperswitch_domain_models/src/router_flow_types/subscriptions.rs
+++
@@ -1,2 +0,0 @@
-#[derive(Debug, Clone)]
-pub struct GetSubscriptionPlanPrices;
--- crates/hyperswitch_domain_models/src/router_request_types/subscriptions.rs
+++
@@ -1,4 +0,0 @@
-#[derive(Debug, Clone)]
-pub struct GetSubscriptionPlanPricesRequest {
-    pub plan_price_id: String,
-}
