065c00df6cf34286ebe5f61973bc9238fdb02e46
Prasunna Soppa <prasunna.soppa@juspay.in>
2025-09-23 02:12:17 +0530

    fix open api spec file

--- crates/openapi/src/openapi.rs
+++ crates/openapi/src/openapi.rs
@@ -381,6 +381,7 @@ Never share your secret api keys. Keep them guarded and secure.
         api_models::enums::PaymentLinkSdkLabelType,
         api_models::enums::OrganizationType,
         api_models::enums::PaymentLinkShowSdkTerms,
+        api_models::enums::ExternalVaultEnabled,
         api_models::admin::MerchantConnectorCreate,
         api_models::admin::AdditionalMerchantData,
         api_models::admin::ConnectorWalletDetails,
