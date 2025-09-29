fce4ad67c6827128a58afff4c932eafdc1a1b142
Chikke Srujan <chikke.srujan@Chikke-Srujan-V9P7D4K9V0.local>
2025-09-29 11:38:38 +0530

    clippy fix

--- crates/router/src/core/payments/operations/payment_update_intent.rs
+++ crates/router/src/core/payments/operations/payment_update_intent.rs
@@ -298,7 +298,7 @@ impl<F: Send + Clone> GetTracker<F, payments::PaymentIntentData<F>, PaymentsUpda
                 .or(payment_intent.allowed_payment_method_types),
             active_attempt_id,
             enable_partial_authorization: enable_partial_authorization
-                .unwrap_or(payment_intent.enable_partial_authorization),
+                .or(payment_intent.enable_partial_authorization),
             ..payment_intent
         };
 
