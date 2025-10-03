5a8668d336cf570703f752685073fbc640e2bbee
aadityaguptaa <aditya.gupta.001@juspay.in>
2025-10-03 15:57:32 +0530

    removes sepa integration

--- crates/hyperswitch_connectors/src/connectors/trustpayments.rs
+++ crates/hyperswitch_connectors/src/connectors/trustpayments.rs
@@ -878,17 +878,6 @@ static TRUSTPAYMENTS_SUPPORTED_PAYMENT_METHODS: LazyLock<SupportedPaymentMethods
             },
         );
 
-        trustpayments_supported_payment_methods.add(
-            enums::PaymentMethod::BankRedirect,
-            enums::PaymentMethodType::Sepa,
-            PaymentMethodDetails {
-                mandates: enums::FeatureStatus::NotSupported,
-                refunds: enums::FeatureStatus::Supported,
-                supported_capture_methods: supported_capture_methods.clone(),
-                specific_features: None,
-            },
-        );
-
         trustpayments_supported_payment_methods
     });
 
