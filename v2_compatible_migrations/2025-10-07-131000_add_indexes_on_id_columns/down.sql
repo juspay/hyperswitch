21e1295ddedcd6c4703ece5ec5d57fee6ccfd3af
hrithikesh026 <hrithikesh.vm@juspay.in>
2025-10-07 20:20:19 +0530

    chore: create unique indexes on required id columns for v2 application

--- v2_compatible_migrations/2025-10-07-131000_add_indexes_on_id_columns/down.sql
+++ v2_compatible_migrations/2025-10-07-131000_add_indexes_on_id_columns/down.sql
@@ -1,6 +1,7 @@
--- Drop indexes on id columns
--- This will remove the indexes created for id column performance optimization
+-- Drop unique indexes on id columns
+-- This will remove the unique indexes created for id column performance optimization and data integrity
 
+-- tracker tables
 DROP INDEX IF EXISTS customers_id_index;
 
 DROP INDEX IF EXISTS payment_intent_id_index;
@@ -10,3 +11,12 @@ DROP INDEX IF EXISTS payment_attempt_id_index;
 DROP INDEX IF EXISTS payment_methods_id_index;
 
 DROP INDEX IF EXISTS refund_id_index;
+
+-- Accounts tables
+DROP INDEX IF EXISTS business_profile_id_index;
+
+DROP INDEX IF EXISTS merchant_account_id_index;
+
+DROP INDEX IF EXISTS merchant_connector_account_id_index;
+
+DROP INDEX IF EXISTS organization_id_index;
