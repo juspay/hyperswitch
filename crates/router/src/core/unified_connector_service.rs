d81d3b60a32a6880f686b699a96345e2d0438fe2
Kanika Chaudhary <kanika.c@juspay.in>
2025-10-09 19:21:01 +0530

    addressed comments

--- crates/router/src/core/unified_connector_service.rs
+++ crates/router/src/core/unified_connector_service.rs
@@ -346,10 +346,13 @@ pub fn build_unified_connector_service_payment_method(
                     };
                     PaymentMethod::UpiCollect(upi_details)
                 }
-                hyperswitch_domain_models::payment_method_data::UpiData::UpiIntent(_)
-                | hyperswitch_domain_models::payment_method_data::UpiData::UpiQr(_) => {
+                hyperswitch_domain_models::payment_method_data::UpiData::UpiIntent(_) => {
                     let upi_details = payments_grpc::UpiIntent { app_name: None };
                     PaymentMethod::UpiIntent(upi_details)
+                }
+                hyperswitch_domain_models::payment_method_data::UpiData::UpiQr(_) => {
+                    let upi_details = payments_grpc::UpiQr {};
+                    PaymentMethod::UpiQr(upi_details)
                 }
             };
 
