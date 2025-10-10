59d1816494c86385ce383d27fa3136adc1a07d69
Gaurav Rawat <104276743+GauravRawat369@users.noreply.github.com>
2025-10-11 02:43:03 +0530

    fix(subscription): update down migration to drop item_price_id instead of price_id

--- migrations/2025-10-07-160015_add-plan-id-and-price-id-to-subscription/down.sql
+++ migrations/2025-10-07-160015_add-plan-id-and-price-id-to-subscription/down.sql
@@ -1,3 +1,3 @@
 -- This file should undo anything in `up.sql`
 ALTER TABLE subscription DROP COLUMN IF EXISTS plan_id;
-ALTER TABLE subscription DROP COLUMN IF EXISTS price_id;
+ALTER TABLE subscription DROP COLUMN IF EXISTS item_price_id;
