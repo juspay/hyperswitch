20bf7e141ef2d11d3407392af21dd92b33e697c7
cb-nabeel <nabeel.hussain@chargebee.com>
2025-10-12 12:18:50 +0530

    minor fix

--- crates/hyperswitch_connectors/src/connectors/chargebee/transformers.rs
+++ crates/hyperswitch_connectors/src/connectors/chargebee/transformers.rs
@@ -313,6 +313,8 @@ impl From<ChargebeePaymentStatus> for common_enums::AttemptStatus {
 pub struct ChargebeePaymentsResponse {
     status: ChargebeePaymentStatus,
     id: String,
+    amount: MinorUnit,
+    currency_code: enums::Currency,
 }
 
 impl<F, T> TryFrom<ResponseRouterData<F, ChargebeePaymentsResponse, T, PaymentsResponseData>>
@@ -383,6 +385,8 @@ impl From<RefundStatus> for enums::RefundStatus {
 pub struct RefundResponse {
     id: String,
     status: RefundStatus,
+    amount: MinorUnit,
+    currency_code: enums::Currency,
 }
 
 impl TryFrom<RefundsResponseRouterData<Execute, RefundResponse>> for RefundsRouterData<Execute> {
