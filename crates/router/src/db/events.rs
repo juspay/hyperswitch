e39884f02fa9754775e32936257c0aa343649f93
Soham Mirikar <soham.mirikar@juspay.in>
2025-10-13 01:17:10 +0530

    removed redundant fields

--- crates/router/src/db/events.rs
+++ crates/router/src/db/events.rs
@@ -1176,9 +1176,6 @@ mod tests {
                 webhook_url: Some(masking::Secret::new(
                     "https://example.com/webhooks".to_string(),
                 )),
-                payment_created_enabled: None,
-                payment_succeeded_enabled: Some(true),
-                payment_failed_enabled: None,
                 payment_statuses_enabled: None,
                 refund_statuses_enabled: None,
                 payout_statuses_enabled: None,
@@ -1243,9 +1240,6 @@ mod tests {
                 webhook_url: Some(masking::Secret::new(
                     "https://example.com/webhooks".to_string(),
                 )),
-                payment_created_enabled: None,
-                payment_succeeded_enabled: Some(true),
-                payment_failed_enabled: None,
                 payment_statuses_enabled: None,
                 refund_statuses_enabled: None,
                 payout_statuses_enabled: None,
