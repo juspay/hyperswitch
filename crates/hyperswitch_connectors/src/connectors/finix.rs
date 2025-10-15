b6129c33ca86f79f0df3e5810feefa3acebdcae1
nihtin <nithingowdan77@gmail.com>
2025-10-15 11:37:25 +0530

    update

--- crates/hyperswitch_connectors/src/connectors/finix.rs
+++ crates/hyperswitch_connectors/src/connectors/finix.rs
@@ -1052,7 +1052,7 @@ static FINIX_SUPPORTED_PAYMENT_METHODS: LazyLock<SupportedPaymentMethods> = Lazy
         enums::PaymentMethod::Wallet,
         PaymentMethodType::GooglePay,
         PaymentMethodDetails {
-            mandates: enums::FeatureStatus::NotSupported,
+            mandates: enums::FeatureStatus::Supported,
             refunds: enums::FeatureStatus::Supported,
             supported_capture_methods: default_capture_methods.clone(),
             specific_features: None,
