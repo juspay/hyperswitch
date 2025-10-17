6f1a72b05eb82a4747cddecd51a51c8b865039aa
Sweta-Kumari-Sharma <ss6175983@gmail.com>
2025-10-17 23:55:03 +0530

    db-domain-api models conversion added

--- crates/router/src/db/events.rs
+++ crates/router/src/db/events.rs
@@ -968,14 +968,16 @@ mod tests {
         },
     };
     use diesel_models::{
-        business_profile::WebhookDetails,
         enums::{self},
         events::EventMetadata,
     };
     use futures::future::join_all;
     use hyperswitch_domain_models::{
-        master_key::MasterKeyInterface, merchant_account::MerchantAccountSetter,
+        business_profile::{MultipleWebhookDetail, WebhookDetails},
+        master_key::MasterKeyInterface,
+        merchant_account::MerchantAccountSetter,
     };
+    use strum::IntoEnumIterator;
     use time::macros::datetime;
     use tokio::time::{timeout, Duration};
 
@@ -1281,16 +1283,19 @@ mod tests {
                 webhook_version: None,
                 webhook_username: None,
                 webhook_password: None,
-                webhook_url: Some(masking::Secret::new(
-                    "https://example.com/webhooks".to_string(),
-                )),
                 payment_created_enabled: None,
                 payment_succeeded_enabled: Some(true),
                 payment_failed_enabled: None,
                 payment_statuses_enabled: None,
                 refund_statuses_enabled: None,
                 payout_statuses_enabled: None,
-                multiple_webhooks_list: None,
+                multiple_webhooks_list: Some(vec![MultipleWebhookDetail {
+                    webhook_endpoint_id:
+                        common_utils::generate_webhook_endpoint_id_of_default_length(),
+                    webhook_url: masking::Secret::new("https://example.com/webhooks".to_string()),
+                    events: common_enums::EventType::iter().collect(),
+                    status: common_enums::OutgoingWebhookEndpointStatus::Active,
+                }]),
             }),
             sub_merchants_enabled: None,
             parent_merchant_id: None,
@@ -1348,16 +1353,19 @@ mod tests {
                 webhook_version: None,
                 webhook_username: None,
                 webhook_password: None,
-                webhook_url: Some(masking::Secret::new(
-                    "https://example.com/webhooks".to_string(),
-                )),
                 payment_created_enabled: None,
                 payment_succeeded_enabled: Some(true),
                 payment_failed_enabled: None,
                 payment_statuses_enabled: None,
                 refund_statuses_enabled: None,
                 payout_statuses_enabled: None,
-                multiple_webhooks_list: None,
+                multiple_webhooks_list: Some(vec![MultipleWebhookDetail {
+                    webhook_endpoint_id:
+                        common_utils::generate_webhook_endpoint_id_of_default_length(),
+                    webhook_url: masking::Secret::new("https://example.com/webhooks".to_string()),
+                    events: common_enums::EventType::iter().collect(),
+                    status: common_enums::OutgoingWebhookEndpointStatus::Active,
+                }]),
             }),
             metadata: None,
             routing_algorithm: None,
