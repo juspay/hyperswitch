c598405225a79c619e09c9bc6b0acf8163ce1e37
Shivansh Mathur <shivansh.mathur@juspay.in>
2025-10-22 10:22:52 +0530

    metrics addition

--- crates/injector/src/lib.rs
+++ crates/injector/src/lib.rs
@@ -1,10 +1,12 @@
 pub mod consts;
 pub mod injector;
+pub mod metrics;
 pub mod types;
 pub mod vault_metadata;
 
 // Re-export all functionality
 pub use consts::*;
 pub use injector::*;
+pub use metrics::*;
 pub use types::*;
 pub use vault_metadata::*;
