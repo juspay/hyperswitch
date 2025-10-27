4748d51be1d93dc0f525d171f750951835b3f3a5
Sahkal Poddar <sahkal.poddar@juspay.in>
2025-10-27 12:33:07 +0530

    added transstatus check

--- crates/router/src/core/unified_authentication_service/utils.rs
+++ crates/router/src/core/unified_authentication_service/utils.rs
@@ -252,7 +252,10 @@ pub async fn external_authentication_update_trackers<F: Clone, Req>(
             UasAuthenticationResponseData::PostAuthentication {
                 authentication_details,
             } => {
-                let trans_status = authentication_details.trans_status.unwrap_or_default();
+                let trans_status = authentication_details
+                    .trans_status
+                    .ok_or(ApiErrorResponse::InternalServerError)
+                    .attach_printable("missing trans_status in PostAuthentication Details")?;
 
                 authentication_details
                     .dynamic_data_details
