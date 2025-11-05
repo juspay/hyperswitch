35caa193473118228213ebad89998f2be40f38d9
Mrudul Vajpayee <124863642+mrudulvajpayee4935@users.noreply.github.com>
2025-11-05 15:21:43 +0530

    fix(connectors): [Adyen] add migrated payment method check (#10094)

--- crates/router/src/core/relay/utils.rs
+++ crates/router/src/core/relay/utils.rs
@@ -149,6 +149,7 @@ pub async fn construct_relay_refund_router_data<F>(
         l2_l3_data: None,
         minor_amount_capturable: None,
         authorized_amount: None,
+        is_migrated_card: None,
     };
 
     Ok(router_data)
