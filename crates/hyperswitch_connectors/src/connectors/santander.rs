9e044556800ed5582b33230b316688c077c34557
Sayak Bhattacharya <sayak.b@Sayak-Bhattacharya-G092THXJ34.local>
2025-11-06 13:21:37 +0530

    fix: resolved pr comments

--- crates/hyperswitch_connectors/src/connectors/santander.rs
+++ crates/hyperswitch_connectors/src/connectors/santander.rs
@@ -1097,10 +1097,6 @@ impl ConnectorSpecifications for Santander {
     fn get_supported_webhook_flows(&self) -> Option<&'static [enums::EventClass]> {
         Some(&SANTANDER_SUPPORTED_WEBHOOK_FLOWS)
     }
-
-    fn requires_different_access_token_per_payment_method_type(&self) -> bool {
-        true
-    }
 }
 
 impl ConnectorAccessTokenSuffix for Santander {
