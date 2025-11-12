7aad69f0fa051f799b69cb10513447c01bd07053
Mrudul Vajpayee <124863642+mrudulvajpayee4935@users.noreply.github.com>
2025-11-12 15:04:43 +0530

    fix(connectors): [Adyen] Revert adyen fix mit (#10217)

--- crates/router/src/core/payments/transformers.rs
+++ crates/router/src/core/payments/transformers.rs
@@ -199,7 +199,6 @@ where
         l2_l3_data: None,
         minor_amount_capturable: None,
         authorized_amount: None,
-        is_migrated_card: None,
     };
     Ok(router_data)
 }
@@ -546,7 +545,6 @@ pub async fn construct_payment_router_data_for_authorize<'a>(
         l2_l3_data: None,
         minor_amount_capturable: None,
         authorized_amount: None,
-        is_migrated_card: None,
     };
 
     Ok(router_data)
@@ -894,7 +892,6 @@ pub async fn construct_payment_router_data_for_capture<'a>(
         l2_l3_data: None,
         minor_amount_capturable: None,
         authorized_amount: None,
-        is_migrated_card: None,
     };
 
     Ok(router_data)
@@ -1028,7 +1025,6 @@ pub async fn construct_router_data_for_psync<'a>(
         l2_l3_data: None,
         minor_amount_capturable: None,
         authorized_amount: None,
-        is_migrated_card: None,
     };
 
     Ok(router_data)
@@ -1393,7 +1389,6 @@ pub async fn construct_payment_router_data_for_sdk_session<'a>(
         l2_l3_data: None,
         minor_amount_capturable: None,
         authorized_amount: None,
-        is_migrated_card: None,
     };
 
     Ok(router_data)
@@ -1621,7 +1616,6 @@ pub async fn construct_payment_router_data_for_setup_mandate<'a>(
         l2_l3_data: None,
         minor_amount_capturable: None,
         authorized_amount: None,
-        is_migrated_card: None,
     };
 
     Ok(router_data)
@@ -1873,14 +1867,7 @@ where
         access_token: None,
         session_token: None,
         reference_id: None,
-        payment_method_status: payment_data
-            .payment_method_info
-            .as_ref()
-            .map(|info| info.status),
-        is_migrated_card: payment_data
-            .payment_method_info
-            .as_ref()
-            .map(|info| info.is_migrated_card()),
+        payment_method_status: payment_data.payment_method_info.map(|info| info.status),
         payment_method_token: payment_data
             .pm_token
             .map(|token| types::PaymentMethodToken::Token(Secret::new(token))),
@@ -2078,11 +2065,7 @@ pub async fn construct_payment_router_data_for_update_metadata<'a>(
         access_token: None,
         session_token: None,
         reference_id: None,
-        payment_method_status: payment_data
-            .payment_method_info
-            .as_ref()
-            .map(|info| info.status),
-        is_migrated_card: None,
+        payment_method_status: payment_data.payment_method_info.map(|info| info.status),
         payment_method_token: payment_data
             .pm_token
             .map(|token| types::PaymentMethodToken::Token(Secret::new(token))),
