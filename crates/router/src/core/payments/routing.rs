f7444dbc85b3f30f793fb7548143bba58cf28f3a
Jagan Elavarasan <jaganelavarasan@gmail.com>
2025-11-19 17:15:17 +0530

    fix clippy errors

--- crates/router/src/core/payments/routing.rs
+++ crates/router/src/core/payments/routing.rs
@@ -2745,7 +2745,6 @@ pub async fn get_active_mca_ids(
     let db_mcas = state
         .store
         .find_merchant_connector_account_by_merchant_id_and_disabled_list(
-            &state.into(),
             &key_store.merchant_id,
             false,
             key_store,
