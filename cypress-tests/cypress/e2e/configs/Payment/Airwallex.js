2037934865af977f6d4affb11cd0192b5d24747e
Swangi Kumari <swangi.kumari@juspay.in>
2025-12-04 16:15:23 +0530

    refactor: fix cypress

--- cypress-tests/cypress/e2e/configs/Payment/Airwallex.js
+++ cypress-tests/cypress/e2e/configs/Payment/Airwallex.js
@@ -83,6 +83,23 @@ const payment_method_data_no3ds = {
 
 export const connectorDetails = {
   card_pm: {
+    PaymentWithBilling :{
+      Request: {
+        currency: "USD",
+        customer_acceptance: null,
+        setup_future_usage: "on_session",
+        billing:{
+          country:"US"
+        }
+      },
+      Response: {
+        status: 200,
+        body: {
+          status: "requires_payment_method",
+          setup_future_usage: "on_session",
+        },
+      },
+    },
     PaymentIntent: {
       Request: {
         currency: "USD",
