ad77fef759c0a418c07c2b2449930d72c49cd81a
PiX <69745008+pixincreate@users.noreply.github.com>
2025-12-04 23:52:40 +0530

    fix: refund throwing 4xx during psync

--- crates/hyperswitch_connectors/src/connectors/payload.rs
+++ crates/hyperswitch_connectors/src/connectors/payload.rs
@@ -815,9 +815,6 @@ impl ConnectorIntegration<RSync, RefundsData, RefundsResponseData> for Payload {
                 .url(&types::RefundSyncType::get_url(self, req, connectors)?)
                 .attach_default_headers()
                 .headers(types::RefundSyncType::get_headers(self, req, connectors)?)
-                .set_body(types::RefundSyncType::get_request_body(
-                    self, req, connectors,
-                )?)
                 .build(),
         ))
     }
