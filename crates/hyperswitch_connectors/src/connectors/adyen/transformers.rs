7c65a8a18248a7a2660fe93151e4c082e91939bc
Sayak Bhattacharya <sayak.b@Sayak-Bhattacharya-G092THXJ34.local>
2025-12-05 17:15:37 +0530

    fix: resolved pr comments

--- crates/hyperswitch_connectors/src/connectors/adyen/transformers.rs
+++ crates/hyperswitch_connectors/src/connectors/adyen/transformers.rs
@@ -2994,12 +2994,14 @@ impl
             .get_connector_testing_data()
             .map(AdyenTestingData::try_from)
             .transpose()?;
-        let test_holder_name = testing_data.and_then(|test_data| test_data.holder_name);
-        let holder_name = Some(
-            item.router_data
-                .request
-                .get_card_holder_name_from_additional_payment_method_data()?,
-        );
+        let holder_name = match item
+            .router_data
+            .request
+            .get_card_holder_name_from_additional_payment_method_data()
+        {
+            Ok(name) => Some(name),
+            Err(_) => testing_data.and_then(|test_data| test_data.holder_name),
+        };
         let payment_method = match mandate_ref_id {
             payments::MandateReferenceId::ConnectorMandateId(connector_mandate_ids) => {
                 let adyen_mandate = AdyenMandate {
@@ -3012,7 +3014,7 @@ impl
                             .get_connector_mandate_id()
                             .ok_or_else(missing_field_err("mandate_id"))?,
                     ),
-                    holder_name: test_holder_name.or(holder_name),
+                    holder_name,
                 };
                 Ok::<PaymentMethod<'_>, Self::Error>(PaymentMethod::AdyenMandatePaymentMethod(
                     Box::new(adyen_mandate),
