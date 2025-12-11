a2f541957bf845381fcc5380914b5a92eefcdb4f
nihtin <nithingowdan77@gmail.com>
2025-12-11 17:24:03 +0530

    fix(connector): [gigadat] make strict type and mask gigadat webhook payload

--- crates/hyperswitch_connectors/src/connectors/gigadat/transformers.rs
+++ crates/hyperswitch_connectors/src/connectors/gigadat/transformers.rs
@@ -25,7 +25,7 @@ use hyperswitch_domain_models::{
     router_flow_types::PoQuote, router_response_types::PayoutsResponseData,
     types::PayoutsRouterData,
 };
-use hyperswitch_interfaces::errors;
+use hyperswitch_interfaces::errors::{self};
 use masking::{PeekInterface, Secret};
 use serde::{Deserialize, Serialize};
 
@@ -623,7 +623,23 @@ pub struct GigadatWebhookQueryParameters {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct GigadatWebhookKeyValue {
-    pub key: String,
-    pub value: String,
+#[serde(rename_all = "camelCase")]
+pub struct GigadatWebhookKeyValueBody {
+    #[serde(rename = "type")]
+    pub webhook_type: String,
+    pub final_type: Option<String>,
+    pub cpi_type: Option<String>,
+    // donot remove the below fields
+    pub name: Option<Secret<String>>,
+    pub mobile: Option<Secret<String>>,
+    pub user_id: Option<Secret<String>>,
+    pub email: Option<Secret<String>>,
+    pub financial_institution: Option<Secret<String>>,
+}
+
+impl GigadatWebhookKeyValueBody {
+    pub fn decode_from_url(body_str: &str) -> Result<Self, errors::ConnectorError> {
+        serde_urlencoded::from_str(body_str)
+            .map_err(|_| errors::ConnectorError::WebhookBodyDecodingFailed)
+    }
 }
