12d9f6d9571ec92833cd440756c19571c2171712
Vani Gupta <vani.gupta@juspay.in>
2025-12-12 15:53:45 +0530

    wpg-fastaccess

--- crates/hyperswitch_connectors/src/connectors/worldpayxml.rs
+++ crates/hyperswitch_connectors/src/connectors/worldpayxml.rs
@@ -1199,7 +1199,6 @@ impl webhooks::IncomingWebhook for Worldpayxml {
             utils::deserialize_xml_to_struct(request.body)?;
         let order_code = body.notify.order_status_event.order_code.clone();
         if worldpayxml::is_refund_event(body.notify.order_status_event.payment.last_event)
-            && !order_code.starts_with("payout_")
         {
             return Ok(api_models::webhooks::ObjectReferenceId::RefundId(
                 api_models::webhooks::RefundIdType::ConnectorRefundId(order_code),
@@ -1211,7 +1210,8 @@ impl webhooks::IncomingWebhook for Worldpayxml {
             ));
         }
         #[cfg(feature = "payouts")]
-        if order_code.starts_with("payout_") {
+        if worldpayxml::is_payout_event(body.notify.order_status_event.payment.last_event)
+        {
             return Ok(api_models::webhooks::ObjectReferenceId::PayoutId(
                 api_models::webhooks::PayoutIdType::ConnectorPayoutId(order_code),
             ));
@@ -1227,8 +1227,7 @@ impl webhooks::IncomingWebhook for Worldpayxml {
             utils::deserialize_xml_to_struct(request.body)?;
         #[cfg(feature = "payouts")]
         {
-            let order_code = body.notify.order_status_event.order_code.clone();
-            if order_code.starts_with("payout_") {
+            if worldpayxml::is_payout_event(body.notify.order_status_event.payment.last_event) {
                 return Ok(worldpayxml::get_payout_webhook_event(
                     body.notify.order_status_event.payment.last_event,
                 ));
