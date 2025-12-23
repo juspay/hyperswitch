ad0d1b305fb634d2f64a9a6b2be1c9a67324271a
Vani Gupta <vani.gupta@juspay.in>
2025-12-23 18:20:44 +0530

    map-error-code-airwallex

--- crates/hyperswitch_connectors/src/connectors/airwallex.rs
+++ crates/hyperswitch_connectors/src/connectors/airwallex.rs
@@ -144,7 +144,10 @@ impl ConnectorCommon for Airwallex {
             Ok(response) => {
                 event_builder.map(|i| i.set_error_response_body(&response));
                 router_env::logger::info!(connector_response=?response);
-
+                let network_error_message = response
+                    .provider_original_response_code
+                    .clone()
+                    .and_then(airwallex::map_error_code_to_message);
                 Ok(ErrorResponse {
                     status_code,
                     code: response.code,
@@ -153,8 +156,8 @@ impl ConnectorCommon for Airwallex {
                     attempt_status: None,
                     connector_transaction_id: None,
                     network_advice_code: None,
-                    network_decline_code: None,
-                    network_error_message: None,
+                    network_decline_code: response.provider_original_response_code.clone(),
+                    network_error_message,
                     connector_metadata: None,
                 })
             }
