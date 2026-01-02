6a8e4e3acbbf7e7b8307f997e9df9c060356f269
Amitsingh Tanwar <amitsingh.tanwar@juspay.in>
2026-01-02 12:41:28 +0530

    chore: code refinement

--- crates/hyperswitch_connectors/src/connectors/nuvei/transformers.rs
+++ crates/hyperswitch_connectors/src/connectors/nuvei/transformers.rs
@@ -158,9 +158,9 @@ impl TryFrom<(&types::PaymentsPreAuthenticateRouterData, String)>
             user_token_id: item.customer_id.clone(),
             billing_address,
             url_details: UrlDetails {
-                success_url: "https://google.com/".to_string(),
-                failure_url: "https://google.com/".to_string(),
-                pending_url: "https://google.com/".to_string(),
+                success_url: return_url.clone(),
+                failure_url: return_url.clone(),
+                pending_url: return_url,
             },
         })
     }
@@ -220,9 +220,9 @@ impl TryFrom<(&types::PaymentsPreProcessingRouterData, String)> for NuveiThreeDS
             user_token_id: item.customer_id.clone(),
             billing_address,
             url_details: UrlDetails {
-                success_url: "https://google.com/".to_string(), 
-                failure_url: "https://google.com/".to_string(), 
-                pending_url: "https://google.com/".to_string(),
+                success_url: return_url.clone(),
+                failure_url: return_url.clone(),
+                pending_url: return_url,
             },
         })
     }
@@ -1155,9 +1155,9 @@ where
             device_details,
             external_scheme_details: request_data.external_scheme_details,
             url_details: Some(UrlDetails {
-                success_url: "https://google.com/".to_string(), 
-                failure_url: "https://google.com/".to_string(), 
-                pending_url: "https://google.com/".to_string(), 
+                success_url: return_url.clone(),
+                failure_url: return_url.clone(),
+                pending_url: return_url,
             }),
             ..request_data
         })
