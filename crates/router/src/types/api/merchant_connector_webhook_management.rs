6dfba31039cab8c298f6ac346cb45ec7dba94f05
AkshayaFoiger <akshaya.shankar@juspay.in>
2026-01-05 16:50:27 +0530

    fix clippy

--- crates/router/src/types/api/merchant_connector_webhook_management.rs
+++ crates/router/src/types/api/merchant_connector_webhook_management.rs
@@ -1,5 +1,5 @@
 #[cfg(feature = "v1")]
 pub use api_models::merchant_connector_webhook_management::{
-    ConnectorWebhookRegisterRequest, RegisterConnectorWebhookResponse,
+    ConnectorWebhookListResponse, ConnectorWebhookRegisterRequest, RegisterConnectorWebhookResponse,
 };
 pub use hyperswitch_domain_models::router_flow_types::merchant_connector_webhook_management::ConnectorWebhookRegister;
