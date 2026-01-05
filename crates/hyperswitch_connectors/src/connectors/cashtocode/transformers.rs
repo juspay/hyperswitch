e882fc0d062be618e80ffbb1dca909c3caa102ff
Aishwariyaa Anand <124241367+Aishwariyaa-Anand@users.noreply.github.com>
2026-01-05 21:26:24 +0530

    feat(core): add connector_response_reference_id for ErrorResponse (#10816)

--- crates/hyperswitch_connectors/src/connectors/cashtocode/transformers.rs
+++ crates/hyperswitch_connectors/src/connectors/cashtocode/transformers.rs
@@ -241,6 +241,7 @@ impl TryFrom<PaymentsResponseRouterData<CashtocodePaymentsResponse>>
                     reason: Some(error_data.error_description),
                     attempt_status: None,
                     connector_transaction_id: None,
+                    connector_response_reference_id: None,
                     network_advice_code: None,
                     network_decline_code: None,
                     network_error_message: None,
