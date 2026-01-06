8f6c08ecda124e7cc9271982e6c78ec0dd081738
Shivansh Mathur <shivansh.mathur@juspay.in>
2026-01-06 21:58:18 +0530

    removing early return

--- crates/router/src/core/payment_methods.rs
+++ crates/router/src/core/payment_methods.rs
@@ -1103,18 +1103,15 @@ pub async fn create_payment_method_card_core(
         Encryptable<hyperswitch_domain_models::address::Address>,
     >,
 ) -> RouterResult<(api::PaymentMethodResponse, domain::PaymentMethod)> {
-    let card_data = req
-        .payment_method_data
-        .as_ref()
-        .and_then(|data| match data {
-            api::PaymentMethodCreateData::Card(card) => Some(card),
-            _ => None,
-        })
-        .ok_or(errors::ApiErrorResponse::InvalidDataValue {
-            field_name: "payment_method_data",
-        })?;
 
-    payments_core::helpers::validate_card_expiry( &card_data.card_exp_month, &card_data.card_exp_year, )?;
+    match req.payment_method_data {
+        api::PaymentMethodCreateData::Card(ref card_data) => {
+            payments_core::helpers::validate_card_expiry(&card_data.card_exp_month, &card_data.card_exp_year)?;
+        }
+        ref other => {
+            logger::warn!("Payment method data is not CardDetail, got: {:?}", other);
+        }
+    }
 
     let db = &*state.store;
 
