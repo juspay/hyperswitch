53fe857753930045566d22ff3c10f11a2cf7fea7
Aniket Burman <aniket.burman@Aniket-Burman-JDXHW2PH34.local>
2026-01-07 19:27:38 +0530

    error

--- cypress-tests/cypress/e2e/spec/Payment/00038-PaymentWebhookFixture.cy.js
+++ cypress-tests/cypress/e2e/spec/Payment/00038-PaymentWebhookFixture.cy.js
@@ -8,30 +8,29 @@ let connector;
 let expected_intent_status;
 
 describe("Payment Webhook Tests", () => {
-  before("seed global state", () => {
-    let skip = true;
+  before("seed global state", function () {
+  let skip = true;
 
-    cy.task("getGlobalState")
-      .then((state) => {
-        globalState = new State(state);
-        connector = globalState.get("connectorId");
+  cy.task("getGlobalState")
+    .then((state) => {
+      globalState = new State(state);
+      connector = globalState.get("connectorId");
 
-        // Continue running test against a connector that is added in the include list
-        if (
-          utils.shouldIncludeConnector(
-            connector,
-            utils.CONNECTOR_LISTS.INCLUDE.PAYMENTS_WEBHOOK
-          )
-        ) {
-          skip = false;
-        }
-      })
-      .then(() => {
-        if (!skip) {
-          this.skip();
-        }
-      });
-  });
+      if (
+        utils.shouldIncludeConnector(
+          connector,
+          utils.CONNECTOR_LISTS.INCLUDE.PAYMENTS_WEBHOOK
+        )
+      ) {
+        skip = false;
+      }
+    })
+    .then(() => {
+      if (!skip) {
+        this.skip();
+      }
+    });
+});
 
   after("flush global state", () => {
     cy.task("setGlobalState", globalState.data);
