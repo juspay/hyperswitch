b7e837ebd175a8f06a17110a08616886bc31a66c
Ayush <114248859+ayush22667@users.noreply.github.com>
2026-01-07 19:34:50 +0530

    feat(gsm): Update unified code and message for card decline errors in Stripe tests

--- cypress-tests/cypress/e2e/configs/Payment/Stripe.js
+++ cypress-tests/cypress/e2e/configs/Payment/Stripe.js
@@ -296,8 +296,8 @@ export const connectorDetails = {
           error_code: "card_declined",
           error_message:
             "message - Your card was declined., decline_code - generic_decline",
-          unified_code: "UE_3000",
-          unified_message: "Technical issue with PSP",
+          unified_code: "UE_1000",
+          unified_message: "Issue with Payment Method details",
         },
       },
     },
