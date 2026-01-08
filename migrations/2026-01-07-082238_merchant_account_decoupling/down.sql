64cfe0434aafb35198e1b095cf3a36866d3790d8
kanikabansal-juspay <kanika.bansal@juspay.in>
2026-01-08 17:46:33 +0530

    fix: address comments

--- migrations/2026-01-07-082238_merchant_account_decoupling/down.sql
+++ migrations/2026-01-07-082238_merchant_account_decoupling/down.sql
@@ -1,5 +1,8 @@
 -- This file should undo anything in `up.sql`
 -- NOTE: The order of these statements is important and must be preserved
+
+-- Roles with account_manage, but without webhooks_manage and api_keys_manage,
+-- should move back to NOT having account_manage.
 UPDATE roles
 SET groups = array_remove(groups, 'account_manage')
 WHERE
@@ -9,6 +12,8 @@ WHERE
     OR 'api_keys_manage' = ANY (groups)
   );
 
+-- Roles with account_view, but without webhooks_view and api_keys_view,
+-- should move back to NOT having account_view.
 UPDATE roles
 SET groups = array_remove(groups, 'account_view')
 WHERE
@@ -18,6 +23,8 @@ WHERE
     OR 'api_keys_view' = ANY (groups)
   );
 
+-- Roles with account_manage, webhooks_manage and api_keys_manage
+-- should retain only account_manage.
 UPDATE roles
 SET groups = array_remove(
                array_remove(groups, 'webhooks_manage'),
@@ -28,6 +35,8 @@ WHERE
   AND 'webhooks_manage' = ANY (groups)
   AND 'api_keys_manage' = ANY (groups);
 
+-- Roles with account_view, webhooks_view and api_keys_view
+-- should retain only account_view.
 UPDATE roles
 SET groups = array_remove(
                array_remove(groups, 'webhooks_view'),
--- migrations/2026-01-07-082238_merchant_account_decoupling/up.sql
+++ migrations/2026-01-07-082238_merchant_account_decoupling/up.sql
@@ -1,14 +1,20 @@
 -- Your SQL goes here
 -- NOTE: The order of these statements is important and must be preserved
+
+-- Roles with account_manage must also manage webhooks and API keys
 UPDATE roles
 SET groups = array_cat(groups, ARRAY['webhooks_manage', 'api_keys_manage'])
 WHERE 'account_manage' = ANY(groups);
 
+-- Roles with account_view (but not account_manage)
+-- must also have view access to webhooks and API keys
 UPDATE roles
 SET groups = array_cat(groups, ARRAY['webhooks_view', 'api_keys_view'])
 WHERE 'account_view' = ANY(groups)
   AND NOT 'account_manage' = ANY(groups);
 
+-- Roles with ANY *_manage permission
+-- but without account_manage, must explicitly have account_manage
 UPDATE roles
 SET groups = array_append(groups, 'account_manage')
 WHERE
@@ -19,17 +25,7 @@ WHERE
     WHERE g LIKE '%_manage'
   );
 
+-- Roles without account_view must explicitly have account_view
 UPDATE roles
 SET groups = array_append(groups, 'account_view')
-WHERE
-  'account_view' <> ALL (groups)
-  AND NOT EXISTS (
-    SELECT 1
-    FROM unnest(groups) AS g
-    WHERE g LIKE '%_manage'
-  )
-  AND EXISTS (
-    SELECT 1
-    FROM unnest(groups) AS g
-    WHERE g LIKE '%_view'
-  );
+WHERE 'account_view' <> ALL (groups);
