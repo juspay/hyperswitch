800dfb07fe0ee1b9e77debf28dc606c636f46c7f
kanikabansal-juspay <kanika.bansal@juspay.in>
2026-01-13 16:18:43 +0530

    fix: address comments

--- crates/router/src/routes/app.rs
+++ crates/router/src/routes/app.rs
@@ -2686,7 +2686,7 @@ impl User {
                 .route(web::post().to(user::user_merchant_account_create)),
         );
         route = route.service(
-            web::resource("/merchant_details")
+            web::resource("/merchant-details")
                 .route(web::get().to(user::retrieve_merchant_account_details)),
         );
         route = route.service(
