33150707da03383784a2559665ca9fb21d70a372
Apoorv Dixit <apoorv.dixit@juspay.in>
2026-01-23 02:46:06 +0530

    fix: add index concurrently

--- migrations/2026-01-20-095924_add_new_index_for_payment_intent_and_attempt_for_payment_id_processor_merchant_id/down.sql
+++
@@ -1,4 +0,0 @@
--- This file should undo anything in `up.sql`
-DROP INDEX IF EXISTS payment_intent_processor_merchant_id_payment_id_index;
-
-DROP INDEX IF EXISTS payment_attempt_processor_merchant_id_payment_id_index;
