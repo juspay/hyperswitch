fabbc13249d7b8b56beb3a83f9a2278358e2af32
Amitsingh Tanwar <amitsingh.tanwar@juspay.in>
2026-01-22 16:55:44 +0530

    chore: code refinement

--- crates/router/src/core/unified_connector_service/transformers.rs
+++ crates/router/src/core/unified_connector_service/transformers.rs
@@ -4646,6 +4646,12 @@ impl transformers::ForeignTryFrom<payments_grpc::PaymentServiceAuthenticateRespo
                         payments_grpc::identifier::IdType::NoResponseIdMarker(_) => None,
                     })
             });
+        let authentication_data = response
+            .authentication_data
+            .clone()
+            .map(router_request_types::UcsAuthenticationData::foreign_try_from)
+            .transpose()?
+            .map(Box::new);
 
         let resource_id: router_request_types::ResponseId = match response
             .transaction_id
@@ -4725,7 +4731,7 @@ impl transformers::ForeignTryFrom<payments_grpc::PaymentServiceAuthenticateRespo
                     network_txn_id: response.network_txn_id.clone(),
                     connector_response_reference_id,
                     incremental_authorization_allowed: None,
-                    authentication_data: None,
+                    authentication_data,
                     charges: None,
                 },
                 status,
