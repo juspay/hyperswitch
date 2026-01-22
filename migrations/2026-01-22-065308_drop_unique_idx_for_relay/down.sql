142a77f550ac16853f9be75f7e7daae653ebd1b6
Sk Sakil Mostak <skmahim71@gmail.com>
2026-01-22 21:14:48 +0530

    refactor: remove migration

--- migrations/2026-01-22-065308_drop_unique_idx_for_relay/down.sql
+++
@@ -1,2 +0,0 @@
--- This file should undo anything in `up.sql`
-CREATE UNIQUE INDEX relay_profile_id_connector_reference_id_index ON relay USING btree (profile_id, connector_reference_id);
--- migrations/2026-01-22-065308_drop_unique_idx_for_relay/up.sql
+++
@@ -1,2 +0,0 @@
--- Drop the unique constraint over connector_reference_id to allow multiple capture for relay
-DROP INDEX relay_profile_id_connector_reference_id_index;
