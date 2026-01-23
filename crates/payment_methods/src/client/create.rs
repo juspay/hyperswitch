affcf2fe99db7f35b2c0ad1cfa71c84169825798
Prasunna Soppa <prasunna.soppa@juspay.in>
2026-01-23 13:06:30 +0530

    code refactoring

--- crates/payment_methods/src/client/create.rs
+++ crates/payment_methods/src/client/create.rs
@@ -1,9 +1,15 @@
 //! Create payment method flow types and dummy models.
 
-use common_utils::request::{Method, RequestContent};
-use hyperswitch_interfaces::micro_service::MicroserviceClientError;
-use serde::Deserialize;
-use serde_json::Value;
+use api_models::payments;
+use common_utils::{
+    id_type, pii,
+    request::{Method, RequestContent},
+    types::MinorUnit,
+};
+use hyperswitch_interfaces::micro_service::{MicroserviceClientError, MicroserviceClientErrorKind};
+use masking::Secret;
+use serde::{Deserialize, Serialize};
+use time::PrimitiveDateTime;
 
 /// V1-facing create flow type.
 #[derive(Debug)]
@@ -15,8 +21,8 @@ pub struct CreatePaymentMethodV1Request {
     pub payment_method: common_enums::PaymentMethod,
     pub payment_method_type: common_enums::PaymentMethodType,
     pub metadata: Option<pii::SecretSerdeValue>,
-    pub customer_id: id_types::CustomerId, // Payment method data will be saved when customer acceptance is given, hence customer id will always be present
-    pub payment_method_data: api_models::payments::PaymentMethodData,
+    pub customer_id: id_type::CustomerId, // Payment method data will be saved when customer acceptance is given, hence customer id will always be present
+    pub payment_method_data: payments::PaymentMethodData,
     pub billing: Option<payments::Address>,
     pub network_tokenization: Option<common_types::payment_methods::NetworkTokenization>,
     pub storage_type: Option<common_enums::StorageType>,
@@ -27,7 +33,7 @@ pub struct ModularPMCreateRequest {
     pub payment_method_type: common_enums::PaymentMethod,
     pub payment_method_subtype: common_enums::PaymentMethodType,
     pub metadata: Option<pii::SecretSerdeValue>,
-    pub customer_id: id_types::CustomerId, // Payment method data will be saved when customer acceptance is given, hence customer id will always be present
+    pub customer_id: id_type::CustomerId, // Payment method data will be saved when customer acceptance is given, hence customer id will always be present
     pub payment_method_data: PaymentMethodCreateData,
     pub billing: Option<payments::Address>,
     pub psp_tokenization: Option<common_types::payment_methods::PspTokenization>,
@@ -37,7 +43,7 @@ pub struct ModularPMCreateRequest {
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum PaymentMethodCreateData {
-    Card(pm_api_models::CardDetail),
+    Card(api_models::payment_methods::CardDetail),
 }
 
 #[derive(Clone, Debug, Deserialize)]
@@ -45,17 +51,21 @@ pub struct ModularPMCreateResponse {
     //payment method id
     pub id: String,
     pub merchant_id: id_type::MerchantId,
-    pub customer_id: Option<CustomerId>,
+    pub customer_id: Option<id_type::CustomerId>,
     pub payment_method_type: Option<common_enums::PaymentMethod>,
     pub payment_method_subtype: Option<common_enums::PaymentMethodType>,
     pub recurring_enabled: Option<bool>,
-    pub created: Option<time::PrimitiveDateTime>,
-    pub last_used_at: Option<time::PrimitiveDateTime>,
+    pub created: Option<PrimitiveDateTime>,
+    pub last_used_at: Option<PrimitiveDateTime>,
     pub payment_method_data: Option<PaymentMethodResponseData>,
     pub connector_tokens: Option<Vec<ConnectorTokenDetails>>,
-    pub network_token: Option<pm_api_models::NetworkTokenResponse>,
+    pub network_token: Option<api_models::payment_methods::NetworkTokenResponse>,
     pub storage_type: Option<common_enums::StorageType>,
-    pub card_cvc_token_storage: Option<CardCVCTokenStorageDetails>,
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize, Clone)]
+pub enum PaymentMethodResponseData {
+    Card(api_models::payment_methods::CardDetailFromLocker),
 }
 
 #[derive(Clone, Debug)]
@@ -63,41 +73,53 @@ pub struct CreatePaymentMethodResponse {
     //payment method id
     pub payment_method_id: String,
     pub merchant_id: id_type::MerchantId,
-    pub customer_id: Option<CustomerId>,
+    pub customer_id: Option<id_type::CustomerId>,
     pub payment_method_type: Option<common_enums::PaymentMethod>,
     pub payment_method_subtype: Option<common_enums::PaymentMethodType>,
     pub recurring_enabled: Option<bool>,
-    pub created: Option<time::PrimitiveDateTime>,
-    pub last_used_at: Option<time::PrimitiveDateTime>,
+    pub created: Option<PrimitiveDateTime>,
+    pub last_used_at: Option<PrimitiveDateTime>,
     pub payment_method_data: Option<PaymentMethodResponseData>,
     pub connector_tokens: Option<Vec<ConnectorTokenDetails>>,
-    pub network_token: Option<pm_api_models::NetworkTokenResponse>
+    pub network_token: Option<api_models::payment_methods::NetworkTokenResponse>,
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ConnectorTokenDetails {
+    pub connector_id: id_type::MerchantConnectorAccountId,
+    pub token_type: common_enums::TokenizationType,
+    pub status: common_enums::ConnectorTokenStatus,
+    pub connector_token_request_reference_id: Option<String>,
+    pub original_payment_authorized_amount: Option<MinorUnit>,
+    pub original_payment_authorized_currency: Option<common_enums::Currency>,
+    pub metadata: Option<pii::SecretSerdeValue>,
+    pub token: Secret<String>,
+}
 
-impl TryFrom<api_models::payments::PaymentMethodData> for PaymentMethodCreateData {
-    type Error = errors::ApiErrorResponse;
+impl TryFrom<payments::PaymentMethodData> for PaymentMethodCreateData {
+    type Error = MicroserviceClientError;
 
-    fn try_from(value: api_models::payments::PaymentMethodData) -> Result<Self, Self::Error> {
+    fn try_from(value: payments::PaymentMethodData) -> Result<Self, Self::Error> {
         match value {
-            api_models::payments::PaymentMethodData::Card(card) => {
-                let card_detail = pm_api_models::CardDetail {
+            payments::PaymentMethodData::Card(card) => {
+                let card_detail = api_models::payment_methods::CardDetail {
                     card_number: card.card_number,
                     card_exp_month: card.card_exp_month,
                     card_exp_year: card.card_exp_year,
                     card_holder_name: card.card_holder_name,
                     nick_name: card.nick_name,
                     card_issuing_country: card.card_issuing_country,
-                    card_network: None,
+                    card_network: card.card_network,
                     card_issuer: card.card_issuer,
                     card_type: None,
-                    card_cvc: None,
+                    card_cvc: Some(card.card_cvc),
                     card_issuing_country_code: card.card_issuing_country_code,
                 };
                 Ok(PaymentMethodCreateData::Card(card_detail))
             }
-            _ => Err(errors::ApiErrorResponse::NotSupported {
-                message: "Unsupported payment method type for modular PM creation".to_string(),
+            _ => Err(MicroserviceClientError {
+                operation: "CreatePaymentMethodV1Request to ModularPMCreateRequest".to_string(),
+                kind: MicroserviceClientErrorKind::InvalidRequest("Unsupported payment method type for modular PM creation".to_string()),
             }),
         }
     }
@@ -107,10 +129,10 @@ impl TryFrom<&CreatePaymentMethodV1Request> for ModularPMCreateRequest {
     type Error = MicroserviceClientError;
 
     fn try_from(request: &CreatePaymentMethodV1Request) -> Result<Self, Self::Error> {
-        let payment_method_data = PaymentMethodCreateData::try_from(payment_method_data)?;
+        let payment_method_data = PaymentMethodCreateData::try_from(request.payment_method_data.clone())?;
         Ok(Self {
-            payment_method: request.payment_method,
-            payment_method_type: request.payment_method_type,
+            payment_method_type: request.payment_method,
+            payment_method_subtype: request.payment_method_type,
             metadata: request.metadata.clone(),
             customer_id: request.customer_id.clone(),
             payment_method_data,
@@ -144,7 +166,7 @@ impl TryFrom<ModularPMCreateResponse> for CreatePaymentMethodResponse {
 
 impl CreatePaymentMethod {
     fn build_body(&self, request: ModularPMCreateRequest) -> Option<RequestContent> {
-        Some(RequestContent::Json(Box::new(request.payload)))
+        Some(RequestContent::Json(Box::new(request)))
     }
 }
 
