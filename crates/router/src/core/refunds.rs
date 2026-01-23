d69b8fd8ca4cf2a088366aa32315fc65a70cbfa0
AkshayaFoiger <akshaya.shankar@juspay.in>
2026-01-23 15:27:04 +0530

    add ? operator to prevent_refund_after_post_capture_void

--- crates/router/src/core/refunds.rs
+++ crates/router/src/core/refunds.rs
@@ -94,7 +94,7 @@ pub async fn refund_create_core(
                 .attach_printable("refund amount validation against payment intent failed")
         })?;
 
-    payment_intent.prevent_refund_after_post_capture_void();
+    payment_intent.prevent_refund_after_post_capture_void()?;
 
     // Amount is not passed in request refer from payment intent.
     amount = req
