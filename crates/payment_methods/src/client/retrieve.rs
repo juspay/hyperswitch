59506889914076154ed30bd8d84fe6993c30d877
Gaurav Rawat <104276743+GauravRawat369@users.noreply.github.com>
2026-01-26 16:05:21 +0530

    refractor(retrieve): resolve comments

--- crates/payment_methods/src/client/retrieve.rs
+++ crates/payment_methods/src/client/retrieve.rs
@@ -1,16 +1,14 @@
 //! Retrieve payment method flow types and models.
 
 use api_models::payment_methods::{
-    CardDetailFromLocker, PaymentMethodId, PaymentMethodResponse as RetrievePaymentMethodResponse,
+    PaymentMethodId, PaymentMethodResponse as RetrievePaymentMethodResponse,
 };
-use cards::CardNumber;
-use common_enums::{PaymentMethod, PaymentMethodType};
-use common_utils::{id_type, pii, request::Method};
-use hyperswitch_domain_models::payment_method_data::NetworkTokenDetailsPaymentMethod;
+use common_utils::request::Method;
 use hyperswitch_interfaces::micro_service::{MicroserviceClientError, MicroserviceClientErrorKind};
-use serde::Deserialize;
-use time;
 
+use crate::types::{
+    ModularPMRetrieveResponse, ModularPMRetrieveResquest, PaymentMethodResponseData,
+};
 /// V1-facing retrieve flow type.
 #[derive(Debug)]
 pub struct RetrievePaymentMethod;
@@ -19,89 +17,6 @@ pub struct RetrievePaymentMethod;
 #[derive(Debug)]
 pub struct RetrievePaymentMethodV1Request {
     pub payment_method_id: PaymentMethodId,
-}
-
-/// V2 modular service request payload.
-#[derive(Clone, Debug)]
-pub struct ModularPMRetrieveResquest {
-    pub payment_method_id: PaymentMethodId,
-}
-
-/// V2 PaymentMethodResponse as returned by the V2 API.
-/// This is a copy of the V2 PaymentMethodResponse struct from api_models for use in V1-only builds.
-#[derive(Clone, Debug, Deserialize)]
-pub struct ModularPMRetrieveResponse {
-    pub id: String,
-    pub merchant_id: id_type::MerchantId,
-    pub customer_id: Option<id_type::CustomerId>,
-    pub payment_method_type: Option<PaymentMethod>,
-    pub payment_method_subtype: Option<PaymentMethodType>,
-    pub recurring_enabled: Option<bool>,
-    #[serde(default, with = "common_utils::custom_serde::iso8601::option")]
-    pub created: Option<time::PrimitiveDateTime>,
-    #[serde(default, with = "common_utils::custom_serde::iso8601::option")]
-    pub last_used_at: Option<time::PrimitiveDateTime>,
-    pub payment_method_data: Option<PaymentMethodResponseData>,
-    pub connector_tokens: Option<Vec<ConnectorTokenDetails>>,
-    pub network_token: Option<NetworkTokenResponse>,
-    pub storage_type: Option<common_enums::StorageType>,
-    pub card_cvc_token_storage: Option<CardCVCTokenStorageDetails>,
-}
-
-/// V2 ConnectorTokenDetails (for deserialization, ignored in transformation)
-#[derive(Clone, Debug, Deserialize)]
-pub struct ConnectorTokenDetails {
-    pub connector_id: id_type::MerchantConnectorAccountId,
-    pub token_type: common_enums::TokenizationType,
-    pub status: common_enums::ConnectorTokenStatus,
-    pub connector_token_request_reference_id: Option<String>,
-    pub original_payment_authorized_amount: Option<common_utils::types::MinorUnit>,
-    pub original_payment_authorized_currency: Option<common_enums::Currency>,
-    pub metadata: Option<pii::SecretSerdeValue>,
-    pub token: masking::Secret<String>,
-}
-
-/// V2 NetworkTokenResponse (for deserialization, ignored in transformation)
-#[derive(Clone, Debug, Deserialize)]
-pub struct NetworkTokenResponse {
-    pub payment_method_data: NetworkTokenDetailsPaymentMethod,
-}
-
-/// V2 CardCVCTokenStorageDetails (for deserialization, ignored in transformation)
-#[derive(Clone, Debug, Deserialize)]
-pub struct CardCVCTokenStorageDetails {
-    pub is_stored: bool,
-
-    #[serde(default, with = "common_utils::custom_serde::iso8601::option")]
-    pub expires_at: Option<time::PrimitiveDateTime>,
-}
-
-/// V2 PaymentMethodResponseData enum
-#[derive(Clone, Debug, Deserialize)]
-#[serde(deny_unknown_fields)]
-#[serde(rename_all = "snake_case")]
-#[serde(rename = "payment_method_data")]
-pub enum PaymentMethodResponseData {
-    Card(CardDetailFromLockerV2),
-}
-
-/// V2 CardDetailFromLocker for deserialization
-#[derive(Clone, Debug, Deserialize)]
-pub struct CardDetailFromLockerV2 {
-    pub issuer_country: Option<common_enums::CountryAlpha2>,
-    pub last4_digits: Option<String>,
-    #[serde(skip)]
-    pub card_number: Option<CardNumber>,
-    pub expiry_month: Option<masking::Secret<String>>,
-    pub expiry_year: Option<masking::Secret<String>>,
-    pub card_holder_name: Option<masking::Secret<String>>,
-    pub card_fingerprint: Option<masking::Secret<String>>,
-    pub nick_name: Option<masking::Secret<String>>,
-    pub card_network: Option<common_enums::CardNetwork>,
-    pub card_isin: Option<String>,
-    pub card_issuer: Option<String>,
-    pub card_type: Option<String>,
-    pub saved_to_locker: bool,
 }
 
 impl TryFrom<&RetrievePaymentMethodV1Request> for ModularPMRetrieveResquest {
@@ -125,25 +40,8 @@ impl TryFrom<ModularPMRetrieveResponse> for RetrievePaymentMethodResponse {
         let customer_id = v2_resp.customer_id;
 
         // Convert card details from V2 to V1 format
-        let card = v2_resp.payment_method_data.map(|pmd| match pmd {
-            PaymentMethodResponseData::Card(v2_card) => CardDetailFromLocker {
-                scheme: None,
-                issuer_country: v2_card.issuer_country.map(|c| c.to_string()),
-                issuer_country_code: None,
-                last4_digits: v2_card.last4_digits,
-                card_number: v2_card.card_number,
-                expiry_month: v2_card.expiry_month,
-                expiry_year: v2_card.expiry_year,
-                card_token: None,
-                card_holder_name: v2_card.card_holder_name,
-                card_fingerprint: v2_card.card_fingerprint,
-                nick_name: v2_card.nick_name,
-                card_network: v2_card.card_network,
-                card_isin: v2_card.card_isin,
-                card_issuer: v2_card.card_issuer,
-                card_type: v2_card.card_type,
-                saved_to_locker: v2_card.saved_to_locker,
-            },
+        let card = v2_resp.payment_method_data.map(|data| match data {
+            PaymentMethodResponseData::Card(card_detail) => card_detail,
         });
 
         Ok(Self {
