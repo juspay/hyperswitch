3bf271afa128cb7cdd4f759cfa0e45183f8e6487
Shivansh Mathur <shivansh.mathur@juspay.in>
2026-01-29 12:52:49 +0530

    Correcting ddocumentation

--- crates/router/src/configs/defaults.rs
+++ crates/router/src/configs/defaults.rs
@@ -12,7 +12,7 @@ impl Default for super::settings::Server {
             request_body_limit: 16 * 1024, // POST request body is limited to 16KiB
             shutdown_timeout: 30,
             keep_alive: 5,
-            client_request_timeout: 5,
+            client_request_timeout: 5000,
             client_disconnect_timeout: 1000,
             #[cfg(feature = "tls")]
             tls: None,
