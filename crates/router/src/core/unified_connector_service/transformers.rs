6777728813a1e628c1a5e91e52a555dd425b1e76
Shubhodip pal <palshubhodip9@gmail.com>
2026-02-03 13:50:51 +0530

    added redirect data in psync response

--- crates/router/src/core/unified_connector_service/transformers.rs
+++ crates/router/src/core/unified_connector_service/transformers.rs
@@ -12,7 +12,6 @@ use api_models::payments::{
 use common_enums::{AttemptStatus, AuthenticationType, AuthorizationStatus, RefundStatus};
 use common_utils::{
     ext_traits::Encode,
-    request::Method,
     types::{self, AmountConvertor, MinorUnit, StringMajorUnitForConnector},
 };
 use diesel_models::enums as storage_enums;
@@ -173,6 +172,7 @@ impl
             metadata: None,
             connector_metadata: None,
             return_url: router_data.request.router_return_url.clone(),
+            test_mode: None,
         })
     }
 }
@@ -365,6 +365,9 @@ impl
                 .tokenization
                 .map(payments_grpc::Tokenization::foreign_from)
                 .map(Into::into),
+            continue_redirection_url: None,
+            threeds_completion_indicator: None,
+            redirection_response: None,
         })
     }
 }
@@ -532,6 +535,9 @@ impl
                 .tokenization
                 .map(payments_grpc::Tokenization::foreign_from)
                 .map(Into::into),
+            continue_redirection_url: None,
+            threeds_completion_indicator: None,
+            redirection_response: None,
         })
     }
 }
@@ -727,6 +733,7 @@ impl
             ),
             merchant_account_metadata: None,
             metadata: None,
+            test_mode: router_data.test_mode,
         })
     }
 }
@@ -856,6 +863,7 @@ impl
                 .map(payments_grpc::BrowserInformation::foreign_try_from)
                 .transpose()?,
             connector_metadata: None,
+            capture_method: None,
         })
     }
 }
@@ -1029,6 +1037,7 @@ impl
                 .map(payments_grpc::BrowserInformation::foreign_try_from)
                 .transpose()?,
             connector_metadata: None,
+            capture_method: None,
         })
     }
 }
@@ -1113,6 +1122,7 @@ impl transformers::ForeignTryFrom<&RouterData<Capture, PaymentsCaptureData, Paym
                 .change_context(UnifiedConnectorServiceError::RequestEncodingFailed)?
                 .map(|s| s.into()),
             merchant_account_metadata,
+            test_mode: router_data.test_mode,
         })
     }
 }
@@ -3628,53 +3638,6 @@ impl transformers::ForeignTryFrom<router_request_types::UcsAuthenticationData>
     }
 }
 
-impl transformers::ForeignTryFrom<payments_grpc::RedirectForm> for RedirectForm {
-    type Error = error_stack::Report<UnifiedConnectorServiceError>;
-
-    fn foreign_try_from(value: payments_grpc::RedirectForm) -> Result<Self, Self::Error> {
-        match value.form_type {
-            Some(payments_grpc::redirect_form::FormType::Form(form)) => Ok(Self::Form {
-                endpoint: form.clone().endpoint,
-                method: Method::foreign_try_from(form.clone().method())?,
-                form_fields: form.clone().form_fields,
-            }),
-            Some(payments_grpc::redirect_form::FormType::Html(html)) => Ok(Self::Html {
-                html_data: html.html_data,
-            }),
-            Some(payments_grpc::redirect_form::FormType::Uri(_)) => Err(
-                UnifiedConnectorServiceError::RequestEncodingFailedWithReason(
-                    "URI form type is not implemented".to_string(),
-                )
-                .into(),
-            ),
-            None => Err(
-                UnifiedConnectorServiceError::RequestEncodingFailedWithReason(
-                    "Missing form type".to_string(),
-                )
-                .into(),
-            ),
-        }
-    }
-}
-
-impl transformers::ForeignTryFrom<payments_grpc::HttpMethod> for Method {
-    type Error = error_stack::Report<UnifiedConnectorServiceError>;
-
-    fn foreign_try_from(value: payments_grpc::HttpMethod) -> Result<Self, Self::Error> {
-        tracing::debug!("Converting gRPC HttpMethod: {:?}", value);
-        match value {
-            payments_grpc::HttpMethod::Get => Ok(Self::Get),
-            payments_grpc::HttpMethod::Post => Ok(Self::Post),
-            payments_grpc::HttpMethod::Put => Ok(Self::Put),
-            payments_grpc::HttpMethod::Delete => Ok(Self::Delete),
-            payments_grpc::HttpMethod::Unspecified => {
-                Err(UnifiedConnectorServiceError::ResponseDeserializationFailed)
-                    .attach_printable("Invalid Http Method")
-            }
-        }
-    }
-}
-
 impl transformers::ForeignTryFrom<storage_enums::FutureUsage> for payments_grpc::FutureUsage {
     type Error = error_stack::Report<UnifiedConnectorServiceError>;
 
@@ -4835,6 +4798,7 @@ impl transformers::ForeignTryFrom<&MandateData> for payments_grpc::SetupMandateD
         Ok(Self {
             update_mandate_id: mandate_data.update_mandate_id.clone(),
             customer_acceptance,
+            mandate_type: None,
         })
     }
 }
@@ -5378,6 +5342,7 @@ impl transformers::ForeignTryFrom<&RouterData<api::Void, PaymentsCancelData, Pay
                 .change_context(UnifiedConnectorServiceError::RequestEncodingFailed)?
                 .map(|s| s.into()),
             merchant_account_metadata,
+            test_mode: router_data.test_mode,
         })
     }
 }
