4ce1359f50ef10a8c5cebc6096e766aedf155937
vasanthp-jus <vasanth.p@juspay.in>
2026-02-05 18:36:39 +0530

    ci(cypress): added Calida connector

--- cypress-tests/cypress/e2e/configs/Payment/Commons.js
+++ cypress-tests/cypress/e2e/configs/Payment/Commons.js
@@ -418,6 +418,14 @@ export const payment_methods_enabled = [
         payment_experience: "invoke_sdk_client",
       },
       {
+        payment_method_type: "bluecode",
+        payment_experience: "redirect_to_url",
+        minimum_amount: 1,
+        maximum_amount: 68607706,
+        recurring_enabled: false,
+        installment_payment_enabled: false,
+      },
+      {
         payment_method_type: "skrill",
         payment_experience: "redirect_to_url",
         minimum_amount: 1,
@@ -778,6 +786,24 @@ export const connectorDetails = {
             country: "PL",
             first_name: "john",
             last_name: "doe",
+          },
+        },
+      },
+    }),
+    Bluecode: getCustomExchange({
+      Request: {
+        payment_method: "wallet",
+        payment_method_type: "bluecode",
+        payment_method_data: {
+          wallet: {
+            bluecode_redirect: {},
+          },
+        },
+        billing: {
+          ...standardBillingAddress,
+          address: {
+            ...standardBillingAddress.address,
+            country: "AT",
           },
         },
       },
@@ -1541,7 +1567,7 @@ export const connectorDetails = {
           card: {
             card_number: "4242424242424242",
             card_exp_month: "01",
-            card_exp_year: "2026",
+            card_exp_year: "2035",
             card_holder_name: "joseph Doe",
             card_cvc: "123",
           },
@@ -1568,7 +1594,7 @@ export const connectorDetails = {
           card: {
             card_number: "4242424242424242",
             card_exp_month: "01",
-            card_exp_year: "2026",
+            card_exp_year: "2035",
             card_holder_name: "joseph Doe",
             card_cvc: "123",
           },
