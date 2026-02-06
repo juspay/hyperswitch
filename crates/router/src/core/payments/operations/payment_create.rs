edeeefabfd7d4e5fa670142909c2f30267803c0b
Prasunna Soppa <prasunna.soppa@juspay.in>
2026-02-06 13:43:44 +0530

    fix billing address fetch in payments flow

--- crates/router/src/core/payments/operations/payment_create.rs
+++ crates/router/src/core/payments/operations/payment_create.rs
@@ -1,7 +1,7 @@
 use std::marker::PhantomData;
 
 use api_models::{
-    enums::FrmSuggestion, mandates::RecurringDetails, payment_methods::PaymentMethodsData,
+    enums::FrmSuggestion, mandates::RecurringDetails, payment_methods::PaymentMethodsData, payments::GetAddressFromPaymentMethodData,
 };
 use async_trait::async_trait;
 use common_types::payments as common_payments_types;
@@ -574,8 +574,11 @@ impl<F: Send + Clone + Sync> GetTracker<F, PaymentData<F>, api::PaymentsRequest>
         let payment_method_data_billing = request
             .payment_method_data
             .as_ref()
-            .and_then(|pmd| pmd.billing.clone())
-            .or(payment_method_with_raw_data.as_ref().and_then(|pm| {
+            .and_then(|pmd| pmd.payment_method_data.as_ref())
+            .and_then(|payment_method_data_billing| {
+                payment_method_data_billing.get_billing_address()
+            }).map(From::from);
+        let pm_pmd_billing = payment_method_with_raw_data.as_ref().and_then(|pm| {
                 pm.payment_method
                     .0
                     .payment_method_billing_address
@@ -583,13 +586,15 @@ impl<F: Send + Clone + Sync> GetTracker<F, PaymentData<F>, api::PaymentsRequest>
                     .map(|decrypted_data| decrypted_data.into_inner().expose())
                     .and_then(|decrypted_value| {
                         decrypted_value
-                            .parse_value("payment method billing address")
+                            .parse_value::<hyperswitch_domain_models::address::Address>("payment method billing address")
                             .ok()
                     })
-            }));
+            }).map(From::from);
+
+        let add =payment_method_data_billing.or(pm_pmd_billing); 
 
         let unified_address = address
-            .unify_with_payment_method_data_billing(payment_method_data_billing.map(From::from));
+            .unify_with_payment_method_data_billing(add);
 
         let payment_data = PaymentData {
             flow: PhantomData,
