877e9b3ba984b4c255558b89fe97371eef945dfc
Prasunna Soppa <prasunna.soppa@juspay.in>
2026-03-11 19:00:08 +0530

    fix external vault retrieve flow

--- crates/router/src/core/payment_methods.rs
+++ crates/router/src/core/payment_methods.rs
@@ -3402,7 +3402,7 @@ pub async fn vault_payment_method_external_v1(
     access_token::create_access_token(
         state,
         &connector_data,
-        merchant_account,
+        merchant_account.get_id(),
         &mut old_router_data,
     )
     .await?;
