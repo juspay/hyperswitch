2333348eafb4b167292ebc85095469a251a27371
Sridhar Ratnakumar <sridhar.ratnakumar@juspay.in>
2026-03-17 13:23:03 -0400

    refactor(ci): replace CI formatting job with Nix rustfmt git-hook

--- nix/modules/flake/git-hooks.nix
+++ nix/modules/flake/git-hooks.nix
@@ -3,9 +3,17 @@
   imports = [
     (inputs.git-hooks + /flake-module.nix)
   ];
-  perSystem = { ... }: {
+  perSystem = { pkgs, ... }: {
     pre-commit.settings.hooks = {
       nixpkgs-fmt.enable = true;
+      rustfmt = {
+        enable = true;
+        # Nightly required: .rustfmt.toml uses unstable options (group_imports, imports_granularity)
+        packageOverrides = {
+          cargo = pkgs.rust-bin.nightly.latest.minimal;
+          rustfmt = pkgs.rust-bin.nightly.latest.rustfmt;
+        };
+      };
     };
   };
 }
