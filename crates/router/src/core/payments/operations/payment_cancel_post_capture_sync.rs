4cd2ae2563b8808f9371d16b416b3bedfc138381
AkshayaFoiger <akshaya.shankar@juspay.in>
2026-03-25 13:04:00 +0530

    fix clippy

--- crates/router/src/core/payments/operations/payment_cancel_post_capture_sync.rs
+++ crates/router/src/core/payments/operations/payment_cancel_post_capture_sync.rs
@@ -290,6 +290,7 @@ impl<F: Clone + Send + Sync> Domain<F, api::PaymentsCancelPostCaptureSyncBody, P
         _state: &SessionState,
         _processor: &domain::Processor,
         _payment_data: &mut PaymentData<F>,
+        _business_profile: &domain::Profile,
     ) -> errors::CustomResult<bool, errors::ApiErrorResponse> {
         Ok(false)
     }
