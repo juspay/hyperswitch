8d3be1725475fbcabf50cf9933ff4bb5f15d84bd
awasthi21 <107559116+awasthi21@users.noreply.github.com>
2026-04-07 12:36:31 +0530

    feat(core): Enable config-based UAS eligibility at org and merchant level (#11729)
    Co-authored-by: hyperswitch-bot[bot] <148525504+hyperswitch-bot[bot]@users.noreply.github.com>
    Co-authored-by: likhinbopanna <131246334+likhinbopanna@users.noreply.github.com>

--- crates/router/src/core/payments.rs
+++ crates/router/src/core/payments.rs
@@ -777,6 +777,7 @@ where
 
         let is_eligible_for_uas = helpers::is_merchant_eligible_authentication_service(
             platform.get_processor().get_account().get_id(),
+            platform.get_processor().get_account().get_org_id(),
             state,
         )
         .await?;
@@ -10809,6 +10810,7 @@ pub async fn payment_external_authentication<F: Clone + Sync>(
 
     let authentication_response = if helpers::is_merchant_eligible_authentication_service(
         platform.get_processor().get_account().get_id(),
+        platform.get_processor().get_account().get_org_id(),
         &state,
     )
     .await?
