015ae1f1b4df4cd850fcc45de97724bf46771b25
Prasunna Soppa <prasunna.soppa@juspay.in>
2026-04-22 13:46:56 +0530

    fix ext sdk token flow

--- crates/router/src/core/payments/operations/payment_recurrence.rs
+++ crates/router/src/core/payments/operations/payment_recurrence.rs
@@ -397,6 +397,7 @@ impl<F: Send + Clone + Sync> GetTracker<F, PaymentData<F>, api::PaymentsRequest>
             is_l2_l3_enabled: business_profile.is_l2_l3_enabled,
             external_authentication_data: None,
             client_session_id: None,
+            vault_session_details: None,
         };
 
         let get_trackers_response = operations::GetTrackerResponse {
