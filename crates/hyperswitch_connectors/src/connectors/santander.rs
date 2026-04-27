8c1adba729b86f5b1c297a55a2d77dadd8d61187
Sayak Bhattacharya <sayak.b@juspay.in>
2026-04-27 18:47:09 +0530

    fix: resolved clippy errors

--- crates/hyperswitch_connectors/src/connectors/santander.rs
+++ crates/hyperswitch_connectors/src/connectors/santander.rs
@@ -2106,10 +2106,7 @@ impl ConnectorSpecifications for Santander {
         match current_flow {
             // Journey 2/3/4 CIT
             Some(CurrentFlowInfo::SetupMandate { request_data, .. }) => {
-                match request_data.payment_method_type {
-                    Some(enums::PaymentMethodType::PixAutomaticoQr) => true,
-                    _ => false,
-                }
+                matches!(request_data.payment_method_type, Some(enums::PaymentMethodType::PixAutomaticoQr))
             }
             Some(CurrentFlowInfo::CompleteAuthorize { .. })
             | Some(CurrentFlowInfo::Authorize { .. })
