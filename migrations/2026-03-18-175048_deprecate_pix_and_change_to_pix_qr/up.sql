103e9ba5b148150966e93be8da6673b407195395
Sayak Bhattacharya <sayak.b@juspay.in>
2026-04-27 18:07:39 +0530

    feat(db): rearranging the queries

--- migrations/2026-03-18-175048_deprecate_pix_and_change_to_pix_qr/up.sql
+++ migrations/2026-03-18-175048_deprecate_pix_and_change_to_pix_qr/up.sql
@@ -1,4 +1,14 @@
 -- Your SQL goes here
+UPDATE payment_attempt
+SET payment_method_data = jsonb_set(
+    payment_method_data #- '{bank_transfer,pix}', 
+    '{bank_transfer,pix_qr}', 
+    payment_method_data->'bank_transfer'->'pix',
+    true
+)
+WHERE payment_method_type = 'pix' 
+  AND payment_method_data->'bank_transfer' ? 'pix';
+  
 UPDATE dynamic_routing_stats
 SET payment_method_type = 'pix_qr'
 WHERE payment_method_type = 'pix';
@@ -10,16 +20,6 @@ WHERE payment_method_type = 'pix';
 UPDATE payment_attempt
 SET payment_method_type = 'pix_qr'
 WHERE payment_method_type = 'pix';
-
-UPDATE payment_attempt
-SET payment_method_data = jsonb_set(
-    payment_method_data #- '{bank_transfer,pix}', 
-    '{bank_transfer,pix_qr}', 
-    payment_method_data->'bank_transfer'->'pix',
-    true
-)
-WHERE payment_method_type = 'pix' 
-  AND payment_method_data->'bank_transfer' ? 'pix';
 
 UPDATE payment_attempt
 SET straight_through_algorithm = jsonb_set(
