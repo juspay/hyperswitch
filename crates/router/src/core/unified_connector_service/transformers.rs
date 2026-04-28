bcca48fafec2cd3f40c7cd70c0b4867f5f814754
Sk Sakil Mostak <skmahim71@gmail.com>
2026-04-28 15:57:31 +0530

    chore: add a todo comment

--- crates/router/src/core/unified_connector_service/transformers.rs
+++ crates/router/src/core/unified_connector_service/transformers.rs
@@ -6991,7 +6991,7 @@ impl transformers::ForeignTryFrom<&api_models::payouts::PixBankTransfer>
             bank_account_number: item.bank_account_number.clone(),
             pix_key: item.pix_key.clone(),
             tax_id: item.tax_id.clone(),
-            // pix_emv: item.pix_emv.clone(), to be added in future when UCS supports it
+            // TODO: pix_emv: item.pix_emv.clone(), to be added in future when UCS supports it
         })
     }
 }
