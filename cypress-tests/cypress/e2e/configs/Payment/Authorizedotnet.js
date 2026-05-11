630df0f7f5eba52a286e5050bbf1269654572c21
HS-QUALIBOT <qa-bot@juspay.in>
2026-05-11 15:55:02 +0530

    refactor: remove obsolete response fields from payment connector tests
    Remove setup_future_usage and connector_mandate_id null checks from Adyen and Authorizedotnet payment test configs.

--- cypress-tests/cypress/e2e/configs/Payment/Authorizedotnet.js
+++ cypress-tests/cypress/e2e/configs/Payment/Authorizedotnet.js
@@ -81,8 +81,7 @@ export const connectorDetails = {
         status: 200,
         body: {
           status: "requires_payment_method",
-          setup_future_usage: "on_session",
-          connector_mandate_id: null,
+          
         },
       },
     },
