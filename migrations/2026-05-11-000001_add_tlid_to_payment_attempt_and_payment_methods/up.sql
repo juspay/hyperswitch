d7e3fefc82cb4e86cf8344a10ce3e472e8ee825d
Ayush <114248859+ayush22667@users.noreply.github.com>
2026-05-13 12:17:34 +0530

    feat(schema): remove NULL constraint from network_transaction_link_id in payment methods and attempts

--- migrations/2026-05-11-000001_add_tlid_to_payment_attempt_and_payment_methods/up.sql
+++ migrations/2026-05-11-000001_add_tlid_to_payment_attempt_and_payment_methods/up.sql
@@ -1,5 +1,5 @@
 ALTER TABLE payment_attempt
-    ADD COLUMN IF NOT EXISTS network_transaction_link_id VARCHAR(255) NULL;
+    ADD COLUMN IF NOT EXISTS network_transaction_link_id VARCHAR(255);
 
 ALTER TABLE payment_methods
-    ADD COLUMN IF NOT EXISTS network_transaction_link_id VARCHAR(255) NULL;
+    ADD COLUMN IF NOT EXISTS network_transaction_link_id VARCHAR(255);
