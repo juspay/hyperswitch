462fa21f097517708ef9f0c74b86f5c203026f4c
Vani Gupta <vani.gupta@juspay.in>
2026-05-18 16:18:17 +0530

    fix

--- crates/hyperswitch_domain_models/src/customer.rs
+++ crates/hyperswitch_domain_models/src/customer.rs
@@ -864,12 +864,11 @@ pub async fn update_connector_customer_in_customers(
         .and_then(|connector_customer| connector_customer.clone().expose().as_object().cloned())
         .unwrap_or_default();
 
-    let updated_connector_customer_map =
-        connector_customer_id.as_ref().map(|connector_customer_id| {
-            let connector_customer_value = Value::String(connector_customer_id.clone());
-            connector_customer_map.insert(connector_label.to_string(), connector_customer_value);
-            connector_customer_map
-        });
+    let updated_connector_customer_map = connector_customer_id.map(|connector_customer_id| {
+        let connector_customer_value = Value::String(connector_customer_id);
+        connector_customer_map.insert(connector_label.to_string(), connector_customer_value);
+        connector_customer_map
+    });
 
     updated_connector_customer_map
         .map(Value::Object)
