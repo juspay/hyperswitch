8afeab5671cbfe916e99138688eba59966aff79e
Prasunna Soppa <prasunna.soppa@juspay.in>
2026-06-02 12:20:31 +0530

    fix openapi spec

--- crates/openapi/src/openapi.rs
+++ crates/openapi/src/openapi.rs
@@ -767,6 +767,7 @@ Never share your secret api keys. Keep them guarded and secure.
         api_models::payments::AmazonPayDeliveryPrice,
         api_models::payments::AmazonPayShippingMethod,
         api_models::payments::VaultDetails,
+        api_models::payments::InternalVaultSessionDetails,
         api_models::payment_methods::RequiredFieldInfo,
         api_models::payment_methods::DefaultPaymentMethod,
         api_models::payment_methods::MaskedBankDetails,
