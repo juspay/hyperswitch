cbfdc310ce1ac3da7368221ba594b974c46461a7
hrithikesh026 <hrithikesh.vm@juspay.in>
2026-06-04 12:48:20 +0530

    chore: revert one more update function call change

--- crates/router/src/core/webhooks/incoming.rs
+++ crates/router/src/core/webhooks/incoming.rs
@@ -2771,18 +2771,15 @@ async fn update_connector_mandate_details(
                             };
 
                         state
-                        .store
-                        .update_payment_attempt_with_payment_id_processor_merchant_id_attempt_id(
-                            &payment_attempt.payment_id,
-                            &payment_attempt.processor_merchant_id,
-                            &payment_attempt.attempt_id,
-                            payment_attempt.clone(),
-                            attempt_update,
-                            platform.get_processor().get_account().storage_scheme,
-                            platform.get_processor().get_key_store(),
-                        )
-                        .await
-                        .to_not_found_response(errors::ApiErrorResponse::PaymentNotFound)?;
+                            .store
+                            .update_payment_attempt_with_attempt_id(
+                                payment_attempt.clone(),
+                                attempt_update,
+                                platform.get_processor().get_account().storage_scheme,
+                                platform.get_processor().get_key_store(),
+                            )
+                            .await
+                            .to_not_found_response(errors::ApiErrorResponse::PaymentNotFound)?;
 
                         insert_mandate_details(
                             &payment_attempt,
