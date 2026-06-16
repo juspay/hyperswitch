70ac2892c85d4da58eb784e987e148db3a5f6a4d
hrithikesh026 <hrithikesh.vm@juspay.in>
2026-06-16 13:47:38 +0530

    chore: add coverage of address implementation

--- crates/diesel_models/src/address.rs
+++ crates/diesel_models/src/address.rs
@@ -72,6 +72,7 @@ pub struct EncryptableAddress {
 
 #[derive(Clone, Debug, AsChangeset, router_derive::DebugAsDisplay, Serialize, Deserialize)]
 #[diesel(table_name = address)]
+#[router_derive::apply_changeset(target = Address)]
 pub struct AddressUpdateInternal {
     pub city: Option<String>,
     pub country: Option<enums::CountryAlpha2>,
@@ -88,26 +89,4 @@ pub struct AddressUpdateInternal {
     pub updated_by: String,
     pub email: Option<Encryption>,
     pub origin_zip: Option<Encryption>,
-}
-
-impl AddressUpdateInternal {
-    pub fn create_address(self, source: Address) -> Address {
-        Address {
-            city: self.city,
-            country: self.country,
-            line1: self.line1,
-            line2: self.line2,
-            line3: self.line3,
-            state: self.state,
-            zip: self.zip,
-            first_name: self.first_name,
-            last_name: self.last_name,
-            phone_number: self.phone_number,
-            country_code: self.country_code,
-            modified_at: self.modified_at,
-            updated_by: self.updated_by,
-            origin_zip: self.origin_zip,
-            ..source
-        }
-    }
 }
