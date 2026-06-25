b502608bd31d2cd161e1ac44f90fac6d692cd5f4
Vani Gupta <vani.gupta@juspay.in>
2026-06-25 16:33:07 +0530

    fix:clippyv2

--- crates/router/src/core/payments/transformers.rs
+++ crates/router/src/core/payments/transformers.rs
@@ -1837,6 +1837,7 @@ pub async fn construct_payment_router_data_for_setup_mandate<'a>(
         feature_metadata: None,
         connector_intent_metadata: None,
         merchant_order_reference_id: None,
+        mit_category: None,
     };
     let connector_mandate_request_reference_id = payment_data
         .payment_attempt
