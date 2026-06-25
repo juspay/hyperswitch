4923bc764fcd6c40f0458f82b837b677e9f3899d
Akshaya Shankar <131388445+AkshayaFoiger@users.noreply.github.com>
2026-06-25 13:09:48 +0530

    Update migrations/2026-06-25-054230_add_surcharge_event_types/down.sql
    Co-authored-by: Hrithikesh <61539176+hrithikesh026@users.noreply.github.com>

--- migrations/2026-06-25-054230_add_surcharge_event_types/down.sql
+++ migrations/2026-06-25-054230_add_surcharge_event_types/down.sql
@@ -1,7 +1,1 @@
--- Note: PostgreSQL does not support removing values from ENUM types
--- To downgrade, you would need to recreate the ENUM type
-DELETE FROM pg_enum
-WHERE enumlabel in ('surcharge_payment_succeeded','surcharge_refund_succeeded')
-AND enumtypid = (
-  SELECT oid FROM pg_type WHERE typname = 'EventType'
-); 
+SELECT 1;
