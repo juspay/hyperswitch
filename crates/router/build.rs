06a7ec61a22a753312849b04fb612b18d07855b2
AkshayaFoiger <akshaya.shankar@juspay.in>
2026-07-08 20:18:19 +0530

    undo build stack

--- crates/router/build.rs
+++ crates/router/build.rs
@@ -2,7 +2,7 @@ fn main() {
     // Set thread stack size to 11 MiB for debug builds
     // Reference: https://doc.rust-lang.org/std/thread/#stack-size
     #[cfg(debug_assertions)]
-    println!("cargo:rustc-env=RUST_MIN_STACK=18534336"); // 11 * 1024 * 1024 = 11 MiB
+    println!("cargo:rustc-env=RUST_MIN_STACK=11534336"); // 11 * 1024 * 1024 = 11 MiB
 
     #[cfg(feature = "vergen")]
     router_env::vergen::generate_cargo_instructions();
