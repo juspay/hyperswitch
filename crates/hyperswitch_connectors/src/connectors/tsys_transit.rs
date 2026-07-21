e490e8c57c1f3ccc4c80f3d49e9e00cdfaa47b1e
AkshayaFoiger <akshaya.shankar@juspay.in>
2026-07-21 17:21:35 +0530

    make merchant_url  a String

--- crates/hyperswitch_connectors/src/connectors/tsys_transit.rs
+++ crates/hyperswitch_connectors/src/connectors/tsys_transit.rs
@@ -76,7 +76,7 @@ pub struct TsysTransitMetadataObject {
     pub tsys_transit: Option<serde_json::Value>,
     pub merchant_street_address: Option<Secret<String>>,
     pub customer_service_number: Option<Secret<String>>,
-    pub merchant_url: Option<Secret<String>>,
+    pub merchant_url: Option<String>,
 }
 
 impl TryFrom<&Option<common_utils::pii::SecretSerdeValue>> for TsysTransitMetadataObject {
