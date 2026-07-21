8a044d9ee9604e1f70f6ebb62f07fb1b6f006ec2
AkshayaFoiger <akshaya.shankar@juspay.in>
2026-07-21 15:50:21 +0530

    add wasm keys

--- crates/connector_configs/src/connector.rs
+++ crates/connector_configs/src/connector.rs
@@ -203,6 +203,9 @@ pub struct ConfigMetadata {
     pub kid: Option<InputData>,
     pub account_holder_name: Option<InputData>,
     pub private_key: Option<InputData>,
+    pub merchant_street_address: Option<String>,
+    pub customer_service_number: Option<String>,
+    pub merchant_url: Option<String>,
 }
 
 #[serde_with::skip_serializing_none]
