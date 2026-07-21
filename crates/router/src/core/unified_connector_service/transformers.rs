7b0b7e125eba14eea0a5c92658fc2cb0cc18d2e5
Shubhodip Pal <palshubhodip9@gmail.com>
2026-07-21 13:49:22 +0530

    feat(ucs): Added payment_channel field in Repeat Payment request

--- crates/router/src/core/unified_connector_service/transformers.rs
+++ crates/router/src/core/unified_connector_service/transformers.rs
@@ -2439,6 +2439,13 @@ impl
                 }),
             auth_type: Some(auth_type.into()),
             complete_authorize_url: router_data.request.complete_authorize_url.clone(),
+            payment_channel: router_data
+                .request
+                .payment_channel
+                .as_ref()
+                .map(payments_grpc::PaymentChannel::foreign_try_from)
+                .transpose()?
+                .map(|payment_channel| payment_channel.into()),
         })
     }
 }
