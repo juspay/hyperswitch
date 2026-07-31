bd6e9e7ea27d3066d96b6469ee35f28df2fe41e6
Kashif <mohammed.kashif@juspay.in>
2026-07-31 11:01:31 +0530

    refactor: use conditional cfg to propagate request ID to vault

--- crates/router/src/core/payment_methods/vault.rs
+++ crates/router/src/core/payment_methods/vault.rs
@@ -1995,13 +1995,18 @@ pub async fn call_to_vault<V: pm_types::VaultingInterface>(
 ) -> CustomResult<String, errors::VaultError> {
     let locker = &state.conf.locker;
     let jwekey = state.conf.jwekey.get_inner();
-    let mut additional_headers = additional_headers.unwrap_or_default();
-    if let Some(request_id) = state.request_id.as_ref() {
-        additional_headers.insert(
-            common_utils::consts::X_REQUEST_ID.to_string(),
-            request_id.to_string(),
-        );
-    }
+    let additional_headers = if cfg!(feature = "ext_services_latency") {
+        let mut additional_headers = additional_headers.unwrap_or_default();
+        if let Some(request_id) = state.request_id.as_ref() {
+            additional_headers.insert(
+                common_utils::consts::X_REQUEST_ID.to_string(),
+                request_id.to_string(),
+            );
+        }
+        additional_headers
+    } else {
+        additional_headers.unwrap_or_default()
+    };
 
     let request = create_vault_request::<V>(
         jwekey,
