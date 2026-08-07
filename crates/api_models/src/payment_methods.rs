7806655f1aef083e27ed3c5b107a86c9abb0b0cf
Shankar Singh C <83439957+ShankarSinghC@users.noreply.github.com>
2026-08-07 11:59:59 +0530

    docs(openapi): add API reference for list payment methods client SDK endpoint (#13583)

--- crates/api_models/src/payment_methods.rs
+++ crates/api_models/src/payment_methods.rs
@@ -2983,6 +2983,7 @@ pub struct ResponsePaymentMethodsEnabledForClient {
     pub data: Option<PaymentMethodSubtypeSpecificDataForClient>,
 
     /// Payment experience options for this method (wallets, pay_later, etc.)
+    #[schema(value_type = Option<Vec<PaymentExperience>>)]
     pub payment_experience: Option<Vec<api_enums::PaymentExperience>>,
 
     /// Whether to collect shipping details from the wallet connector (null for non-wallet)
@@ -3008,6 +3009,7 @@ pub struct ResponsePaymentMethodsEnabledForClient {
 pub enum WalletPaymentMethodDataForClient {
     ApplePay(Box<PaymentMethodDataWalletInfo>),
     GooglePay(Box<PaymentMethodDataWalletInfo>),
+    #[schema(value_type = PaypalRedirection)]
     PayPal(Box<payments::PaypalRedirection>),
 }
 
