fbe2c43a55f497317ec58d1cc2276972bd0fad81
venkatakarthikm-maker <venkatakarthik.m@juspay.in>
2026-08-12 17:10:25 +0530

    Removing stale configs

--- cypress-tests/cypress/e2e/configs/Payment/Givepayments.js
+++ cypress-tests/cypress/e2e/configs/Payment/Givepayments.js
@@ -1080,11 +1080,4 @@ export const connectorDetails = {
       },
     },
   },
-  webhook: {
-    TransactionIdConfig: {
-      // Defines how to locate and parse the payment reference ID from connector-specific webhook payloads
-      path: "payload.id",
-      type: "string",
-    },
-  },
 };
