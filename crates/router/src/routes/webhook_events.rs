e83ee046846e2a6171431c095f72310a4fdf217d
Ayush Anand <ayush.anand@juspay.in>
2026-08-14 00:37:58 +0530

    refactor(webhook_events): update closure parameters to use unit type for unused variables

--- crates/router/src/routes/webhook_events.rs
+++ crates/router/src/routes/webhook_events.rs
@@ -36,7 +36,7 @@ pub async fn list_initial_webhook_delivery_attempts(
         state,
         &req,
         request_internal,
-        |state, _, request_internal, _| {
+        |state, _: (), request_internal, _| {
             webhook_events::list_initial_delivery_attempts(
                 state,
                 request_internal.merchant_id,
@@ -121,7 +121,7 @@ pub async fn list_webhook_delivery_attempts(
         state,
         &req,
         request_internal,
-        |state, _, request_internal, _| {
+        |state, _: (), request_internal, _| {
             webhook_events::list_delivery_attempts(
                 state,
                 request_internal.merchant_id,
@@ -159,7 +159,7 @@ pub async fn retry_webhook_delivery_attempt(
         state,
         &req,
         request_internal,
-        |state, _, request_internal, _| {
+        |state, _: (), request_internal, _| {
             webhook_events::retry_delivery_attempt(
                 state,
                 request_internal.merchant_id,
