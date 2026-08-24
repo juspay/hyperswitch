c77d3e4475c19d9d922812e304349f58913a1677
Rohan Mali <rohan.mali@juspay.in>
2026-08-24 08:32:48 +0000

    fix(rev-rec): add card data in payment intent (#13778)
    Co-authored-by: Rohan Mali <rohan.mali@Rohan-Mali-LNXHFW59HW.local>
    Co-authored-by: hyperswitch-bot[bot] <148525504+hyperswitch-bot[bot]@users.noreply.github.com>

--- crates/hyperswitch_domain_models/src/lib.rs
+++ crates/hyperswitch_domain_models/src/lib.rs
@@ -893,6 +893,9 @@ impl ApiModelToDieselModelConvertor<ApiBillingConnectorAdditionalCardInfo>
         Self {
             card_issuer: from.card_issuer,
             card_network: from.card_network,
+            card_type: from.card_type,
+            card_issuing_country: from.card_issuing_country,
+            card_isin: from.card_isin,
         }
     }
 
@@ -900,6 +903,9 @@ impl ApiModelToDieselModelConvertor<ApiBillingConnectorAdditionalCardInfo>
         ApiBillingConnectorAdditionalCardInfo {
             card_issuer: self.card_issuer,
             card_network: self.card_network,
+            card_type: self.card_type,
+            card_issuing_country: self.card_issuing_country,
+            card_isin: self.card_isin,
         }
     }
 }
