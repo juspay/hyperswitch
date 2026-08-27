df197263c0dde1daca2028f5a76b2073ca1a0fe3
Ritam Kabiraj <ritam.kabiraj@juspay.in>
2026-08-27 13:19:34 +0530

    frm-FRAMEWORK

--- crates/common_enums/src/connector_enums.rs
+++ crates/common_enums/src/connector_enums.rs
@@ -210,6 +210,7 @@ pub enum Connector {
     Worldpayxml,
     Worldpaymodular,
     Signifyd,
+    Nsure,
     Plaid,
     Riskified,
     SanlamPayshield,
@@ -452,6 +453,7 @@ impl Connector {
             | Self::Zen
             | Self::Zsl
             | Self::Signifyd
+            | Self::Nsure
             | Self::Plaid
             | Self::Razorpay
             | Self::Riskified
