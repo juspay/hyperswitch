1f2874dbf01f0e14d4d7d5112e1fc0af5556c10a
Akshaya Shankar <131388445+AkshayaFoiger@users.noreply.github.com>
2026-08-28 17:10:58 +0530

    feat(connectors): [worldpay][checkout] add payment_account_reference support (#13900)

--- crates/hyperswitch_connectors/src/connectors/worldpayxml/transformers.rs
+++ crates/hyperswitch_connectors/src/connectors/worldpayxml/transformers.rs
@@ -279,6 +279,7 @@ pub struct Payment {
     fast_funds: Option<bool>,
     #[serde(rename = "ISO8583ReturnCode")]
     return_code: Option<ReturnCode>,
+    card_p_a_r: Option<String>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -4009,7 +4010,7 @@ fn process_payment_response(
             incremental_authorization_allowed: None,
             authentication_data: None,
             charges: None,
-            payment_account_reference: None,
+            payment_account_reference: payment_data.card_p_a_r.clone(),
         })
     }
 }
