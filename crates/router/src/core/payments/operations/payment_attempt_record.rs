7415bd3af3d6af917f2c5de0aed156e6fa78a28d
Nishanth Challa <nishanth.challa@juspay.in>
2026-09-01 18:56:23 +0530

    fix the invoice_billing__start_at_time

--- crates/router/src/core/payments/operations/payment_attempt_record.rs
+++ crates/router/src/core/payments/operations/payment_attempt_record.rs
@@ -202,6 +202,7 @@ impl<F: Send + Clone + Sync>
             connector_customer_id: request.connector_customer_id.clone(),
             retry_count: request.retry_count,
             invoice_next_billing_time: request.invoice_next_billing_time,
+            invoice_billing_started_at_time: request.invoice_billing_started_at_time,
             triggered_by: request.triggered_by,
             card_network: request.card_network.clone(),
             card_issuer: request.card_issuer.clone(),
