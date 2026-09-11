juspay/hyperswitch

commit f35edab780c97dd12efdd366246ff3f7fbc0e940

feat(router): add bin and issuer details for apple pay and google pay (#14055)

crates/api_models/Cargo.toml
@@ -61,6 +61,7 @@ hyperswitch_masking = { version = "0.0.1", default-features = false, features = 
     "time",
 ] }
 router_derive = { version = "0.1.0", path = "../router_derive" }
+router_env = { version = "0.1.0", path = "../router_env" }
 smithy = { version = "0.1.0", path = "../smithy" }
 smithy-core = { version = "0.1.0", path = "../smithy-core" }
 

crates/api_models/src/payment_methods.rs
@@ -20,6 +20,7 @@ use common_utils::{
 };
 use error_stack::ResultExt;
 use hyperswitch_masking::PeekInterface;
+use router_env::logger;
 use rust_decimal::{
     prelude::{FromPrimitive, ToPrimitive},
     Decimal,
@@ -1893,7 +1894,7 @@ impl From<payments::additional_info::WalletAdditionalDataForCard> for PaymentMet
         Self {
             last4: item.last4,
             card_network: item.card_network,
-            card_type: item.card_type,
+            card_type: item.payment_method_data_type,
             card_exp_month: item.card_exp_month,
             card_exp_year: item.card_exp_year,
             auth_code: item.auth_code,
@@ -1907,11 +1908,19 @@ impl From<PaymentMethodDataWalletInfo> for payments::additional_info::WalletAddi
         Self {
             last4: item.last4,
             card_network: item.card_network,
-            card_type: item.card_type,
+            payment_method_data_type: item.card_type,
             card_exp_month: item.card_exp_month,
             card_exp_year: item.card_exp_year,
             auth_code: item.auth_code,
             email: item.email,
+            device_pan_bin: None,
+            card_bin: None,
+            card_subtype: None,
+            card_segment_type: None,
+            funding_source: None,
+            card_type: None,
+            issuer_name: None,
+            issuer_country: None,
         }
     }
 }
@@ -1942,13 +1951,34 @@ impl From<payments::ApplepayPaymentMethod> for PaymentMethodDataWalletInfo {
 impl TryFrom<PaymentMethodDataWalletInfo> for Box<payments::ApplepayPaymentMethod> {
     type Error = error_stack::Report<errors::ValidationError>;
     fn try_from(item: PaymentMethodDataWalletInfo) -> Result<Self, Self::Error> {
+        let card_type = item.card_type.clone().get_required_value("card_type")?;
         Ok(Self::new(payments::ApplepayPaymentMethod {
             display_name: item.last4.get_required_value("last4")?,
             network: item.card_network.get_required_value("card_network")?,
-            pm_type: item.card_type.get_required_value("card_type")?,
+            pm_type: card_type.clone(),
+            // If `card_type` doesn't parse into a known `CardType` variant, it is treated as
+            // `None` instead of erroring.
+            card_type: card_type
+                .to_uppercase()
+                .parse::<api_enums::CardType>()
+                .inspect_err(|error| {
+                    logger::error!(
+                        ?error,
+                        unparsed_card_type = %card_type,
+                        "Received an unrecognized card_type value from Apple Pay; defaulting to None"
+                    )
+                })
+                .ok(),
             card_exp_month: item.card_exp_month,
             card_exp_year: item.card_exp_year,
             auth_code: item.auth_code,
+            device_pan_bin: None,
+            card_bin: None,
+            card_subtype: None,
+            card_segment_type: None,
+            funding_source: None,
+            issuer_name: None,
+            issuer_country: None,
         }))
     }
 }

crates/api_models/src/payments.rs
@@ -5936,8 +5936,28 @@ pub struct ApplepayPaymentMethod {
     /// The card's expiry year
     #[schema(value_type = Option<String>, example = "003925")]
     pub card_exp_year: Option<Secret<String>>,
+    /// Bin of the DPAN (device PAN) obtained from decrypting the Apple Pay payment data
+    pub device_pan_bin: Option<String>,
+    /// Bin of the underlying card, provided by the connector when it resolves the DPAN
+    pub card_bin: Option<String>,
+    // The card's type (eg. Credit, Debit), as returned by the connector
+    #[schema(value_type = Option<CardType>)]
+    pub card_type: Option<api_enums::CardType>,
     /// Unique authorisation code generated for the payment
     pub auth_code: Option<String>,
+    /// The card's product/subtype, as returned by the connector
+    pub card_subtype: Option<String>,
+    /// The card's segment (e.g. consumer, commercial), as returned by the connector
+    #[schema(value_type = Option<CardSegmentType>)]
+    pub card_segment_type: Option<api_enums::CardSegmentType>,
+    /// The card's funding source (e.g. credit, debit), as returned by the connector
+    #[schema(value_type = Option<FundingSource>)]
+    pub funding_source: Option<api_enums::FundingSource>,
+    /// The name of the card issuer, as returned by the connector
+    pub issuer_name: Option<String>,
+    /// The country of the card issuer, as returned by the connector
+    #[schema(value_type = Option<CountryAlpha2>, example = "US")]
+    pub issuer_country: Option<api_enums::CountryAlpha2>,
 }
 
 #[derive(
@@ -9641,11 +9661,19 @@ impl From<AdditionalPaymentData> for PaymentMethodDataResponse {
                                     .collect::<String>(),
                             ),
                             card_network: Some(apple_pay_pm.network.clone()),
-                            card_type: Some(apple_pay_pm.pm_type.clone()),
+                            payment_method_data_type: Some(apple_pay_pm.pm_type.clone()),
                             card_exp_month: apple_pay_pm.card_exp_month,
                             card_exp_year: apple_pay_pm.card_exp_year,
                             auth_code: apple_pay_pm.auth_code,
                             email: None,
+                            device_pan_bin: apple_pay_pm.device_pan_bin,
+                            card_bin: apple_pay_pm.card_bin,
+                            card_subtype: apple_pay_pm.card_subtype,
+                            card_segment_type: apple_pay_pm.card_segment_type,
+                            funding_source: apple_pay_pm.funding_source,
+                            card_type: apple_pay_pm.card_type,
+                            issuer_name: apple_pay_pm.issuer_name,
+                            issuer_country: apple_pay_pm.issuer_country,
                         },
                     ))),
                 })),

crates/api_models/src/payments/additional_info.rs
@@ -456,22 +456,44 @@ pub struct UpiCollectAdditionalData {
 )]
 #[smithy(namespace = "com.hyperswitch.smithy.types")]
 pub struct WalletAdditionalDataForCard {
-    /// Last 4 digits of the card number
-    #[smithy(value_type = "Option<String>")]
-    pub last4: Option<String>,
-    /// The information of the payment method
-    #[smithy(value_type = "Option<String>")]
-    pub card_network: Option<String>,
     /// The type of payment method
     #[serde(rename = "type")]
     #[smithy(value_type = "Option<String>")]
-    pub card_type: Option<String>,
+    pub payment_method_data_type: Option<String>,
+    /// The information of the payment method
+    #[smithy(value_type = "Option<String>")]
+    pub card_network: Option<String>,
+    /// The card's type (e.g. credit, debit), as returned by the connector
+    #[schema(value_type = Option<CardType>)]
+    pub card_type: Option<api_enums::CardType>,
+    /// The card's product/subtype, as returned by the connector
+    pub card_subtype: Option<String>,
+    /// The card's segment (e.g. consumer, commercial), as returned by the connector
+    #[schema(value_type = Option<CardSegmentType>)]
+    pub card_segment_type: Option<api_enums::CardSegmentType>,
+    /// The card's funding source (e.g. credit, debit), as returned by the connector
+    #[schema(value_type = Option<FundingSource>)]
+    pub funding_source: Option<api_enums::FundingSource>,
+    /// Last 4 digits of the card number
+    #[smithy(value_type = "Option<String>")]
+    pub last4: Option<String>,
+    /// Bin of the underlying card
+    #[schema(value_type = Option<String>, example = "411111")]
+    pub card_bin: Option<String>,
+    /// Bin of the DPAN (device PAN) obtained from decrypting the wallet payment data.
+    #[schema(value_type = Option<String>, example = "411111")]
+    pub device_pan_bin: Option<String>,
     /// The card's expiry month
     #[schema(value_type = Option<String>, example = "03")]
     pub card_exp_month: Option<Secret<String>>,
     /// The card's expiry year
     #[schema(value_type = Option<String>, example = "25")]
     pub card_exp_year: Option<Secret<String>>,
+    /// The name of the card issuer, as returned by the connector
+    pub issuer_name: Option<String>,
+    /// The country of the card issuer, as returned by the connector
+    #[schema(value_type = Option<CountryAlpha2>, example = "US")]
+    pub issuer_country: Option<api_enums::CountryAlpha2>,
     /// Unique authorisation code generated for the payment
     #[schema(value_type = Option<String>, example = "009825")]
     pub auth_code: Option<String>,

crates/common_enums/src/enums.rs
@@ -3571,6 +3571,9 @@ pub enum CardSegmentType {
 pub enum CardType {
     Credit,
     Debit,
+    Prepaid,
+    Store,
+    ChargeCard,
 }
 
 impl CardType {
@@ -3578,6 +3581,9 @@ impl CardType {
         match self {
             Self::Credit => "Credit",
             Self::Debit => "Debit",
+            Self::Prepaid => "Prepaid",
+            Self::Store => "Store",
+            Self::ChargeCard => "Charge Card",
         }
     }
 }

crates/common_types/src/payments.rs
@@ -733,6 +733,43 @@ impl GpayTokenizationData {
     }
 }
 impl GPayPredecryptData {
+    /// Bin of the decrypted PAN, when it is a tokenized DPAN (`auth_method` is `CRYPTOGRAM_3DS`)
+    pub fn get_device_pan_bin(&self) -> Option<String> {
+        match self.auth_method {
+            Some(enums::GooglePayAuthMethod::Cryptogram) => {
+                Some(self.application_primary_account_number.get_card_isin())
+            }
+            None if self.cryptogram.is_some() => {
+                Some(self.application_primary_account_number.get_card_isin())
+            }
+            Some(enums::GooglePayAuthMethod::PanOnly) | None => None,
+        }
+    }
+
+    /// Bin of the decrypted PAN, when it is the underlying card's real PAN (`auth_method` is
+    /// `PAN_ONLY`)
+    pub fn get_card_bin(&self) -> Option<String> {
+        match self.auth_method {
+            Some(enums::GooglePayAuthMethod::PanOnly) => {
+                Some(self.application_primary_account_number.get_card_isin())
+            }
+            None if self.cryptogram.is_none() => {
+                Some(self.application_primary_account_number.get_card_isin())
+            }
+            Some(enums::GooglePayAuthMethod::Cryptogram) | None => None,
+        }
+    }
+
+    /// The decrypted PAN's card expiry month
+    pub fn get_card_exp_month(&self) -> Secret<String> {
+        self.card_exp_month.clone()
+    }
+
+    /// The decrypted PAN's card expiry year
+    pub fn get_card_exp_year(&self) -> Secret<String> {
+        self.card_exp_year.clone()
+    }
+
     /// Get the four-digit expiration year from the Google Pay pre-decrypt data
     pub fn get_four_digit_expiry_year(&self) -> Result<Secret<String>, errors::ValidationError> {
         let mut year = self.card_exp_year.peek().clone();
@@ -827,6 +864,23 @@ pub struct ApplePayPredecryptData {
     #[schema(value_type = ApplePayCryptogramData)]
     #[smithy(value_type = "ApplePayCryptogramData")]
     pub payment_data: ApplePayCryptogramData,
+}
+
+impl ApplePayPredecryptData {
+    /// The decrypted PAN's card expiry month
+    pub fn get_application_expiration_month(&self) -> Secret<String> {
+        self.application_expiration_month.clone()
+    }
+
+    /// The decrypted PAN's card expiry year
+    pub fn get_application_expiration_year(&self) -> Secret<String> {
+        self.application_expiration_year.clone()
+    }
+
+    /// Bin of the decrypted PAN
+    pub fn get_device_pan_bin(&self) -> String {
+        self.application_primary_account_number.get_card_isin()
+    }
 }
 
 #[derive(

crates/hyperswitch_connectors/src/connectors/checkout/transformers.rs
@@ -1510,6 +1510,66 @@ pub struct Source {
     avs_check: Option<String>,
     cvv_check: Option<String>,
     payment_account_reference: Option<String>,
+    /// The card's funding type
+    card_type: Option<CheckoutCardType>,
+    /// The card's category
+    card_category: Option<CheckoutCardCategory>,
+    /// The name of the card issuer
+    issuer: Option<String>,
+    /// The country of the card issuer
+    issuer_country: Option<CountryAlpha2>,
+    /// The card's product/subtype, e.g. "Visa Classic"
+    product_type: Option<String>,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum CheckoutCardType {
+    Credit,
+    Debit,
+    Prepaid,
+    Charge,
+    #[serde(rename = "DEFERRED DEBIT")]
+    DeferredDebit,
+}
+
+impl From<CheckoutCardType> for common_enums::FundingSource {
+    fn from(card_type: CheckoutCardType) -> Self {
+        match card_type {
+            CheckoutCardType::Credit => Self::Credit,
+            CheckoutCardType::Debit => Self::Debit,
+            CheckoutCardType::Prepaid => Self::Prepaid,
+            CheckoutCardType::Charge => Self::ChargeCard,
+            CheckoutCardType::DeferredDebit => Self::DeferredDebit,
+        }
+    }
+}
+
+impl From<CheckoutCardType> for common_enums::CardType {
+    fn from(card_type: CheckoutCardType) -> Self {
+        match card_type {
+            CheckoutCardType::Credit => Self::Credit,
+            CheckoutCardType::Debit | CheckoutCardType::DeferredDebit => Self::Debit,
+            CheckoutCardType::Prepaid => Self::Prepaid,
+            CheckoutCardType::Charge => Self::ChargeCard,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum CheckoutCardCategory {
+    Consumer,
+    Commercial,
+}
+
+impl From<CheckoutCardCategory> for common_enums::CardSegmentType {
+    fn from(card_category: CheckoutCardCategory) -> Self {
+        match card_category {
+            CheckoutCardCategory::Consumer => Self::Consumer,
+            CheckoutCardCategory::Commercial => Self::Commercial,
+        }
+    }
 }
 
 #[derive(Clone, Debug, Default, Eq, PartialEq, Deserialize, Serialize)]
@@ -2596,6 +2656,11 @@ impl TryFrom<&webhooks::IncomingWebhookRequestDetails<'_>> for PaymentsResponse 
                 avs_check: src.avs_check.clone(),
                 cvv_check: src.cvv_check.clone(),
                 payment_account_reference: src.payment_account_reference.clone(),
+                card_type: src.card_type.clone(),
+                card_category: src.card_category.clone(),
+                issuer: src.issuer.clone(),
+                issuer_country: src.issuer_country,
+                product_type: src.product_type.clone(),
             }),
             scheme_id: None,
             processing: None,
@@ -2666,10 +2731,39 @@ fn convert_to_additional_payment_method_connector_response(
 ) -> Option<AdditionalPaymentMethodConnectorResponse> {
     match payment_method_type {
         Some(enums::PaymentMethodType::GooglePay) => {
-            Some(AdditionalPaymentMethodConnectorResponse::GooglePay { auth_code })
+            Some(AdditionalPaymentMethodConnectorResponse::GooglePay {
+                auth_code,
+                device_pan_bin: None,
+                card_bin: None,
+                card_subtype: source.and_then(|source| source.product_type.clone()),
+                card_segment_type: source
+                    .and_then(|source| source.card_category.clone())
+                    .map(common_enums::CardSegmentType::from),
+                funding_source: source
+                    .and_then(|source| source.card_type.clone())
+                    .map(common_enums::FundingSource::from),
+                card_type: source
+                    .and_then(|source| source.card_type.as_ref())
+                    .map(|card_type| common_enums::CardType::from(card_type.clone())),
+                issuer_name: source.and_then(|source| source.issuer.clone()),
+                issuer_country: source.and_then(|source| source.issuer_country),
+            })
         }
         Some(enums::PaymentMethodType::ApplePay) => {
-            Some(AdditionalPaymentMethodConnectorResponse::ApplePay { auth_code })
+            Some(AdditionalPaymentMethodConnectorResponse::ApplePay {
+                auth_code,
+                device_pan_bin: None,
+                card_bin: None,
+                card_subtype: source.and_then(|source| source.product_type.clone()),
+                card_segment_type: source
+                    .and_then(|source| source.card_category.clone())
+                    .map(common_enums::CardSegmentType::from),
+                funding_source: source
+                    .and_then(|source| source.card_type.clone())
+                    .map(common_enums::FundingSource::from),
+                issuer_name: source.and_then(|source| source.issuer.clone()),
+                issuer_country: source.and_then(|source| source.issuer_country),
+            })
         }
         _ => {
             let payment_checks = source.map(|code| {

crates/hyperswitch_connectors/src/connectors/worldpayxml/transformers.rs
@@ -222,6 +222,26 @@ struct OrderStatus {
 struct Token {
     authenticated_shopper_i_d: Option<String>,
     token_details: TokenDetails,
+    payment_instrument: Option<TokenPaymentInstrument>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct TokenPaymentInstrument {
+    emvco_token_details: Option<EmvcoTokenDetailsResponse>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct EmvcoTokenDetailsResponse {
+    derived: Option<EmvcoTokenDetailsDerived>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct EmvcoTokenDetailsDerived {
+    card_brand: Option<String>,
+    card_sub_brand: Option<String>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -608,6 +628,27 @@ struct WorldpayXmlAmount {
     currency_code: api_models::enums::Currency,
     #[serde(rename = "@exponent")]
     exponent: String,
+    #[serde(
+        rename = "@debitCreditIndicator",
+        skip_serializing_if = "Option::is_none"
+    )]
+    debit_credit_indicator: Option<DebitCreditIndicator>,
+}
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum DebitCreditIndicator {
+    Credit,
+    Debit,
+}
+
+impl DebitCreditIndicator {
+    fn as_card_type(self) -> common_enums::CardType {
+        match self {
+            Self::Credit => common_enums::CardType::Credit,
+            Self::Debit => common_enums::CardType::Debit,
+        }
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -1779,6 +1820,7 @@ impl TryFrom<&WorldpayxmlRouterData<&PaymentsAuthorizeRouterData>> for PaymentSe
             currency_code: item.router_data.request.currency.to_owned(),
             exponent,
             value: item.amount.to_owned(),
+            debit_credit_indicator: None,
         };
         let shopper = get_shopper_details(item.router_data, accept_header, user_agent_header)?;
         let billing_address = item
@@ -1906,6 +1948,7 @@ impl TryFrom<&WorldpayxmlRouterData<&PaymentsCaptureRouterData>> for PaymentServ
                             .number_of_digits_after_decimal_point()
                             .to_string(),
                         value: item.amount.to_owned(),
+                        debit_credit_indicator: None,
                     },
                 }),
                 cancel_refund: None,
@@ -1973,6 +2016,7 @@ impl<F> TryFrom<&WorldpayxmlRouterData<&RefundsRouterData<F>>> for PaymentServic
                             .number_of_digits_after_decimal_point()
                             .to_string(),
                         value: item.amount.to_owned(),
+                        debit_credit_indicator: None,
                     },
                 }),
             },
@@ -2197,6 +2241,7 @@ impl<F>
                         )?;
                         let connector_response = get_connector_response_data(
                             &payment_data,
+                            order_status.token.as_ref(),
                             item.data.request.payment_method_type,
                         );
                         let response = process_payment_response(
@@ -2534,6 +2579,7 @@ impl<F>
 
                 let connector_response = get_connector_response_data(
                     &payment_data,
+                    order_status.token.as_ref(),
                     item.data.request.payment_method_type,
                 );
                 let response = process_payment_response(
@@ -2805,6 +2851,7 @@ impl TryFrom<WorldpayxmlRouterData<&PaymentsCompleteAuthorizeRouterData>> for Pa
                 currency_code: item.router_data.request.currency.to_owned(),
                 exponent,
                 value: item.amount.to_owned(),
+                debit_credit_indicator: None,
             };
             let shopper = get_shopper_details_cauth(
                 item.router_data,
@@ -2932,6 +2979,7 @@ impl<F>
                 )?;
                 let connector_response = get_connector_response_data(
                     &payment_data,
+                    order_status.token.as_ref(),
                     item.data.request.payment_method_type,
                 );
                 let response = process_payment_response(
@@ -3031,6 +3079,7 @@ impl<F>
 
                 let connector_response = get_connector_response_data(
                     &payment_data,
+                    order_status.token.as_ref(),
                     item.data.request.payment_method_type,
                 );
                 let response = process_payment_response(
@@ -3642,6 +3691,7 @@ impl TryFrom<&WorldpayxmlRouterData<&PayoutsRouterData<PoFulfill>>> for PaymentS
             currency_code: item.router_data.request.destination_currency.to_owned(),
             exponent,
             value: item.amount.to_owned(),
+            debit_credit_indicator: None,
         };
 
         let auth = WorldpayxmlAuthType::try_from(&item.router_data.connector_auth_type)
@@ -3992,6 +4042,7 @@ fn get_mandate_reference(
 /// to the auth code exposed in the connector response.
 fn get_connector_response_data(
     payment_data: &Payment,
+    token: Option<&Token>,
     payment_method_type: Option<enums::PaymentMethodType>,
 ) -> Option<ConnectorResponseData> {
     let auth_code = payment_data
@@ -4000,15 +4051,50 @@ fn get_connector_response_data(
         .and_then(|authorisation_id| authorisation_id.id.clone())
         .map(|id| id.expose())?;
 
+    let issuer_name = payment_data.issuer_name.clone();
+    // Worldpay can return "N/A" here instead of an ISO alpha-2 code; parse leniently.
+    let issuer_country = payment_data
+        .issuer_country_code
+        .as_deref()
+        .and_then(|code| code.parse::<common_enums::CountryAlpha2>().ok());
+    let card_subtype = token
+        .and_then(|token| token.payment_instrument.as_ref())
+        .and_then(|payment_instrument| {
+            payment_instrument
+                .emvco_token_details
+                .as_ref()
+                .and_then(|emvco_token_details| emvco_token_details.derived.as_ref())
+                .and_then(|derived| derived.card_sub_brand.clone())
+        });
+
     let additional_payment_method_data = match payment_method_type {
         Some(enums::PaymentMethodType::GooglePay) => {
             AdditionalPaymentMethodConnectorResponse::GooglePay {
                 auth_code: Some(auth_code),
+                device_pan_bin: None,
+                card_bin: None,
+                card_subtype,
+                card_segment_type: None,
+                funding_source: None,
+                card_type: payment_data
+                    .amount
+                    .as_ref()
+                    .and_then(|amount| amount.debit_credit_indicator)
+                    .map(DebitCreditIndicator::as_card_type),
+                issuer_name,
+                issuer_country,
             }
         }
         Some(enums::PaymentMethodType::ApplePay) => {
             AdditionalPaymentMethodConnectorResponse::ApplePay {
                 auth_code: Some(auth_code),
+                device_pan_bin: None,
+                card_bin: None,
+                card_subtype,
+                card_segment_type: None,
+                funding_source: None,
+                issuer_name,
+                issuer_country,
             }
         }
         _ => AdditionalPaymentMethodConnectorResponse::Card {

commit ad8670b6056f64d5c608c0b1e938611ca6ccc970

fix(api_models): keep `router_env` off the wasm32 target
The wasm build has been failing on main since #14055, which added a
`router_env` dependency to `api_models` for a single diagnostic log call.
`router_env` depends on tokio and the OpenTelemetry exporters unconditionally,
and tokio pulls `mio`, whose `sys` module is empty on wasm32 -- so
`make euclid-wasm` dies with 47 unresolved-import errors inside mio.

`euclid_wasm` depends on `api_models`, so the dependency now lives under
`[target.'cfg(not(target_arch = "wasm32"))'.dependencies]` and the one
`logger::error!` call is gated to match. Server builds are unchanged: they
already enable `router_env` directly, and `api_models` takes its own actix-web
through the `errors` and `control_center_theme` features rather than from
`router_env`'s defaults.

Co-Authored-By: Claude Opus 5 <noreply@anthropic.com>

crates/api_models/Cargo.toml
@@ -61,9 +61,14 @@ hyperswitch_masking = { version = "0.0.1", default-features = false, features = 
     "time",
 ] }
 router_derive = { version = "0.1.0", path = "../router_derive" }
-router_env = { version = "0.1.0", path = "../router_env" }
 smithy = { version = "0.1.0", path = "../smithy" }
 smithy-core = { version = "0.1.0", path = "../smithy-core" }
+
+# `router_env` depends on tokio and the OpenTelemetry exporters, neither of which builds for
+# wasm32 (tokio pulls `mio`, whose `sys` module is empty there). `euclid_wasm` depends on this
+# crate, so the logger is only wired up off that target.
+[target.'cfg(not(target_arch = "wasm32"))'.dependencies]
+router_env = { version = "0.1.0", path = "../router_env" }
 
 [lints]
 workspace = true

crates/api_models/src/payment_methods.rs
@@ -20,6 +20,7 @@ use common_utils::{
 };
 use error_stack::ResultExt;
 use hyperswitch_masking::PeekInterface;
+#[cfg(not(target_arch = "wasm32"))]
 use router_env::logger;
 use rust_decimal::{
     prelude::{FromPrimitive, ToPrimitive},
@@ -1962,11 +1963,14 @@ impl TryFrom<PaymentMethodDataWalletInfo> for Box<payments::ApplepayPaymentMetho
                 .to_uppercase()
                 .parse::<api_enums::CardType>()
                 .inspect_err(|error| {
+                    #[cfg(not(target_arch = "wasm32"))]
                     logger::error!(
                         ?error,
                         unparsed_card_type = %card_type,
                         "Received an unrecognized card_type value from Apple Pay; defaulting to None"
-                    )
+                    );
+                    #[cfg(target_arch = "wasm32")]
+                    let _ = error;
                 })
                 .ok(),
             card_exp_month: item.card_exp_month,