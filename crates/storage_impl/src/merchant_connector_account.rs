8dbb5e71a38c45c99fc382ef1690921d8e0dd567
Nithin N <57832822+Nithin1506200@users.noreply.github.com>
2026-09-15 23:14:16 +0530

    feat(applepay): add payment processing detail input type for simplified flow (#14225)
    (cherry picked from commit 7c6512d2b1658c6867edecaf921cf2506e67c541)

--- crates/storage_impl/src/merchant_connector_account.rs
+++ crates/storage_impl/src/merchant_connector_account.rs
@@ -735,12 +735,14 @@ impl<T: DatabaseStore> MerchantConnectorAccountInterface for RouterStore<T> {
                 storage::MerchantConnectorAccountUpdateInternal,
             ),
         ) -> Result<(), error_stack::Report<StorageError>> {
-            Conversion::convert(merchant_connector_account)
-                .await
-                .change_context(StorageError::EncryptionError)?
-                .update(connection, mca_update)
-                .await
-                .map_err(|error| report!(StorageError::from(error)))?;
+            Box::pin(
+                Conversion::convert(merchant_connector_account)
+                    .await
+                    .change_context(StorageError::EncryptionError)?
+                    .update(connection, mca_update),
+            )
+            .await
+            .map_err(|error| report!(StorageError::from(error)))?;
             Ok(())
         }
 
@@ -849,23 +851,25 @@ impl<T: DatabaseStore> MerchantConnectorAccountInterface for RouterStore<T> {
 
         let update_call = || async {
             let conn = pg_accounts_connection_write(self).await?;
-            Conversion::convert(this)
-                .await
-                .change_context(Self::Error::EncryptionError)?
-                .update(&conn, merchant_connector_account)
-                .await
-                .map_err(|error| report!(Self::Error::from(error)))
-                .async_and_then(|item| async {
-                    item.convert(
-                        self.get_keymanager_state()
-                            .attach_printable("Missing KeyManagerState")?,
-                        key_store.key.get_inner(),
-                        key_store.merchant_id.clone().into(),
-                    )
+            Box::pin(
+                Conversion::convert(this)
                     .await
-                    .change_context(Self::Error::DecryptionError)
-                })
+                    .change_context(Self::Error::EncryptionError)?
+                    .update(&conn, merchant_connector_account),
+            )
+            .await
+            .map_err(|error| report!(Self::Error::from(error)))
+            .async_and_then(|item| async {
+                item.convert(
+                    self.get_keymanager_state()
+                        .attach_printable("Missing KeyManagerState")?,
+                    key_store.key.get_inner(),
+                    key_store.merchant_id.clone().into(),
+                )
                 .await
+                .change_context(Self::Error::DecryptionError)
+            })
+            .await
         };
 
         #[cfg(feature = "accounts_cache")]
@@ -929,25 +933,27 @@ impl<T: DatabaseStore> MerchantConnectorAccountInterface for RouterStore<T> {
 
         let update_call = || async {
             let conn = pg_accounts_connection_write(self).await?;
-            Conversion::convert(this)
-                .await
-                .change_context(Self::Error::EncryptionError)?
-                .update(&conn, merchant_connector_account)
-                .await
-                .map_err(|error| report!(Self::Error::from(error)))
-                .async_and_then(|item| async {
-                    item.convert(
-                        self.get_keymanager_state()
-                            .attach_printable("Missing KeyManagerState")?,
-                        key_store.key.get_inner(),
-                        common_utils::types::keymanager::Identifier::Merchant(
-                            key_store.merchant_id.clone(),
-                        ),
-                    )
+            Box::pin(
+                Conversion::convert(this)
                     .await
-                    .change_context(Self::Error::DecryptionError)
-                })
+                    .change_context(Self::Error::EncryptionError)?
+                    .update(&conn, merchant_connector_account),
+            )
+            .await
+            .map_err(|error| report!(Self::Error::from(error)))
+            .async_and_then(|item| async {
+                item.convert(
+                    self.get_keymanager_state()
+                        .attach_printable("Missing KeyManagerState")?,
+                    key_store.key.get_inner(),
+                    common_utils::types::keymanager::Identifier::Merchant(
+                        key_store.merchant_id.clone(),
+                    ),
+                )
                 .await
+                .change_context(Self::Error::DecryptionError)
+            })
+            .await
         };
 
         #[cfg(feature = "accounts_cache")]
