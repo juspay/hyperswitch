16f88e6fe32040fb618510afb47ade715b209b1d
Yang Fu <fuyang854771076@gmail.com>
2026-09-17 08:31:02 +0000

    feat(connector): add Stripe hosted Checkout support

--- crates/router/src/core/payments/operations/payment_complete_authorize.rs
+++ crates/router/src/core/payments/operations/payment_complete_authorize.rs
@@ -193,6 +193,7 @@ impl<F: Send + Clone + Sync> GetTracker<F, PaymentData<F>, api::PaymentsRequest>
                     &mandate_type,
                     &token,
                     &request.ctp_service_details,
+                    request.connector_metadata.as_ref(),
                 )?;
             }
         }
@@ -604,6 +605,7 @@ impl<F: Send + Clone + Sync> ValidateRequest<F, api::PaymentsRequest, PaymentDat
             })?;
 
         helpers::validate_payment_method_fields_present(request)?;
+        helpers::validate_stripe_hosted_checkout_request(request)?;
 
         let _mandate_type =
             helpers::validate_mandate(request, payments::is_operation_confirm(self))?;
